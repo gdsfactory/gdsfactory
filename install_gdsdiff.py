54017ad8d9c7017b8115ac311e38f2c73c3b383a
joaquin <j>
2020-01-24 12:44:45 -0800

    appending gdsdiff commands to gitconfig and gitattributes

--- install_gdsdiff.py
+++ install_gdsdiff.py
@@ -9,31 +9,25 @@ git_attributes_path = home / ".gitattributes"
 git_config_str = open(git_config_path).read()
 git_attributes_str = open(git_attributes_path).read()
 
-if "gdsdiff" not in git_config_str:
+if "gds_diff" not in git_config_str:
     print("gdsdiff shows boolean differences in Klayout")
     print("git diff FILE.GDS")
-    print("We need to append the gdsdiff command to your ~/.gitattributes")
+    print("Appending the gdsdiff command to your ~/.gitconfig")
 
-    answer = input("are you ok appending gdsdiff to your .gitconfig? (y/n)")
+    config = configparser.RawConfigParser()
+    config.read(git_config_path)
+    key = 'diff "gds_diff"'
 
-    if answer.lower().startswith("y"):
+    if key not in config.sections():
+        config.add_section(key)
+        config.set(key, "command", "python -m gdsdiff.gds_diff_git")
+        config.set(key, "binary", "True")
 
-        config = configparser.RawConfigParser()
-        config.read(git_config_path)
-        key = 'diff "gds_diff"'
+        with open(git_config_path, "w") as f:
+            config.write(f, space_around_delimiters=True)
 
-        if key not in config.sections():
-            config.add_section(key)
-            config.set(key, "command", "python -m gdsdiff.gds_diff_git")
-            config.set(key, "binary", "True")
+if "gds_diff" not in git_attributes_str:
+    print("Appending the gdsdiff command to your ~/.gitattributes")
 
-            with open(git_config_path, "w") as f:
-                print("installing gds_diff")
-                config.write(f, space_around_delimiters=True)
-
-if "gdsdiff" not in git_attributes_str:
-    answer = input("are you ok appending gdsdiff to your .gitattributes? (y/n)")
-
-    if answer.lower().startswith("y"):
-        with open(git_attributes_path, "a") as f:
-            f.write("*.gds diff=gds_diff\n")
+    with open(git_attributes_path, "a") as f:
+        f.write("*.gds diff=gds_diff\n")
