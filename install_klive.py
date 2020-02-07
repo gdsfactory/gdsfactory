aeb64733d2cedb39afa92a7e7f4655b2e103ec4a
joaquin <joaquin>
2020-02-07 11:31:38 -0800

    fixed install_klive
    Former-commit-id: 8ce4af28759908738ad854b4ad6671b7f505f571

--- install_klive.py
+++ install_klive.py
@@ -4,30 +4,27 @@ import shutil
 import sys
 
 if sys.platform == "win32":
-    KLAYOUT_FOLDER = "KLayout"
+    klayout_folder = "KLayout"
 else:
-    KLAYOUT_FOLDER = ".klayout"
+    klayout_folder = ".klayout"
 
 
-CWD = pathlib.Path(__file__).resolve().parent
-HOME = pathlib.Path.home()
-MACRO_PATH = CWD / "klive.lym"
-MACROS_PATH = HOME / KLAYOUT_FOLDER / "pymacros"
+cwd = pathlib.Path(__file__).resolve().parent
+home = pathlib.Path.home()
+src = cwd / "klayout" / "pymacros" / "klive.lym"
+dest = home / klayout_folder / "pymacros" / "klive.lym"
+dest_folder = home / klayout_folder / "pymacros"
 
 
 def install_klive():
     """ Builds and installs the extension """
-    if os.path.exists(MACRO_PATH):
+    if os.path.exists(dest):
         print("klive already installed")
         return
 
-    # Make build directory
-    if not os.path.exists(MACROS_PATH):
-        os.makedirs(MACROS_PATH)
-
-    # Try to copy to the right place
-    shutil.copyfile(MACRO_PATH, MACROS_PATH)
-    print("klive installed to {}".format(MACRO_PATH))
+    dest_folder.mkdir(exist_ok=True)
+    shutil.copyfile(src, dest)
+    print("klive installed to {}".format(dest))
 
 
 if __name__ == "__main__":
