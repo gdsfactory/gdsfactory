8da8b35d67c953850d95bcdff2beb829e18f02da
joaquin <joaquin>
2020-02-08 17:01:45 -0800

    added install generic tech script
    Former-commit-id: 692198ee9c3ce17570c96bf6af72f0ad58251926

---
+++ install_generic_tech.py
@@ -0,0 +1,35 @@
+"""
+symlink generic tech to klayout
+
+"""
+
+import os
+import pathlib
+import sys
+
+if sys.platform == "win32":
+    klayout_folder = "KLayout"
+else:
+    klayout_folder = ".klayout"
+
+
+cwd = pathlib.Path(__file__).resolve().parent
+home = pathlib.Path.home()
+src = cwd / "klayout" / "tech"
+dest_folder = home / klayout_folder / "tech"
+dest = home / klayout_folder / "tech" / "generic"
+
+
+def install_generic_tech():
+    """ installs generic layermap """
+    if os.path.exists(dest):
+        print("generic tech already installed")
+        return
+
+    dest_folder.mkdir(exist_ok=True)
+    os.symlink(src, dest)
+    print(f"generic layermap installed to {dest}")
+
+
+if __name__ == "__main__":
+    install_generic_tech()
