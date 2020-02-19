32232dd81e84e56f332a9373f9c9b8e6b601bc2b
Joaquin Matres <joaquin>
2020-02-19 08:35:45 -0800

    fix ring, now allows custom factories

--- samples/14_component_connectivity.py
+++ samples/14_component_connectivity.py
@@ -15,17 +15,20 @@ def ring(
     length_y=2.0,
     length_x=4.0,
     gap=0.2,
+    wg_width=0.5,
 ):
     """ single bus ring
     """
     c = pp.Component()
 
     # define subcells
-    coupler90 = pp.call_if_func(coupler90, gap=gap)
-    waveguide_x = pp.call_if_func(waveguide, length=length_x)
-    waveguide_y = pp.call_if_func(waveguide, length=length_y)
-    bend = pp.call_if_func(bend)
-    coupler_straight = pp.call_if_func(coupler_straight, gap=gap, length=length_x)
+    coupler90 = pp.call_if_func(coupler90, gap=gap, width=wg_width)
+    waveguide_x = pp.call_if_func(waveguide, length=length_x, width=wg_width)
+    waveguide_y = pp.call_if_func(waveguide, length=length_y, width=wg_width)
+    bend = pp.call_if_func(bend, width=wg_width)
+    coupler_straight = pp.call_if_func(
+        coupler_straight, gap=gap, length=length_x, width=wg_width
+    )
 
     # add references to subcells
     cbl = c << coupler90
@@ -52,6 +55,6 @@ def ring(
 
 
 if __name__ == "__main__":
-    c = ring()
+    c = ring(wg_width=0.4)
     pp.write_gds(c, "ring.gds")
     pp.show(c)
