a9f8bfdd33635b1daf1723391925c6a5d44a766f
Joaquin Matres <joaquin>
2020-04-18 21:53:10 -0700

    added fixture to remove any build folder cache
    Former-commit-id: 3c613bcee395455f02ffc4732132445bcd803946

--- pp/conftest.py
+++ pp/conftest.py
@@ -7,5 +7,10 @@ from pp import CONFIG
 @pytest.fixture(autouse=True)
 def cleandir(request):
     build_folder = CONFIG["build_directory"]
-    if build_folder.exists() and "noautofixt" in request.keywords:
+    module_path = CONFIG["module_path"]
+
+    if build_folder.exists() and "noautofixt" not in request.keywords:
+        shutil.rmtree(build_folder)
+
+    for build_folder in module_path.glob("**/build"):
         shutil.rmtree(build_folder)
