1de751ba40531e23ca1e00398c505c4b2811ec76
joaquin <j>
2020-05-31 21:40:59 -0700

    added ring_single_dut component
    Former-commit-id: 27c0d7b5bd37b1cb36a733ba544f82a036f68f09

--- pp/components/cavity.py
+++ pp/components/cavity.py
@@ -1,10 +1,13 @@
 import pp
 from pp.components.coupler import coupler
-from pp.components.loop_mirror import loop_mirror
+
+# from pp.components.loop_mirror import loop_mirror
+from pp.container import container
 
 
+@container
 @pp.autoname
-def cavity(mirror=loop_mirror, coupler=coupler, length=0.1, gap=0.2, wg_width=0.5):
+def cavity(component, coupler=coupler, length=0.1, gap=0.2, wg_width=0.5):
     """ creates a cavity from a coupler and a mirror
     it will connect the W0 port of the mirror to both E1 and W1 ports of the coupler creating a resonant cavity
 
@@ -19,7 +22,7 @@ def cavity(mirror=loop_mirror, coupler=coupler, length=0.1, gap=0.2, wg_width=0.
              W0    length      E0
 
     """
-    mirror = pp.call_if_func(mirror)
+    mirror = pp.call_if_func(component)
     coupler = pp.call_if_func(coupler, length=length, gap=gap, wg_width=wg_width)
 
     c = pp.Component()
@@ -37,5 +40,5 @@ def cavity(mirror=loop_mirror, coupler=coupler, length=0.1, gap=0.2, wg_width=0.
 if __name__ == "__main__":
     from pp.components.dbr import dbr
 
-    c = cavity(mirror=dbr, pins=True)
+    c = cavity(component=dbr(), pins=True)
     pp.show(c)
