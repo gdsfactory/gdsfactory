7b8007eef352e38e543ceb45ea6afb5cd344d2ff
joaquin <j>
2020-10-02 09:40:47 -0700

    add tapers and grating couplers only to optical ports
    Former-commit-id: 728ed39dd5940d3197ccba84c987872f73aada96

--- pp/add_tapers.py
+++ pp/add_tapers.py
@@ -3,39 +3,37 @@ from pp.container import container
 from pp.components.taper import taper
 
 
-@pp.port.deco_rename_ports
-def add_tapers(component, taper=taper, new_component=True):
-    """ returns tapered component
-    can add tapers over the same component or create a new component"""
+def add_taper_elements(component, taper=taper):
+    """returns ports and taper elements for a component"""
     taper = pp.call_if_func(taper)
-
-    if new_component:
-        c = pp.Component(name=component.name + "_t")
-        c.add_ref(component)
-    else:
-        c = component
+    ports = []
+    elements = []
+    c = pp.Component()
 
     for port_name, port in component.ports.copy().items():
-        taper_ref = c << pp.call_if_func(taper)
-        taper_ref.connect(taper_ref.ports["2"].name, port)
-        if not new_component:
-            c.ports.pop(port_name)
-        c.add_port(name=port_name, port=taper_ref.ports["1"])
-    return c
+        if port.port_type == "optical":
+            taper_ref = c << pp.call_if_func(taper)
+            taper_ref.connect(taper_ref.ports["2"].name, port)
+            elements.append(taper_ref)
+            ports.append(taper_ref.ports["1"])
+    return ports, elements
 
 
 @container
 @pp.port.deco_rename_ports
-def add_tapers2(component, taper=taper, suffix="t"):
-    """ returns tapers for component """
+def add_tapers(component, taper=taper, suffix="t", port_type="optical"):
+    """returns component optical tapers for component """
 
     taper = pp.call_if_func(taper)
     c = pp.Component(name=f"{component.name}_{suffix}")
 
     for port_name, port in component.ports.copy().items():
-        taper_ref = c << pp.call_if_func(taper)
-        taper_ref.connect(taper_ref.ports["2"].name, port)
-        c.add_port(name=port_name, port=taper_ref.ports["1"])
+        if port.port_type == port_type:
+            taper_ref = c << pp.call_if_func(taper)
+            taper_ref.connect(taper_ref.ports["2"].name, port)
+            c.add_port(name=port_name, port=taper_ref.ports["1"])
+        else:
+            c.add_port(name=port_name, port=port)
     c.add_ref(component)
     return c
 
@@ -43,7 +41,14 @@ def add_tapers2(component, taper=taper, suffix="t"):
 if __name__ == "__main__":
     c = pp.c.waveguide(width=2)
     t = pp.c.taper(width2=2)
-    cc = add_tapers2(component=c, taper=t, suffix="t")
-    print(cc.ports.keys())
-    print(cc.settings.keys())
-    pp.show(cc)
+
+    # cc = add_tapers(component=c, taper=t, suffix="t")
+    # print(cc.ports.keys())
+    # print(cc.settings.keys())
+    # pp.show(cc)
+
+    ports, elements = add_taper_elements(component=c, taper=t)
+    c.ports = ports
+    c.add(elements)
+    pp.show(c)
+    print(c.ports)
