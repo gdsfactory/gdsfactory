9bccd90f0f9726b4ee45a4716dacac4cc20295dc
joaquin <j>
2020-10-14 16:52:27 -0700

    added pack tutorial
    Former-commit-id: 26d8bd6031d5e2b0a0ab4aa4fd7e94120336c09e

--- pp/tests/test_label_in_component_move.py
+++ pp/tests/test_label_in_component_move.py
@@ -1,23 +1,37 @@
 import pp
 
 
-def component_with_label():
+def component_with_label_float():
     c = pp.Component("ellipse_with_label")
     c << pp.c.ellipse()
     c.add_label(text="demo", position=(0.0, 0.0), layer=pp.LAYER.TEXT)
     return c
 
 
-def test_move():
-    """ needs fixing """
-    c = component_with_label()
+def component_with_label_int():
+    c = pp.Component("ellipse_with_label")
+    c << pp.c.ellipse()
+    c.add_label(text="demo", position=(0, 0), layer=pp.LAYER.TEXT)
+    return c
+
+
+def test_move_float_with_int():
+    """ fixed """
+    c = component_with_label_float()
     c.x = 10
-    # c.x = 10
     c.movex(10)
 
 
-if __name__ == "__main__":
-    # test_move()
-    c = component_with_label()
+def test_move_int_with_float():
+    """ needs fixing """
+    c = component_with_label_int()
     c.x = 10.0
-    pp.show(c)
+    c.movex(10.0)
+
+
+if __name__ == "__main__":
+    test_move_float_with_int()
+    test_move_int_with_float()
+    # c = component_with_label()
+    # c.x = 10.0
+    # pp.show(c)
