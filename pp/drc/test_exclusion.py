496bce7385962d1ff9feed6bebe6b90b15070820
joaquin <j>
2020-11-14 10:48:02 -0800

    added test_inclusion
    Former-commit-id: 920748c7fcf34ba3fc17966871f4e0bb2d879389

--- pp/drc/test_exclusion.py
+++ pp/drc/test_exclusion.py
@@ -16,11 +16,11 @@ def get_device(space, width=0.5, layer1=(1, 0), layer2=(2, 0)):
 @pytest.mark.parametrize(
     "space,min_space,area_expected", [(0.16, 0.1, 0), (0.1, 0.11, 50000)]
 )
-def test_compute_area(space, min_space, area_expected):
+def test_exclusion(space, min_space, area_expected):
     c = get_device(space=space)
     area = check_exclusion(c, min_space=min_space)
     assert np.isclose(area, area_expected)
 
 
 if __name__ == "__main__":
-    test_compute_area()
+    test_exclusion()
