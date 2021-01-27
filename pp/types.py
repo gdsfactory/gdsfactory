1d62f16bc7b93459feee6893b8644bc86caa12db
joaquin <j>
2021-01-26 22:03:36 -0800

    fixing more type errors

--- pp/types.py
+++ pp/types.py
@@ -3,6 +3,8 @@
 from pathlib import Path
 from typing import Callable, Dict, List, Tuple, Union
 
+from numpy import float64, int64, ndarray
+
 from pp.component import Component, ComponentReference
 from pp.port import Port
 
@@ -13,6 +15,9 @@ ComponentOrReference = Union[Component, ComponentReference]
 NameToFunctionDict = Dict[str, Callable]
 Number = Union[float, int]
 Route = Dict[str, Union[List[ComponentReference], Dict[str, Port], float]]
+Number = Union[float64, int64, float, int]
+Coordinate = Union[Tuple[Number, Number], ndarray, List[Number]]
+Coordinates = Union[List[Coordinate], ndarray, List[Number], Tuple[Number, ...]]
 
 
 def get_name_to_function_dict(*functions) -> Dict[str, Callable]:
