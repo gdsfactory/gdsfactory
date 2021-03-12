a1943f481ee121da4b211393f7a93118dcbdf948
joaquin <j>
2021-03-11 19:48:16 -0800

    244

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_install_requires():
 setup(
     name="gdsfactory",
     url="https://github.com/gdsfactory/gdsfactory",
-    version="2.4.3",
+    version="2.4.4",
     author="PsiQ",
     scripts=["pp/pf.py"],
     description="python libraries to generate GDS layouts",
