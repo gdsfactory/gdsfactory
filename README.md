ba5a643bf7d87885485c4e27a9c4b0cec69d4d18
joaquin <j>
2021-03-17 21:03:01 -0700

    bumpversion to 245

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# gdsfactory 2.4.4
+# gdsfactory 2.4.5
 
 [![](https://readthedocs.org/projects/gdsfactory/badge/?version=latest)](https://gdsfactory.readthedocs.io/en/latest/?badge=latest)
 [![](https://img.shields.io/pypi/v/gdsfactory)](https://pypi.org/project/gdsfactory/)
--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_install_requires():
 setup(
     name="gdsfactory",
     url="https://github.com/gdsfactory/gdsfactory",
-    version="2.4.4",
+    version="2.4.5",
     author="PsiQ",
     scripts=["pp/pf.py"],
     description="python libraries to generate GDS layouts",
