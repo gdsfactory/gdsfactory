3da9075a4f1e99d4ead9fd1c7aa831e3b2e8f970
joaquin <j>
2021-05-03 08:56:15 -0700

    simpler xsections
    Former-commit-id: 548222540ec76026894b21a39f780a1047aaa645

--- pp/components/straight.py
+++ pp/components/straight.py
@@ -1,50 +1,43 @@
 """Straight waveguide."""
-from typing import Iterable, Optional
 
 from pp.add_padding import get_padding_points
 from pp.cell import cell
 from pp.component import Component
-from pp.cross_section import strip
+from pp.cross_section import cross_section
 from pp.path import extrude
 from pp.path import straight as straight_path
 from pp.snap import snap_to_grid
 from pp.tech import TECH
-from pp.types import CrossSectionFactory, Layer
 
 
 @cell
-def straight(
-    length: float = 10.0,
-    npoints: int = 2,
-    layers_cladding: Optional[Iterable[Layer]] = TECH.waveguide.strip.layers_cladding,
-    cladding_offset: float = TECH.waveguide.strip.cladding_offset,
-    cross_section_factory: Optional[CrossSectionFactory] = None,
-    **cross_section_settings
-) -> Component:
+def straight(length: float = 10.0, npoints: int = 2, **settings) -> Component:
     """Returns a Straight waveguide.
 
     Args:
         length: of straight
         npoints: number of points
-        cross_section: cross_section or function that returns a cross_section
-        **cross_section_settings
+        settings: cross_section settings to extrude paths
 
     """
-    cross_section_factory = cross_section_factory or strip
-
     p = straight_path(length=length, npoints=npoints)
-    cross_section = cross_section_factory(**cross_section_settings)
-    c = extrude(p, cross_section)
+    x = cross_section(**settings)
+    c = extrude(p, x)
     c.length = snap_to_grid(length)
-    c.width = cross_section.info["width"]
-    points = get_padding_points(
-        component=c,
-        default=0,
-        bottom=cladding_offset,
-        top=cladding_offset,
-    )
-    for layer in layers_cladding or []:
-        c.add_polygon(points, layer=layer)
+    c.width = x.info["width"]
+
+    if x.info["layers_cladding"]:
+        layers_cladding = x.info["layers_cladding"]
+        cladding_offset = x.info["cladding_offsetcomponent"]
+
+        points = get_padding_points(
+            component=c,
+            default=0,
+            bottom=cladding_offset,
+            top=cladding_offset,
+        )
+        for layer in layers_cladding or []:
+            c.add_polygon(points, layer=layer)
     return c
 
 
@@ -52,13 +45,19 @@ if __name__ == "__main__":
     # c = straight(length=10.0)
     # c.pprint()
 
-    c = straight(
-        length=10.001,
-        width=0.5,
-        cross_section={"clad": dict(width=3, offset=0, layer=(111, 0))},
-    )
-    c = straight()
-    # c = straight(cross_section_factory=TECH.waveguide.rib_slab90)
+    # c = straight(
+    #     length=10.001,
+    #     width=0.5,
+    #     cross_section={"clad": dict(width=3, offset=0, layer=(111, 0))},
+    # )
+
+    # c = straight(settings=TECH.waveguide.metal_routing)
+    # c = straight(settings=TECH.waveguide.rib_slab90)
+
+    settings = TECH.waveguide.strip
+    settings.update(width=2)
+    c = straight(**settings)
+
     # print(c.name)
     # print(c.length)
     # print(c.ports)
