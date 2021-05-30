4d7b6420265bcfadf7e874f9fe328e3e68791e7e
joaquin <j>
2021-05-30 09:24:15 -0700

    more type annotations

--- pp/pf.py
+++ pp/pf.py
@@ -2,10 +2,8 @@
 """
 import os
 import pathlib
-import re
 import shlex
 import subprocess
-import time
 
 import click
 from click.core import Context, Option
@@ -13,7 +11,7 @@ from click.core import Context, Option
 import pp
 import pp.build as pb
 from pp import CONFIG, klive
-from pp.config import logging, print_config
+from pp.config import print_config
 from pp.gdsdiff.gdsdiff import gdsdiff
 from pp.install import install_gdsdiff, install_generic_tech, install_klive
 from pp.layers import LAYER
@@ -31,43 +29,6 @@ cwd = pathlib.Path.cwd()
 LAYER_LABEL = LAYER.LABEL
 
 
-def shorten_command(cmd):
-    """Shortens a command if possible"""
-    match = re.search(r"(\w+\.(py|drc))", cmd)
-    return match.group() if match else cmd
-
-
-def run_command(command):
-    """Run a command and keep track of some context"""
-    logging.info("Running `{}`".format(command))
-
-    # Run the process and handle errors
-    time0 = time.time()
-    process = subprocess.Popen(
-        shlex.split(command), stdout=subprocess.PIPE, stderr=subprocess.PIPE
-    )
-    stdout, stderr = process.communicate()
-    total_time = time.time() - time0
-
-    # Either show that ther was an error, or just leave it
-    if process.returncode == 0:
-        message = "`{}` ran without errors in {:.2f}s.".format(
-            shorten_command(command), total_time
-        )
-        logging.info(message)
-        if stdout.strip():
-            message = "Output of `{}`:".format(shorten_command(command))
-            logging.info(message)
-            logging.info(stdout.strip(), extra={"raw": True})
-    else:
-        message = "Error in `{}`".format(shorten_command(command))
-        logging.error(message)
-        raw = stdout.strip() + "\n" + stderr.strip()
-        logging.error(raw, extra={"raw": True})
-
-    return command, process.returncode
-
-
 def print_version(ctx: Context, param: Option, value: bool) -> None:
     """Prints the version"""
     if not value or ctx.resilient_parsing:
@@ -78,7 +39,7 @@ def print_version(ctx: Context, param: Option, value: bool) -> None:
 
 @click.command(name="delete")
 @click.argument("logfile", default="main", required=False)
-def log_delete(logfile):
+def log_delete(logfile: str) -> None:
     """Deletes logs"""
     if not os.path.exists(log_directory):
         print("No logs found.")
@@ -95,7 +56,7 @@ CONFIG
 
 @click.command(name="config")
 @click.argument("key", required=False, default=None)
-def config_get(key):
+def config_get(key: str) -> None:
     """Shows key values from CONFIG"""
     print_config(key)
 
@@ -106,14 +67,14 @@ MASKS
 
 
 @click.group()
-def mask():
+def mask() -> None:
     """Commands for building masks"""
     pass
 
 
 @click.command(name="clean")
 @click.option("--force", "-f", default=False, help="Force deletion", is_flag=True)
-def build_clean(force):
+def build_clean(force: bool) -> None:
     """Deletes the build folder and contents"""
     message = "Delete {}. Are you sure?".format(CONFIG["build_directory"])
     if force or click.confirm(message, default=True):
@@ -122,13 +83,13 @@ def build_clean(force):
 
 @click.command(name="build_devices")
 @click.argument("regex", required=False, default=".*")
-def build_devices(regex):
+def build_devices(regex) -> None:
     """Build all devices described in devices/"""
     pb.build_devices(regex)
 
 
 @click.command(name="build_does")
-def build_does():
+def build_does() -> None:
     """Build does defined in doe.yml"""
     print("this is deprecated")
     import_custom_doe_factories()
@@ -138,7 +99,7 @@ def build_does():
 
 @click.command(name="write_metadata")
 @click.argument("label_layer", required=False, default=LAYER_LABEL)
-def mask_merge(label_layer):
+def mask_merge(label_layer) -> None:
     """merge JSON/Markdown from build/devices into build/mask"""
 
     gdspath = CONFIG["mask_gds"]
@@ -152,7 +113,7 @@ def mask_merge(label_layer):
 @click.command(name="write_labels")
 @click.argument("gdspath", default=None)
 @click.argument("label_layer", required=False, default=LAYER_LABEL)
-def write_mask_labels(gdspath, label_layer):
+def write_mask_labels(gdspath: str, label_layer) -> None:
     """Find test and measurement labels."""
     if gdspath is None:
         gdspath = CONFIG["mask_gds"]
@@ -167,7 +128,7 @@ EXTRA
 
 @click.command()
 @click.argument("filepath")
-def merge_cells(filepath):
+def merge_cells(filepath: str) -> None:
     """Merge GDS cells into a top level."""
     filepath = pathlib.Path(filepath)
     filepath_out = filepath / "merged.gds"
@@ -185,7 +146,7 @@ def merge_cells(filepath):
 
 @click.command()
 @click.argument("filename")
-def show(filename):
+def show(filename: str) -> None:
     """Show a GDS file using klive"""
     klive.show(filename)
 
@@ -193,7 +154,7 @@ def show(filename):
 @click.command()
 @click.argument("gdspath1")
 @click.argument("gdspath2")
-def diff(gdspath1, gdspath2):
+def diff(gdspath1: str, gdspath2: str) -> None:
     """Show boolean difference between two GDS files."""
     import pp
 
@@ -202,7 +163,7 @@ def diff(gdspath1, gdspath2):
 
 
 @click.command()
-def install():
+def install() -> None:
     """Install Klive, gdsdiff and generic tech"""
     install_generic_tech()
     install_klive()
@@ -210,7 +171,7 @@ def install():
 
 
 @click.command()
-def test():
+def test() -> None:
     """Run tests using pytest.
     Strictly speaking you should just run `pytest` directly."""
 
