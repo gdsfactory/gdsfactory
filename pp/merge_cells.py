50ee870d1ec329547ea7bb86b19bf90881b4e508
joaquin <j>
2021-06-09 07:30:58 -0700

    added CLI functions

--- pp/merge_cells.py
+++ pp/merge_cells.py
@@ -1,11 +1,17 @@
+import pathlib
 from typing import Iterable
 
 from pp.component import Component
 from pp.import_gds import import_gds
-from pp.types import ComponentOrPath
+from pp.types import ComponentOrPath, PathType
 
 
 def merge_cells(cells: Iterable[ComponentOrPath]) -> Component:
+    """Combine all GDS files or gdsfactory components into a gdsfactory component.
+
+    Args:
+        cells: List of gdspaths or Components
+    """
     c = Component()
 
     for cell in cells:
@@ -14,6 +20,12 @@ def merge_cells(cells: Iterable[ComponentOrPath]) -> Component:
         c << cell
 
     return c
+
+
+def merge_cells_from_directory(dirpath: PathType) -> Component:
+    """Merges GDS cells from a directory into a single Component"""
+    dirpath = pathlib.Path(dirpath)
+    return merge_cells(dirpath.glob("*.gds"))
 
 
 if __name__ == "__main__":
--- pp/pf.py
+++ pp/pf.py
@@ -4,6 +4,7 @@ import os
 import pathlib
 import shlex
 import subprocess
+from typing import Optional
 
 import click
 from click.core import Context, Option
@@ -14,11 +15,13 @@ from pp import CONFIG, klive
 from pp.config import print_config
 from pp.gdsdiff.gdsdiff import gdsdiff
 from pp.install import install_gdsdiff, install_generic_tech, install_klive
-from pp.layers import LAYER
+from pp.layers import LAYER, lyp_to_dataclass
 from pp.mask.merge_json import merge_json
 from pp.mask.merge_markdown import merge_markdown
 from pp.mask.merge_test_metadata import merge_test_metadata
 from pp.mask.write_labels import write_labels
+from pp.merge_cells import merge_cells_from_directory
+from pp.types import PathType
 
 # from pp.write_doe_from_yaml import write_doe_from_yaml
 from pp.write_doe_from_yaml import import_custom_doe_factories
@@ -62,6 +65,52 @@ def config_get(key: str) -> None:
 
 
 """
+GDS
+"""
+
+
+@click.group()
+def gds() -> None:
+    """Commands for dealing with GDS files"""
+    pass
+
+
+@click.command(name="layermap_to_dataclass")
+@click.argument("filepath")
+@click.option("--force", "-f", default=False, help="Force deletion", is_flag=True)
+def layermap_to_dataclass(filepath, force: bool) -> None:
+    """Converts klayout LYP to a dataclass"""
+    filepath_lyp = pathlib.Path(filepath)
+    filepath_py = filepath_lyp.with_suffix(".py")
+    if not filepath_lyp.exists():
+        raise FileNotFoundError(f"{filepath_lyp} not found")
+    if not force and filepath_py.exists():
+        raise FileExistsError(f"found {filepath_py}")
+    lyp_to_dataclass(lyp_filepath=filepath_lyp)
+
+
+@click.command(name="write_cells")
+@click.argument("gdspath")
+def write_cells(gdspath) -> None:
+    """Write each all level cells into separate GDS files."""
+    pp.gds.write_cells(gdspath)
+
+
+@click.command(name="merge_gds_from_directory")
+@click.argument("dirpath", required=False, default=None)
+@click.argument("gdspath", required=False, default=None)
+def merge_gds_from_directory(
+    dirpath: Optional[PathType] = None, gdspath: Optional[PathType] = None
+) -> None:
+    """Merges GDS cells from a directory into a single GDS."""
+    dirpath = dirpath or pathlib.Path.cwd()
+    gdspath = gdspath or pathlib.Path.cwd() / "merged.gds"
+    c = merge_cells_from_directory(dirpath=dirpath)
+    c.write_gds(gdspath=gdspath)
+    c.show()
+
+
+"""
 MASKS
 """
 
@@ -89,12 +138,13 @@ def build_devices(regex) -> None:
 
 
 @click.command(name="build_does")
-def build_does() -> None:
+@click.argument("yamlpath")
+def build_does(yamlpath: str) -> None:
     """Build does defined in doe.yml"""
     print("this is deprecated")
     import_custom_doe_factories()
     # write_doe_from_yaml()
-    pb.build_does()
+    pb.build_does(yamlpath)
 
 
 @click.command(name="write_metadata")
@@ -107,7 +157,7 @@ def mask_merge(label_layer) -> None:
 
     merge_json()
     merge_markdown()
-    merge_test_metadata(config_path=CONFIG["config_path"])
+    merge_test_metadata(gdspath=gdspath)
 
 
 @click.command(name="write_labels")
@@ -202,6 +252,10 @@ mask.add_command(build_does)
 mask.add_command(mask_merge)
 mask.add_command(write_mask_labels)
 
+gds.add_command(layermap_to_dataclass)
+gds.add_command(write_cells)
+gds.add_command(merge_gds_from_directory)
+
 cli.add_command(config_get)
 cli.add_command(mask)
 cli.add_command(merge_cells)
@@ -209,6 +263,7 @@ cli.add_command(show)
 cli.add_command(test)
 cli.add_command(install)
 cli.add_command(diff)
+cli.add_command(gds)
 
 
 if __name__ == "__main__":
