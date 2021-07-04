d05f6a2798f58dc66203d72fe07fdab9d41a8afa
joaquin <j>
2021-07-04 09:35:48 -0700

    Bump version: 2.5.8 → 2.5.9
    Former-commit-id: 7b7f35ff7501aacdee84ff87037f7281b2b8a017

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def get_install_requires():
 setup(
     name="gdsfactory",
     url="https://github.com/gdsfactory/gdsfactory",
-    version="2.5.8",
+    version="2.5.9",
     author="gdsfactory community",
     scripts=["pp/pf.py"],
     description="python libraries to generate GDS layouts",
