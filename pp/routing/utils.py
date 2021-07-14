fc88250b2184f4ebdfb3652c2ad1757863a1c640
joaquin <j>
2021-07-14 10:33:44 -0700

    expose utils, sort_ports and fanout in pp.routing
    Former-commit-id: 43e95b9a313ec58c877714f01010a449669cd822

--- pp/routing/utils.py
+++ pp/routing/utils.py
@@ -6,7 +6,7 @@ from pp.port import Port
 
 
 def flip(port: Port) -> Port:
-    """Flip a phidl Port"""
+    """Flip Port orientation."""
     return Port(port.name, port.midpoint, port.width, port.orientation + 180)
 
 
@@ -34,7 +34,9 @@ def direction_ports_from_list_ports(optical_ports: List[Port]) -> Dict[str, List
 
 
 def check_ports_have_equal_spacing(list_ports: List[Port]) -> float64:
-    if not list_ports:
+    if not isinstance(list_ports, list):
+        raise ValueError(f"list_ports should be a list of ports, got {list_ports}")
+    if len(list_ports) == 0:
         raise ValueError("list_ports should not be empty")
 
     angle = get_list_ports_angle(list_ports)
@@ -46,24 +48,15 @@ def check_ports_have_equal_spacing(list_ports: List[Port]) -> float64:
     seps = [round(abs(c2 - c1), 5) for c1, c2 in zip(xys[1:], xys[:-1])]
     different_seps = set(seps)
     if len(different_seps) > 1:
-        raise ValueError(
-            "Ports should have the same separation. \
-        Got {}".format(
-                different_seps
-            )
-        )
+        raise ValueError("Ports should have the same separation. Got {different_seps}")
 
     return different_seps.pop()
 
 
 def get_list_ports_angle(list_ports: List[Port]) -> Union[float64, int]:
+    """Returns the orientation/angle (in degrees) of a list of ports."""
     if not list_ports:
         return None
     if len(set([p.angle for p in list_ports])) > 1:
-        raise ValueError(
-            "All port angles should be the same.\
-        Got {}".format(
-                list_ports
-            )
-        )
+        raise ValueError(f"All port angles should be the same. Got {list_ports}")
     return list_ports[0].orientation
