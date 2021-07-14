bbe4021f6d9b89f391eb3a53d19a3ca6ccc4aaa7
joaquin <j>
2021-07-14 10:39:47 -0700

    Bump version: 2.6.3 → 2.6.4

--- pp/__init__.py
+++ pp/__init__.py
@@ -125,7 +125,7 @@ __all__ = [
     "tech",
     "TECH",
 ]
-__version__ = "2.6.3"
+__version__ = "2.6.4"
 
 
 if __name__ == "__main__":
--- pp/config.py
+++ pp/config.py
@@ -11,7 +11,7 @@ You can access all the config dictionary with `print_config` as well as a partic
 
 """
 
-__version__ = "2.6.3"
+__version__ = "2.6.4"
 import json
 import os
 import pathlib
