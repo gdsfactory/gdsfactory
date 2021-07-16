0e1906e3c128d6861adf45f59583394c1a2d6398
joaquin <j>
2021-07-16 13:59:38 -0700

    waveguide accepts dict(component='fabc_nitride_cband')
    Former-commit-id: 4e7cceb00144f227c532adecb577c8732304723c

--- pp/component_from/__init__.py
+++ pp/component_from/__init__.py
@@ -1,7 +1,7 @@
+from pp.component_from.component_from_phidl import phidl
+from pp.component_from.component_from_picwriter import picwriter
 from pp.component_from.gds import gds
 from pp.component_from.gdspaths import gdsdir, gdspaths
 from pp.component_from.netlist import netlist
-from pp.component_from.phidl import phidl
-from pp.component_from.picwriter import picwriter
 
 __all__ = ["phidl", "picwriter", "gds", "netlist", "gdspaths", "gdsdir"]
