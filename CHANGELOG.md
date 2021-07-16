fdea1402df934baf1c7120b67a750d9275e364d5
joaquin <j>
2021-07-16 14:28:04 -0700

    add pp.remove and pp.read

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -43,6 +43,7 @@ from contracts import contract
 - move set_plot_options to pp.klive.set_plot_options, stop overriding phidl's set_plot_options in `pp.__init__`
 - move pp.merge_cells into pp.component_from.gdspaths and pp.component_from.gdsdir
 - waveguide accepts dict(component='fabc_nitride_cband')
+- add pp.remove and pp.read
 
 ## 2.6.5
 
--- pp/component_from/gds.py
+++ pp/component_from/gds.py
@@ -1,6 +1,4 @@
 """Read component GDS, JSON metadata and CSV ports."""
-import csv
-import json
 from pathlib import Path
 from typing import Union
 
@@ -9,48 +7,22 @@ from pp.component import Component
 from pp.import_gds import import_gds
 
 
-def remove_gds_labels(component: Component, layer=pp.LAYER.LABEL_SETTINGS) -> None:
-    """Returns same component without labels."""
-    for c in list(component.get_dependencies(recursive=True)) + [component]:
-        old_label = [
-            label for label in c.labels if label.layer == pp.LAYER.LABEL_SETTINGS
-        ]
-        if len(old_label) > 0:
-            for label in old_label:
-                c.labels.remove(label)
+def gds(gdspath: Union[str, Path], **kwargs) -> Component:
+    """Returns Component with ports (CSV) and metadata (JSON) info (if any).
+    Args:
+        gdspath: path of GDS file
+        cellname: cell of the name to import (None) imports top cell
+        flatten: if True returns flattened (no hierarchy)
+        snap_to_grid_nm: snap to different nm grid (does not snap if False)
 
-
-def gds(gdspath: Union[str, Path]) -> Component:
-    """Returns Component with ports (CSV) and metadata (JSON) info (if any)."""
+    """
 
     if not gdspath.exists():
         raise FileNotFoundError(f"No such file '{gdspath}'")
+    component = import_gds(gdspath)
 
-    ports_filepath = gdspath.with_suffix(".ports")
-    metadata_filepath = gdspath.with_suffix(".json")
-
-    c = import_gds(gdspath)
-
-    if ports_filepath.exists():
-        with open(str(ports_filepath), newline="") as csvfile:
-            reader = csv.reader(csvfile, delimiter=",", quotechar="|")
-            for r in reader:
-                layer_type = int(r[5].strip().strip("("))
-                data_type = int(r[6].strip().strip(")"))
-                c.add_port(
-                    name=r[0],
-                    midpoint=[float(r[1]), float(r[2])],
-                    orientation=int(r[3]),
-                    width=float(r[4]),
-                    layer=(layer_type, data_type),
-                )
-
-    if metadata_filepath.exists():
-        with open(metadata_filepath) as f:
-            data = json.load(f)
-        cell_settings = data["cells"][c.name]
-        c.settings.update(cell_settings)
-    return c
+    pp.read.read_ports(component=component, gdspath=gdspath)
+    return component
 
 
 if __name__ == "__main__":
