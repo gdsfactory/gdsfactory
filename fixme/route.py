3c81d0d5ece5f69c6a5a2f24fa0938149402e221
joaquin <j>
2021-07-22 17:59:33 -0700

    fixed notebooks

--- fixme/route.py
+++ fixme/route.py
@@ -16,6 +16,6 @@ if __name__ == "__main__":
     waveguide = "nitride"
     c = pp.components.mzi2x2(waveguide=waveguide)
     cc = pp.routing.add_fiber_single(
-        component=c, auto_widen=False, waveguide=waveguide, with_align_ports=False
+        component=c, auto_widen=False, waveguide=waveguide, with_loopback=False
     )
     cc.show()
