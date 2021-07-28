ccacaf5382c7462091d58a8f22f3b1abc99a8cf3
joaquin <j>
2021-07-27 17:53:40 -0700

    sparameters needs some fix

--- plugins/tidy3d/gtidy3d/get_sparameters.py
+++ plugins/tidy3d/gtidy3d/get_sparameters.py
@@ -14,30 +14,42 @@ def get_sparameters(sim: td.Simulation) -> np.ndarray:
 
     sim = run_simulation(sim)
 
-    def get_power(monitor):
+    def get_amplitude(monitor):
         f, b = sim.data(monitor)["mode_amps"]
-        F, B = np.abs(f) ** 2, np.abs(b) ** 2
-        return F, B
+        return np.squeeze(f), np.squeeze(b)
 
     monitors = sim.monitors
-    norm, _ = get_power(monitors[1])
-    norm = np.squeeze(norm)
 
-    if len(monitors) == 5:
-        _, incident, reflect, top, bot = monitors
-        S = np.zeros((2, 2))
-        S[0, 0] = get_power(incident)[-1]
-        S[1, 0] = get_power(reflect)[-1]
-        S[0, 1] = get_power(top)[0]
-        S[1, 1] = get_power(bot)[0]
+    n = len(monitors) - 1
+    S = np.zeros((n, n), dtype=np.complex128)
 
-    elif len(monitors) == 3:
-        _, incident, reflect = monitors
-        S = np.zeros((2, 2))
-        S[0, 0] = S[1, 1] = get_power(incident)[-1]
-        S[1, 0] = S[0, 1] = get_power(reflect)[-1]
+    for i, monitor_i in enumerate(monitors):
+        for j, monitor_j in enumerate(monitors):
+            if i > 0 and j > 0:
+                if monitor_i.name.startswith("W"):
+                    ai, bi = get_amplitude(monitor_i)
+                else:
+                    bi, ai = get_amplitude(monitor_i)
+                if monitor_j.name.startswith("W"):
+                    aj, bj = get_amplitude(monitor_j)
+                else:
+                    bj, aj = get_amplitude(monitor_j)
+                S[i - i, j - 1] = bi / aj
 
-    S = S / norm
+    # if len(monitors) == 5:
+    #     _, incident, reflect, top, bot = monitors
+    #     S = np.zeros((2, 2))
+    #     S[0, 0] = get_amplitude(incident)[-1]
+    #     S[1, 0] = get_amplitude(reflect)[-1]
+    #     S[0, 1] = get_amplitude(top)[0]
+    #     S[1, 1] = get_amplitude(bot)[0]
+
+    # elif len(monitors) == 3:
+    #     _, incident, reflect = monitors
+    #     S = np.zeros((2, 2))
+    #     S[0, 0] = S[1, 1] = get_amplitude(incident)[-1]
+    #     S[1, 0] = S[0, 1] = get_amplitude(reflect)[-1]
+    # S = S / norm
     return S
 
 
@@ -47,5 +59,39 @@ if __name__ == "__main__":
 
     c = pp.components.straight(length=2)
     sim = gm.get_simulation(c)
-    s = get_sparameters(sim)
-    print(s)
+    # s = get_sparameters(sim)
+    # print(s)
+
+    sim = run_simulation(sim)
+
+    def get_amplitude(monitor):
+        f, b = sim.data(monitor)["mode_amps"]
+        # return f, b
+        return np.squeeze(f), np.squeeze(b)
+
+    monitors = sim.monitors
+
+    n = len(monitors) - 1
+    S = np.zeros((n, n), dtype=np.complex128)
+
+    # for i, monitor_i in enumerate(monitors):
+    #     for j, monitor_j in enumerate(monitors):
+    #         if i > 0 and j > 0:
+    #             # ai, bi = get_amplitude(monitor_i)
+    #             # aj, bj = get_amplitude(monitor_j)
+    #             # S[i-1, j-1] = bi / aj
+    #             if monitor_i.name.startswith("W"):
+    #                 ai, bi = get_amplitude(monitor_i)
+    #             else:
+    #                 bi, ai = get_amplitude(monitor_i)
+    #             if monitor_j.name.startswith("W"):
+    #                 aj, bj = get_amplitude(monitor_j)
+    #             else:
+    #                 bj, aj = get_amplitude(monitor_j)
+    #             S[i - 1, j - 1] = bi / aj
+
+    _, incident, reflect = monitors
+    a1, b1 = get_amplitude(incident)
+    b2, a2 = get_amplitude(reflect)
+    S[0, 0] = S[1, 1] = b1 / a1  # S11
+    S[1, 0] = S[0, 1] = b2 / a1  # S12
