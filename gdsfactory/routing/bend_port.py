226ab262f320e961c7408ad3931f9ba847038e43
joaquin <j>
2021-08-20 07:24:40 -0700

    fix bend_port
    Former-commit-id: 27e8318341aad8a6868a5ed60c1997e4ba146cf0

--- gdsfactory/routing/bend_port.py
+++ gdsfactory/routing/bend_port.py
@@ -1,12 +1,17 @@
 import gdsfactory as gf
 from gdsfactory.components.bend_circular import bend_circular
-from gdsfactory.types import ComponentFactory, ComponentOrFactory, CrossSectionFactory
+from gdsfactory.types import (
+    ComponentFactory,
+    ComponentOrFactory,
+    CrossSectionFactory,
+    PortName,
+)
 
 
 @gf.cell
 def bend_port(
     component: ComponentOrFactory,
-    port_name: str = "DC_0",
+    port_name: PortName = 1,
     cross_section: CrossSectionFactory = gf.cross_section.metal3,
     bend: ComponentFactory = bend_circular,
     angle: int = 180,
@@ -31,22 +36,16 @@ def bend_port(
     b = c << bend(angle=angle, cross_section=cross_section, **kwargs)
     b.connect(1, ref.ports[port_name])
 
-    bend_ports = b.ports.copy()
-    bend_ports.pop(1)
-    bend_port_name = list(bend_ports.keys())[0]
-
     s = c << gf.c.straight(length=length, cross_section=cross_section)
-    s.connect(2, b.ports[bend_port_name])
+    s.connect(2, b.ports[2])
 
-    ports = ref.ports.copy()
-    ports.pop(port_name)
-    c.add_ports(ports)
-    c.add_port(2, port=s.ports[1])
-    gf.port.auto_rename_ports(c)
+    c.add_ports(ref.get_ports_list())
+    c.ports.pop(port_name)
+    c.add_port(port_name, port=s.ports[1])
     return c
 
 
 if __name__ == "__main__":
     component = gf.c.straight_heater_metal()
-    c = bend_port(component=component, length=component.settings["length"])
+    c = bend_port(component=component, length=component.settings["length"], port_name=1)
     c.show()
