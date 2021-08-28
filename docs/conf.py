ac4df70ec4a3fefe30233b208267b427aeb51e1b
joaquin <j>
2021-08-27 22:29:49 -0700

    Bump version: 3.1.0 → 3.1.1

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 from recommonmark.transform import AutoStructify
 
 project = "gdsfactory"
-version = "3.1.0"
+version = "3.1.1"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
--- gdsfactory/__init__.py
+++ gdsfactory/__init__.py
@@ -130,7 +130,7 @@ __all__ = [
     "types",
     "write_doe",
 ]
-__version__ = "3.1.0"
+__version__ = "3.1.1"
 
 
 if __name__ == "__main__":
