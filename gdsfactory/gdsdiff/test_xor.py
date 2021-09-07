72cfe4fbea52a1939cf1c79759bbdd434d12bdcc
joaquin <j>
2021-09-07 16:32:00 -0700

    fix test_xor

--- gdsfactory/gdsdiff/test_xor.py
+++ gdsfactory/gdsdiff/test_xor.py
@@ -5,14 +5,14 @@ from gdsfactory.gdsdiff.gdsdiff import gdsdiff
 def test_differences():
     c1 = gf.c.straight(length=2)
     c2 = gf.c.straight(length=3)
-    c = gdsdiff(c1, c2, hash_geometry=False)
+    c = gdsdiff(c1, c2)
     assert c.references[-1].area() == 0.5
 
 
 def test_no_differences():
     c1 = gf.c.straight(length=2)
     c2 = gf.c.straight(length=2)
-    c = gdsdiff(c1, c2, hash_geometry=False)
+    c = gdsdiff(c1, c2)
     assert c.references[-1].area() == 0
 
 
@@ -21,5 +21,5 @@ if __name__ == "__main__":
     # test_differences()
     c1 = gf.c.straight(length=2)
     c2 = gf.c.straight(length=2)
-    c = gdsdiff(c1, c2, hash_geometry=False)
+    c = gdsdiff(c1, c2)
     c.show()
