30d4041148e3d2898b78e9f6c81b6055f531f782
joaquin <j>
2021-09-30 14:27:52 -0700

    remove_labels

--- gdsfactory/remove/__init__.py
+++ gdsfactory/remove/__init__.py
@@ -1,4 +1,3 @@
 from gdsfactory.remove.remove_empty_cells import remove_empty_cells
-from gdsfactory.remove.remove_labels import remove_labels
 
-__all__ = ["remove_empty_cells", "remove_labels"]
+__all__ = ["remove_empty_cells"]
