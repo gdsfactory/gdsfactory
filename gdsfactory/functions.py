1069bab432ce24d6b4660a93fa94a12adcb3fe83
joaquin <j>
2021-09-29 17:48:43 -0700

    using memoize from tooz as cache

--- gdsfactory/functions.py
+++ gdsfactory/functions.py
@@ -1,8 +1,3 @@
-""" """
+from toolz import memoize
 
-from functools import lru_cache
-
-
-def cache(user_function, /):
-    'Simple lightweight unbounded cache.  Sometimes called "memoize".'
-    return lru_cache(maxsize=None)(user_function)
+cache = memoize
