cde71f1986a670571a2952d1b52a19da03bb8208
joaquin <j>
2021-10-10 12:24:14 -0700

    moving functions to export
    Former-commit-id: 4e11042f624361f6d9a09bc9e75759b35ec0f06d

---
+++ gdsfactory/export/__init__.py
@@ -0,0 +1,5 @@
+from gdsfactory.export.to_3d import to_3d
+from gdsfactory.export.to_np import to_np
+from gdsfactory.export.to_stl import to_stl
+
+__all__ = ("to_3d", "to_stl", "to_np")
--- gdsfactory/read/__init__.py
+++ gdsfactory/read/__init__.py
@@ -1,7 +1,8 @@
 from gdsfactory.read.component_from_phidl import phidl
 from gdsfactory.read.component_from_picwriter import picwriter
+from gdsfactory.read.from_np import from_np
 from gdsfactory.read.gds import gds
 from gdsfactory.read.gdspaths import gdsdir, gdspaths
 from gdsfactory.read.read_ports import read_ports
 
-__all__ = ["phidl", "picwriter", "gds", "gdspaths", "gdsdir", "read_ports"]
+__all__ = ["phidl", "picwriter", "gds", "gdspaths", "gdsdir", "read_ports", "from_np"]
