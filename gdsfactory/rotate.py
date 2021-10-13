c44b4517f2f118dbd3a37a3c97e1934f357356e3
joaquin <j>
2021-10-13 06:45:03 -0700

    add `tests/test_rotate.py` to ensure cache is working
    Former-commit-id: dd65456050fa74ce6190a308ad78e409814e8b0e

---
+++ gdsfactory/rotate.py
@@ -0,0 +1,38 @@
+from gdsfactory.cell import cell
+from gdsfactory.component import Component
+
+
+@cell
+def rotate(
+    component: Component,
+    angle: int = 90,
+) -> Component:
+    """Returns rotated component inside a new component.
+
+    Most times you just need to place a reference and rotate it.
+    This rotate function just encapsulates the rotated reference into a new component.
+
+    Args:
+        component:
+        angle: in degrees
+    """
+    component_new = Component()
+    component_new.component = component
+    ref = component_new.add_ref(component)
+    ref.rotate(angle)
+    component_new.add_ports(ref.ports)
+    component_new.copy_settings_from(component)
+    return component_new
+
+
+if __name__ == "__main__":
+    import gdsfactory as gf
+
+    c = gf.components.mmi1x2()
+    cr = rotate(component=c)
+    cr.show()
+    # print(component_rotated)
+
+    # component_rotated.pprint
+    # component_netlist = component.get_netlist()
+    # component.pprint_netlist()
---
+++ gdsfactory/tests/test_component_from_yaml_uid.py
@@ -0,0 +1,37 @@
+import gdsfactory as gf
+
+mirror_port = """
+name: mirror_port
+
+instances:
+    mmi_long:
+      component: mmi1x2
+      settings:
+        width_mmi: 4.5
+        length_mmi: 5
+placements:
+    mmi_long:
+        port: o1
+        x: 20
+        y: 10
+        mirror: True
+
+ports:
+    o1: mmi_long,o3
+    o2: mmi_long,o2
+    o3: mmi_long,o1
+"""
+
+
+# def test_components_ports() -> None:
+#     c1 = gf.read.from_yaml(mirror_port)
+#     assert c1.uid
+
+
+if __name__ == "__main__":
+    """FIXME"""
+    c1 = gf.read.from_yaml(mirror_port)
+    c2 = gf.read.from_yaml(mirror_port)
+    print(c1.uid)
+    print(c2.uid)
+    c2.show()
