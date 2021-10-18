458a1f93a62863b861d2141bbac14735dea054db
joaquin <j>
2021-10-17 22:48:24 -0700

    Bump version: 3.3.9 → 3.4.0

--- docs/conf.py
+++ docs/conf.py
@@ -1,7 +1,7 @@
 from recommonmark.transform import AutoStructify
 
 project = "gdsfactory"
-version = "3.3.9"
+version = "3.4.0"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -11,7 +11,7 @@ You can access the config dictionary with `print_config`
 
 """
 
-__version__ = "3.3.9"
+__version__ = "3.4.0"
 import json
 import os
 import pathlib
