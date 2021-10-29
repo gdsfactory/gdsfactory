5e48a83aea0ce48ade4ef1543114407c98cf5aa4
joaquin <j>
2021-10-29 08:38:54 -0700

    avoid trying to cast floats to ints

---
+++ gdsfactory/samples/pdk/fab_e/config.py
@@ -0,0 +1,13 @@
+from gdsfactory.samples.pdk.fab_e.schema import CrossSectionModel, SectionModel, Tech
+
+layers = dict(wg=(1, 0), wg_clad=(1, 2), pin=(100, 0), heater=(11, 0))
+xs_strip = CrossSectionModel()
+xs_strip_heater_metal = CrossSectionModel(
+    width=0.5, sections=[SectionModel(layer=layers["heater"], width=2.5)]
+)
+cross_sections = dict(xs_strip=xs_strip, xs_strip_heater_metal=xs_strip_heater_metal)
+TECH = Tech(cross_sections=cross_sections, layers=layers)
+
+
+if __name__ == "__main__":
+    print(xs_strip_heater_metal)
