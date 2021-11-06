06a832983944af97cbc163288e0be909a53b9199
joaquin <j>
2021-11-06 14:06:35 -0700

    make a setup for

--- setup.py
+++ setup.py
@@ -6,6 +6,11 @@ def get_install_requires():
         return [line.strip() for line in f.readlines() if not line.startswith("-")]
 
 
+def get_install_requires_dev():
+    with open("requirements_dev.txt", "r") as f:
+        return [line.strip() for line in f.readlines() if not line.startswith("-")]
+
+
 setup(
     name="gdsfactory",
     url="https://github.com/gdsfactory/gdsfactory",
@@ -22,4 +27,8 @@ setup(
         [console_scripts]
         gf=gdsfactory.gf:gf
     """,
+    extras_require={
+        "all": list(set(get_install_requires() + get_install_requires_dev())),
+        "basic": get_install_requires(),
+    },
 )
