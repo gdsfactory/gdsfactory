999245cdb04a08f601ad923bd79045789eb07a0a
joaquin <j>
2021-11-07 12:01:03 -0800

    find mode dispersion
    Former-commit-id: 2b4d6e61fcba6bd35af038e79d0da6d22326ddce

--- gdsfactory/simulation/mpb/__init__.py
+++ gdsfactory/simulation/mpb/__init__.py
@@ -1,5 +1,5 @@
+from gdsfactory.simulation.mpb.find_mode_dispersion import find_mode_dispersion
 from gdsfactory.simulation.mpb.find_modes import find_modes
-from gdsfactory.simulation.mpb.find_modes_dispersion import find_modes_dispersion
 from gdsfactory.simulation.mpb.find_neff_vs_width import (
     find_neff_vs_width,
     plot_neff_vs_width,
@@ -10,7 +10,7 @@ from gdsfactory.simulation.mpb.plot_modes import plot_modes
 __all__ = [
     "find_modes",
     "find_neff_vs_width",
-    "find_modes_dispersion",
+    "find_mode_dispersion",
     "get_mode_solver_rib",
     "plot_modes",
     "plot_neff_vs_width",
