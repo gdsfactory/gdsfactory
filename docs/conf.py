4c077e2646939e4afe53b621fc44f08282208c6a
joaquin <j>
2021-11-15 07:58:29 -0800

    Bump version: 3.5.7 → 3.5.8
    Former-commit-id: 20ddf01552658fead8d408bb36ac5f9ae37c9d7d

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 project = "gdsfactory"
-release = "3.5.7"
+release = "3.5.8"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
--- gdsfactory/__init__.py
+++ gdsfactory/__init__.py
@@ -120,7 +120,7 @@ __all__ = [
     "write_doe",
     "Label",
 ]
-__version__ = "3.5.7"
+__version__ = "3.5.8"
 
 
 if __name__ == "__main__":
