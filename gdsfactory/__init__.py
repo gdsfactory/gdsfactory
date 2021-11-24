81963f6ad3861b693b0916f88e5365077d6bad30
joaquin <j>
2021-11-24 14:48:23 -0800

    Bump version: 3.6.2 → 3.6.3
    Former-commit-id: 34f64cb2f7d1bed8d07a8bc18fb33b5939cbdd0d

--- gdsfactory/__init__.py
+++ gdsfactory/__init__.py
@@ -120,7 +120,7 @@ __all__ = [
     "write_doe",
     "Label",
 ]
-__version__ = "3.6.2"
+__version__ = "3.6.3"
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_install_requires_dev():
 setup(
     name="gdsfactory",
     url="https://github.com/gdsfactory/gdsfactory",
-    version="3.6.2",
+    version="3.6.3",
     author="gdsfactory community",
     scripts=["gdsfactory/gf.py"],
     description="python libraries to generate GDS layouts",
