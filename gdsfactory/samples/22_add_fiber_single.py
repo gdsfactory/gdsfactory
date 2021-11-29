a671318b7c660d68db00f3c753f5299e4e3f0fbf
joaquin <j>
2021-11-29 11:27:37 -0800

    fix tutorial
    Former-commit-id: bc1f64a1d8ee6e5dc1d91c85839c3b4c124fe8e7

--- gdsfactory/samples/22_add_fiber_single.py
+++ gdsfactory/samples/22_add_fiber_single.py
@@ -9,7 +9,7 @@ from gdsfactory.samples.big_device import big_device
 
 def test_fiber_single() -> Component:
     w = h = 18 * 50
-    c = big_device(port_pitch=50.0, h=h, w=w)
+    c = big_device(spacing=50.0, size=(w, h))
     return gf.routing.add_fiber_single(component=c, zero_port="W1")
 
 
