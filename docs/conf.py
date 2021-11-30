20ebf144fc7169791e978827861e78a09644fa3e
joaquin <j>
2021-11-29 19:15:05 -0800

    Bump version: 3.6.4 → 3.6.5

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 project = "gdsfactory"
-release = "3.6.4"
+release = "3.6.5"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
--- gdsfactory/__init__.py
+++ gdsfactory/__init__.py
@@ -120,7 +120,7 @@ __all__ = [
     "write_doe",
     "Label",
 ]
-__version__ = "3.6.4"
+__version__ = "3.6.5"
 
 
 if __name__ == "__main__":
