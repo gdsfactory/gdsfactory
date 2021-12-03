657246b7de761e4b41959f2c5a746aa4608e169f
joaquin <j>
2021-12-03 14:05:17 -0800

    add_fiber_array and add_fiber_single can also get a component that has no child_info
    Former-commit-id: a42953a03a9459d70a77746e6bebb218461ae71e

--- gdsfactory/components/cutback_component.py
+++ gdsfactory/components/cutback_component.py
@@ -18,6 +18,7 @@ def cutback_component(
     port2: str = "o2",
     bend180: ComponentFactory = bend_euler180,
     straight: ComponentFactory = straight_function,
+    mirror: bool = False,
 ) -> Component:
     """Returns a daisy chain of components for measuring their loss.
 
@@ -30,6 +31,7 @@ def cutback_component(
         port2: name of second optical port
         bend180: ubend
         straight: waveguide function to connect both sides
+        mirror: Flips component. Useful when 'o2' is the port that you want to route to
 
     """
     component = component() if callable(component) else component
@@ -51,7 +53,10 @@ def cutback_component(
     s = ""
     for i in range(rows):
         s += "AB" * cols
-        s += "D" if i % 2 == 0 else "C"
+        if mirror:
+            s += "C" if i % 2 == 0 else "D"
+        else:
+            s += "D" if i % 2 == 0 else "C"
 
     s = s[:-1]
     s += "-_"
@@ -62,11 +67,16 @@ def cutback_component(
 
     s = s[:-1]
 
-    # Create the component from the sequence
-    c = component_sequence(sequence=s, symbol_to_component=symbol_to_component)
+    seq = component_sequence(sequence=s, symbol_to_component=symbol_to_component)
+
+    c = gf.Component()
+    ref = c << seq
+    c.add_ports(ref.ports)
+
     n = len(s) - 2
     c.copy_child_info(component)
     c.info["components"] = n
+
     # c.info["parent_name"] = f"loopback_{component.info_child.name}_{n}"
     return c
 
@@ -77,75 +87,11 @@ component_flipped = gf.partial(taper, width2=0.5, width1=3)
 straight_long = gf.partial(straight_function, length=20)
 
 
-@gf.cell
-def cutback_component_mirror(
-    component: ComponentFactory = component_flipped,
-    cols: int = 4,
-    rows: int = 5,
-    radius: int = 10,
-    port1: str = "o2",
-    port2: str = "o1",
-    bend180: ComponentFactory = bend_euler180,
-    straight: ComponentFactory = straight_long,
-) -> Component:
-    """Returns a daisy chain of components for measuring their loss.
-
-    Flips component. Useful when 'o2' is the port that you want to route to
-
-    Args:
-        component: for cutback
-        cols
-        rows
-        radius: for bend
-        port1: name of first optical port
-        port2: name of second optical port
-        bend180: ubend
-        straight: waveguide function to connect both sides
-
-    """
-    component = component() if callable(component) else component
-    bendu = bend180(radius=radius)
-    straight_component = straight()
-
-    # Define a map between symbols and (component, input port, output port)
-    symbol_to_component = {
-        "A": (component, port1, port2),
-        "B": (component, port2, port1),
-        "D": (bendu, "o1", "o2"),
-        "C": (bendu, "o2", "o1"),
-        "-": (straight_component, "o1", "o2"),
-        "_": (straight_component, "o2", "o1"),
-    }
-
-    s = ""
-    for i in range(rows):
-        s += "AB" * cols
-        s += "C" if i % 2 == 0 else "D"
-
-    s = s[:-1]
-    s += "-_"
-
-    for i in range(rows):
-        s += "AB" * cols
-        s += "D" if (i + rows + 1) % 2 == 0 else "C"
-
-    s = s[:-1]
-
-    c = component_sequence(sequence=s, symbol_to_component=symbol_to_component)
-    n = len(s) - 2
-    c.copy_child_info(component)
-    c.info["components"] = n
-    c.info["parent_name"] = f"loopback_{component.info_child.name}_{n}"
-    return c
-
-
-# cutback_component_mirror = gf.partial(
-#     cutback_component, port2="o1", port1="o2", component=component_flipped, straight=straight_long
-# )
+cutback_component_mirror = gf.partial(cutback_component, mirror=True)
 
 
 if __name__ == "__main__":
-    c = cutback_component()
-    # c = cutback_component_mirror()
-    c = gf.routing.add_fiber_single(c)
+    # c = cutback_component()
+    c = cutback_component_mirror(component=component_flipped)
+    # c = gf.routing.add_fiber_single(c)
     c.show()
