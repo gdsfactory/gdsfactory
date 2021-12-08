674d2925787c39837f5daf18a223ae824e091bc4
joaquin <j>
2021-12-08 10:14:20 +0100

    add more tests for import_gds

--- gdsfactory/tests/test_import_gds.py
+++ gdsfactory/tests/test_import_gds.py
@@ -1,5 +1,5 @@
 import gdsfactory as gf
-from gdsfactory.import_gds import import_gds
+from gdsfactory.import_gds import add_ports_from_markers_inside, import_gds
 
 
 def test_import_gds_snap_to_grid() -> None:
@@ -12,13 +12,48 @@ def test_import_gds_snap_to_grid() -> None:
         assert gf.snap.is_on_grid(y, 5)
 
 
-def test_import_gds_hierarchy() -> None:
+def test_import_gds_hierarchy() -> gf.Component:
     c0 = gf.components.mzi()
     gdspath = c0.write_gds()
+    gf.clear_cache()
+
     c = import_gds(gdspath)
     assert len(c.get_dependencies()) == 3, len(c.get_dependencies())
+    assert c.name == "mzi", c.name
+    return c
+
+
+def test_import_ports() -> gf.Component:
+    """Make sure you can import the ports"""
+    c0 = gf.components.mzi(decorator=gf.add_pins)
+    gdspath = c0.write_gds()
+    c0x1 = c0.ports["o1"].x
+    c0x2 = c0.ports["o2"].x
+    gf.clear_cache()
+
+    c1 = import_gds(gdspath, decorator=add_ports_from_markers_inside)
+    c1x1 = c1.ports["o1"].x
+    c1x2 = c1.ports["o2"].x
+
+    assert c0x1 == c1x1
+    assert c0x2 == c1x2
+    return c1
+
+
+def test_import_gds_add_padding() -> gf.Component:
+    """Make sure you can import the ports"""
+    c0 = gf.components.mzi(decorator=gf.add_pins)
+    gdspath = c0.write_gds()
+    gf.clear_cache()
+
+    c1 = import_gds(gdspath, decorator=gf.add_padding_container, name="mzi")
+    c1.name == "mzi"
+    return c1
 
 
 if __name__ == "__main__":
-    # test_import_gds_hierarchy()
-    test_import_gds_snap_to_grid()
+    # c = test_import_gds_hierarchy()
+    # c = test_import_ports()
+    c = test_import_gds_add_padding()
+    c.show()
+    # test_import_gds_snap_to_grid()
