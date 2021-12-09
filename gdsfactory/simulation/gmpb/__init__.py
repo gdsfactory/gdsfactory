bf70b12061f5307454c934ccbf057820ac686331
joaquin <j>
2021-12-09 11:37:13 +0100

    find_modes coupler
    Former-commit-id: 79a398ff1a25cad99437f76d68cb03a354145aa5

--- gdsfactory/simulation/gmpb/__init__.py
+++ gdsfactory/simulation/gmpb/__init__.py
@@ -1,3 +1,8 @@
+from gdsfactory.simulation.gmpb import coupler, waveguide
+from gdsfactory.simulation.gmpb.find_coupling_vs_gap import (
+    find_coupling_vs_gap,
+    plot_coupling_vs_gap,
+)
 from gdsfactory.simulation.gmpb.find_mode_dispersion import find_mode_dispersion
 from gdsfactory.simulation.gmpb.find_modes import find_modes
 from gdsfactory.simulation.gmpb.find_neff_vs_width import (
@@ -11,8 +16,12 @@ __all__ = [
     "find_modes",
     "find_neff_vs_width",
     "find_mode_dispersion",
+    "find_coupling_vs_gap",
     "get_mode_solver_rib",
     "get_mode_solver_coupler",
     "plot_neff_vs_width",
+    "plot_coupling_vs_gap",
+    "coupler",
+    "waveguide",
 ]
 __version__ = "0.0.2"
