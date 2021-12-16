0d94f7c0dc9e5debbd1a06cee2d1c90e54685902
joaquin <j>
2021-12-16 12:36:28 +0100

    Bump version: 3.8.8 → 3.8.9

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 project = "gdsfactory"
-release = "3.8.8"
+release = "3.8.9"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def get_install_requires_dev():
 setup(
     name="gdsfactory",
     url="https://github.com/gdsfactory/gdsfactory",
-    version="3.8.8",
+    version="3.8.9",
     author="gdsfactory community",
     scripts=["gdsfactory/gf.py"],
     description="python libraries to generate GDS layouts",
