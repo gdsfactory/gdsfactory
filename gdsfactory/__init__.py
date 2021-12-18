0606e9951af0e8fa3065a9d0330d639ca9e35396
joaquin <j>
2021-12-18 07:06:38 +0100

    Bump version: 3.8.13 → 3.8.14
    Former-commit-id: 6a5c71b985277712ff1a3cca1c2c36cba012c696

--- gdsfactory/__init__.py
+++ gdsfactory/__init__.py
@@ -125,7 +125,7 @@ __all__ = [
     "sweep",
     "Label",
 ]
-__version__ = "3.8.13"
+__version__ = "3.8.14"
 
 
 if __name__ == "__main__":
--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -11,7 +11,7 @@ You can access the config dictionary with `print_config`
 
 """
 
-__version__ = "3.8.13"
+__version__ = "3.8.14"
 import json
 import os
 import pathlib
