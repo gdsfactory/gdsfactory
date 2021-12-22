5150e8f9fa48cc98b76196313a13f42359b6e3fd
joaquin <j>
2021-12-22 15:20:05 +0100

    better messages
    Former-commit-id: 4866e59905e83ae95436171fdf5599ffb44a7ff5

--- gdsfactory/tests/test_phidl_component.py
+++ gdsfactory/tests/test_phidl_component.py
@@ -5,7 +5,7 @@ import gdsfactory as gf
 
 def component_phidl(function_name: str, **kwargs) -> gf.Component:
     if not hasattr(pg, function_name):
-        raise ValueError(f"{function_name} not in {dir(pg)}")
+        raise ValueError(f"{function_name!r} not in {dir(pg)}")
     component_function = getattr(pg, function_name)
     device = component_function(**kwargs)
     return gf.read.from_phidl(device)
