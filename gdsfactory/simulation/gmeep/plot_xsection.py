8859c86e4a0c6ffb3d0dcd4bdd61f150dbe2f9d4
simbilod <sb30@princeton.edu>
2021-12-29 23:06:28 -0500

    high resolution on MEEP makes the index mismatch lower

--- gdsfactory/simulation/gmeep/plot_xsection.py
+++ gdsfactory/simulation/gmeep/plot_xsection.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
     from gdsfactory.components import straight
     from gdsfactory.simulation.gmeep import get_simulation
 
-    c = straight(length=2, width=0.5)
+    c = straight(length=2, width=0.45)
     c = add_padding(c.copy(), default=0, bottom=3, top=3, layers=[(100, 0)])
 
     sim_dict = get_simulation(
@@ -26,12 +26,20 @@ if __name__ == "__main__":
         port_source_offset=-0.1,
         port_field_monitor_offset=-0.1,
         port_margin=2.5,
-        # kwargs
-        eps_averaging=False,
     )
+
+    plt.subplot(1, 2, 1)
     plot_xsection(
         sim_dict["sim"],
         center=sim_dict["monitors"]["o1"].regions[0].center,
         size=sim_dict["monitors"]["o1"].regions[0].size,
     )
+
+    from gdsfactory.simulation.modes import find_modes, get_mode_solver_rib
+
+    ms = get_mode_solver_rib(wg_width=0.45, sy=5, sz=3)
+    modes = find_modes(mode_solver=ms, res=50)
+
+    plt.subplot(1, 2, 2)
+    modes[1].plot_eps()
     plt.show()
