525d38d6ab4f8e3b3c800b24a4514f466484d1fd
joaquin <j>
2021-12-30 16:15:42 +0100

    cdsem_straight_density

--- gdsfactory/components/cdsem_straight_density.py
+++ gdsfactory/components/cdsem_straight_density.py
@@ -6,44 +6,46 @@ from gdsfactory.component import Component
 from gdsfactory.components.straight import straight as straight_function
 from gdsfactory.components.text_rectangular import text_rectangular
 from gdsfactory.cross_section import strip
-from gdsfactory.types import ComponentFactory, CrossSectionFactory
+from gdsfactory.types import ComponentFactory, CrossSectionFactory, Floats, Optional
+
+text_rectangular_mini = partial(text_rectangular, size=1)
+
+widths = 10 * [0.3]
+gaps = 10 * [0.3]
 
 
 @cell
 def cdsem_straight_density(
-    width: float = 0.3,
-    trench_width: float = 0.3,
-    x: float = 420.0,
-    y: float = 50.0,
-    margin: float = 2.0,
+    widths: Floats = widths,
+    gaps: Floats = gaps,
+    length: float = 420.0,
     label: str = "",
-    straight: ComponentFactory = straight_function,
     cross_section: CrossSectionFactory = strip,
-    text: ComponentFactory = text_rectangular,
+    text: Optional[ComponentFactory] = text_rectangular_mini,
 ) -> Component:
     """Returns sweep of dense straight lines
 
     Args:
-        width: width
-        trench_width: trench_width
+        widths: list of widths
+        gaps: list of gaps
+        length: of the lines
+        label: defaults to widths[0] gaps[0]
+        cross_section:
+        text: optional function for text
     """
     c = Component()
-    period = width + trench_width
-    n_o_lines = int((y - 2 * margin) / period)
-    length = x - 2 * margin
+    label = label or f"{int(widths[0]*1e3)} {int(gaps[0]*1e3)}"
 
-    cross_section = partial(cross_section, width=width)
-    tooth = straight(length=length, cross_section=cross_section)
+    ymin = 0
+    for width, gap in zip(widths, gaps):
+        cross_section = partial(cross_section, width=width)
+        tooth_ref = c << straight_function(length=length, cross_section=cross_section)
+        tooth_ref.ymin = ymin
+        ymin += width + gap
 
-    for i in range(n_o_lines):
-        tooth_ref = c.add_ref(tooth)
-        tooth_ref.movey((-n_o_lines / 2 + 0.5 + i) * period)
-        c.absorb(tooth_ref)
-
-    marker_label = text(text=f"{label}")
-    _marker_label = c.add_ref(marker_label)
-    _marker_label.move((length + 3, 10.0))
-    c.absorb(_marker_label)
+    if text:
+        marker_label = c << text(text=f"{label}")
+        marker_label.xmin = tooth_ref.xmax + 5
     return c
 
 
