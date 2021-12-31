f63000945f05bea9342d0051b269b1d31592b787
joaquin <j>
2021-12-31 14:33:28 +0100

    Bump version: 3.9.11 → 3.9.12

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 project = "gdsfactory"
-release = "3.9.11"
+release = "3.9.12"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -11,7 +11,7 @@ You can access the config dictionary with `print_config`
 
 """
 
-__version__ = "3.9.11"
+__version__ = "3.9.12"
 import json
 import os
 import pathlib
