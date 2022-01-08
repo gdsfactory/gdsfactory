98a535d8b88b2a3a7def8211365b85ebf6ca71cd
joaquin <j>
2022-01-08 07:03:11 -0800

    Bump version: 3.9.21 → 3.9.22
    Former-commit-id: 095c17d0b4a994f0abf803d11c7be00029c186ca

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_install_requires_pip():
 setup(
     name="gdsfactory",
     url="https://github.com/gdsfactory/gdsfactory",
-    version="3.9.21",
+    version="3.9.22",
     author="gdsfactory community",
     scripts=["gdsfactory/gf.py"],
     description="python libraries to generate GDS layouts",
