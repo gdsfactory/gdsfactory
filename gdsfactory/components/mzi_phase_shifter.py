ddc20d3cd7301483176a2172b80931f1705dc81e
joaquin <j>
2022-01-18 08:11:06 -0800

    add mzi_pads_center and add_fidutials

--- gdsfactory/components/mzi_phase_shifter.py
+++ gdsfactory/components/mzi_phase_shifter.py
@@ -13,11 +13,12 @@ if __name__ == "__main__":
     # c = mzi_phase_shifter(
     #     straight_x_top=gf.c.straight_pin, straight_x_bot=gf.c.straight_pin
     # )
-    c = mzi_phase_shifter(
-        # straight_x_top=gf.c.straight_heater_doped_rib,
-        straight_x_bot=gf.c.straight_heater_doped_rib,
-        delta_length=20,
-        length_x=500,
-    )
+    # c = mzi_phase_shifter(
+    #     # straight_x_top=gf.c.straight_heater_doped_rib,
+    #     straight_x_bot=gf.c.straight_heater_doped_rib,
+    #     delta_length=20,
+    #     length_x=500,
+    # )
+    c = mzi_phase_shifter()
     c.show()
     print(c.name)
