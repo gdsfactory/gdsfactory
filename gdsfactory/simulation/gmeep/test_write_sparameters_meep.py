63f4b5fc6a73448461f242e02a3df80e5b9842a8
simbilod <sb30@princeton.edu>
2022-01-17 20:45:20 -0500

    implementing manually-defined symmetries for S-parameter calculation
    Former-commit-id: 34b01667831c6652d21ea79c8192f051e7e6d647 [formerly 4c01540b98209e15494ab41844a22a809a989eb0 [formerly 239b738e592fa6ea8b7d59ebd7097ec0c7404673]]
    Former-commit-id: 4c01540b98209e15494ab41844a22a809a989eb0
    Former-commit-id: 0a9c892eba2280f61c2f2a4cfaf77ed402fdfd58

--- gdsfactory/simulation/gmeep/test_write_sparameters_meep.py
+++ gdsfactory/simulation/gmeep/test_write_sparameters_meep.py
@@ -29,10 +29,7 @@ def test_sparameterNxN_crossing(dataframe_regression):
     and sweeps over the ports of all orientations
     Uses low resolution 2D simulations to run faster
     """
-    # c = gf.components.crossing()
-    c = gf.components.straight(length=2)
-    p = 3
-    c = gf.add_padding_container(c, default=0, top=p, bottom=p)
+    c = gf.components.crossing()
     df = write_sparameters_meep(c, overwrite=True, animate=False)
 
     # Check reciprocity
@@ -55,5 +52,44 @@ def test_sparameterNxN_crossing(dataframe_regression):
         dataframe_regression.check(df)
 
 
+def test_sparameterNxN_symmetries(dataframe_regression):
+    """Checks the duplication of Sparameters when using port_symmetry toggle"""
+
+    # No symmetry toggle
+    c = gf.components.crossing()
+    df = write_sparameters_meep(c, overwrite=True, animate=False)
+
+    # Symmetry toggle
+    df_symm = write_sparameters_meep(
+        c,
+        overwrite=True,
+        animate=False,
+        port_symmetries={
+            "o1": {
+                "s11": ["s22", "s33", "s44"],
+                "s21": ["s21", "s34", "s43"],
+                "s31": ["s13", "s24", "s42"],
+                "s41": ["s14", "s23", "s32"],
+            }
+        },
+    )
+
+    # Compare symmetry to no symmetry
+    for i in range(1, len(c.ports) + 1):
+        for j in range(1, len(c.ports) + 1):
+            assert np.allclose(
+                df["s{}{}m".format(i, j)].to_numpy(),
+                df_symm["s{}{}m".format(i, j)].to_numpy(),
+                atol=1e-02,
+            )
+            assert np.allclose(
+                df["s{}{}a".format(i, j)].to_numpy(),
+                df_symm["s{}{}a".format(i, j)].to_numpy(),
+                atol=1e-02,
+            )
+    if dataframe_regression:
+        dataframe_regression.check(df)
+
+
 if __name__ == "__main__":
-    test_sparameterNxN_crossing(None)
+    test_sparameterNxN_straight(False)
