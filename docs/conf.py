3b0ac761aca790865fceced837b416d3550138a3
joaquin <j>
2022-01-20 17:37:30 -0800

    Bump version: 3.10.11 → 3.10.12
    Former-commit-id: 91c3aeeb7e39890db89009a3903fa83dae14595d

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ from gdsfactory.types import ComponentFactoryDict
 autodoc_type_aliases = {ComponentFactoryDict: "ComponentFactoryDict"}
 
 project = "gdsfactory"
-release = "3.10.11"
+release = "3.10.12"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
--- gdsfactory/__init__.py
+++ gdsfactory/__init__.py
@@ -128,7 +128,7 @@ __all__ = [
     "write_cells",
     "Label",
 ]
-__version__ = "3.10.11"
+__version__ = "3.10.12"
 
 
 if __name__ == "__main__":
