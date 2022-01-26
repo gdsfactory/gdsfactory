9f41a882de4d424796c923123dfd131425c59861
joaquin <j>
2022-01-25 19:08:43 -0800

    delete `tech.Library` as it's not being used. You can just use a dict of functions instead
    Former-commit-id: 5409fa4bd91d98cfad0f371d20a3f13036c1f7b5 [formerly 337600c7f989244b906c7d0ac31457899ef070d2 [formerly 8e0d150fbe1da933cfd7fdad4fd12d39f60e8bb3]]
    Former-commit-id: 337600c7f989244b906c7d0ac31457899ef070d2
    Former-commit-id: 429f01dcabec6ddbb41cf9061feb28dca68c7eb6

--- gdsfactory/containers.py
+++ gdsfactory/containers.py
@@ -1,6 +1,5 @@
 from gdsfactory.add_grating_couplers import (
     add_grating_couplers,
-    add_grating_couplers_with_loopback_fiber_array,
     add_grating_couplers_with_loopback_fiber_single,
 )
 from gdsfactory.add_padding import add_padding_container
@@ -15,28 +14,24 @@ from gdsfactory.routing.add_electrical_pads_top import add_electrical_pads_top
 from gdsfactory.routing.add_fiber_array import add_fiber_array
 from gdsfactory.routing.add_fiber_single import add_fiber_single
 from gdsfactory.routing.fanout2x2 import fanout2x2
-from gdsfactory.tech import Library
 
-container_library = Library(name="generic_containers")
-container_library.register(
-    [
-        add_electrical_pads_shortest,
-        add_electrical_pads_top,
-        add_fiber_array,
-        add_fiber_single,
-        add_grating_couplers,
-        add_grating_couplers_with_loopback_fiber_single,
-        add_grating_couplers_with_loopback_fiber_array,
-        add_padding_container,
-        add_termination,
-        cavity,
-        extend_ports,
-        fanout2x2,
-        ring_single_dut,
-        bend_port,
-        rotate,
-    ]
+container_factory = dict(
+    add_electrical_pads_shortest=add_electrical_pads_shortest,
+    add_electrical_pads_top=add_electrical_pads_top,
+    add_fiber_array=add_fiber_array,
+    add_fiber_single=add_fiber_single,
+    add_grating_couplers=add_grating_couplers,
+    add_grating_couplers_with_loopback_fiber_single=add_grating_couplers_with_loopback_fiber_single,
+    # add_grating_couplers_with_loopback_fiber_array=add_grating_couplers_with_loopback_fiber_array,
+    add_padding_container=add_padding_container,
+    add_termination=add_termination,
+    cavity=cavity,
+    extend_ports=extend_ports,
+    fanout2x2=fanout2x2,
+    ring_single_dut=ring_single_dut,
+    bend_port=bend_port,
+    rotate=rotate,
 )
 
 
-__all__ = list(container_library.factory.keys())
+__all__ = list(container_factory.keys())
