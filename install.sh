e59b0595a78fc6bb25ccb80eb15e1b64a42d4f0f
joaquin <j>
2022-01-25 20:47:54 -0800

    add gitpood config

--- install.sh
+++ install.sh
@@ -1,6 +1,4 @@
 #!/bin/sh
 
-pip install -r requirements.txt --upgrade
-pip install -r requirements_full.txt --upgrade
-pip install -r requirements_dev.txt --upgrade
+pip install -e .[dev]
 pre-commit install
