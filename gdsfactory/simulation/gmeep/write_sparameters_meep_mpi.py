46d358f4607d6e797c63bc66afa49c76efe70680
joaquin <j>
2022-01-28 09:46:24 -0800

    rename `padding_north`, `padding_west`, `padding_east`, `padding_south` -> `ymargin_top`, `xmargin_left`, `xmargin_rigth`, `ymargin_bot` for consitency of the meep plugin with the Lumerical plugin.
    Former-commit-id: d26a9613ed192376f5118546838e194a838d1e88

--- gdsfactory/simulation/gmeep/write_sparameters_meep_mpi.py
+++ gdsfactory/simulation/gmeep/write_sparameters_meep_mpi.py
@@ -80,8 +80,8 @@ def write_sparameters_meep_mpi(
         dispersive: use dispersive models for materials (requires higher resolution)
         extend_ports_length: to extend ports beyond the PML
         layer_stack: Dict of layer number (int, int) to thickness (um)
-        t_clad_top: thickness for cladding above core
-        t_clad_bot: thickness for cladding below core
+        zmargin_top: thickness for cladding above core
+        zmargin_bot: thickness for cladding below core
         tpml: PML thickness (um)
         clad_material: material for cladding
         is_3d: if True runs in 3D
@@ -158,11 +158,11 @@ def write_sparameters_meep_mpi(
 
 
 write_sparameters_meep_mpi_east_west = gf.partial(
-    write_sparameters_meep_mpi, padding_north=3, padding_south=3
+    write_sparameters_meep_mpi, ymargin_top=3, ymargin_bot=3
 )
 
 write_sparameters_meep_mpi_west_north = gf.partial(
-    write_sparameters_meep_mpi, padding_south=3, padding_east=3
+    write_sparameters_meep_mpi, ymargin_bot=3, xmargin_rigth=3
 )
 
 
