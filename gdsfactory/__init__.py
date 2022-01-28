446508cca270fed0b00e7578b406e88939063f89
joaquin <j>
2022-01-28 08:49:34 -0800

    Bump version: 3.12.6 → 3.12.7
    Former-commit-id: 0092809b9b3a4a0454856b70e15e9cc25e2f8989 [formerly 3af6fbfc67a4d1086b46c93777d9e91be6c4ebc1 [formerly 3dc55e78f702c101fb43d208d42b6c5bf89048f5]]
    Former-commit-id: 3af6fbfc67a4d1086b46c93777d9e91be6c4ebc1
    Former-commit-id: 96a41b680b6284c2fa2d1e5fc5d50603a6b20c45

--- gdsfactory/__init__.py
+++ gdsfactory/__init__.py
@@ -128,7 +128,7 @@ __all__ = [
     "write_cells",
     "Label",
 ]
-__version__ = "3.12.6"
+__version__ = "3.12.7"
 
 
 if __name__ == "__main__":
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_install_requires_full():
 setup(
     name="gdsfactory",
     url="https://github.com/gdsfactory/gdsfactory",
-    version="3.12.6",
+    version="3.12.7",
     author="gdsfactory community",
     scripts=["gdsfactory/gf.py"],
     description="python library to generate GDS layouts",
