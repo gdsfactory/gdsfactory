ad63549abe5c811f3870ba6d6d5f34b1f689e990
joaquin <j>
2022-01-29 09:45:24 -0800

    Bump version: 3.12.9 → 3.12.10
    Former-commit-id: e71b51cc36c94cee1505429fa069f565471db68a

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ from gdsfactory.types import ComponentFactoryDict
 autodoc_type_aliases = {ComponentFactoryDict: "ComponentFactoryDict"}
 
 project = "gdsfactory"
-release = "3.12.9"
+release = "3.12.10"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -11,7 +11,7 @@ You can access the config dictionary with `print_config`
 
 """
 
-__version__ = "3.12.9"
+__version__ = "3.12.10"
 import io
 import json
 import os
