75d5173d3746fd21875bf3d5b5cfacb2a43f2538
joaquin <j>
2022-02-01 19:40:16 -0800

    tidy3d update to latest

--- gdsfactory/simulation/tidy3d/materials.py
+++ gdsfactory/simulation/tidy3d/materials.py
@@ -1,57 +1,53 @@
+from typing import Optional
+
 import tidy3d as td
-import tidy3d.material_library as mat
-
-MATERIALS = [m for m in dir(mat) if not m.startswith("_")]
+from tidy3d.material_library import material_library
 
 
-def get_index(
+def get_epsilon(
     wavelength: float = 1.55,
     name: str = "cSi",
 ) -> float:
-    """Returns refractive index from material database.
-
-    Args: wavelength: wavelength (um) name: material name
-
-    """
-    if name not in MATERIALS:
-        raise ValueError(f"{name} not in {MATERIALS}")
-
-    medium = getattr(mat, name)
-    medium.name = name
-    frequency = 3e8 / wavelength
-    return medium().epsilon(frequency)
-
-
-def get_material(
-    wavelength: float = 1.55,
-    name: str = "cSi",
-) -> td.Medium:
-    """Returns Medium from materials database
+    """Return permittivity from material database.
 
     Args:
         wavelength: wavelength (um)
         name: material name
+    """
+    medium = get_medium(wavelength=wavelength, name=name)
+    frequency = 3e8 / wavelength
+    return medium.eps_model(frequency)
+
+
+def get_medium(
+    wavelength: float = 1.55, name: str = "cSi", n: Optional[float] = None
+) -> td.Medium:
+    """Return Medium from materials database
+
+    Args:
+        wavelength: wavelength (um)
+        name: material name
+        n: optional index
 
     """
-    if name not in MATERIALS:
-        raise ValueError(f"{name} not in {MATERIALS}")
+    if name not in material_library:
+        materials = list(material_library.keys())
+        raise ValueError(f"{name!r} not in {materials}")
 
     # FIXME: need to remove this.
+    if n:
+        return td.Medium(permittivity=n ** 2)
     if name == "SiO2":
-        return td.Medium(epsilon=1.45)
+        return td.Medium(permittivity=1.45 ** 2)
     elif name in ["cSi", "si"]:
-        return td.Medium(n=3.48)
+        return td.Medium(permittivity=3.48 ** 2)
     elif name in ["SiN", "Si3N4"]:
-        return td.Medium(n=2.0)
+        return td.Medium(permittivity=2.0 ** 2)
     else:
-        raise ValueError(f"not implemetned material, name = {name}")
-
-    medium = getattr(mat, name)
-    medium.name = name
-    return medium
+        raise ValueError(f"not implemetned material {name!r}")
 
 
 if __name__ == "__main__":
-    # print(get_index(name="aSi"))
-    # SiO2 = get_material(name="SiO2")
-    pass
+    print(get_epsilon(name="cSi"))
+    # m = get_medium(name="SiO2")
+    # m = td.Medium(permittivity=1.45 ** 2)
