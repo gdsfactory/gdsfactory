8a4eb460ae92017d0635dbb64bc1b5b548494da0
joaquin <j>
2022-02-02 10:33:44 -0800

    Bump version: 4.0.4 → 4.0.5

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ from gdsfactory.types import ComponentFactoryDict
 autodoc_type_aliases = {ComponentFactoryDict: "ComponentFactoryDict"}
 
 project = "gdsfactory"
-release = "4.0.4"
+release = "4.0.5"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -11,7 +11,7 @@ You can access the config dictionary with `print_config`
 
 """
 
-__version__ = "4.0.4"
+__version__ = "4.0.5"
 import io
 import json
 import os
