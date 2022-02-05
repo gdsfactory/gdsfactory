49ec6e97abb9207f237c62ce5fb3d69a0d171e7b
joaquin <j>
2022-02-04 16:55:24 -0800

    move components to gdslib gds folder

--- gdsfactory/tests/test_components.py
+++ gdsfactory/tests/test_components.py
@@ -1,5 +1,3 @@
-import pathlib
-
 import pytest
 from pytest_regressions.data_regression import DataRegressionFixture
 
@@ -15,7 +13,6 @@ skip_test = {
 }
 
 components_to_test = set(factory.keys()) - skip_test
-dirpath = pathlib.Path(__file__).absolute().with_suffix(".gds")
 
 
 @pytest.fixture(params=components_to_test, scope="function")
@@ -26,7 +23,7 @@ def component_name(request) -> str:
 def test_gds(component_name: str) -> None:
     """Avoid regressions in GDS geometry shapes and layers."""
     component = factory[component_name]()
-    difftest(component, test_name=component_name, dirpath=dirpath)
+    difftest(component, test_name=component_name)
 
 
 def test_settings(component_name: str, data_regression: DataRegressionFixture) -> None:
@@ -39,11 +36,3 @@ def test_assert_ports_on_grid(component_name: str):
     """Ensure ports are on grid."""
     component = factory[component_name]()
     component.assert_ports_on_grid()
-
-
-if __name__ == "__main__":
-    # c = gf.components.coupler(length=1.0 + 1e-4)
-    # c = gf.components.coupler()
-    # c.assert_ports_on_grid()
-    # print(c.ports)
-    print(dirpath)
