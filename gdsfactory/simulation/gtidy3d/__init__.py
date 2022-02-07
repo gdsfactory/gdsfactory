65a649df25225c711cafc1a7cca4d758a8d7608b
joaquin <j>
2022-02-07 09:00:49 -0800

    add xmargin_left, xmargin_right
    Former-commit-id: 841f15f6d437538f6e970ac35854288c730941f7 [formerly ab4bbcad09d00ccaa182b9a97c052b37d20f8711 [formerly 5474e00ebeb03dafec1dadebe399620aa95d5924]]
    Former-commit-id: ab4bbcad09d00ccaa182b9a97c052b37d20f8711
    Former-commit-id: 0016d2baa0a4822b5caa0c8132e2ba255e58387b

--- gdsfactory/simulation/gtidy3d/__init__.py
+++ gdsfactory/simulation/gtidy3d/__init__.py
@@ -1,16 +1,23 @@
 """gdsfactory tidy3d plugin
 
-[tidy3D is a FDTD GPU commercial](https://simulation.cloud/)
+[tidy3D is a fast GPU based commercial FDTD solver](https://simulation.cloud/)
 """
 
 from gdsfactory.simulation.gtidy3d import materials
 from gdsfactory.simulation.gtidy3d.get_results import get_results
-from gdsfactory.simulation.gtidy3d.get_simulation import get_simulation, plot_simulation
+from gdsfactory.simulation.gtidy3d.get_simulation import (
+    get_simulation,
+    plot_simulation,
+    plot_simulation_xz,
+    plot_simulation_yz,
+)
 from gdsfactory.simulation.gtidy3d.get_sparameters import get_sparameters
 
-__version__ = "0.0.1"
+__version__ = "0.0.2"
 __all__ = [
     "plot_simulation",
+    "plot_simulation_xz",
+    "plot_simulation_yz",
     "get_sparameters",
     "get_simulation",
     "get_results",
