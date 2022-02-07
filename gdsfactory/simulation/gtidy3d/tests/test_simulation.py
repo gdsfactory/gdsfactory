f0476c321b0976ea291142bd0cca81110206926f
joaquin <j>
2022-02-07 11:51:24 -0800

    add tests
    Former-commit-id: 712766ac96bca4d952609ff6368e6fd93cf2c4cb [formerly 58e5139fcbf8b53ebfe27ab55ce255702d85c0b8 [formerly 62116740626765d3cf10fc00ac070c9e278e4094]]
    Former-commit-id: 58e5139fcbf8b53ebfe27ab55ce255702d85c0b8
    Former-commit-id: c374d018740ccd2fcdc01df6bd1010a61da0f997

--- gdsfactory/simulation/gtidy3d/tests/test_simulation.py
+++ gdsfactory/simulation/gtidy3d/tests/test_simulation.py
@@ -1,3 +1,5 @@
+import pathlib
+
 from jsondiff import diff
 from omegaconf import OmegaConf
 
@@ -6,34 +8,44 @@ import gdsfactory.simulation.gtidy3d as gt
 from gdsfactory.simulation.gtidy3d.get_results import get_sim_hash
 
 
-def test_simulation_hash():
+def test_simulation_hash() -> None:
     component = gf.components.straight(length=3)
     sim = gt.get_simulation(component=component)
     sim_hash = get_sim_hash(sim)
+
+    sim_hash_reference = "3ad9e452ce489b90f7d8bfaa9c2c81c3"
+
     # print(f"assert hash == {sim_hash!r}")
-    assert sim_hash == "5cf1811250c66398ff88c62f509ae7aa", sim_hash
+    assert sim_hash == sim_hash_reference, f"sim_hash_reference != {sim_hash}"
 
 
-def test_simulation():
+def test_simulation(overwrite: bool = False) -> None:
     """export sim in JSON, and then load it again"""
     component = gf.components.straight(length=3)
     sim = gt.get_simulation(component=component)
-    # sim.to_file("sim_ref.yaml")
+
+    if overwrite:
+        sim.to_file("sim_ref.yaml")  # uncomment to overwrite material
+
     sim.to_file("sim_run.yaml")
 
-    dref = OmegaConf.load("sim_ref.yaml")
-    drun = OmegaConf.load("sim_run.yaml")
+    dirpath = pathlib.Path(__file__).parent
+    dref = OmegaConf.load(dirpath / "sim_ref.yaml")
+    drun = OmegaConf.load(dirpath / "sim_run.yaml")
 
     d = diff(dref, drun)
     assert len(d) == 0, d
 
 
 if __name__ == "__main__":
-    test_simulation()
+    test_simulation_hash()
+
+    # test_simulation(overwrite=True)
+    # test_simulation(overwrite=True)
 
     # component = gf.components.straight(length=3)
     # sim = gt.get_simulation(component=component)
-    # # sim.to_file("sim_ref.yaml")
+    # sim.to_file("sim_ref.yaml")
     # sim.to_file("sim_run.yaml")
 
     # dref = OmegaConf.load("sim_ref.yaml")
