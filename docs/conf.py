ebaf9688027fecfff1424224ae04c2f2d43ce689
joaquin <j>
2022-02-07 12:07:57 -0800

    Bump version: 4.0.11 → 4.0.12
    Former-commit-id: d2402e14c9d55ac16f58dd56727fdbf5301405e7 [formerly c15860d75c64bd79fdfd66141c8a83a95a1f91cc [formerly 19ada86af6afb2a6b048bd045e020cf7584d6605]]
    Former-commit-id: c15860d75c64bd79fdfd66141c8a83a95a1f91cc
    Former-commit-id: 799aed166691d37f1fef236494bddb05fc9e7bdd

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ from gdsfactory.types import ComponentFactoryDict
 autodoc_type_aliases = {ComponentFactoryDict: "ComponentFactoryDict"}
 
 project = "gdsfactory"
-release = "4.0.11"
+release = "4.0.12"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
