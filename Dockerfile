6e66ebbcdc1700f8f8a16087e4d92b1c5f45fbd8
joaquin <j>
2022-02-08 10:22:48 -0800

    Bump version: 4.0.15 → 4.0.16
    Former-commit-id: c4d8c1fb4dec0128ef3f6f142f5775e9971f4d8a

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,1 @@
-FROM joamatab/gdsfactory:4.0.15
+FROM joamatab/gdsfactory:4.0.16
--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ from gdsfactory.types import ComponentFactoryDict
 autodoc_type_aliases = {ComponentFactoryDict: "ComponentFactoryDict"}
 
 project = "gdsfactory"
-release = "4.0.15"
+release = "4.0.16"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
