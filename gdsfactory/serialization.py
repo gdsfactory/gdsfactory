2e49e618e2fffcdde8529eeb8630b0f3cc9860ce
joaquin <j>
2022-02-10 08:47:10 -0800

    using __qualname__ instead of __name__ just in case (no changes seen)

--- gdsfactory/serialization.py
+++ gdsfactory/serialization.py
@@ -21,7 +21,13 @@ def clean_dict(d: Dict[str, Any]) -> Dict[str, Any]:
     return d
 
 
-def clean_value_name(value):
+def get_string(value: Any) -> str:
+    return orjson.dumps(
+        value, option=orjson.OPT_SERIALIZE_NUMPY, default=clean_value_name
+    ).decode()
+
+
+def clean_value_name(value: Any) -> str:
     """Returns a string representation of an object."""
     if isinstance(value, pydantic.BaseModel):
         value = str(value)
@@ -33,35 +39,30 @@ def clean_value_name(value):
         value = str(value)
     elif isinstance(value, np.ndarray):
         value = np.round(value, 3)
-        value = orjson.dumps(
-            value, option=orjson.OPT_SERIALIZE_NUMPY, default=clean_value_name
-        ).decode()
+        value = get_string(value)
     elif callable(value) and isinstance(value, functools.partial):
         sig = inspect.signature(value.func)
         args_as_kwargs = dict(zip(sig.parameters.keys(), value.args))
         args_as_kwargs.update(**value.keywords)
         clean_dict(args_as_kwargs)
         args_as_kwargs.pop("function", None)
-        value = dict(function=value.func.__name__, **args_as_kwargs)
-        value = orjson.dumps(
-            value, option=orjson.OPT_SERIALIZE_NUMPY, default=clean_value_name
-        ).decode()
+        value = dict(function=value.func.__qualname__, **args_as_kwargs)
+        value = get_string(value)
     elif hasattr(value, "to_dict"):
         value = value.to_dict()
-        value = orjson.dumps(
-            value, option=orjson.OPT_SERIALIZE_NUMPY, default=clean_value_name
-        ).decode()
+        value = get_string(value)
     elif isinstance(value, np.float64):
         value = float(value)
         value = str(value)
     elif type(value) in [int, float, str, bool]:
         pass
     elif callable(value) and isinstance(value, toolz.functoolz.Compose):
-        value = [clean_value_json(value.first)] + [
-            clean_value_json(func) for func in value.funcs
+        value = [clean_value_name(value.first)] + [
+            clean_value_name(func) for func in value.funcs
         ]
-    elif callable(value) and hasattr(value, "__name__"):
-        value = value.__name__
+        value = get_string(value)
+    elif callable(value) and hasattr(value, "__qualname__"):
+        value = value.__qualname__
     elif isinstance(value, PathPhidl):
         value = value.hash_geometry()
     elif isinstance(value, pathlib.Path):
@@ -72,14 +73,10 @@ def clean_value_name(value):
             if isinstance(v, dict):
                 d[k] = clean_dict(v)
             else:
-                d[k] = clean_value_json(v)
-        value = orjson.dumps(
-            value, option=orjson.OPT_SERIALIZE_NUMPY, default=clean_value_name
-        ).decode()
+                d[k] = clean_value_name(v)
+        value = get_string(value)
     else:
-        value = orjson.dumps(
-            value, option=orjson.OPT_SERIALIZE_NUMPY, default=clean_value_name
-        ).decode()
+        value = get_string(value)
     return value
 
 
@@ -100,7 +97,7 @@ def clean_value_json(value: Any) -> Any:
         args_as_kwargs.update(**value.keywords)
         clean_dict(args_as_kwargs)
         args_as_kwargs.pop("function", None)
-        value = dict(function=value.func.__name__, **args_as_kwargs)
+        value = dict(function=value.func.__qualname__, **args_as_kwargs)
     elif hasattr(value, "to_dict"):
         value = value.to_dict()
     elif isinstance(value, np.float64):
@@ -111,8 +108,8 @@ def clean_value_json(value: Any) -> Any:
         value = [clean_value_json(value.first)] + [
             clean_value_json(func) for func in value.funcs
         ]
-    elif callable(value) and hasattr(value, "__name__"):
-        value = dict(function=value.__name__)
+    elif callable(value) and hasattr(value, "__qualname__"):
+        value = dict(function=value.__qualname__)
     elif isinstance(value, PathPhidl):
         value = value.hash_geometry()
     elif isinstance(value, pathlib.Path):
@@ -144,7 +141,15 @@ def clean_value_json(value: Any) -> Any:
 if __name__ == "__main__":
     import gdsfactory as gf
 
-    c = gf.c.straight()
-    d = clean_value_json(c.ports)
+    # c = gf.c.straight()
+    # d = clean_value_json(c.ports)
     # d = clean_value_json(c.get_ports_list())
-    print(d, type(d))
+    # print(d, type(d))
+    # f = gf.partial(gf.c.straight, length=3)
+
+    f = gf.cross_section.strip
+    f = gf.cross_section.cross_section
+
+    # d = clean_value_json(f)
+    d = clean_value_name(f)
+    print(f"{d!r}")
