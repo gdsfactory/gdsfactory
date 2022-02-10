b34d943b0df1e06637560db68c0fd53b6fb5469c
joaquin <j>
2022-02-10 11:09:24 -0800

    Bump version: 4.0.18 → 4.1.0
    Former-commit-id: b6de02a3ae1a511fc9d0d2865835f948af0bf8e6

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_install_requires_full():
 setup(
     name="gdsfactory",
     url="https://github.com/gdsfactory/gdsfactory",
-    version="4.0.18",
+    version="4.1.0",
     author="gdsfactory community",
     scripts=["gdsfactory/gf.py"],
     description="python library to generate GDS layouts",
