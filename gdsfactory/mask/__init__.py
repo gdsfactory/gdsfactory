dab779dfee627e342bd80fc187da432caff4a8e2
joaquin <j>
2022-02-10 10:38:16 -0800

    add read_metadata and add test_metadata_mask
    Former-commit-id: e493712239f61616fd470e55006366eb8c763a07

--- gdsfactory/mask/__init__.py
+++ gdsfactory/mask/__init__.py
@@ -3,6 +3,7 @@ from gdsfactory.mask.merge_markdown import merge_markdown
 from gdsfactory.mask.merge_metadata import merge_metadata
 from gdsfactory.mask.merge_test_metadata import get_cell_from_label, merge_test_metadata
 from gdsfactory.mask.merge_yaml import merge_yaml
+from gdsfactory.mask.read_metadata import read_metadata
 from gdsfactory.mask.write_labels import find_labels, write_labels
 
 __all__ = [
@@ -13,5 +14,6 @@ __all__ = [
     "merge_markdown",
     "merge_metadata",
     "merge_test_metadata",
+    "read_metadata",
     "write_labels",
 ]
---
+++ gdsfactory/tests/test_metadata_mask.py
@@ -0,0 +1,29 @@
+import toolz
+
+import gdsfactory as gf
+
+
+@gf.cell
+def mask(size=(1000, 1000)):
+    ring_te = toolz.compose(gf.routing.add_fiber_array, gf.components.ring_single)
+    rings = gf.grid([ring_te(radius=r) for r in [10, 20, 50]])
+    c = gf.Component()
+    c << gf.components.die(size=size)
+    c << rings
+    return c
+
+
+def test_mask_metadata():
+    m = mask()
+    gdspath = m.write_gds_with_metadata(gdspath="mask.gds")
+    labels_path = gdspath.with_suffix(".csv")
+    gf.mask.write_labels(gdspath=gdspath, layer_label=(66, 0))
+    mask_metadata = gf.mask.read_metadata(gdspath=gdspath)
+    tm = gf.mask.merge_test_metadata(
+        mask_metadata=mask_metadata, labels_path=labels_path
+    )
+    assert len(tm.keys()) == 3
+
+
+if __name__ == "__main__":
+    test_mask_metadata()
