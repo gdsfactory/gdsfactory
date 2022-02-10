c1fda5b48e4d75a5e1f5a0f3e28838771c2e3a75
joaquin <j>
2022-02-10 07:36:38 -0800

    better clean_value
    Former-commit-id: 76951db793cbcafed542fe05526b1023ac29af31 [formerly 683bffa5fc2b3a9828e8b1471b1c3405aa4d3dd3 [formerly d0d6dd0c9fcb02ae2f512ead0687ca3cb9b08dc9]]
    Former-commit-id: 683bffa5fc2b3a9828e8b1471b1c3405aa4d3dd3
    Former-commit-id: 32daf325d1daba4415075274482cac0c654f5179

--- gdsfactory/serialization.py
+++ gdsfactory/serialization.py
@@ -1,7 +1,6 @@
 import copy
 import functools
 import inspect
-import json
 import pathlib
 from typing import Any, Dict
 
@@ -22,23 +21,66 @@ def clean_dict(d: Dict[str, Any]) -> Dict[str, Any]:
     return d
 
 
-def clean_key(key):
-    if isinstance(key, tuple):
-        key = key[0]
-    else:
-        key = str(key)
-
-    return key
-
-
 def clean_value_name(value):
     """Returns a string representation of an object."""
     if isinstance(value, pydantic.BaseModel):
         value = str(value)
+    elif isinstance(value, float) and int(value) == value:
+        value = int(value)
+        value = str(value)
+    elif isinstance(value, (np.int64, np.int32)):
+        value = int(value)
+        value = str(value)
+    elif isinstance(value, np.ndarray):
+        value = np.round(value, 3)
+        value = orjson.dumps(
+            value, option=orjson.OPT_SERIALIZE_NUMPY, default=clean_value_name
+        ).decode()
+    elif callable(value) and isinstance(value, functools.partial):
+        sig = inspect.signature(value.func)
+        args_as_kwargs = dict(zip(sig.parameters.keys(), value.args))
+        args_as_kwargs.update(**value.keywords)
+        clean_dict(args_as_kwargs)
+        args_as_kwargs.pop("function", None)
+        value = dict(function=value.func.__name__, **args_as_kwargs)
+        value = orjson.dumps(
+            value, option=orjson.OPT_SERIALIZE_NUMPY, default=clean_value_name
+        ).decode()
     elif hasattr(value, "to_dict"):
-        value = json.dumps(value.to_dict())
+        value = value.to_dict()
+        value = orjson.dumps(
+            value, option=orjson.OPT_SERIALIZE_NUMPY, default=clean_value_name
+        ).decode()
+    elif isinstance(value, np.float64):
+        value = float(value)
+        value = str(value)
+    elif type(value) in [int, float, str, bool]:
+        pass
+    elif callable(value) and isinstance(value, toolz.functoolz.Compose):
+        value = [clean_value_json(value.first)] + [
+            clean_value_json(func) for func in value.funcs
+        ]
+    elif callable(value) and hasattr(value, "__name__"):
+        value = value.__name__
+    elif isinstance(value, PathPhidl):
+        value = value.hash_geometry()
+    elif isinstance(value, pathlib.Path):
+        value = str(value)
+    elif isinstance(value, dict):
+        d = copy.deepcopy(value)
+        for k, v in d.items():
+            if isinstance(v, dict):
+                d[k] = clean_dict(v)
+            else:
+                d[k] = clean_value_json(v)
+        value = orjson.dumps(
+            value, option=orjson.OPT_SERIALIZE_NUMPY, default=clean_value_name
+        ).decode()
     else:
-        value = orjson.dumps(value, option=orjson.OPT_SERIALIZE_NUMPY).decode()
+        value = orjson.dumps(
+            value, option=orjson.OPT_SERIALIZE_NUMPY, default=clean_value_name
+        ).decode()
+    return value
 
 
 def clean_value_json(value: Any) -> Any:
@@ -85,20 +127,8 @@ def clean_value_json(value: Any) -> Any:
         value = orjson.loads(value_json)
     return value
 
-    # elif isinstance(value, Port):
-    #     value = value.to_dict()
-    # elif isinstance(value, CrossSection):
-    #     # value = value.info
-    #     value = value.to_dict()
-    #     value = copy.deepcopy(value)
-    #     value = clean_dict(value)
-    # elif isinstance(value, Port):
-    #     value = copy.deepcopy(value.settings)
-    #     value = clean_dict(value)
     # elif isinstance(value, DictConfig):
     #     clean_dict(value)
-    # elif isinstance(value, PathPhidl):
-    #     value = f"path_{hash_points(value.points)}"
     # elif isinstance(value, (tuple, list, ListConfig)):
     #     value = [clean_value_json(i) for i in value]
     # elif value is None:
@@ -109,12 +139,12 @@ def clean_value_json(value: Any) -> Any:
     #     value = value.get_name()
     # else:
     #     value = str(value)
-    return value
 
 
 if __name__ == "__main__":
     import gdsfactory as gf
 
     c = gf.c.straight()
-    d = clean_value_json(c.ports)
-    # d["o1"]["midpoint"]
+    d = clean_value_name(c.ports)
+    # d = clean_value_name(c.get_ports_list())
+    print(d, type(d))
