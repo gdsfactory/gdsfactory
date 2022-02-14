465093dd8083bd3a066fc111b57effc40a7c446c
joaquin <j>
2022-02-14 08:38:25 -0800

    add simphony and sipann
    Former-commit-id: 531982a243e4cc4893e544e27ba6a9bcf6de9175 [formerly d92170b62033d6c1902e88f51b394a0fc15002b3 [formerly b18294fa97e965f8db96c257f722cbace0346074]]
    Former-commit-id: d92170b62033d6c1902e88f51b394a0fc15002b3
    Former-commit-id: 2f0dcb29a2b03bed42486419846f7c04243b59d0

---
+++ gdsfactory/simulation/simphony/components/ring_single_simpler.py
@@ -0,0 +1,55 @@
+from simphony.netlist import Subcircuit
+
+from gdsfactory.simulation.simphony.components.coupler_ring import coupler_ring
+from gdsfactory.simulation.simphony.components.straight import straight
+
+
+def ring_single(
+    wg_width=0.5,
+    gap=0.2,
+    length_x=4,
+    bend_radius=5,
+    coupler=coupler_ring,
+    straight=straight,
+):
+    r"""Return Single bus ring made of a ring coupler (cb: bottom)
+
+    FIXME! Sparameters are zero
+
+    .. code::
+
+                wt
+           N0            N1
+           |             |
+            \           /
+             \         /
+           ---=========---
+        W0    length_x    E0
+
+
+    """
+    straight = straight(length=length_x) if callable(straight) else straight
+    coupler = (
+        coupler(length_x=length_x, bend_radius=bend_radius, gap=gap, wg_width=wg_width)
+        if callable(coupler)
+        else coupler
+    )
+
+    # Create the circuit, add all individual instances
+    circuit = Subcircuit("ring_double")
+    circuit.add([(coupler, "cb"), (straight, "wt")])
+
+    circuit.connect_many([("cb", "N0", "wt", "W0"), ("wt", "E0", "cb", "N1")])
+    circuit.elements["cb"].pins["W0"] = "input"
+    circuit.elements["cb"].pins["E0"] = "output"
+    return circuit
+
+
+if __name__ == "__main__":
+    import matplotlib.pyplot as plt
+
+    from gdsfactory.simulationsimphony import plot_circuit
+
+    c = ring_single()
+    plot_circuit(c)
+    plt.show()
