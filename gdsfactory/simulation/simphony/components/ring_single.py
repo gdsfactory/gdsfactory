5d1116fad1ebf1912091d05cc7632210db1a51dd
joaquin <j>
2022-02-14 09:29:59 -0800

    add simphony plugin tests
    Former-commit-id: 1763f646a34c248e0ff288c289a375dff4dcaee1

--- gdsfactory/simulation/simphony/components/ring_single.py
+++ gdsfactory/simulation/simphony/components/ring_single.py
@@ -56,7 +56,7 @@ def ring_single(
     .. plot::
         :include-source:
 
-        import gdsfactory.simulation simphony as gs
+        import gdsfactory.simulation.simphony as gs
         import gdsfactory.simulation.simphony.components as gc
 
         c = gc.ring_single()
