1a4d2c8a23abf3f7cf510f1593b9a6205c697d04
joaquin <j>
2022-02-19 11:34:53 -0800

    tidy3d plugin improvements
    Former-commit-id: 07f9912d97fc0dfeab1c5da55661d459f072d684

--- gdsfactory/simulation/gtidy3d/materials.py
+++ gdsfactory/simulation/gtidy3d/materials.py
@@ -1,4 +1,4 @@
-from typing import Dict, Optional
+from typing import Dict, Union
 
 import tidy3d as td
 from tidy3d.components.medium import PoleResidue
@@ -15,15 +15,15 @@ MATERIAL_NAME_TO_MEDIUM = {
 
 
 def get_epsilon(
+    name: Union[str, float],
     wavelength: float = 1.55,
-    name: str = "cSi",
     material_name_to_medium: Dict[str, PoleResidue] = MATERIAL_NAME_TO_MEDIUM,
 ) -> ComplexNumber:
     """Return permittivity from material database.
 
     Args:
         wavelength: wavelength (um)
-        name: material name
+        name: material name or refractive index.
         material_name_to_medium:
     """
     medium = get_medium(name=name, material_name_to_medium=material_name_to_medium)
@@ -32,15 +32,15 @@ def get_epsilon(
 
 
 def get_index(
+    name: Union[str, float],
     wavelength: float = 1.55,
-    name: str = "cSi",
     material_name_to_medium: Dict[str, PoleResidue] = MATERIAL_NAME_TO_MEDIUM,
 ) -> float:
     """Return refractive index from material database.
 
     Args:
         wavelength: wavelength (um)
-        name: material name
+        name: material name or refractive index.
         material_name_to_medium:
     """
 
@@ -54,28 +54,27 @@ def get_index(
 
 
 def get_medium(
-    name: str,
-    n: Optional[float] = None,
+    name: Union[str, float],
     material_name_to_medium: Dict[str, PoleResidue] = MATERIAL_NAME_TO_MEDIUM,
 ) -> td.Medium:
     """Return Medium from materials database
 
     Args:
-        name: material name from MATERIAL_NAME_TO_MEDIUM
-        n: Optional index
+        name: material name or refractive index.
         material_name_to_medium:
     """
-    name = name.lower()
 
-    if n:
-        m = td.Medium(permittivity=n ** 2)
+    name = name.lower() if isinstance(name, str) else name
+
+    if not isinstance(name, str):
+        m = td.Medium(permittivity=name ** 2)
     # elif name == "sio2":
     #     m = td.Medium(permittivity=1.45 ** 2)
     # elif name in ["csi", "si"]:
     #     m = td.Medium(permittivity=3.48 ** 2)
     # elif name in ["sin", "si3n4"]:
     #     m = td.Medium(permittivity=2.0 ** 2)
-    elif name in MATERIAL_NAME_TO_MEDIUM:
+    elif name in material_name_to_medium:
         m = material_name_to_medium[name]
     else:
         materials = list(material_name_to_medium.keys())
@@ -86,6 +85,7 @@ def get_medium(
 
 
 if __name__ == "__main__":
-    print(get_index(name="cSi"))
+    # print(get_index(name="cSi"))
+    print(get_index(name=3.4))
     # m = get_medium(name="SiO2")
     # m = td.Medium(permittivity=1.45 ** 2)
