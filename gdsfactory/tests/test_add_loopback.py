5ce8370f339d6db965870ab5379a37d9e305d5ba
joaquin <j>
2022-02-21 11:24:54 -0800

    rename gen_loopback() function to add_loopback in gdsfactory.add_loopback
    Former-commit-id: 9f49d23851bac07ea069ff0f29419ab1ba34d83b

--- gdsfactory/tests/test_add_loopback.py
+++ gdsfactory/tests/test_add_loopback.py
@@ -1,6 +1,22 @@
-from gdsfactory.add_loopback import straight_with_loopback
+import gdsfactory as gf
+from gdsfactory.add_loopback import add_loopback
 from gdsfactory.component import Component
 from gdsfactory.difftest import difftest
+
+
+@gf.cell
+def straight_with_loopback():
+    c = gf.Component()
+    wg = c << gf.components.straight()
+    c.add(
+        add_loopback(
+            wg.ports["o1"],
+            wg.ports["o2"],
+            grating=gf.components.grating_coupler_te,
+            inside=False,
+        )
+    )
+    return c
 
 
 def test_add_loopback() -> Component:
