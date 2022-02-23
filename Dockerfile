5f7a45554f68a2c2a522b0d147c59a7b5cba4860
joaquin <j>
2022-02-22 19:26:28 -0800

    Bump version: 4.2.6 → 4.2.7

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,1 @@
-FROM joamatab/gdsfactory:4.2.6
+FROM joamatab/gdsfactory:4.2.7
--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_install_requires_full():
 setup(
     name="gdsfactory",
     url="https://github.com/gdsfactory/gdsfactory",
-    version="4.2.6",
+    version="4.2.7",
     author="gdsfactory community",
     scripts=["gdsfactory/gf.py"],
     description="python library to generate GDS layouts",
