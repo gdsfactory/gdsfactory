b85b0bb35aee3722711d936deb1d552a04b6cf54
joaquin <j>
2022-02-23 10:18:35 -0800

    Bump version: 4.2.8 → 4.2.9

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,1 @@
-FROM joamatab/gdsfactory:4.2.8
+FROM joamatab/gdsfactory:4.2.9
--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ from gdsfactory.types import ComponentFactoryDict
 autodoc_type_aliases = {ComponentFactoryDict: "ComponentFactoryDict"}
 
 project = "gdsfactory"
-release = "4.2.8"
+release = "4.2.9"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
