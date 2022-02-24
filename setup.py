c0e6bf3e66a2fb617dd10111dd92f4fa9f64af9b
joaquin <j>
2022-02-24 15:37:13 -0800

    Bump version: 4.2.10 → 4.2.11

--- setup.py
+++ setup.py
@@ -19,7 +19,7 @@ def get_install_requires_full():
 setup(
     name="gdsfactory",
     url="https://github.com/gdsfactory/gdsfactory",
-    version="4.2.10",
+    version="4.2.11",
     author="gdsfactory community",
     scripts=["gdsfactory/gf.py"],
     description="python library to generate GDS layouts",
