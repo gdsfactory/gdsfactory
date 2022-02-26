604343570ec54c0ecd287a672d407c84b71fc179
joaquin <j>
2022-02-26 10:12:08 -0800

    Bump version: 4.2.12 → 4.2.13

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ from gdsfactory.types import ComponentFactoryDict
 autodoc_type_aliases = {ComponentFactoryDict: "ComponentFactoryDict"}
 
 project = "gdsfactory"
-release = "4.2.12"
+release = "4.2.13"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
