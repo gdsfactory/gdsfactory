cb9f01108b0edf371906ee05d1e3b6af0de79fae
joaquin <j>
2022-03-03 19:27:11 -0800

    Bump version: 4.3.0 → 4.3.1

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,1 @@
-FROM joamatab/gdsfactory:4.3.0
+FROM joamatab/gdsfactory:4.3.1
--- gdsfactory/__init__.py
+++ gdsfactory/__init__.py
@@ -128,7 +128,7 @@ __all__ = [
     "write_cells",
     "Label",
 ]
-__version__ = "4.3.0"
+__version__ = "4.3.1"
 
 
 if __name__ == "__main__":
