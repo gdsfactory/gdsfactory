5f50ff97051ae7bd29714313231602d4bf674d50
joaquin <j>
2022-03-10 10:04:16 -0800

    Bump version: 4.3.9 → 4.3.10
    Former-commit-id: 20df555cdcc9a4bd791ca3ec8634edd6091e6650

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,1 @@
-FROM joamatab/gdsfactory:4.3.9
+FROM joamatab/gdsfactory:4.3.10
--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ from gdsfactory.types import ComponentFactoryDict
 autodoc_type_aliases = {ComponentFactoryDict: "ComponentFactoryDict"}
 
 project = "gdsfactory"
-release = "4.3.9"
+release = "4.3.10"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
