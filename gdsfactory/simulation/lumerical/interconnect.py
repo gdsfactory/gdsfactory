b81147eeba791500a5dafb8ceaa64da7e1ab20b4
thomas <thomaspdorch@gmail.com>
2022-03-11 16:47:26 -0800

    initial progress on Interconnect plugin
    Former-commit-id: 984c58184ac02b8692a9ce77169c2bcc326c360e [formerly 8e12ca8b1551da8155004001a0eb9a3ec10f0a7a [formerly cd0661ea9f23f596b200461122c042815d4c4631]]
    Former-commit-id: 8e12ca8b1551da8155004001a0eb9a3ec10f0a7a
    Former-commit-id: 6b58602ca6cf1b80a163a40ee0d0e483e32ab435

---
+++ gdsfactory/simulation/lumerical/interconnect.py
@@ -0,0 +1,147 @@
+from collections import OrderedDict
+from typing import Optional, Tuple
+
+import lumapi
+import numpy as np
+from lumapi import Lumerical
+from omegaconf import DictConfig
+
+c = 2.9979e8
+pi = np.pi
+
+
+def add_interconnect_element(
+    inc: Lumerical,
+    label: str,
+    model: str,
+    loc: Tuple[int, int] = (200.0, 200.0),
+    flip_vert: bool = False,
+    flip_horiz: bool = False,
+    rotation: float = 0.0
+    # **kwargs
+):
+    """
+
+    Args:
+        inc: Interconnect instance
+        label: label for Interconnect component
+        model:
+        loc:
+        flip_vert:
+        flip_horiz:
+        rotation:
+
+    """
+    props = OrderedDict(
+        [
+            ("name", label),
+            ("x position", loc[0]),
+            ("y position", loc[1]),
+            ("horizontal flipped", float(flip_horiz)),
+            ("vertical flipped", float(flip_vert)),
+            ("rotated", rotation)
+            # kwargs
+        ]
+    )
+    return inc.addelement(model, properties=props)
+
+
+def send_to_interconnect(
+    component,
+    session: Optional[Lumerical] = None,
+    placements: dict = None,
+    simulation_settings: OrderedDict = None,
+    run: bool = True,
+    drop_port_prefix: str = None,
+    **settings
+) -> None:
+    """
+    Args:
+        component: component from which to extract netlist
+        session: Interconnect session
+        placements: x,y pairs for where to place the components in the Interconnect GUI
+        simulation_settings: global settings for Interconnect simulation
+        run: whether to run or return the Interconnect sesson
+        drop_port_prefix: if components are written with some prefix, drop up to and including
+            the prefix character.  (i.e. "c1_input" -> "input")
+    """
+    inc: Lumerical = session or lumapi.INTERCONNECT(hide=False)
+
+    inc.switchtolayout()
+    inc.deleteall()
+
+    c = component
+
+    netlist = c.get_netlist()
+
+    instances: DictConfig = netlist["instances"]
+    connections: DictConfig = netlist["connections"]
+    placements: DictConfig = netlist["placements"] if not placements else placements
+
+    for i, instance in enumerate(instances):
+        model = instances[instance].model
+        # component_settings = instances[instance].settings
+
+        add_interconnect_element(
+            inc=inc,
+            label=instance,
+            model=model,
+            loc=(placements[instance].x, placements[instance].y),
+            rotation=placements[instance].rotation,
+            # **component_settings
+        )
+
+    for connection in connections:
+        element2, port2 = connection.split(",")
+        element1, port1 = connections[connection].split(",")
+
+        if drop_port_prefix:
+            port1 = port1[port1.index(drop_port_prefix) + 1 :]
+            port2 = port2[port2.index(drop_port_prefix) + 1 :]
+
+        # EBeam ports are not named consistently between Klayout and Interconnect..
+        # Best to use another
+        if hasattr(instances[element1]["info"], port1):
+            port1 = instances[element1]["info"][port1]
+        if hasattr(instances[element2]["info"], port2):
+            port2 = instances[element2]["info"][port2]
+        inc.connect(element1, port1, element2, port2)
+
+    if simulation_settings:
+        for param, val in zip(simulation_settings.keys(), simulation_settings.values()):
+            inc.setnamed("::Root Element", param, val)
+
+    if run:
+        # There's nothing to run yet so this will give an error
+        inc.run()
+    else:
+        return inc
+
+
+if __name__ == "__main__":
+    import ubcpdk.components as pdk
+
+    import gdsfactory as gf
+
+    c = gf.Component()
+    gc1 = c << pdk.gc_te1550()
+    gc2 = c << pdk.gc_te1550()
+    gc3 = c << pdk.gc_te1550()
+
+    s = c << pdk.y_splitter()
+
+    gc1.connect(port="opt1", destination=s.ports["opt1"])
+    gc2.connect(port="opt1", destination=s.ports["opt2"])
+    gc3.connect(port="opt1", destination=s.ports["opt3"])
+
+    c.show()
+
+    simulation_settings = OrderedDict(
+        [
+            ("bitrate", 2.5e10),
+        ]
+    )
+
+    send_to_interconnect(c, simulation_settings=simulation_settings, run=False)
+
+    pass
