82c77eed7ea4938bdaf589031fee64cd8fbf4711
thomas <thomaspdorch@gmail.com>
2022-03-16 12:38:25 -0700

    formatting
    Former-commit-id: 80c4da1868ba803da3d69a17ee8302e6eb10d54a [formerly 6e4ad32a5dd695fdf9366f9442486cdfa945be62 [formerly 81040467b3cb07e1a387f316b47f331a4d83bb28]]
    Former-commit-id: 6e4ad32a5dd695fdf9366f9442486cdfa945be62
    Former-commit-id: e102aaf82a4041aa445ea22528e687fcf3bf797d

--- gdsfactory/simulation/lumerical/__init__.py
+++ gdsfactory/simulation/lumerical/__init__.py
@@ -1,3 +1,4 @@
+from gdsfactory.simulation.lumerical.interconnect import run_wavelength_sweep
 from gdsfactory.simulation.lumerical.read import read_sparameters_lumerical
 from gdsfactory.simulation.lumerical.write_sparameters_lumerical import (
     write_sparameters_lumerical,
@@ -10,4 +11,5 @@ __all__ = [
     "read_sparameters_lumerical",
     "write_sparameters_lumerical",
     "write_sparameters_lumerical_components",
+    "run_wavelength_sweep",
 ]
