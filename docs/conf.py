42bb904241713845cccab4f38a6a095010aa7f21
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-03-16 11:04:06 -0700

    Bump version: 4.4.5 → 4.4.6

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ from gdsfactory.types import ComponentFactoryDict
 autodoc_type_aliases = {ComponentFactoryDict: "ComponentFactoryDict"}
 
 project = "gdsfactory"
-release = "4.4.5"
+release = "4.4.6"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
