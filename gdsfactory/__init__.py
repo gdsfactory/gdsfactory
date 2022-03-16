35b0c905b290dc6451cc0b75054d532e1ebb1d8a
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-03-16 11:04:06 -0700

    Bump version: 4.4.5 → 4.4.6
    Former-commit-id: 40b4a79697298ef90e613abaaff0cccd04a9f11d [formerly 71e4ba425d69c4451fd256ceeab2d822750d78ec [formerly 42bb904241713845cccab4f38a6a095010aa7f21]]
    Former-commit-id: 71e4ba425d69c4451fd256ceeab2d822750d78ec
    Former-commit-id: e8682d9b980c872370f6fed05ad9e12fdf534fac

--- gdsfactory/__init__.py
+++ gdsfactory/__init__.py
@@ -127,7 +127,7 @@ __all__ = [
     "write_cells",
     "Label",
 ]
-__version__ = "4.4.5"
+__version__ = "4.4.6"
 
 
 if __name__ == "__main__":
--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -11,7 +11,7 @@ You can access the config dictionary with `print_config`
 
 """
 
-__version__ = "4.4.5"
+__version__ = "4.4.6"
 import io
 import json
 import os
