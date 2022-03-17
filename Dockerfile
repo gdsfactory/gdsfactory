5bb3b83fa34984d088e0e0d181181cc49b5c25e6
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-03-16 20:36:06 -0700

    Bump version: 4.4.7 → 4.4.8
    Former-commit-id: aeb87aa53a823b37b1510597eb3e9a834330c92c [formerly f780b77f50698e3448d737488f9f3b750f73f259 [formerly 3d2f33eb627fbe178e545b490e696b9175fc6e8e]]
    Former-commit-id: f780b77f50698e3448d737488f9f3b750f73f259
    Former-commit-id: f75a0532ee8e2c1ca9c5bd7942942008edc8f50c

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,1 @@
-FROM joamatab/gdsfactory:4.4.7
+FROM joamatab/gdsfactory:4.4.8
--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ from gdsfactory.types import ComponentFactoryDict
 autodoc_type_aliases = {ComponentFactoryDict: "ComponentFactoryDict"}
 
 project = "gdsfactory"
-release = "4.4.7"
+release = "4.4.8"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
