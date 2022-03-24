d4d92b4f6d14e80676d0fb7cf1e1d440855d3f94
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-03-23 18:30:29 -0700

    Bump version: 4.4.14 → 4.4.15

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ from gdsfactory.types import ComponentFactoryDict
 autodoc_type_aliases = {ComponentFactoryDict: "ComponentFactoryDict"}
 
 project = "gdsfactory"
-release = "4.4.14"
+release = "4.4.15"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
