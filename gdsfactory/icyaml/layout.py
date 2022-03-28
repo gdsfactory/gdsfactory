6b41565484bafeade56ca064a4e2d8da89541b8c
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-03-28 07:25:34 -0700

    simpler app layout
    Former-commit-id: 515049b82085167b897fa45c4db001ca7c1ef0a1

--- gdsfactory/icyaml/layout.py
+++ gdsfactory/icyaml/layout.py
@@ -4,7 +4,7 @@ https://github.com/DrGFreeman/YAMLDash
 from pathlib import Path
 
 import dash_bootstrap_components as dbc
-from dash import dcc, html
+from dash import html
 
 SRC_URL = "https://github.com/gdsfactory/gdsfactory"
 YAML_REF_URL = "https://yaml.org/spec/1.2/spec.html"
@@ -14,8 +14,6 @@ theme = dbc.themes.UNITED
 
 defaults_path = Path(__file__).parent.joinpath("defaults")
 
-with open(defaults_path.joinpath("default_schema.yaml"), "r") as f:
-    default_schema = f.read()
 
 with open(defaults_path.joinpath("default.yaml"), "r") as f:
     default_yaml = f.read()
@@ -61,40 +59,11 @@ yaml_col = dbc.Col(
     xl=6,
 )
 
-schema_col = dbc.Col(
-    [
-        html.H2("Schema", id="schema-h2", className="mt-3"),
-        dbc.Tooltip(
-            "A schema allows validation of the YAML data "
-            "against specific requirements.",
-            target="schema-h2",
-            placement="left",
-        ),
-        dbc.Textarea(
-            id="schema_text",
-            placeholder="Enter an optional validation schema here...",
-            value=default_schema,
-            rows=20,
-            spellCheck=False,
-            wrap="off",
-            persistence=True,
-            persistence_type="session",
-        ),
-        dcc.Store(
-            id="schema",
-            storage_type="memory",
-        ),
-        html.Div("", id="schema_feedback", className="invalid-feedback"),
-    ],
-    width=12,
-    xl=6,
-)
-
 
 body = dbc.Container(
     children=[
         dbc.Row(
-            [yaml_col, schema_col],
+            [yaml_col],
         )
     ],
     fluid=True,
