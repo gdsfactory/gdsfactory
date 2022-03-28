515049b82085167b897fa45c4db001ca7c1ef0a1
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-03-28 07:25:34 -0700

    simpler app layout

--- gdsfactory/icyaml/app.py
+++ gdsfactory/icyaml/app.py
@@ -4,6 +4,7 @@ https://github.com/DrGFreeman/YAMLDash
 
 import webbrowser
 from multiprocessing import cpu_count
+from pathlib import Path
 
 import dash
 import jsonschema
@@ -22,6 +23,12 @@ app = dash.Dash(
         "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css",
     ],
 )
+defaults_path = Path(__file__).parent.joinpath("defaults")
+
+with open(defaults_path.joinpath("schema.yaml"), "r") as f:
+    schema_text = f.read()
+
+schema_dict = yaml.safe_load(schema_text)
 
 wsgi_app = app.server
 app.title = "ICYAML- Interactive IC based YAML Validator"
@@ -43,7 +50,6 @@ def run():
 
         print("Listening on 127.0.0.1:8080.")
         print("Press CTRL-C to stop.")
-
         waitress.serve(wsgi_app, listen="127.0.0.1:8080", threads=cpu_count())
 
     except ModuleNotFoundError:
@@ -54,51 +60,10 @@ def run():
 
 
 @app.callback(
-    [
-        Output("schema", "data"),
-        Output("schema_text", "className"),
-        Output("schema_feedback", "children"),
-        Output("schema_feedback", "className"),
-    ],
-    [Input("schema_text", "value")],
-)
-def validate_schema(schema_text):
-    class_name = "form-control"
-
-    if schema_text != "" and schema_text is not None:
-        try:
-            schema_dict = yaml.safe_load(schema_text)
-            jsonschema.validate({}, schema_dict)
-        except jsonschema.exceptions.SchemaError as e:
-            return (
-                None,
-                class_name + " is-invalid",
-                f"Invalid Schema: {e}",
-                "invalid-feedback",
-            )
-        except jsonschema.ValidationError:
-            return (
-                schema_dict,
-                class_name + " is-valid",
-                "Valid Schema",
-                "valid-feedback",
-            )
-        except Exception as e:
-            return (
-                None,
-                class_name + " is-invalid",
-                f"YAML ParsingError: {e}",
-                "invalid-feedback",
-            )
-
-    return (None, class_name, "", "")
-
-
-@app.callback(
     [Output("yaml_text", "className"), Output("yaml_feedback", "children")],
-    [Input("schema", "data"), Input("yaml_text", "value")],
+    [Input("yaml_text", "value")],
 )
-def validate_yaml(schema, yaml_text):
+def validate_yaml(yaml_text, schema=schema_dict):
     class_name = "form-control"
 
     try:
@@ -110,17 +75,14 @@ def validate_yaml(schema, yaml_text):
         return (class_name + " is-invalid", f"YAML ParsingError: {e}")
 
     if yaml_dict is not None:
-        if schema is not None:
-            try:
-                jsonschema.validate(yaml_dict, schema)
-                c = from_yaml(yaml_text)
-                c.show()
-                return class_name + " is-valid", ""
-            except ValueError as e:
-                return (class_name + " is-invalid", f"ValueError {e}")
-            except jsonschema.exceptions.ValidationError as e:
-                return (class_name + " is-invalid", f"Schema ValidationError: {e}")
-        else:
+        try:
+            jsonschema.validate(yaml_dict, schema)
+            c = from_yaml(yaml_text)
+            c.show()
             return class_name + " is-valid", ""
+        except ValueError as e:
+            return (class_name + " is-invalid", f"ValueError {e}")
+        except jsonschema.exceptions.ValidationError as e:
+            return (class_name + " is-invalid", f"Schema ValidationError: {e}")
     else:
         return class_name, ""
