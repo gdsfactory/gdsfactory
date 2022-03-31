3a1c1980d8f3b887834957eb58bd7a3b24b704aa
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-03-31 16:12:15 -0700

    Bump version: 4.5.1 → 4.5.2
    Former-commit-id: b084377346f3c7452510554028f30b2a1e51dc7e

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,1 @@
-FROM joamatab/gdsfactory:4.5.1
+FROM joamatab/gdsfactory:4.5.2
--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ from gdsfactory.types import ComponentFactoryDict
 autodoc_type_aliases = {ComponentFactoryDict: "ComponentFactoryDict"}
 
 project = "gdsfactory"
-release = "4.5.1"
+release = "4.5.2"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
