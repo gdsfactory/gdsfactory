66a76a6a2231bd0e77208723d98666818b090d0b
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-04-16 17:24:15 -0700

    add more type annotations
    Former-commit-id: a3be7cea43cce18ea0fd3c67b47d1aac4cc9e584 [formerly 73914230ede7aaa42502b08ce2002ce0744b9992 [formerly 2de2c5761433ace5194254a39b678bf0a61e9fb4]]
    Former-commit-id: 73914230ede7aaa42502b08ce2002ce0744b9992
    Former-commit-id: e17312648617942daa387521ff9bd6dee2298504

--- gdsfactory/port.py
+++ gdsfactory/port.py
@@ -462,7 +462,7 @@ def flipped(port: Port) -> Port:
     return _port
 
 
-def move_copy(port, x=0, y=0):
+def move_copy(port, x=0, y=0) -> Port:
     _port = port.copy()
     _port.midpoint += (x, y)
     return _port
@@ -547,7 +547,7 @@ def _rename_ports_facing_side_ccw(
             p.name = prefix + direction + str(i)
 
 
-def _rename_ports_counter_clockwise(direction_ports, prefix=""):
+def _rename_ports_counter_clockwise(direction_ports, prefix="") -> None:
     east_ports = direction_ports["E"]
     east_ports.sort(key=lambda p: +p.y)  # sort south to north
 
@@ -566,7 +566,7 @@ def _rename_ports_counter_clockwise(direction_ports, prefix=""):
         p.name = f"{prefix}{i+1}" if prefix else i + 1
 
 
-def _rename_ports_clockwise(direction_ports, prefix: str = ""):
+def _rename_ports_clockwise(direction_ports, prefix: str = "") -> None:
     """Rename ports in the clockwise direction starting from the bottom left (west) corner."""
     east_ports = direction_ports["E"]
     east_ports.sort(key=lambda p: -p.y)  # sort north to south
@@ -587,7 +587,7 @@ def _rename_ports_clockwise(direction_ports, prefix: str = ""):
         p.name = f"{prefix}{i+1}" if prefix else i + 1
 
 
-def _rename_ports_clockwise_top_right(direction_ports, prefix: str = ""):
+def _rename_ports_clockwise_top_right(direction_ports, prefix: str = "") -> None:
     """Rename ports in the clockwise direction starting from the top right corner."""
     east_ports = direction_ports["E"]
     east_ports.sort(key=lambda p: -p.y)  # sort north to south
