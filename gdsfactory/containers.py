8b3187236917783d40b28351afa9aba99bfe13e5
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-04-17 07:54:25 -0700

    add pack_doe and pack_doe_grid to containers

--- gdsfactory/containers.py
+++ gdsfactory/containers.py
@@ -1,3 +1,5 @@
+import sys
+
 from gdsfactory.add_grating_couplers import (
     add_grating_couplers,
     add_grating_couplers_with_loopback_fiber_single,
@@ -7,31 +9,34 @@ from gdsfactory.add_termination import add_termination
 from gdsfactory.components.bend_port import bend_port
 from gdsfactory.components.cavity import cavity
 from gdsfactory.components.extension import extend_ports
+from gdsfactory.components.pack_doe import pack_doe, pack_doe_grid
 from gdsfactory.components.ring_single_dut import ring_single_dut
 from gdsfactory.functions import rotate
+from gdsfactory.get_factories import get_cells
 from gdsfactory.routing.add_electrical_pads_shortest import add_electrical_pads_shortest
 from gdsfactory.routing.add_electrical_pads_top import add_electrical_pads_top
 from gdsfactory.routing.add_fiber_array import add_fiber_array
 from gdsfactory.routing.add_fiber_single import add_fiber_single
 from gdsfactory.routing.fanout2x2 import fanout2x2
 
-container_factory = dict(
-    add_electrical_pads_shortest=add_electrical_pads_shortest,
-    add_electrical_pads_top=add_electrical_pads_top,
-    add_fiber_array=add_fiber_array,
-    add_fiber_single=add_fiber_single,
-    add_grating_couplers=add_grating_couplers,
-    add_grating_couplers_with_loopback_fiber_single=add_grating_couplers_with_loopback_fiber_single,
-    # add_grating_couplers_with_loopback_fiber_array=add_grating_couplers_with_loopback_fiber_array,
-    add_padding_container=add_padding_container,
-    add_termination=add_termination,
-    cavity=cavity,
-    extend_ports=extend_ports,
-    fanout2x2=fanout2x2,
-    ring_single_dut=ring_single_dut,
-    bend_port=bend_port,
-    rotate=rotate,
-)
+__all__ = [
+    "add_electrical_pads_shortest",
+    "add_electrical_pads_top",
+    "add_fiber_array",
+    "add_fiber_single",
+    "add_grating_couplers",
+    "add_grating_couplers_with_loopback_fiber_single",
+    "add_padding_container",
+    "add_termination",
+    "cavity",
+    "extend_ports",
+    "fanout2x2",
+    "ring_single_dut",
+    "bend_port",
+    "rotate",
+    "pack_doe",
+    "pack_doe_grid",
+]
 
 
-__all__ = list(container_factory.keys())
+containers = get_cells(sys.modules[__name__])
--- gdsfactory/tests/test_containers.py
+++ gdsfactory/tests/test_containers.py
@@ -5,26 +5,29 @@ from gdsfactory.components.mmi2x2 import mmi2x2
 from gdsfactory.components.mzi_phase_shifter import mzi_phase_shifter_top_heater_metal
 
 # from gdsfactory.components.spiral_inner_io import spiral_inner_io
-from gdsfactory.containers import container_factory
+from gdsfactory.containers import containers
 from gdsfactory.difftest import difftest
 
-container_names = set(container_factory.keys()) - set(
-    ["add_grating_couplers_wi_e642a51b"]
-)
 component = mzi_phase_shifter_top_heater_metal(splitter=mmi2x2)
+
+skip_test = {
+    "pack_doe",
+    "pack_doe_grid",
+}
+container_names = set(containers.keys()) - skip_test
 
 
 @pytest.mark.parametrize("container_type", container_names)
 def test_settings(container_type: str, data_regression: DataRegressionFixture) -> None:
     """Avoid regressions when exporting settings."""
-    c = container_factory[container_type](component=component)
+    c = containers[container_type](component=component)
     data_regression.check(c.to_dict())
 
 
 @pytest.mark.parametrize("container_type", container_names)
 def test_gds(container_type: str) -> None:
     """Avoid regressions in GDS geometry shapes and layers."""
-    c = container_factory[container_type](component=component)
+    c = containers[container_type](component=component)
     difftest(c)
 
 
