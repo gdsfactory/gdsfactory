bb278b097f811ba21df12a7eb87bce8e3d049256
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-04-22 09:30:52 -0700

    add test_serialize
    Former-commit-id: 4479cad865827de67843c347ae9063a01c41b86a

---
+++ gdsfactory/tests/test_serialize.py
@@ -0,0 +1,21 @@
+import gdsfactory as gf
+from gdsfactory.cross_section import strip
+
+
+@gf.cell
+def demo_cross_section_setting(cross_section=strip()) -> gf.Component:
+    c = gf.components.straight(cross_section=cross_section)
+    return c
+
+
+def test_settings(data_regression, check: bool = True) -> None:
+    """Avoid regressions when exporting settings."""
+    component = demo_cross_section_setting()
+    data_regression.check(component.to_dict())
+
+
+if __name__ == "__main__":
+    # c = demo_cross_section_setting()
+    # d = c.to_dict()
+    # c.show()
+    test_settings()
