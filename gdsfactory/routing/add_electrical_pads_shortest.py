23e98bffc8e459733bcf0268566a67bf8ff52c72
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-04-23 09:31:56 -0700

    replace ComponentOrFactory by ComponentSpec

--- gdsfactory/routing/add_electrical_pads_shortest.py
+++ gdsfactory/routing/add_electrical_pads_shortest.py
@@ -3,13 +3,13 @@ from gdsfactory.component import Component
 from gdsfactory.components.pad import pad as pad_function
 from gdsfactory.port import select_ports_electrical
 from gdsfactory.routing.route_quad import route_quad
-from gdsfactory.types import ComponentOrFactory
+from gdsfactory.types import ComponentSpec
 
 
 @gf.cell
 def add_electrical_pads_shortest(
-    component: Component,
-    pad: ComponentOrFactory = pad_function,
+    component: ComponentSpec = "straight",
+    pad: ComponentSpec = pad_function,
     pad_port_spacing: float = 50.0,
     select_ports=select_ports_electrical,
     port_orientation: float = 90,
@@ -29,12 +29,14 @@ def add_electrical_pads_shortest(
 
     """
     c = Component()
+    component = gf.get_component(component)
+    pad = gf.get_component(pad)
+
     c.component = component
     ref = c << component
     ports = select_ports(ref.ports)
     ports = list(ports.values())
 
-    pad = pad(**kwargs) if callable(pad) else pad
     pad_port_spacing += pad.metadata_child["full"]["size"][0] / 2
 
     for port in ports:
@@ -64,10 +66,10 @@ def add_electrical_pads_shortest(
 
 
 if __name__ == "__main__":
-    import gdsfactory as gf
+    # c = gf.components.cross(length=100, layer=gf.LAYER.M3)
+    # c = gf.components.mzi_phase_shifter()
+    # c = gf.components.straight_heater_metal()
+    # c = add_electrical_pads_shortest(component=c)
 
-    c = gf.components.cross(length=100, layer=gf.LAYER.M3)
-    c = gf.components.mzi_phase_shifter()
-    c = gf.components.straight_heater_metal()
-    cc = add_electrical_pads_shortest(component=c)
-    cc.show()
+    c = add_electrical_pads_shortest()
+    c.show()
