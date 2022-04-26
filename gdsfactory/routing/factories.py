4a6b949358fe5420ffd255a92024012be9896bb6
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-04-26 10:32:52 -0700

    add electrical routes to routing_strategy
    Former-commit-id: d00ad444aa8d722933179e6884653baa1cb8229f [formerly 866f4253237071c5bbed358c174310e40900de6b [formerly 52d32c947fc782ccd22bfeab1265f81933246573]]
    Former-commit-id: 866f4253237071c5bbed358c174310e40900de6b
    Former-commit-id: 32c4264c063791a4456c6381a6a9f54fcafa3131

--- gdsfactory/routing/factories.py
+++ gdsfactory/routing/factories.py
@@ -1,12 +1,21 @@
-from gdsfactory.routing.get_bundle import get_bundle, get_bundle_same_axis_no_grouping
-from gdsfactory.routing.get_bundle_from_steps import get_bundle_from_steps
+from gdsfactory.routing.get_bundle import (
+    get_bundle,
+    get_bundle_electrical,
+    get_bundle_same_axis_no_grouping,
+)
+from gdsfactory.routing.get_bundle_from_steps import (
+    get_bundle_from_steps,
+    get_bundle_from_steps_electrical,
+)
 from gdsfactory.routing.get_bundle_from_waypoints import get_bundle_from_waypoints
 from gdsfactory.routing.get_bundle_path_length_match import get_bundle_path_length_match
 
 routing_strategy = dict(
     get_bundle=get_bundle,
+    get_bundle_electrical=get_bundle_electrical,
     get_bundle_path_length_match=get_bundle_path_length_match,
     get_bundle_same_axis_no_grouping=get_bundle_same_axis_no_grouping,
     get_bundle_from_waypoints=get_bundle_from_waypoints,
     get_bundle_from_steps=get_bundle_from_steps,
+    get_bundle_from_steps_electrical=get_bundle_from_steps_electrical,
 )
