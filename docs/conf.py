523d19eb040d233ba7737aaa1952ae4438c8e273
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-04-26 10:33:49 -0700

    Bump version: 5.2.2 → 5.2.3
    Former-commit-id: ec7673d8b89dfdb4af303000b38379f0e2f81cb1 [formerly eb11c242222c980675d578dfdadd72614a08d210 [formerly 82341244ced9baf7c20feab3f025f6496af8bed9]]
    Former-commit-id: eb11c242222c980675d578dfdadd72614a08d210
    Former-commit-id: acac711f2eb35189223a1a1094f55c926700bebc

--- docs/conf.py
+++ docs/conf.py
@@ -6,7 +6,7 @@ from gdsfactory.types import ComponentFactoryDict
 autodoc_type_aliases = {ComponentFactoryDict: "ComponentFactoryDict"}
 
 project = "gdsfactory"
-release = "5.2.2"
+release = "5.2.3"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
