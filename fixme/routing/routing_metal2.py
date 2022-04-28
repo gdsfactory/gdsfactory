1f0b00881a17ae872f6be93111b9779aeadd1cca
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-04-27 20:54:11 -0700

    input_port or output_port orientation is None
    Former-commit-id: 644000f039f10776081fe56825cfc4bcc8559871

--- fixme/routing/routing_metal2.py
+++ fixme/routing/routing_metal2.py
@@ -7,19 +7,20 @@ import gdsfactory as gf
 
 if __name__ == "__main__":
 
-    c = gf.Component("mzi_with_pads")
+    c = gf.Component("ports_none_orientation")
     # c1 = c << gf.components.pad(port_orientation=0)
     # c2 = c << gf.components.pad(port_orientation=180)
 
     c1 = c << gf.components.pad(port_orientation=None)
     c2 = c << gf.components.pad(port_orientation=None)
-
     c2.movex(200)
+    c2.movey(50)
 
     route = gf.routing.get_route_electrical(
-        c1.ports["e1"],
-        c2.ports["e1"],
+        c1.ports["pad"],
+        c2.ports["pad"],
     )
     c.add(route.references)
-
     c.show()
+
+    # print(c2.ports['pad'].midpoint)
