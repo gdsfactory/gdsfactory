2d60ff8edbd7d0146a4dea11c552c1959db53743
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-05-02 11:03:10 -0700

    update docs
    Former-commit-id: 80f0ddba04066349332bcff15bf2f2bb4f235a1d [formerly d1e19214e140fe0346993eb7a4e426d5e5acad18 [formerly a143a2e326dc8d3747f8130732097b513af74fdb]]
    Former-commit-id: d1e19214e140fe0346993eb7a4e426d5e5acad18
    Former-commit-id: 1051bc32035fd43f1e174361e4dacb0f370a29f0

--- gdsfactory/samples/01_component_with_pins.py
+++ gdsfactory/samples/01_component_with_pins.py
@@ -9,8 +9,16 @@ def straight_sample(length=5, width=1):
     """Returns straight with ports."""
     wg = gf.Component("straight_sample")
     wg.add_polygon([(0, 0), (length, 0), (length, width), (0, width)], layer=(1, 0))
-    wg.add_port(name="o1", midpoint=[0, width / 2], width=width, orientation=180)
-    wg.add_port(name="o2", midpoint=[length, width / 2], width=width, orientation=0)
+    wg.add_port(
+        name="o1", midpoint=[0, width / 2], width=width, orientation=180, layer=(1, 0)
+    )
+    wg.add_port(
+        name="o2",
+        midpoint=[length, width / 2],
+        width=width,
+        orientation=0,
+        layer=(1, 0),
+    )
     return wg
 
 
