4b8152e00923c77be3afa25d304f32b62ba3d953
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-05-03 13:07:20 -0700

    Bump version: 5.4.2 → 5.4.3
    Former-commit-id: aa5147417bd48cfa2c31988c24d8743acc77a72a [formerly 07347ef4d203029bc9369198fa7e5eb646e77a36 [formerly 9c5e74570b06ef68e9fc87f2effd3fae9e0f5e4d]]
    Former-commit-id: 07347ef4d203029bc9369198fa7e5eb646e77a36
    Former-commit-id: dca51b8b160d8319ce4a6c66bdca2ad6141c5fdb

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,1 @@
-FROM joamatab/gdsfactory:5.4.2
+FROM joamatab/gdsfactory:5.4.3
--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 project = "gdsfactory"
-release = "5.4.2"
+release = "5.4.3"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
