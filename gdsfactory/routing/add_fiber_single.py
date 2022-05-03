b1b9bb3b0ae3fc1d0cef42c1160cdd6bae11faf6
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-05-03 05:03:14 -0700

    python3.7 should work now

--- gdsfactory/routing/add_fiber_single.py
+++ gdsfactory/routing/add_fiber_single.py
@@ -196,12 +196,15 @@ def add_fiber_single(
     for i, io_row in enumerate(grating_couplers):
         if isinstance(io_row, list):
             for j, io in enumerate(io_row):
-                if ports := io.get_ports_list(prefix="vertical"):
+                ports = io.get_ports_list(prefix="vertical")
+                if ports:
                     port = ports[0]
                     c.add_port(f"{port.name}_{i}{j}", port=port)
-        elif ports := io_row.get_ports_list(prefix="vertical"):
-            port = ports[0]
-            c.add_port(f"{port.name}_{i}", port=port)
+        else:
+            ports = io_row.get_ports_list(prefix="vertical")
+            if ports:
+                port = ports[0]
+                c.add_port(f"{port.name}_{i}", port=port)
 
     if isinstance(grating_coupler, (list, tuple)):
         grating_coupler = grating_coupler[0]
@@ -209,7 +212,9 @@ def add_fiber_single(
     grating_coupler = call_if_func(grating_coupler)
     if with_loopback:
         length = c.ysize - 2 * gc_port_to_edge
-        wg = c << straight(length=length, cross_section=cross_section, **kwargs)
+        wg = c << gf.get_component(
+            straight, length=length, cross_section=cross_section, **kwargs
+        )
         wg.rotate(90)
         wg.xmax = c.xmin - loopback_xspacing
         wg.ymin = c.ymin + gc_port_to_edge
