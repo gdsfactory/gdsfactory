a0c0c369e8a44cb0dd4a673a50e336fcf8e26549
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-05-05 20:14:51 -0700

    Bump version: 5.5.5 → 5.5.6

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,1 @@
-FROM joamatab/gdsfactory:5.5.5
+FROM joamatab/gdsfactory:5.5.6
--- gdsfactory/__init__.py
+++ gdsfactory/__init__.py
@@ -147,4 +147,4 @@ __all__ = [
     "quickplot2",
     "set_quickplot_options",
 ]
-__version__ = "5.5.5"
+__version__ = "5.5.6"
