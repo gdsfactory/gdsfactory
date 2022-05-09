65374106c4ed0a1ec4e43f9b749ed3f4ef0f05bc
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-05-08 17:20:39 -0700

    Bump version: 5.6.0 → 5.6.1

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,1 @@
-FROM joamatab/gdsfactory:5.6.0
+FROM joamatab/gdsfactory:5.6.1
--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 project = "gdsfactory"
-release = "5.6.0"
+release = "5.6.1"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
