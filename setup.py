8f94d4fa1da50c7948767904a2b4ef54453e27ea
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-05-13 08:56:52 -0700

    add setup.py check
    Former-commit-id: 587391e4e1fefc569d4e01f8bffe0bfa8b819a90 [formerly 13be53fd461f702b903aa65eda109cc94c825ae7 [formerly b8c6ef2f7bae2acaaab1ad1fe4490c85d7c70f69]]
    Former-commit-id: 13be53fd461f702b903aa65eda109cc94c825ae7
    Former-commit-id: 0eba87fae46d7b6d3061c4d18bb4794e03254666

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
 from setuptools import find_packages, setup
+import sys
+
+if sys.version_info < (3, 7):
+    raise RuntimeError("This package requres Python 3.7 or newer")
 
 with open("requirements.txt") as f:
     requirements = [
