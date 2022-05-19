597778b9b147ad6de53342a05f41a1324076df18
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-05-18 21:55:38 -0700

    Bump version: 5.6.11 → 5.6.12
    Former-commit-id: 1b12db5b9c8b2bddae89d5c9b038f57f38af26ba [formerly 3c7c280829169ecaa527a621a257f143621237e8 [formerly 2bfaf83fed7d73c87373fd554fc054783a39b42c]]
    Former-commit-id: 3c7c280829169ecaa527a621a257f143621237e8
    Former-commit-id: 68d452a3af7f8f8ac381724e787c3d8bf80dceb1

--- gdsfactory/__init__.py
+++ gdsfactory/__init__.py
@@ -149,4 +149,4 @@ __all__ = [
     "quickplot2",
     "set_quickplot_options",
 ]
-__version__ = "5.6.11"
+__version__ = "5.6.12"
--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -11,7 +11,7 @@ You can access the config dictionary with `print_config`
 
 """
 
-__version__ = "5.6.11"
+__version__ = "5.6.12"
 import io
 import json
 import os
