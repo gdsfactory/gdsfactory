4c817fe680b1e5a521cc97f9400e7aaca3bcc942
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-05-26 07:10:14 -0700

    Update events.py

--- gdsfactory/events.py
+++ gdsfactory/events.py
@@ -1,9 +1,3 @@
-###################################################################################################################
-# PROPRIETARY AND CONFIDENTIAL
-# THIS SOFTWARE IS THE SOLE PROPERTY AND COPYRIGHT (c) 2021 OF ROCKLEY PHOTONICS LTD.
-# USE OR REPRODUCTION IN PART OR AS A WHOLE WITHOUT THE WRITTEN AGREEMENT OF ROCKLEY PHOTONICS LTD IS PROHIBITED.
-# RPLTD NOTICE VERSION: 1.1.1
-###################################################################################################################
 from typing import Callable
 
 
