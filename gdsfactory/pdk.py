a4b1742e3458847250428140196c7fd0c7e112d4
Troy Tamas <troy.tamas@rockleyphotonics.com>
2022-05-26 23:11:34 +0900

    PDK Events and default decorator (#412)
    * adding events module and events relating to Pdk modifications
    * adding default_decorator attribute to Pdk. adding pdk argument to pdk-related events
    * Update events.py

--- gdsfactory/pdk.py
+++ gdsfactory/pdk.py
@@ -2,7 +2,7 @@ import logging
 import pathlib
 import warnings
 from functools import partial
-from typing import Optional
+from typing import Callable, Optional
 
 from omegaconf import DictConfig
 from pydantic import BaseModel
@@ -10,6 +10,7 @@ from pydantic import BaseModel
 from gdsfactory.components import cells
 from gdsfactory.containers import containers as containers_default
 from gdsfactory.cross_section import cross_sections
+from gdsfactory.events import Event
 from gdsfactory.read.from_yaml import from_yaml
 from gdsfactory.types import (
     CellSpec,
@@ -43,6 +44,7 @@ class Pdk(BaseModel):
     cells: Dict[str, ComponentFactory]
     containers: Dict[str, ComponentFactory] = containers_default
     base_pdk: "Pdk" = None
+    default_decorator: Optional[Callable[[Component], None]] = None
 
     def activate(self) -> None:
         if self.base_pdk:
@@ -71,6 +73,7 @@ class Pdk(BaseModel):
                 warnings.warn(f"Overwriting cell {name!r}")
 
             self.cells[name] = cell
+            on_cell_registered.fire(name=name, cell=cell, pdk=self)
 
     def register_containers(self, **kwargs) -> None:
         """Register container factories."""
@@ -84,6 +87,7 @@ class Pdk(BaseModel):
                 warnings.warn(f"Overwriting container {name!r}")
 
             self.containers[name] = cell
+            on_container_registered.fire(name=name, cell=cell, pdk=self)
 
     def register_cross_sections(self, **kwargs) -> None:
         """Register cross_sections factories."""
@@ -96,6 +100,9 @@ class Pdk(BaseModel):
             if name in self.cross_sections:
                 warnings.warn(f"Overwriting cross_section {name!r}")
             self.cross_sections[name] = cross_section
+            on_cross_section_registered.fire(
+                name=name, cross_section=cross_section, pdk=self
+            )
 
     def register_cells_yaml(
         self,
@@ -128,6 +135,7 @@ class Pdk(BaseModel):
                         f"ERROR: Cell name {name!r} from {filepath} already registered."
                     )
                 self.cells[name] = partial(from_yaml, filepath)
+                on_yaml_cell_registered.fire(name=name, cell=self.cells[name], pdk=self)
                 logger.info(f"{message} cell {name!r}")
 
         for k, v in kwargs.items():
@@ -287,6 +295,27 @@ class Pdk(BaseModel):
                 f"CrossSectionFactory, string or dict), got {type(cross_section)}"
             )
 
+    # _on_cell_registered = Event()
+    # _on_container_registered: Event = Event()
+    # _on_yaml_cell_registered: Event = Event()
+    # _on_cross_section_registered: Event = Event()
+    #
+    # @property
+    # def on_cell_registered(self) -> Event:
+    #     return self._on_cell_registered
+    #
+    # @property
+    # def on_container_registered(self) -> Event:
+    #     return self._on_container_registered
+    #
+    # @property
+    # def on_yaml_cell_registered(self) -> Event:
+    #     return self._on_yaml_cell_registered
+    #
+    # @property
+    # def on_cross_section_registered(self) -> Event:
+    #     return self._on_cross_section_registered
+
 
 GENERIC = Pdk(name="generic", cross_sections=cross_sections, cells=cells)
 _ACTIVE_PDK = GENERIC
@@ -310,9 +339,22 @@ def get_active_pdk() -> Pdk:
 
 def set_active_pdk(pdk: Pdk) -> None:
     global _ACTIVE_PDK
+    old_pdk = _ACTIVE_PDK
     _ACTIVE_PDK = pdk
+    on_pdk_activated.fire(old_pdk=old_pdk, new_pdk=pdk)
 
+
+on_pdk_activated: Event = Event()
+on_cell_registered: Event = Event()
+on_container_registered: Event = Event()
+on_yaml_cell_registered: Event = Event()
+on_cross_section_registered: Event = Event()
+
+on_container_registered.add_handler(on_cell_registered.fire)
+on_yaml_cell_registered.add_handler(on_cell_registered.fire)
 
 if __name__ == "__main__":
     c = _ACTIVE_PDK.get_component("straight")
     print(c.settings)
+    # on_pdk_activated += print
+    # set_active_pdk(GENERIC)
