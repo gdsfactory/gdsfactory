aa90b3ed8752fbef3e1655748293e6f5d378bce2
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-05-27 08:59:39 -0700

    Bump version: 5.8.0 → 5.8.1

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,1 @@
-FROM joamatab/gdsfactory:5.8.0
+FROM joamatab/gdsfactory:5.8.1
--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -11,7 +11,7 @@ You can access the config dictionary with `print_config`
 
 """
 
-__version__ = "5.8.0"
+__version__ = "5.8.1"
 import io
 import json
 import os
