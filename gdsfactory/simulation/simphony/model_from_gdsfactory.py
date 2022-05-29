f46b7bd4d58c83bb0dc31946d26851bef2d3dbab
Water drinker <s39chand@edu.uwaterloo.ca>
2022-05-29 02:51:54 -0400

    Update pin stuff
    Former-commit-id: b16d640c69a57b91d6cb1daf52f3744bc247da5b [formerly a2ca44967e1073b7b5d75fa11dc592457fc83947 [formerly eeeac9ba259a8d86b37c58cd3259752114837fa8]]
    Former-commit-id: a2ca44967e1073b7b5d75fa11dc592457fc83947
    Former-commit-id: e7c46cb30e09f9b620e6089d2c1d81e5defdf0ed

--- gdsfactory/simulation/simphony/model_from_gdsfactory.py
+++ gdsfactory/simulation/simphony/model_from_gdsfactory.py
@@ -30,7 +30,7 @@ def model_from_gdsfactory(
     Model.pin_count = len(pins)
     m = Model()
     for i in range(len(pins)):
-        m.pins.append(Pin(component=m, name=pins[i]))
+        m.pins[i] = Pin(component=m, name=pins[i])
     m.__setattr__("sparams", (f, s))
     m.s_parameters = interpolate_sp
     m.freq_range = (m.sparams[0][0], m.sparams[0][-1])
