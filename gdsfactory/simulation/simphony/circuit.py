e780654622d64b61a6d85aa512dfba27bafe4e0c
Water drinker <s39chand@edu.uwaterloo.ca>
2022-05-29 02:49:07 -0400

    Rewrite the way circuit is built from netlist
    Former-commit-id: b31b0b712137a53558c2b8b4a6da369f27276ff0

--- gdsfactory/simulation/simphony/circuit.py
+++ gdsfactory/simulation/simphony/circuit.py
@@ -1,12 +1,12 @@
 from typing import Dict
 
 from simphony import Model
+from simphony.layout import Circuit
 
 import gdsfactory as gf
 from gdsfactory.component import Component
 from gdsfactory.simulation.simphony.components import model_factory
 from gdsfactory.simulation.simphony.types import ModelFactory
-from simphony.layout import Circuit
 
 
 def component_to_circuit(
@@ -22,16 +22,13 @@ def component_to_circuit(
     netlist = component.get_netlist_dict()
     instances = netlist["instances"]
     connections = netlist["connections"]
-
-    circuit = Circuit(component)
     model_names = []
-    model_name_tuple = []
     component_models = list(model_factory.keys())
 
+    a = 0
     for name, metadata in instances.items():
         component_type = metadata["component"]
         component_settings = metadata["settings"]
-        # print(component_type, component_settings)
 
         if component_type is None:
             raise ValueError(f"instance {name!r} has no component_type")
@@ -46,18 +43,50 @@ def component_to_circuit(
         if not isinstance(model, Model):
             raise ValueError(f"model {model!r} is not a simphony Model")
         model_names.append(name)
-        model_name_tuple.append((model, name))
+        model.name = name
 
-    circuit.add(model_name_tuple)
+        if a == 0:
+            circuit = Circuit(model)
+            a += 1
+        else:
+            circuit._add(model)
+
+    components = circuit._get_components()
 
     for k, v in connections.items():
         model1_name, port1_name = k.split(",")
         model2_name, port2_name = v.split(",")
 
         if model1_name in model_names and model2_name in model_names:
-            circuit.connect(model1_name, port1_name, model2_name, port2_name)
+            index1 = [i for i, c in enumerate(components) if c.name == model1_name]
+            p1_name = [
+                i
+                for c in components
+                if c.name == model1_name
+                for i, p in enumerate(c.pins)
+                if p.name == port1_name
+            ]
+            index2 = [i for i, c in enumerate(components) if c.name == model2_name]
+            p2_name = [
+                i
+                for c in components
+                if c.name == model2_name
+                for i, p in enumerate(c.pins)
+                if p.name == port2_name
+            ]
 
-    circuit.info = netlist
+            circuit._get_components()[index1[0]][p1_name[0]].connect(
+                circuit._get_components()[index2[0]][p2_name[0]]
+            )
+
+    for p in circuit._get_components()[8].pins:
+        print(p.name)
+    a = 0
+    for c in components:
+        for p in c.pins:
+            c[p.name].rename(f"pin{a}")
+            a += 1
+
     return circuit
 
 
