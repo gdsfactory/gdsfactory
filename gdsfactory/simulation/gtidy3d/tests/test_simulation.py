9d51efdb25c1902c1659c4ac44e5f0f404391ba6
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-06-11 09:06:45 -0700

    update to tidy3d 1.4.0

--- gdsfactory/simulation/gtidy3d/tests/test_simulation.py
+++ gdsfactory/simulation/gtidy3d/tests/test_simulation.py
@@ -13,7 +13,7 @@ def test_simulation_hash() -> None:
     sim = gt.get_simulation(component=component)
     sim_hash = get_sim_hash(sim)
 
-    sim_hash_reference = "afa9f33ba84ce4b662b3fcf9158086e5"
+    sim_hash_reference = "fa833515e0082169ec47979ff1f2c2f1"
 
     # print(f"assert hash == {sim_hash!r}")
     assert sim_hash == sim_hash_reference, f"sim_hash_reference = {sim_hash!r}"
