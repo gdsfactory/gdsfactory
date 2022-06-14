d85022884996a5a58fe8044bbafac324e7f1cadb
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-06-13 18:47:09 -0700

    Bump version: 5.9.0 → 5.10.0
    Former-commit-id: e6cbee3729599f833020c269a6960f2173b2f2e3 [formerly f3fd687bb2b7a3187d598284b7c9c4a30ca946cc [formerly cad3a8036235ecdb771ef6ae42f24b2f399ab51e]]
    Former-commit-id: f3fd687bb2b7a3187d598284b7c9c4a30ca946cc
    Former-commit-id: 2f97c46e8861b561b26437a1813387130041d988

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,1 @@
-FROM joamatab/gdsfactory:5.9.0
+FROM joamatab/gdsfactory:5.10.0
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open("README.md") as f:
 setup(
     name="gdsfactory",
     url="https://github.com/gdsfactory/gdsfactory",
-    version="5.9.0",
+    version="5.10.0",
     author="gdsfactory community",
     scripts=["gdsfactory/gf.py"],
     description="python library to generate GDS layouts",
