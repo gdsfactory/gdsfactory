2ece752e495c6c8a7971e4a08751b8f818ab64f0
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-06-17 11:28:54 -0700

    fix reticle

--- gdsfactory/samples/23_reticle.py
+++ gdsfactory/samples/23_reticle.py
@@ -5,21 +5,42 @@ Sample of a reticle
 import gdsfactory as gf
 
 
-def mzi_te_pads(**kwargs):
-    c = gf.c.mzi_phase_shifter_top_heater_metal(**kwargs)
-    c_te = gf.routing.add_fiber_single(c)
-    c_te_pads = gf.routing.add_electrical_pads_top(c_te)
-    return c_te_pads
+def mzi_te_pads1(**kwargs):
+    c = gf.c.mzi_phase_shifter_top_heater_metal(delta_length=40)
+    c = gf.routing.add_fiber_single(c)
+    c = c.rotate(-90)
+    c = gf.routing.add_electrical_pads_top(c)
+    gf.add_labels.add_labels_to_ports_electrical(component=c, prefix=f"elec-{c.name}-")
+    return c
+
+
+def mzi_te_pads2(**kwargs):
+    c = gf.c.mzi_phase_shifter_top_heater_metal(delta_length=40)
+    c = gf.routing.add_fiber_single(c)
+    c = c.rotate(-90)
+    c = gf.routing.add_electrical_pads_top_dc(c)
+    gf.add_labels.add_labels_to_ports_electrical(component=c, prefix=f"elec-{c.name}-")
+    return c
+
+
+def mzi_te_pads3(**kwargs):
+    c = gf.c.mzi_phase_shifter_top_heater_metal(delta_length=40)
+    c = gf.routing.add_fiber_single(c)
+    c = c.rotate(-90)
+    c = gf.routing.add_electrical_pads_shortest(c)
+    gf.add_labels.add_labels_to_ports_vertical_dc(component=c, prefix=f"elec-{c.name}-")
+    return c
 
 
 if __name__ == "__main__":
-    # c = mzi_te_pads()
-    # c.show()
-    c = gf.c.mzi_phase_shifter_top_heater_metal(delta_length=40)
+    c = mzi_te_pads3()
+    c.show()
+
+    # c = gf.c.mzi_phase_shifter_top_heater_metal(delta_length=40)
     # c = gf.routing.add_fiber_single(c)
     # c = c.rotate(-90)
-    c = gf.routing.add_electrical_pads_top(c)
-    gf.add_labels.add_labels_to_ports_electrical(component=c, prefix=f"elec-{c.name}")
-    c.show()
+    # c = gf.routing.add_electrical_pads_top(c)
+    # gf.add_labels.add_labels_to_ports_electrical(component=c, prefix=f"elec-{c.name}")
+    # c.show()
     print(c.get_labels())
-    print(c.get_ports_dict(port_type="electrical").keys())
+    # print(c.get_ports_dict(port_type="electrical").keys())
