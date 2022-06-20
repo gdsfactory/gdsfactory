a52f8659e4b55185ea6b63306b122c08667bbf44
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-06-20 10:21:32 -0700

    Bump version: 5.10.10 → 5.10.11

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 project = "gdsfactory"
-release = "5.10.10"
+release = "5.10.11"
 copyright = "2019, PsiQ"
 author = "PsiQ"
 
--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -11,7 +11,7 @@ You can access the config dictionary with `print_config`
 
 """
 
-__version__ = "5.10.10"
+__version__ = "5.10.11"
 import io
 import json
 import os
