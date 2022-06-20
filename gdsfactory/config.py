af570b8af9691fc087036ded9a32188c2c5716b7
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-06-20 10:05:23 -0700

    Bump version: 5.10.9 → 5.10.10
    Former-commit-id: 0f702bec9dc054b974d5caaf393cacc0c8bf936c [formerly edd7bad523e6b7f43ef8b185aca8ed7778580858]
    Former-commit-id: f5c1ae3d6066dca08ba613b8d873fe08f818c800

--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -11,7 +11,7 @@ You can access the config dictionary with `print_config`
 
 """
 
-__version__ = "5.10.9"
+__version__ = "5.10.10"
 import io
 import json
 import os
--- setup.py
+++ setup.py
@@ -27,7 +27,7 @@ with open("README.md") as f:
 setup(
     name="gdsfactory",
     url="https://github.com/gdsfactory/gdsfactory",
-    version="5.10.9",
+    version="5.10.10",
     author="gdsfactory community",
     scripts=["gdsfactory/gf.py"],
     description="python library to generate GDS layouts",
