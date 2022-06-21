7a0408f3485edb197d7afd3ba6f66147fb9815f8
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-06-21 09:43:44 -0700

    fix tests
    Former-commit-id: f66ed60ad8ed7a9facc959dba594ec2fc368fb2a [formerly a41ccf08d4e1958bf98c208afb6a0e3cbb92e164 [formerly a9c8b881d59c54a0bea0f1116b0a804cd70358fc] [formerly 3556d2347d4d40f8b3f51edaad7c6f2b2117fad6 [formerly 95dadc18257ff3b25cc96b043253eda1e10ef3f1]]]
    Former-commit-id: 3556d2347d4d40f8b3f51edaad7c6f2b2117fad6
    Former-commit-id: 12a20530ea6f1989874daa8cadf179635c9fda80 [formerly c010e76c5239fd70c34c7e1de6cd38bb2fb962fb]
    Former-commit-id: 2c523b66dc42afbc9cdb7cdfdf56238e347472c9

--- gdsfactory/tests/test_netlists.py
+++ gdsfactory/tests/test_netlists.py
@@ -90,6 +90,7 @@ if __name__ == "__main__":
 
     component_type = "ring_double"
     component_type = "ring_single"
+    component_type = "ring_single_array"
     c1 = components[component_type]()
     n = c1.get_netlist()
     yaml_str = OmegaConf.to_yaml(n, sort_keys=True)
