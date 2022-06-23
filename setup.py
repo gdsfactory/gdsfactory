b06a4aa7f057af2b405da069f500d74a777f9b32
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-06-23 16:09:28 -0700

    Bump version: 5.11.0 → 5.11.1

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ with open("README.md") as f:
 setup(
     name="gdsfactory",
     url="https://github.com/gdsfactory/gdsfactory",
-    version="5.11.0",
+    version="5.11.1",
     author="gdsfactory community",
     scripts=["gdsfactory/gf.py"],
     description="python library to generate GDS layouts",
