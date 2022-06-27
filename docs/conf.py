9fd6dc80618c994e16cdd55deaa2afca931190bb
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-06-27 09:14:34 -0700

    Bump version: 5.11.2 → 5.11.3

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 project = "gdsfactory"
-release = "5.11.2"
+release = "5.11.3"
 copyright = "2019, PsiQ"
 
 html_theme = "sphinx_book_theme"
--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -11,7 +11,7 @@ You can access the config dictionary with `print_config`
 
 """
 
-__version__ = "5.11.2"
+__version__ = "5.11.3"
 import io
 import json
 import os
