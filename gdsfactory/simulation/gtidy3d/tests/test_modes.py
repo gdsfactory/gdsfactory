868da681dea75a77aa4c096872aa72c60db8b57f
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-07-05 08:23:52 +0200

    mode solver improvements
    Former-commit-id: 20b6f8182ecba27bc1c2e73dc500b8220ae5dedc [formerly e5a49a0589d4fef321968462e3bfb61f2fe875b6]
    Former-commit-id: 72f3ce4870aeee0d3503d3e9745315d159b25cc8

--- gdsfactory/simulation/gtidy3d/tests/test_modes.py
+++ gdsfactory/simulation/gtidy3d/tests/test_modes.py
@@ -1,9 +1,9 @@
 import numpy as np
 
-from gdsfactory.simulation.gtidy3d.modes import Waveguide, si, sio2
+from gdsfactory.simulation.gtidy3d.modes import Waveguide, group_index, si, sio2
 
 
-def test_index():
+def test_neff_cached():
     c = Waveguide(
         wavelength=1.55,
         wg_width=0.5,
@@ -17,5 +17,34 @@ def test_index():
     assert np.isclose(n0, 2.46586, rtol=0.01), n0
 
 
+def test_neff_no_cache():
+    c = Waveguide(
+        wavelength=1.55,
+        wg_width=0.5,
+        wg_thickness=0.22,
+        slab_thickness=0.0,
+        ncore=si,
+        nclad=sio2,
+        cache=None,
+    )
+    c.compute_modes()
+    n0 = abs(c.neffs[0])
+    assert np.isclose(n0, 2.46586, rtol=0.01), n0
+
+
+def test_ng_no_cache():
+    wg_settings = dict(
+        wavelength=1.55,
+        wg_width=0.5,
+        wg_thickness=0.22,
+        slab_thickness=0.0,
+        ncore=si,
+        nclad=sio2,
+        cache=None,
+    )
+    ng = group_index(**wg_settings)
+    assert np.isclose(ng, 4.169, rtol=0.01), ng
+
+
 if __name__ == "__main__":
-    test_index()
+    test_ng_no_cache()
