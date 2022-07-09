5f35dade1542ac9c3201efa7dbd3ea1e2dcdc630
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-07-09 13:59:52 +0200

    add different klayout modes

--- gdsfactory/geometry/write_drc.py
+++ gdsfactory/geometry/write_drc.py
@@ -95,29 +95,58 @@ def write_drc_deck(rules: List[str], layer_map: Dict[str, Layer]) -> str:
     return "\n".join(script)
 
 
+modes = ["tiled", "default", "deep"]
+
+
 def write_drc_deck_macro(
     name: str = "generic",
     filepath: Optional[PathType] = None,
     shortcut: str = "Ctrl+Shift+D",
+    mode: str = "tiled",
+    threads: int = 4,
+    tile_size: int = 500,
+    tile_borders: Optional[int] = None,
     **kwargs,
 ) -> str:
     """Write klayout DRC macro.
     You can customize the shortcut to run the DRC macro from the Klayout GUI.
 
     Args:
-        name: drc rule deck name
-        filepath: Optional macro path (defaults to .klayout/drc/name.lydrc)
-        shortcut: to run macro from klayout GUI
+        name: drc rule deck name.
+        filepath: Optional macro path (defaults to .klayout/drc/name.lydrc).
+        shortcut: to run macro from klayout GUI.
+        mode: tiled, default or deep (hiearchical).
+        threads: number of threads.
+        tile_size: in um for tile mode.
+        tile_borders: sides for each. Defaults None to automatic.
 
     Keyword Args:
-        rules: list of rules
-        layer_map: layer definitions can be dict or dataclass
+        rules: list of rules.
+        layer_map: layer definitions can be dict or dataclass.
 
-    Keyword Args:
-        rules: list of rules
-        layer_map: layer definitions can be dict or dataclass
+    modes:
+
+    - default
+        - flat polygon handling
+        - single thread
+        - no overhead
+        - use for small layout
+        - no side effects
+    - tiled
+        - need to optimize tile size (maybe 500x500um). Works of each tile individually.
+        - finite lookup range
+        - output is flat
+        - multithreading enable
+    - deep
+        - hierarchical mode
+        - experimental
+        - preserves hierarchical
 
     """
+
+    if mode not in modes:
+        raise ValueError(f"{mode!r} not in {modes}")
+
     script = f"""<?xml version="1.0" encoding="utf-8"?>
 <klayout-macro>
  <description>{name} DRC</description>
@@ -140,11 +169,23 @@ def write_drc_deck_macro(
 # Based on SOEN pdk https://github.com/usnistgov/SOEN-PDK/tree/master/tech/OLMAC
 # http://klayout.de/doc/manual/drc_basic.html
 
-report("generic DRC")
-tiles(100)
-tile_borders(2)
-threads(3)
+report("{name} DRC")
 """
+
+    if mode == "tiled":
+        script += f"""
+threads({threads})
+tiles({tile_size})
+"""
+        if tile_borders:
+            script += f"""
+tile_borders({tile_borders})
+"""
+    elif mode == "deep":
+        script += """
+deep
+"""
+
     script += write_drc_deck(**kwargs)
 
     script += """
@@ -170,7 +211,7 @@ if __name__ == "__main__":
         rule_width(layer="M2", value=2),
         rule_space(layer="M2", value=2),
         rule_separation(layer1="HEATER", layer2="M1", value=1.0),
-        rule_enclosing(layer1="M1", layer2="VIAC", value=0.2),
+        rule_enclosing(layer1="VIAC", layer2="M1", value=0.2),
     ]
 
     drc_rule_deck = write_drc_deck_macro(rules=rules, layer_map=gf.LAYER)
