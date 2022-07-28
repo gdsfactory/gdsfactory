b1cd321ae62b52c090b02590f834b21d2287fbf6
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-07-28 17:30:48 +0200

    fix filepath
    Former-commit-id: 16c8dc1cc202a83560ddb59cb5c2e7f99028351f [formerly 596b5325d2b1c5b11ff3f5e49dfeb3002b5bac11 [formerly bfb5f22ba144163cc511b0fd9d780fd0d9636330] [formerly ae44f02e2f964c6c6424e124e87d91af8b0ba7b6 [formerly dddc8d6c32f9c92484f9cb468df609376fbf5f1a]]]
    Former-commit-id: ae44f02e2f964c6c6424e124e87d91af8b0ba7b6
    Former-commit-id: b7af08bbf3c7b88a6ae3837721397b541dec1eda [formerly 938d37e3231806db1111dc4dde95aaf793a6a089]
    Former-commit-id: e2a7ca16544c6c118af63c58ea4305466666ac2d

--- gdsfactory/simulation/sax/read.py
+++ gdsfactory/simulation/sax/read.py
@@ -1,10 +1,11 @@
-"""read Sparameters from CSV file and return sax model.
+"""read Sparameters from a CSV file and returns sax model.
 
 TODO: write dat to csv converter
 (from lumerical interconnect to SAX format and the other way around)
 """
 
 from functools import partial
+from pathlib import PosixPath
 from typing import Union
 
 import numpy as np
@@ -15,13 +16,12 @@ from scipy.interpolate import interp1d
 import gdsfactory as gf
 from gdsfactory.config import sparameters_path
 from gdsfactory.simulation.get_sparameters_path import get_sparameters_path_lumerical
-from gdsfactory.types import PathType
 
 wl_cband = np.linspace(1.500, 1.600, 128)
 
 
 def sdict_from_csv(
-    filepath: Union[PathType, pd.DataFrame],
+    filepath: Union[str, PosixPath, pd.DataFrame],
     wl: Float = wl_cband,
     xkey: str = "wavelengths",
     xunits: float = 1,
@@ -38,7 +38,7 @@ def sdict_from_csv(
         xunits: x units in um from the loaded file (um). 1 means 1um.
         prefix: for the sparameters column names in file.
     """
-    df = pd.read_csv(filepath) if not isinstance(filepath, pd.DataFrame) else filepath
+    df = filepath if isinstance(filepath, pd.DataFrame) else pd.read_csv(filepath)
     keys = list(df.keys())
 
     if xkey not in df:
@@ -68,7 +68,7 @@ def sdict_from_csv(
     }
 
 
-def demo_mmi_lumerical_csv() -> None:
+def _demo_mmi_lumerical_csv() -> None:
     import matplotlib.pyplot as plt
     from plot_model import plot_model
 
@@ -90,7 +90,6 @@ grating_coupler_elliptical = gf.partial(
     sdict_from_csv,
     filepath=sparameters_path / "grating_coupler_ellipti_9d85a0c6_18c08cac.csv",
 )
-
 
 model_factory = dict(
     mmi1x2=mmi1x2, mmi2x2=mmi2x2, grating_coupler_elliptical=grating_coupler_elliptical
