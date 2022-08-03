21b2394d9da1e789135ab9c0399f66baa99472f1
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-08-03 11:32:12 -0700

    Bump version: 5.14.7 → 5.14.8
    Former-commit-id: a79a4724b3b4983318573764ff803584848618d0 [formerly 1def17db17ebd4913cce09be8bd1bd6cf649461b]
    Former-commit-id: 609581fc34702495a2b8f0947013367b488e3e79

--- setup.py
+++ setup.py
@@ -32,7 +32,7 @@ with open("README.md") as f:
 setup(
     name="gdsfactory",
     url="https://github.com/gdsfactory/gdsfactory",
-    version="5.14.7",
+    version="5.14.8",
     author="gdsfactory community",
     description="python library to generate GDS layouts",
     long_description=long_description,
