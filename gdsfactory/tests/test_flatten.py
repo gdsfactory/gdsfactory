28a7d7b433e650d4b63dd80ae8c6270a43287fd5
Troy Tamas <troy.tamas@rockleyphotonics.com>
2022-08-08 12:37:08 +0900

    fixing copy and flatten functions to prevent side effects
    Former-commit-id: d5b41abec875753c7d1474afec1a34a129d1736a [formerly 4173a9daeade44325577b3cc5e19af68ff97ea51 [formerly 4f8f998d0b895a47b7e2d8d7036405f237c65b64] [formerly ebb96df8beb5d879cc907bfd0fdde768d476a85b [formerly c3742360bbf2f74a9984c25475df8063e80af6f4]]]
    Former-commit-id: ebb96df8beb5d879cc907bfd0fdde768d476a85b
    Former-commit-id: 26b9288039d36c81fd8d7b2a82c8094c594d745c [formerly fbd4534b43110f1ef76739cdeaf0c830b81d8065]
    Former-commit-id: 4f6d328be06a5e93e4a5a57385022e061a134113

--- gdsfactory/tests/test_flatten.py
+++ gdsfactory/tests/test_flatten.py
@@ -10,6 +10,30 @@ def test_flatten() -> None:
     assert c1.name != c2.name, f"{c1.name} must be different from {c2.name}"
 
 
+def test_two_flats_in_one():
+    c = gf.Component()
+    c1 = gf.components.straight()
+    c2 = c1.flatten()
+    c3 = c1.flatten()
+    c3.add_label("I'm different")
+
+    c << c1
+    r2 = c << c2
+    r3 = c << c3
+    r2.movey(-100)
+    r3.movey(-200)
+    assert c2.name != c3.name
+
+
+def test_flattened_cell_keeps_info():
+    c1 = gf.components.straight()
+    c2 = c1.flatten()
+    assert (
+        len(c1.info) > 0
+    ), "This test doesn't make any sense unless there is some info to copy"
+    assert c1.info == c2.info
+
+
 if __name__ == "__main__":
     c1 = gf.components.mzi()
     c2 = c1.flatten(single_layer=(2, 0))
