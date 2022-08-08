46a9a3705e6d05e57160910d71c55acc19900fdc
Troy Tamas <troy.tamas@rockleyphotonics.com>
2022-08-08 18:38:29 +0900

    adding docstring
    Former-commit-id: 0330fa1816d6885df2221ff7189d57e45753da31 [formerly a19320e36f011a3baa2cbf59cbc0a7648b69a4c5 [formerly 5fcbd0278f00323abb4b04b4348ab48a6c6bc8fd] [formerly 71dde867c9bf4eb2ba392b682cea95a04b576e25 [formerly 724f17412ba3599ebc7625dcca2413e7e288bf1f]]]
    Former-commit-id: 71dde867c9bf4eb2ba392b682cea95a04b576e25
    Former-commit-id: e7cac20a09f508a97989d63d0703ed72d84831ca [formerly 8ddec4157dd11de06ace6a36293cfbf5c5132ffa]
    Former-commit-id: 220989ecd9b4b78e7f70a460b8c5fbe4bb0f26a5

--- gdsfactory/flatten_invalid_refs.py
+++ gdsfactory/flatten_invalid_refs.py
@@ -9,6 +9,13 @@ from gdsfactory.snap import is_on_grid
 
 
 def flatten_invalid_refs(component: Component, grid_size: int = 1):
+    """
+    Flattens all references of the component with invalid GDS transformations (i.e. non-90 deg rotations or sub-grid translations). This is an in-place operation.
+
+    Args:
+        component: the component to fix (in place)
+        grid_size: the GDS grid size, in nm (any translations with higher resolution than this are considered invalid)
+    """
     refs = component.references.copy()
     for ref in refs:
         origin_is_on_grid = all([is_on_grid(x, grid_size) for x in ref.origin])
