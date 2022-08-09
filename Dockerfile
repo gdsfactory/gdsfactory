6c68946914b44b21bece91b445df7220c3a21f99
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-08-09 09:18:28 -0700

    Bump version: 5.15.3 → 5.16.0
    Former-commit-id: 2672b002d161a97e028064468d57a6f2207437fd [formerly f658b6c9b19fa4cb6a5b03663c69788fc70874a9 [formerly 4af6c338f0e7306e7023578db701732950b310f7] [formerly ccd5bd025d4dec23dfa93e55c4e59ba91eda5edd [formerly 7d124aa50c617f05bef6607bc6c02ed90abb790b]]]
    Former-commit-id: ccd5bd025d4dec23dfa93e55c4e59ba91eda5edd
    Former-commit-id: 9f29b547345c1d022a1c118722334744fcb21b77 [formerly e732ad4e5cb68e3a5bcc86b20617960e99739608]
    Former-commit-id: 55dfc802b89caaf3d7a0d2c25f84c661bdac8be8

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,1 @@
-FROM joamatab/gdsfactory:5.15.3
+FROM joamatab/gdsfactory:5.16.0
--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 project = "gdsfactory"
-release = "5.15.3"
+release = "5.16.0"
 copyright = "2020, MIT License"
 
 html_theme = "sphinx_book_theme"
