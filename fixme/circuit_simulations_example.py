4da7c3119c9c0ffd7cc6a64eb6bec0408f427f60
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-08-09 08:59:26 -0700

    add fixme for circuit_simulations
    Former-commit-id: 37d4e7571b97048ea883a66823384be1f7af2375 [formerly 472169c3d2ce7dc070f9e57b83da37d90f74ee6a]
    Former-commit-id: 23902fd791dc711600640a1dbe8ec337f6f3d315

---
+++ fixme/circuit_simulations_example.py
@@ -0,0 +1,50 @@
+"""Demo of non-hierarchical circuit simulations.
+"""
+import jax.numpy as jnp
+import sax
+import gdsfactory as gf
+
+
+def straight(wl=1.5, length=10.0, neff=2.4) -> sax.SDict:
+    return sax.reciprocal({("o1", "o2"): jnp.exp(2j * jnp.pi * neff * length / wl)})
+
+
+def mmi1x2():
+    """Assumes a perfect 1x2 splitter"""
+    return sax.reciprocal(
+        {
+            ("o1", "o2"): 0.5**0.5,
+            ("o1", "o3"): 0.5**0.5,
+        }
+    )
+
+
+def bend_euler(wl=1.5, length=20.0):
+    """ "Let's assume a reduced transmission for the euler bend compared to a straight"""
+    return {k: 0.99 * v for k, v in straight(wl=wl, length=length).items()}
+
+
+models = {
+    "bend_euler": bend_euler,
+    "mmi1x2": mmi1x2,
+    "straight": straight,
+}
+
+if __name__ == "__main__":
+    import numpy as np
+    import matplotlib.pyplot as plt
+
+    c = gf.components.mzi()
+    c.show(show_ports=True)
+    netlist = c.get_netlist_dict()
+    circuit = sax.circuit_from_netlist(netlist=netlist, models=models)
+    wl = np.linspace(1.5, 1.6)
+    S = circuit(wl=wl)
+
+    plt.figure(figsize=(14, 4))
+    plt.title("MZI")
+    plt.plot(1e3 * wl, jnp.abs(S["o1", "o2"]) ** 2)
+    plt.xlabel("λ [nm]")
+    plt.ylabel("T")
+    plt.grid(True)
+    plt.show()
