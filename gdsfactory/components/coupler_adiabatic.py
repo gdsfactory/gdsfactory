c0df2461c4e8cf1c908cb74d94524ef5067dd57a
SkandanC <s39chand@edu.uwaterloo.ca>
2022-08-14 12:13:43 -0400

    Test files
    Former-commit-id: 266a0de03c70e1d1aef6d5fadbab95afbf386325 [formerly 92cfb4893da4c3cf145cb46f5354174fba139f10]
    Former-commit-id: 502be3b75dc1dd36e68f28bcf193f3f4f59d377e

--- gdsfactory/components/coupler_adiabatic.py
+++ gdsfactory/components/coupler_adiabatic.py
@@ -1,11 +1,10 @@
-from typing import Tuple
-
-import picwriter.components as pc
+from gdspy import Path
+from phidl import Device as Component
+from scipy.special import binom
 
 import gdsfactory as gf
-from gdsfactory.component import Component
-from gdsfactory.components.waveguide_template import strip
-from gdsfactory.types import ComponentSpec, CrossSectionSpec
+from gdsfactory.cross_section import strip
+from gdsfactory.types import CrossSectionSpec
 
 
 @gf.cell
@@ -17,22 +16,17 @@ def coupler_adiabatic(
     input_wg_sep: float = 3.0,
     output_wg_sep: float = 3.0,
     dw: float = 0.1,
-    port: Tuple[int, int] = (0, 0),
-    direction: str = "EAST",
-    waveguide_template: ComponentSpec = strip,
-    cross_section: CrossSectionSpec = "strip",
+    cross_section: CrossSectionSpec = strip,
     **kwargs
-) -> Component:
+):
     """Returns 50/50 adiabatic coupler.
 
     Design based on asymmetric adiabatic 3dB coupler designs, such as those.
-
     - https://doi.org/10.1364/CLEO.2010.CThAA2,
     - https://doi.org/10.1364/CLEO_SI.2017.SF1I.5
     - https://doi.org/10.1364/CLEO_SI.2018.STh4B.4
 
     Has input Bezier curves, with poles set to half of the x-length of the S-bend.
-
     I is the first half of input S-bend where input widths taper by +dw and -dw
     II is the second half of the S-bend straight with constant, unbalanced widths
     III is the region where the two asymmetric straights gradually come together
@@ -50,46 +44,361 @@ def coupler_adiabatic(
         output_wg_sep: Separation of the two straights at the output, center-to-center.
         dw: Change in straight width.
             In Region 1, top arm tapers to width+dw/2.0, bottom taper to width-dw/2.0.
-        port: coordinate of the input port (top left).
-        direction: for component NORTH, WEST, SOUTH, EAST,or angle in radians.
-        waveguide_template: object or function.
+        cross_section: spec.
 
     Keyword Args:
-       wg_width: in um.
-       wg_layer: gf.LAYER.WG[0].
-       wg_datatype: gf.LAYER.WG[1].
-       clad_layer: gf.LAYER.WGCLAD[0].
-       clad_datatype: gf.LAYER.WGCLAD[1].
-       bend_radius: in um.
-       cladding_offset: in um.
-
+        cross_section kwargs.
     """
+    # Control points for input and output S-bends
+    control_points_input_top = [
+        (0, 0),
+        (length1 / 2.0, 0),
+        (length1 / 2.0, -input_wg_sep / 2.0 + wg_sep / 2.0),
+        (length1, -input_wg_sep / 2.0 + wg_sep / 2.0),
+    ]
 
-    c = pc.AdiabaticCoupler(
-        gf.call_if_func(waveguide_template, **kwargs),
-        length1=length1,
-        length2=length2,
-        length3=length3,
-        wg_sep=wg_sep,
-        input_wg_sep=input_wg_sep,
-        output_wg_sep=output_wg_sep,
-        dw=dw,
-        port=port,
-        direction=direction,
+    control_points_input_bottom = [
+        (0, -input_wg_sep),
+        (length1 / 2.0, -input_wg_sep),
+        (length1 / 2.0, -input_wg_sep / 2.0 - wg_sep / 2.0),
+        (length1, -input_wg_sep / 2.0 - wg_sep / 2.0),
+    ]
+
+    control_points_output_top = [
+        (length1 + length2, -input_wg_sep / 2.0 + wg_sep / 2.0),
+        (
+            length1 + length2 + length3 / 2.0,
+            -input_wg_sep / 2.0 + wg_sep / 2.0,
+        ),
+        (
+            length1 + length2 + length3 / 2.0,
+            -input_wg_sep / 2.0 + output_wg_sep / 2.0,
+        ),
+        (
+            length1 + length2 + length3,
+            -input_wg_sep / 2.0 + output_wg_sep / 2.0,
+        ),
+    ]
+
+    control_points_output_bottom = [
+        (length1 + length2, -input_wg_sep / 2.0 - wg_sep / 2.0),
+        (
+            length1 + length2 + length3 / 2.0,
+            -input_wg_sep / 2.0 - wg_sep / 2.0,
+        ),
+        (
+            length1 + length2 + length3 / 2.0,
+            -input_wg_sep / 2.0 - output_wg_sep / 2.0,
+        ),
+        (
+            length1 + length2 + length3,
+            -input_wg_sep / 2.0 - output_wg_sep / 2.0,
+        ),
+    ]
+
+    # Base bezier function
+    def _bezier_curve(t, control_points):
+        """Returns bezier coordinates.
+
+        Args:
+            t: 1D array of points varying between 0 and 1.
+        """
+        xs = 0.0
+        ys = 0.0
+        n = len(control_points) - 1
+        for k in range(n + 1):
+            ank = binom(n, k) * (1 - t) ** (n - k) * t ** k
+            xs += ank * control_points[k][0]
+            ys += ank * control_points[k][1]
+
+        return (xs, ys)
+
+    # Bend specific bezier functions
+    def _bezier_inp_top(t):
+        return _bezier_curve(t, control_points_input_top)
+
+    def _bezier_inp_bottom(t):
+        return _bezier_curve(t, control_points_input_bottom)
+
+    def _bezier_output_bottom(t):
+        return _bezier_curve(t, control_points_output_bottom)
+
+    def _bezier_output_top(t):
+        return _bezier_curve(t, control_points_output_top)
+
+    c = Component()
+
+    x = gf.get_cross_section(cross_section, **kwargs)
+
+    wg_width = x.width
+
+    # Top input S-bend
+    top_input = Path(width=wg_width)
+    top_input.parametric(
+        _bezier_inp_top,
+        max_points=199,
+        tolerance=0.00001,
+        final_width=wg_width + dw / 2.0,
+        layer=gf.get_layer(x.layer)[0],
+        datatype=gf.get_layer(x.layer)[1],
+    )
+    c.add(top_input)
+
+    # Bottom input S-bend
+    bot_input = Path(width=wg_width)
+    bot_input.parametric(
+        _bezier_inp_bottom,
+        max_points=199,
+        tolerance=0.00001,
+        final_width=wg_width - dw / 2.0,
+        layer=gf.get_layer(x.layer)[0],
+        datatype=gf.get_layer(x.layer)[1],
+    )
+    c.add(bot_input)
+
+    # Top output S-bend
+    top_output = Path(width=wg_width)
+    top_output.parametric(
+        _bezier_output_top,
+        max_points=199,
+        tolerance=0.00001,
+        layer=gf.get_layer(x.layer)[0],
+        datatype=gf.get_layer(x.layer)[1],
+    )
+    c.add(top_output)
+
+    # Bottom output S-bend
+    bot_output = Path(width=wg_width)
+    bot_output.parametric(
+        _bezier_output_bottom,
+        max_points=199,
+        tolerance=0.00001,
+        layer=gf.get_layer(x.layer)[0],
+        datatype=gf.get_layer(x.layer)[1],
+    )
+    c.add(bot_output)
+
+    # Instantiate S-bend cladding objects
+    try:
+        top_input_cladding = Path(width=2 * x.cladding_offsets[0] + wg_width + dw / 2.0)
+        bot_input_cladding = Path(width=2 * x.cladding_offsets[0] + wg_width - dw / 2.0)
+        top_output_cladding = Path(width=2 * x.cladding_offsets[0] + wg_width)
+        bot_output_cladding = Path(width=2 * x.cladding_offsets[0] + wg_width)
+    except (TypeError, KeyError):
+        top_input_cladding = Path(width=2 * 2 + wg_width + dw / 2.0)
+        bot_input_cladding = Path(width=2 * 2 + wg_width - dw / 2.0)
+        top_output_cladding = Path(width=2 * 2 + wg_width + wg_width)
+        bot_output_cladding = Path(width=2 * 2 + wg_width + wg_width)
+
+    # Get cladding layers
+    try:
+        if kwargs["cladding_layers"] is None:
+            kwargs["cladding_layers"] = (111, 0)
+        else:
+            layers = []
+            for layer in kwargs["cladding_layers"]:
+                layers.append(gf.get_layer(layer))
+            kwargs["cladding_layers"] = layers
+    except KeyError:
+        kwargs["cladding_layers"] = (111, 0)
+
+    # Define cladding
+    top_input_cladding.parametric(
+        _bezier_inp_top,
+        max_points=199,
+        tolerance=0.00001,
+        layer=kwargs["cladding_layers"][0],
+        datatype=kwargs["cladding_layers"][1],
+    )
+    c.add(top_input_cladding)
+
+    bot_input_cladding.parametric(
+        _bezier_inp_bottom,
+        max_points=199,
+        tolerance=0.00001,
+        layer=kwargs["cladding_layers"][0],
+        datatype=kwargs["cladding_layers"][1],
+    )
+    c.add(bot_input_cladding)
+
+    top_output_cladding.parametric(
+        _bezier_output_top,
+        max_points=199,
+        tolerance=0.00001,
+        layer=kwargs["cladding_layers"][0],
+        datatype=kwargs["cladding_layers"][1],
+    )
+    c.add(top_output_cladding)
+
+    bot_output_cladding.parametric(
+        _bezier_output_bottom,
+        max_points=199,
+        tolerance=0.00001,
+        layer=kwargs["cladding_layers"][0],
+        datatype=kwargs["cladding_layers"][1],
+    )
+    c.add(bot_output_cladding)
+
+    # Kwargs for top and bottom taper. Derive from input
+    # kwargs, and then modify if necessary
+    kwargs_top_taper = kwargs
+    kwargs_bot_taper = kwargs
+
+    try:
+        update_offset_top = []
+        update_offset_bot = []
+
+        for offset in kwargs["cladding_offsets"]:
+            update_offset_top.append(offset)
+            update_offset_bot.append(offset)
+        kwargs_top_taper["cladding_offsets"] = update_offset_top
+        kwargs_bot_taper["cladding_offsets"] = update_offset_bot
+    except KeyError:
+        kwargs_top_taper["cladding_offsets"] = [2 * x.cladding_offsets[0]]
+        kwargs_bot_taper["cladding_offsets"] = [2 * x.cladding_offsets[0]]
+
+    try:
+        update_layer_top = []
+        update_layer_bot = []
+        for layer in kwargs["cladding_layers"]:
+            update_layer_top.append(gf.get_layer(layer))
+            update_layer_bot.append(gf.get_layer(layer))
+        kwargs_top_taper["cladding_layers"] = update_layer_top
+        kwargs_bot_taper["cladding_layers"] = update_layer_bot
+    except (TypeError, KeyError):
+        kwargs_top_taper["cladding_layers"] = [(111, 0)]
+        kwargs_bot_taper["cladding_layers"] = [(111, 0)]
+
+    # Define taper sections
+    taper_top = gf.components.taper2(
+        length=length2,
+        width1=wg_width + dw / 2.0,
+        width2=wg_width,
+        cross_section=cross_section,
+        **kwargs_top_taper
     )
 
-    c = gf.read.from_picwriter(c)
+    taper_bottom = gf.components.taper2(
+        length=length2,
+        width1=wg_width - dw / 2.0,
+        width2=wg_width,
+        cross_section=cross_section,
+        **kwargs_bot_taper
+    )
 
-    x = gf.get_cross_section(cross_section)
-    if x.add_bbox:
-        c = x.add_bbox(c)
+    # Generate gdsfactory component from phidl object
+    c = gf.read.from_phidl(c)
+
+    # Add ports to easily connect taper objects to S-bend
+    c.add_port(
+        "o2", (0, 0), wg_width, 180, layer="PORT", cross_section=x.copy(width=wg_width)
+    )
+    c.add_port(
+        "o1",
+        (0, -input_wg_sep),
+        wg_width,
+        180,
+        layer="PORT",
+        cross_section=x.copy(width=wg_width),
+    )
+    try:
+        c.add_port(
+            "top_taper_connect",
+            (length1, -input_wg_sep / 2.0 + wg_sep / 2.0),
+            wg_width,
+            0,
+            layer="PORT",
+            cross_section=x.copy(width=2 * x.cladding_offsets[0] + wg_width),
+        )
+        c.add_port(
+            "bottom_taper_connect",
+            (length1, -input_wg_sep / 2.0 - wg_sep / 2.0),
+            wg_width,
+            0,
+            layer="PORT",
+            cross_section=x.copy(width=2 * x.cladding_offsets[0] + wg_width),
+        )
+    except TypeError:
+        c.add_port(
+            "top_taper_connect",
+            (length1, -input_wg_sep / 2.0 + wg_sep / 2.0),
+            wg_width,
+            0,
+            layer="PORT",
+            cross_section=x.copy(width=2 * 2 + wg_width),
+        )
+        c.add_port(
+            "bottom_taper_connect",
+            (length1, -input_wg_sep / 2.0 - wg_sep / 2.0),
+            wg_width,
+            0,
+            layer="PORT",
+            cross_section=x.copy(width=2 * 2 + wg_width),
+        )
+
+    # Add tapers to component
+    taper_top_ref = c << taper_top
+
+    taper_bottom_ref = c << taper_bottom
+
+    # Connect tapers
+    taper_top_ref.connect("o1", c.ports["top_taper_connect"])
+    taper_bottom_ref.connect("o1", c.ports["bottom_taper_connect"])
+
+    # Redefine components ports
+    c.ports = {
+        "o1": c.ports["o1"],
+        "o2": c.ports["o2"],
+    }
+
+    c.add_port(
+        "o3",
+        (
+            length1 + length2 + length3,
+            -input_wg_sep / 2.0 + output_wg_sep / 2.0,
+        ),
+        wg_width,
+        0,
+        layer="PORT",
+        cross_section=x.copy(width=wg_width),
+    )
+
+    c.add_port(
+        "o4",
+        (
+            length1 + length2 + length3,
+            -input_wg_sep / 2.0 - output_wg_sep / 2.0,
+        ),
+        wg_width,
+        0,
+        layer="PORT",
+        cross_section=x.copy(width=wg_width),
+    )
+
+    c.absorb(taper_top_ref)
+    c.absorb(taper_bottom_ref)
+
+    # Create new component and combine all polygons to form final polygon
+    c2 = gf.Component()
+
+    c2 << gf.geometry.union(c, by_layer=True)
+
+    if x.info:
+        c2.info.update(x.info)
+
     if x.add_pins:
-        c = x.add_pins(c)
-    return c
+        c2 = x.add_pins(c2)
+
+    if x.add_bbox:
+        c2 = x.add_bbox(c2)
+
+    c2.ports = c.ports
+
+    return c2
 
 
 if __name__ == "__main__":
 
-    c = coupler_adiabatic(length3=5)
+    c = coupler_adiabatic(length3=5, cladding_offsets=[0.5])
     print(c.ports)
     c.show(show_ports=True)
