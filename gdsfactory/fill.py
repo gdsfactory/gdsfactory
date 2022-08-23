4d095e30ca2253a60243373fa8148971d7dd71af
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-08-23 09:06:55 -0700

    fill demo
    Former-commit-id: f3492b144e707d08ab5cc6a8342eb52de30cfd63 [formerly fac78d1a6985a762e6a850b175b412b59e523798 [formerly e5c4b1a7e2327ac03b54dac1baa85a98a9572487] [formerly 132cd48beaa26e2333d1988f7441019fadbb6890 [formerly fab0ce3e0c2b31e17f3b1763d2de75d07e5689a2]]]
    Former-commit-id: 132cd48beaa26e2333d1988f7441019fadbb6890
    Former-commit-id: 7703975f24b5b6b6cb4d03f35f9b58e190e63db4 [formerly 7fe642a624038a894873226963807fe9b4ad0407]
    Former-commit-id: d689bb77f247de34d92f12fa82c601d1000f348b

--- gdsfactory/fill.py
+++ gdsfactory/fill.py
@@ -196,3 +196,30 @@ if __name__ == "__main__":
         # bbox=(100.0, 100.0),
     )
     c.show(show_ports=True)
+
+    # import gdsfactory as gf
+    # coupler_lengths = [10, 20, 30, 40, 50, 60, 70, 80]
+    # coupler_gaps = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8]
+    # delta_lengths = [10, 100, 200, 300, 400, 500, 500]
+
+    # mzi = gf.components.mzi_lattice(
+    #     coupler_lengths=coupler_lengths,
+    #     coupler_gaps=coupler_gaps,
+    #     delta_lengths=delta_lengths,
+    # )
+
+    # # Add fill
+    # c = gf.Component("component_with_fill")
+    # layers = [(1, 0)]
+    # fill_size = [0.5, 0.5]
+
+    # c << gf.fill_rectangle(
+    #     mzi,
+    #     fill_size=fill_size,
+    #     fill_layers=layers,
+    #     margin=5,
+    #     fill_densities=[0.8] * len(layers),
+    #     avoid_layers=layers,
+    # )
+    # c << mzi
+    # c.show(show_ports=True)
