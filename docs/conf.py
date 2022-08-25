977c71d828b63d9a056ca9f017671ec13afb5b24
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-08-25 12:14:01 -0700

    Bump version: 5.22.2 → 5.22.3
    Former-commit-id: a933e5ad5c92e46f05c20da658e6260576beaf97 [formerly cecb929b71feb14b2773395ab26219c216ea27b1]
    Former-commit-id: 1704de04983236ec110ef01676a0dc393da5c9c0

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 project = "gdsfactory"
-release = "5.22.2"
+release = "5.22.3"
 copyright = "2020, MIT License"
 
 html_theme = "sphinx_book_theme"
--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -12,7 +12,7 @@ priority:
 You can access the config dictionary with `print_config`
 """
 
-__version__ = "5.22.2"
+__version__ = "5.22.3"
 import io
 import json
 import os
