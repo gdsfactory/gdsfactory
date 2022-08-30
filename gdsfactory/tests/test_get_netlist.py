da1961e18b229437849651fde772d598cc288285
Troy Tamas <troy.tamas@rockleyphotonics.com>
2022-08-30 13:37:28 +0900

    adding failing test
    Former-commit-id: 386bd5bb8bebbfaece4ba2dd9ce1930ad6e12208 [formerly 8ce688031fa0ba69709df913a630d7b5bc19506a [formerly 7db679ea64a60b3ea5deeb8b465bcb1684c977e4] [formerly 83a8b36b30fd888d637941f15036ef9652f59761 [formerly e434eac921b40a6e588c29dd53a63ba389109ade]]]
    Former-commit-id: 83a8b36b30fd888d637941f15036ef9652f59761
    Former-commit-id: e99b4ec9680f3add9cc6dc3c1743f230e4bb2de0 [formerly 93d5710010bf06a53178da7b1f2c0d976530ebe6]
    Former-commit-id: 7fafc5902577e35f43c3d97999aaf77bd430791f

--- gdsfactory/tests/test_get_netlist.py
+++ gdsfactory/tests/test_get_netlist.py
@@ -7,6 +7,59 @@ def test_get_netlist_cell_array() -> None:
     assert len(n.keys()) == 5
 
 
+def test_get_netlist_simple():
+    c = gf.Component()
+    i1 = c.add_ref(gf.components.straight(), "i1")
+    i2 = c.add_ref(gf.components.straight(), "i2")
+    i3 = c.add_ref(gf.components.straight(), "i3")
+    i2.connect("o2", i1.ports["o1"])
+    i3.movey(-100)
+    netlist = c.get_netlist()
+    connections = netlist["connections"]
+    assert len(connections) == 1
+    cpairs = list(connections.items())
+    extracted_port_pair = set(cpairs[0])
+    expected_port_pair = {"i2,o2", "i1,o1"}
+    assert extracted_port_pair == expected_port_pair
+
+
+def test_get_netlist_tiny():
+    c = gf.Component()
+    cc = gf.components.straight(length=0.002)
+    i1 = c.add_ref(cc, "i1")
+    i2 = c.add_ref(cc, "i2")
+    i3 = c.add_ref(cc, "i3")
+    i4 = c.add_ref(cc, "i4")
+
+    i2.connect("o2", i1.ports["o1"])
+    i3.connect("o2", i2.ports["o1"])
+    i4.connect("o2", i3.ports["o1"])
+
+    netlist = c.get_netlist(tolerance=5)
+    connections = netlist["connections"]
+    assert len(connections) == 3
+    # cpairs = list(connections.items())
+    # extracted_port_pair = set(cpairs[0])
+    # expected_port_pair = {'i2,o2', 'i1,o1'}
+    # assert extracted_port_pair == expected_port_pair
+
+
+def test_get_netlist_rotated():
+    c = gf.Component()
+    i1 = c.add_ref(gf.components.straight(), "i1")
+    i2 = c.add_ref(gf.components.straight(), "i2")
+    i1.rotate(35)
+    i2.connect("o2", i1.ports["o1"])
+
+    netlist = c.get_netlist()
+    connections = netlist["connections"]
+    assert len(connections) == 1
+    cpairs = list(connections.items())
+    extracted_port_pair = set(cpairs[0])
+    expected_port_pair = {"i2,o2", "i1,o1"}
+    assert extracted_port_pair == expected_port_pair
+
+
 if __name__ == "__main__":
     c = gf.c.array()
     n = c.get_netlist()
