15b316048363e8ad8e2cd41517f361d26d5a2540
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-09-19 10:26:31 -0700

    move install klayout and klayout-gui to post_install for linux only

--- install.sh
+++ install.sh
@@ -5,3 +5,15 @@ pip install -e .
 pre-commit install
 gf tool install
 make plugins
+
+
+if [[ $(uname -s) == Linux ]]; then
+    if [[ ${INSTALLER_PLAT} != linux-* ]]; then
+        mamba install -c flaport klayout
+        mamba install -c flaport klayout-gui
+    fi
+else  # macOS
+    if [[ ${INSTALLER_PLAT} != osx-* ]]; then
+        exit 1
+    fi
+fi
