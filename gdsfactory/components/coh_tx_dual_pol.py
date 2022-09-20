ccd709bfebfcc0b241c8754468dc9b63fb347d27
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-09-20 07:58:20 -0700

    add coherent tx rx

--- gdsfactory/components/coh_tx_dual_pol.py
+++ gdsfactory/components/coh_tx_dual_pol.py
@@ -1,59 +1,48 @@
-from functools import partial
 from typing import Optional
 
 import gdsfactory as gf
 from gdsfactory.cell import cell
 from gdsfactory.component import Component
-from gdsfactory.components.bend_euler import bend_euler
-from gdsfactory.components.coh_tx_single_pol import coh_tx_single_pol
-from gdsfactory.components.straight import straight as straight_function
 from gdsfactory.routing.get_route import get_route
 from gdsfactory.types import ComponentSpec, CrossSectionSpec
-
-default_single_pol_coh_tx = partial(coh_tx_single_pol)
 
 
 @cell
 def coh_tx_dual_pol(
-    bend: ComponentSpec = bend_euler,
-    straight: ComponentSpec = straight_function,
-    cross_section: CrossSectionSpec = "strip",
     splitter: ComponentSpec = "mmi1x2",
     combiner: Optional[ComponentSpec] = None,
-    spol_coh_tx: ComponentSpec = default_single_pol_coh_tx,
-    single_pol_tx_spacing: float = 10.0,
-    splitter_coh_tx_spacing: float = 40.0,
+    spol_coh_tx: ComponentSpec = "coh_tx_single_pol",
+    yspacing: float = 10.0,
+    xspacing: float = 40.0,
     input_coupler: Optional[ComponentSpec] = None,
     output_coupler: Optional[ComponentSpec] = None,
+    cross_section: CrossSectionSpec = "strip",
+    **kwargs
 ) -> Component:
     """Dual polarization coherent transmitter.
 
     Args:
-        bend: 90 degrees bend library.
-        straight: straight function.
-        cross_section: for routing (splitter to mzms and mzms to combiners).
         splitter: splitter function.
         combiner: combiner function.
-        spol_coh_tx: function generating a coherent tx for a single polarization
-        single_pol_coh_tx_spacing: vertical spacing between each single polarization coherent transmitter
-        splitter_coh_tx_spacing: horizontal spacing between the splitter and combiner and the single pol coh txs
-        input_coupler: Optional coupler to add before the splitter
-        output_coupler: Optioncal coupler to add after the combiner
+        spol_coh_tx: function generating a coherent tx for a single polarization.
+        yspacing: vertical spacing between each single polarization coherent tx.
+        xspacing: horizontal spacing between splitter and combiner.
+        input_coupler: Optional coupler to add before the splitter.
+        output_coupler: Optioncal coupler to add after the combiner.
+        cross_section: for routing (splitter to mzms and mzms to combiners).
+        kwargs: cross_section settings.
+
      .. code::
 
-                             ___ single_pol_tx__
-                             |                  |
-                             |                  |
-                             |                  |
-    (in_coupler)---splitter==|                  |==combiner---(out_coupler)
-                             |                  |
-                             |                  |
-                             |___ single_pol_tx_|
+                                 ___ single_pol_tx__
+                                 |                  |
+                                 |                  |
+                                 |                  |
+        (in_coupler)---splitter==|                  |==combiner---(out_coupler)
+                                 |                  |
+                                 |                  |
+                                 |___ single_pol_tx_|
     """
-    bend_spec = bend
-    bend = gf.get_component(bend, cross_section=cross_section)
-    straight_spec = straight
-
     spol_coh_tx = gf.get_component(spol_coh_tx)
 
     # ----- Draw single pol coherent transmitters -----
@@ -65,66 +54,61 @@ def coh_tx_dual_pol(
     single_tx_2 = c << spol_coh_tx
 
     # Separate the two receivers
-    single_tx_2.movey(single_tx_1.ymin - single_pol_tx_spacing - single_tx_2.ymax)
+    single_tx_2.movey(single_tx_1.ymin - yspacing - single_tx_2.ymax)
 
     # ------------ Splitters and combiners ---------------
-
     splitter = gf.get_component(splitter)
     sp = c << splitter
-    sp.x = single_tx_1.xmin - splitter_coh_tx_spacing
+    sp.x = single_tx_1.xmin - xspacing
     sp.y = (single_tx_1.ports["o1"].y + single_tx_2.ports["o1"].y) / 2
 
     route = get_route(
         sp.ports["o2"],
         single_tx_1.ports["o1"],
-        straight=straight_spec,
-        bend=bend_spec,
         cross_section=cross_section,
         with_sbend=False,
+        **kwargs
     )
     c.add(route.references)
 
     route = get_route(
         sp.ports["o3"],
         single_tx_2.ports["o1"],
-        straight=straight_spec,
-        bend=bend_spec,
         cross_section=cross_section,
         with_sbend=False,
+        **kwargs
     )
     c.add(route.references)
 
-    if combiner is not None:
+    if combiner:
         combiner = gf.get_component(combiner)
         comb = c << combiner
         comb.mirror()
 
-        comb.x = single_tx_1.xmax + splitter_coh_tx_spacing
+        comb.x = single_tx_1.xmax + xspacing
         comb.y = (single_tx_1.ports["o2"].y + single_tx_2.ports["o2"].y) / 2
 
         route = get_route(
             comb.ports["o2"],
             single_tx_1.ports["o2"],
-            straight=straight_spec,
-            bend=bend_spec,
             cross_section=cross_section,
             with_sbend=False,
+            **kwargs
         )
         c.add(route.references)
 
         route = get_route(
             comb.ports["o3"],
             single_tx_2.ports["o2"],
-            straight=straight_spec,
-            bend=bend_spec,
             cross_section=cross_section,
             with_sbend=False,
+            **kwargs
         )
         c.add(route.references)
 
     # ------- In and out couplers (if indicated) -----
 
-    if input_coupler is not None:
+    if input_coupler:
         # Add input coupler
         in_coupler = gf.get_component(input_coupler)
         in_coup = c << in_coupler
@@ -133,11 +117,10 @@ def coh_tx_dual_pol(
     else:
         c.add_port("o1", port=sp.ports["o1"])
 
-    if output_coupler is not None:
-
+    if output_coupler:
         output_coupler = gf.get_component(output_coupler)
         out_coup = c << output_coupler
-        if combiner is not None:
+        if combiner:
             # Add output coupler
             out_coup.connect("o1", comb.ports["o1"])
         else:
@@ -150,29 +133,30 @@ def coh_tx_dual_pol(
             route = get_route(
                 single_tx_1.ports["o2"],
                 out_coup.ports["o1"],
-                straight=straight_spec,
-                bend=bend_spec,
                 cross_section=cross_section,
                 with_sbend=False,
+                **kwargs
             )
             c.add(route.references)
 
             route = get_route(
                 single_tx_2.ports["o2"],
                 out_coup.ports["o2"],
-                straight=straight_spec,
-                bend=bend_spec,
                 cross_section=cross_section,
                 with_sbend=False,
+                **kwargs
             )
             c.add(route.references)
     else:
-        c.add_port("o2", port=comb.ports["o1"])
-
-    # ------ Extract electrical ports (if no pads) -------
+        c.add_port("o2", port=single_tx_1.ports["o2"])
+        c.add_port("o3", port=single_tx_2.ports["o2"])
 
     c.add_ports(single_tx_1.get_ports_list(port_type="electrical"), prefix="pol1")
     c.add_ports(single_tx_2.get_ports_list(port_type="electrical"), prefix="pol2")
     c.auto_rename_ports()
-
     return c
+
+
+if __name__ == "__main__":
+    c = coh_tx_dual_pol()
+    c.show(show_ports=True)
