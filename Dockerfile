3e6051c9c7bf084d2983e16f5d4c93db433cfa92
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-09-21 08:25:16 -0700

    Bump version: 5.33.6 → 5.33.7
    Former-commit-id: 81a596a4c661e914dcce16f3a451f9d67742a0a7 [formerly b4974ae18781fc6cd2a7e2291b0791ec2c7ca84c]
    Former-commit-id: 26b4387dddb7eb124c33f887ac0a3471c44bf121

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,1 @@
-FROM joamatab/gdsfactory:5.33.6
+FROM joamatab/gdsfactory:5.33.7
--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 project = "gdsfactory"
-release = "5.33.6"
+release = "5.33.7"
 copyright = "2020, MIT License"
 
 html_theme = "sphinx_book_theme"
