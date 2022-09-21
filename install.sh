d91452b4acf9ca03aa8a59ee36c6a6ed55b7391b
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-09-20 21:57:34 -0700

    upgrade package to python3.9
    Former-commit-id: 946152d6effaa1d7953dba5fba9758cf41bc5644 [formerly 2d336cb383462d199240f764b185b50a512087f1 [formerly 338ca8d707dc45798e41224d760947cf43517bf0] [formerly a2d9790ac952302bd0557fa04f4d55d1098b290e [formerly e54dcd1f26bd0b4d53e82fc901c58b71863d5cb5]]]
    Former-commit-id: a2d9790ac952302bd0557fa04f4d55d1098b290e
    Former-commit-id: c9962595541458e5b15aa5a7fa2796d5c6d2fecc [formerly a4644c4f46ad0e492d9d7cad6173837b5d10d70c]
    Former-commit-id: 7f1fc9094b14361fb0b6ee8fb199039c5e35e08b

--- install.sh
+++ install.sh
@@ -2,18 +2,16 @@
 
 mamba install pymeep=*=mpi_mpich_* -y
 
-pip install gdsfactory[tidy3d]
-pip install lytest simphony sax sklearn
 pip install jax jaxlib
+pip install gdsfactory[tidy3d]
+pip install gdsfactory[full] --upgrade
 
 
-if [[ $(uname -s) == Linux ]]; then
-    if [[ ${INSTALLER_PLAT} != linux-* ]]; then
-        mamba install -c flaport klayout
-        mamba install -c flaport klayout-gui
-    fi
-else  # macOS
-    if [[ ${INSTALLER_PLAT} != osx-* ]]; then
-        exit 1
-    fi
-fi
+# if [[ $(uname -s) == Linux ]]; then
+#     if [[ ${INSTALLER_PLAT} != linux-* ]]; then
+#         mamba install -c flaport klayout -y
+#         mamba install -c flaport klayout-gui -y
+#     fi
+# else  # macOS
+#   echo 'finished'
+# fi
