02d365784e1027f48a1b5c06c28732eb05c34365
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-09-21 08:01:56 -0700

    add jax and jaxlib from conda
    Former-commit-id: ee02f4f0276b74e543f307333c20d3f931d3825d [formerly adaeeca9c95df1e829e117a7636c1599d7a2f7c1 [formerly e98ff72c75e2af58e42e75831c227ef7ec7d521f] [formerly 0c1ea84929fcec3f6a33a49cf45a3edbc64e5620 [formerly 508d2868c797a7ebd89c80bc1394c16010fb0413]]]
    Former-commit-id: 0c1ea84929fcec3f6a33a49cf45a3edbc64e5620
    Former-commit-id: 007401ece84f45721d1376ef424d63688da73f3d [formerly fcfe87dd44049a7339e2dc87dce94a595b859d71]
    Former-commit-id: 766c88bd756048f5cb7c1f11548010f8fea28c90

--- install.sh
+++ install.sh
@@ -1,11 +1,7 @@
 #!/bin/bash
 
-mamba install pymeep=*=mpi_mpich_* -y
-
-pip install jax jaxlib
 pip install gdsfactory[tidy3d]
 pip install gdsfactory[full] --upgrade
-
 gf tool install
 
 # if [[ $(uname -s) == Linux ]]; then
