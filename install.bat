1f8b19b434838ca6501b74674815c84f48f31994
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-09-22 14:41:54 -0700

    install for windows clones gdsfactory repo in home folder

--- install.bat
+++ install.bat
@@ -3,9 +3,9 @@ pip install lytest simphony sax sklearn klayout
 pip install gdsfactory==5.33.9
 gf tool install
 
-if exist "%USERPROFILE%\gdsfactory" (goto SKIP_INSTALL)
-cd %USERPROFILE%
+if exist "%USERPROFILE%\Desktop\gdsfactory" (goto SKIP_INSTALL)
+cd %USERPROFILE%\Desktop
 git clone https://github.com/gdsfactory/gdsfactory.git
 
 :SKIP_INSTALL
-echo Conda is already installed!
+echo gdsfactory installed
