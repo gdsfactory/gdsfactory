689dba22689dc21346045b693cec09069451c8a4
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-09-22 15:29:14 -0700

    Bump version: 5.33.10 → 5.33.11

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 project = "gdsfactory"
-release = "5.33.10"
+release = "5.33.11"
 copyright = "2020, MIT License"
 
 html_theme = "sphinx_book_theme"
--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -12,7 +12,7 @@ priority:
 You can access the config dictionary with `print_config`
 """
 
-__version__ = "5.33.10"
+__version__ = "5.33.11"
 import io
 import json
 import os
