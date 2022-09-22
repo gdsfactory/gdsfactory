df596b9e87d3cdf6366085e86b25ea986de101fa
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-09-22 09:25:56 -0700

    add install.bat
    Former-commit-id: 3e26ea83b54fad7731b79e8d71e0c11d0c58426a [formerly fb106eebe8e367ce5d323839aed55ce76a4029e1]
    Former-commit-id: 22ac184d2877cbbb98e8630b9e6fe3589230a249

--- install.bat
+++ install.bat
@@ -1,2 +1,9 @@
 pip install lytest simphony sax sklearn klayout
 gf tool install
+
+if exist "%USERPROFILE%\gdsfactory" (goto SKIP_INSTALL)
+cd %USERPROFILE%
+git clone https://github.com/gdsfactory/gdsfactory.git
+
+:SKIP_INSTALL
+echo Conda is already installed!
