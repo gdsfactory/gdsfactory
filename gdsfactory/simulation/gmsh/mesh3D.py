e1710416f84fde6ca18288fcda6a3ddf6363be2b
simbilod <sb30@princeton.edu>
2022-09-26 11:30:05 -0700

    minimal surfaces meshing
    Former-commit-id: 909d981c9c863c374da20ea50752731c7f5f061f [formerly f68b28caba3b5648833db7b68feac39953ad86e2]
    Former-commit-id: 8f9d1487d68ac267f5fe238e1a8fa718e1fc3334

--- gdsfactory/simulation/gmsh/mesh3D.py
+++ gdsfactory/simulation/gmsh/mesh3D.py
@@ -1,27 +1,93 @@
 from typing import Dict, Optional, Tuple
 
-import numpy as np
 import pygmsh
 
-from gdsfactory.geometry.boolean import boolean
 from gdsfactory.pdk import get_layer_stack
 from gdsfactory.tech import LayerStack
 from gdsfactory.types import ComponentOrReference, Layer
-from gdsfactory import Component
 
+
+def surface_loop_from_vertices(model, xmin, xmax, ymin, ymax, zmin, zmax, resolution):
+    """Returns surface loop of prism from bounding box.
+
+    Args:
+        xmin: minimal x-value for bounding box
+        xmax: maximal x-value for bounding box
+        ymin: minimal y-value for bounding box
+        ymax: maximal y-value for bounding box
+        zmin: minimal z-value for bounding box
+        zmax: maximal z-value for bounding box
+
+    """
+    channel_surfaces = []
+    for coords in [
+        [
+            [xmin, ymin, zmin],
+            [xmin, ymin, zmax],
+            [xmin, ymax, zmax],
+            [xmin, ymax, zmin],
+        ],
+        [
+            [xmax, ymin, zmin],
+            [xmax, ymin, zmax],
+            [xmax, ymax, zmax],
+            [xmax, ymax, zmin],
+        ],
+        [
+            [xmax, ymin, zmin],
+            [xmax, ymin, zmax],
+            [xmin, ymin, zmax],
+            [xmin, ymin, zmin],
+        ],
+        [
+            [xmax, ymax, zmin],
+            [xmax, ymax, zmax],
+            [xmin, ymax, zmax],
+            [xmin, ymax, zmin],
+        ],
+        [
+            [xmin, ymin, zmin],
+            [xmin, ymax, zmin],
+            [xmax, ymax, zmin],
+            [xmax, ymin, zmin],
+        ],
+        [
+            [xmin, ymin, zmax],
+            [xmin, ymax, zmax],
+            [xmax, ymax, zmax],
+            [xmax, ymin, zmax],
+        ],
+    ]:
+        points = []
+        for coord in coords:
+            points.append(model.add_point(coord, mesh_size=resolution))
+        channel_lines = [
+            model.add_line(points[i], points[i + 1]) for i in range(-1, len(points) - 1)
+        ]
+        channel_loop = model.add_curve_loop(channel_lines)
+        channel_surfaces.append(model.add_plane_surface(channel_loop))
+    surface_loop = model.add_surface_loop(channel_surfaces)
+    return channel_surfaces, surface_loop
 
 
 def mesh3D(
     component: ComponentOrReference,
     base_resolution: float = 0.2,
     refine_resolution: Optional[Dict[Layer, float]] = None,
-    padding: Tuple[float, float, float, float, float, float] = (1.0, 1.0, 1.0, 1.0, 1.0, 1.0),
+    padding: Tuple[float, float, float, float, float, float] = (
+        1.0,
+        1.0,
+        1.0,
+        1.0,
+        1.0,
+        1.0,
+    ),
     layer_stack: Optional[LayerStack] = None,
     exclude_layers: Optional[Tuple[Layer, ...]] = None,
 ):
-    """Returns gmsh 3D geometry of component
+    """Returns gmsh 3D geometry of component.
 
-    Similar to component.to_3d(), but returns a gmsh mesh with: 
+    Similar to component.to_3d(), but returns a gmsh mesh with:
     - layer-dependent mesh resolution
     - different physical blocks for different objects
     - sub-object labels for introduction in physical solvers (e.g. edges)
@@ -42,8 +108,8 @@ def mesh3D(
 
     model = geometry.__enter__()
 
-    zmin_cell = np.inf
-    zmax_cell = -np.inf
+    zmin_cell = 0  # np.inf
+    zmax_cell = 1.22  # -np.inf
 
     bbox = component.bbox
     xmin_cell = bbox[0][0] - padding[0]
@@ -60,7 +126,7 @@ def mesh3D(
             refine_dict[layer] = base_resolution
 
     # Features
-    blocks = []
+    # blocks = []
     for layer, polygons in component.get_polygons(by_spec=True).items():
         if (
             layer not in exclude_layers
@@ -76,20 +142,20 @@ def mesh3D(
             if zmax_layer > zmax_cell:
                 zmax_cell = zmax_layer
 
-            num_layers = int(height/refine_dict[layer])
+            # num_layers = int(height/refine_dict[layer])
 
-            i = 0
-            for polygon in polygons:
-                points = [model.add_point([polygon_point[0], polygon_point[1], zmin_layer], mesh_size=refine_dict[layer]) for polygon_point in polygon]
-                polygon_lines = [
-                    model.add_line(points[i], points[i + 1]) for i in range(-1, len(points) - 1)
-                ]
-                polygon_loop = model.add_curve_loop(polygon_lines)
-                polygon_surface = model.add_plane_surface(polygon_loop)
-                polygon_top, polygon_volume, polygon_lat = model.extrude(polygon_surface, [0,0,height], num_layers=int(height/refine_dict[layer]))
-                model.add_physical(polygon_volume, f"{layer}_{i}")
-                blocks.append("{layer}_{i}")
-                i += 1
+            # i = 0
+            # for polygon in polygons:
+            #     points = [model.add_point([polygon_point[0], polygon_point[1], zmin_layer], mesh_size=refine_dict[layer]) for polygon_point in polygon]
+            #     polygon_lines = [
+            #         model.add_line(points[i], points[i + 1]) for i in range(-1, len(points) - 1)
+            #     ]
+            #     polygon_loop = model.add_curve_loop(polygon_lines)
+            #     polygon_surface = model.add_plane_surface(polygon_loop)
+            #     polygon_top, polygon_volume, polygon_lat = model.extrude(polygon_surface, [0,0,height], num_layers=int(height/refine_dict[layer]))
+            #     model.add_physical(polygon_volume, f"{layer}_{i}")
+            #     blocks.append("{layer}_{i}")
+            #     i += 1
 
     zmin_cell -= padding[4]
     zmax_cell += padding[5]
@@ -145,34 +211,49 @@ def mesh3D(
     #     plane_surface = model.add_plane_surface(channel_loop)
     #     cell_planes.append(plane_surface)
 
-    for coords in [[
-                    [xmin_cell, ymin_cell, zmin_cell],
-                    [xmin_cell, ymax_cell, zmin_cell],
-                    [xmax_cell, ymax_cell, zmin_cell],
-                    [xmax_cell, ymin_cell, zmin_cell]
-                ]]:        
-        points = []
-        for coord in coords:
-            points.append(model.add_point(coord, mesh_size=base_resolution))
+    channel_surfaces, surface_loop = surface_loop_from_vertices(
+        model,
+        xmin=xmin_cell,
+        xmax=xmax_cell,
+        ymin=ymin_cell,
+        ymax=ymax_cell,
+        zmin=0,
+        zmax=1,
+        resolution=0.1,
+    )
+    i = 0
+    for surface in channel_surfaces:
+        print(surface)
+        model.add_physical(surface, f"oxide_{i}")
+        i += 1
 
-        channel_lines = [
-            model.add_line(points[i], points[i + 1]) for i in range(-1, len(points) - 1)
-        ]
-        channel_loop = model.add_curve_loop(channel_lines)
-        plane_surface = model.add_plane_surface(channel_loop)
+    # for coords in [[
+    #                 [xmin_cell, ymin_cell, zmin_cell],
+    #                 [xmin_cell, ymax_cell, zmin_cell],
+    #                 [xmax_cell, ymax_cell, zmin_cell],
+    #                 [xmax_cell, ymin_cell, zmin_cell]
+    #             ]]:
+    #     points = []
+    #     for coord in coords:
+    #         points.append(model.add_point(coord, mesh_size=base_resolution))
 
-    top, volume, lat = model.extrude(plane_surface, [0,0,zmax_cell-zmin_cell], num_layers=int((zmax_cell-zmin_cell)/base_resolution))
-    bottom = plane_surface
+    #     channel_lines = [
+    #         model.add_line(points[i], points[i + 1]) for i in range(-1, len(points) - 1)
+    #     ]
+    #     channel_loop = model.add_curve_loop(channel_lines)
+    #     plane_surface = model.add_plane_surface(channel_loop)
 
-    model.add_physical(volume, "oxide")
-    model.add_physical(lat, "lat")
-    model.add_physical(top, "top")
-    model.add_physical(bottom, "bottom")
+    # top, volume, lat = model.extrude(plane_surface, [0,0,zmax_cell-zmin_cell], num_layers=int((zmax_cell-zmin_cell)/base_resolution))
+    # bottom = plane_surface
+
+    # model.add_physical(volume, "oxide")
+    # model.add_physical(lat, "lat")
+    # model.add_physical(top, "top")
+    # model.add_physical(bottom, "bottom")
 
     geometry.generate_mesh(dim=3, verbose=True)
 
     return geometry
-
 
 
 if __name__ == "__main__":
@@ -208,10 +289,6 @@ if __name__ == "__main__":
 
     def create_mesh(mesh, cell_type, prune_z=False):
         cells = mesh.get_cells_type(cell_type)
-
-        import pdb
-        pdb.set_trace()
-
         cell_data = mesh.get_cell_data("gmsh:physical", cell_type)
         points = mesh.points
         return meshio.Mesh(
@@ -223,9 +300,8 @@ if __name__ == "__main__":
     # line_mesh = create_mesh(mesh_from_file, "line", prune_z=True)
     # meshio.write("facet_mesh.xdmf", line_mesh)
 
-    triangle_mesh = create_mesh(mesh_from_file, "tetra", prune_z=True)
+    triangle_mesh = create_mesh(mesh_from_file, "triangle", prune_z=True)
     meshio.write("mesh.xdmf", triangle_mesh)
-
 
     # for layer, polygons in heaters.get_polygons(by_spec=True).items():
     #     print(layer, polygons)
