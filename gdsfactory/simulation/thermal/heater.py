931dc443c3f693e2c4799a78f59d6546c82776a6
HelgeGehring <HelgeGehring@gmail.com>
2022-09-27 23:51:45 -0700

    started dynamic simulations, off by a factor of 2
    Former-commit-id: 72ec2ca4ba234ed2271171471a5d8e11da705035 [formerly 95345d4e3ac9fb6de82260a382ee523ba46b2a5e [formerly 741c573ce09e430a41838dbb2378e00971ab08b6] [formerly 1c68eb482a9ac1a0a25345ca4fa74b1e729f530e [formerly e21b443304359087752fb6c8aaf95968ec40eff2]]]
    Former-commit-id: 1c68eb482a9ac1a0a25345ca4fa74b1e729f530e
    Former-commit-id: f9510e4fcaadb3d6bc10bd95d6fcf1adcce7f7d4 [formerly 2a19380a8c8c691999c406b198b0936de832c732]
    Former-commit-id: 2b316797c96df1c7122b00fa5df25014bd363737

--- gdsfactory/simulation/thermal/heater.py
+++ gdsfactory/simulation/thermal/heater.py
@@ -1,4 +1,4 @@
-from typing import Dict
+from typing import Dict, Iterator, Tuple
 
 import numpy as np
 import skfem
@@ -10,6 +10,7 @@ from skfem import (
     LinearForm,
     asm,
     condense,
+    penalize,
     solve,
 )
 from skfem.helpers import dot, grad
@@ -19,6 +20,7 @@ def solve_thermal(
     mesh_filename: str,
     thermal_conductivity: Dict[str, float],
     specific_conductivity: Dict[str, float],
+    thermal_diffusivity: Dict[str, float],
     currents: Dict[str, float],
 ):
     """Thermal simulation.
@@ -27,6 +29,7 @@ def solve_thermal(
         mesh_filename: Name of the mesh to load
         thermal_conductivity: thermal conductivity in W/m‧K
         specific_conductivity: specific conductivity in S/m
+        thermal_diffusivity:
         currents: current flowing through the layer in A
 
     Returns:
@@ -91,6 +94,101 @@ def solve_thermal(
     ax.set_axis_on()
     ax.figure.tight_layout()
     ax.show()
+    from scipy.sparse.linalg import splu
+
+    print("max temp steady", np.max(temperature))
+    u_init = temperature * 0.01
+
+    thermal_diffusivity_p0 = basis0.zeros()
+    for domain in thermal_diffusivity.keys():
+        thermal_diffusivity_p0[basis0.get_dofs(elements=domain)] = thermal_diffusivity[
+            domain
+        ]
+
+    thermal_diffusivity_p0 *= 1e12  # 1e-12 -> conversion from m^2 -> um^2
+
+    @BilinearForm
+    def diffusivity_laplace(u, v, w):
+        print(
+            "factor-laplace",
+            np.unique(w["thermal_diffusivity"] / w["thermal_conductivity"]),
+        )
+        return (
+            dot(grad(u) * w["thermal_conductivity"], grad(v))
+            * w["thermal_diffusivity"]
+            / w["thermal_conductivity"]
+        )
+
+    @BilinearForm
+    def mass(u, v, _):
+        return u * v
+
+    @LinearForm
+    def pre_factor(_, v, w):
+        return w["thermal_diffusivity"] / w["thermal_conductivity"] * v
+
+    L = asm(
+        diffusivity_laplace,
+        basis,
+        thermal_diffusivity=basis0.interpolate(thermal_diffusivity_p0),
+        thermal_conductivity=basis0.interpolate(thermal_conductivity_p0),
+    )
+    M = asm(mass, basis)
+
+    dt = 0.2e-6
+    print("dt =", dt)
+    theta = 0.5  # Crank–Nicolson
+    L0, M0 = penalize(L, M, D=basis.get_dofs(mesh.boundaries["bottom"]))
+    A = M0 + theta * L0 * dt
+    B = M0 - (1 - theta) * L0 * dt
+
+    backsolve = splu(A.T).solve  # .T as splu prefers CSC
+
+    def evolve(t: float, u: np.ndarray) -> Iterator[Tuple[float, np.ndarray]]:
+        while True:
+            # print(np.linalg.norm(u, np.inf))
+            yield t, u
+            t, u = t + dt, backsolve(
+                B @ u
+                + thermal_diffusivity["(47, 0)"]
+                / thermal_conductivity["(47, 0)"]
+                * 1e24
+                * joule_heating_rhs
+                * dt
+            )
+            print("max temp step", np.max(u))
+            print(
+                "factor",
+                thermal_diffusivity["(47, 0)"] / thermal_conductivity["(47, 0)"] * 1e24,
+            )
+
+    from pathlib import Path
+
+    from matplotlib.animation import FuncAnimation
+    from skfem.visuals.matplotlib import plot
+
+    ax = draw(mesh, boundaries_only=True)
+    ax.set_axis_on()
+    ax = plot(mesh, temperature, ax=ax, shading="gouraud")
+    title = ax.set_title("t = 0.00")
+    field = ax.get_children()[1]  # vertex-based temperature-colour
+    fig = ax.get_figure()
+    from mpl_toolkits.axes_grid1 import make_axes_locatable
+
+    divider = make_axes_locatable(ax)
+    cax = divider.append_axes("right", size="5%", pad=0.05)
+    fig.colorbar(field, cax=cax)
+
+    def update(event):
+        t, u = event
+        title.set_text(f"$t$ = {t * 1e6:.2f}us")
+        field.set_array(u)
+
+    animation = FuncAnimation(
+        fig, update, evolve(0.0, u_init), repeat=False, interval=30, save_count=200
+    )
+
+    animation.save(str(Path(__file__).with_suffix(".gif")), "imagemagick")
 
 
 if __name__ == "__main__":
@@ -99,6 +197,8 @@ if __name__ == "__main__":
 
     gf.tech.LAYER_STACK.layers["heater"].thickness = 0.13
     gf.tech.LAYER_STACK.layers["heater"].zmin = 2.2
+    print(gf.tech.LAYER_STACK.layers.keys())
+    # gf.tech.LAYER_STACK.layers["core"].thickness = 2
 
     heater1 = gf.components.straight_heater_metal(length=50, heater_width=2)
     heater2 = gf.components.straight_heater_metal(length=50, heater_width=2).move(
@@ -107,7 +207,7 @@ if __name__ == "__main__":
 
     heaters = gf.Component("heaters")
     heaters << heater1
-    heaters << heater2
+    # heaters << heater2
     heaters.show()
 
     geometry = mesh2D(
@@ -116,7 +216,7 @@ if __name__ == "__main__":
         base_resolution=0.4,
         exclude_layers=((1, 10),),
         padding=(10, 10, 1, 1),
-        refine_resolution={(1, 0): 0.05, (47, 0): 0.02},
+        refine_resolution={(1, 0): 0.1, (47, 0): 0.1},
     )
 
     import gmsh
@@ -129,5 +229,12 @@ if __name__ == "__main__":
         mesh_filename="mesh.msh",
         thermal_conductivity={"(47, 0)": 28, "oxide": 1.38, "(1, 0)": 148},
         specific_conductivity={"(47, 0)_0": 2.3e6},
+        thermal_diffusivity={
+            "(47, 0)": 28 / 598 / 5240,
+            "oxide": 1.38 / 709 / 2203,
+            "(1, 0)": 148 / 711 / 2330,
+        },
+        # specific_heat={"(47, 0)_0": 598, 'oxide': 709, '(1, 0)': 711},
+        # density={"(47, 0)_0": 5240, 'oxide': 2203, '(1, 0)': 2330},
         currents={"(47, 0)_0": 0.007},
     )
