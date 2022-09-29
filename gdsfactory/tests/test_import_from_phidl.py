3d0feef3c82c965a0d74da302d1815f3c2ed7fa4
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-09-29 10:45:19 -0700

    fix tests
    Former-commit-id: 806fffd54eac38047c2a72e1c58a66e815e324a2 [formerly 455343430f2c1ec9862fc9e51bedb18d45d690a3]
    Former-commit-id: 8156d236ece1634f845be182bbda442a184748ab

--- gdsfactory/tests/test_import_from_phidl.py
+++ gdsfactory/tests/test_import_from_phidl.py
@@ -8,7 +8,7 @@ def test_import_from_phidl(data_regression: DataRegressionFixture) -> None:
     """Read ports from markers."""
     import phidl.geometry as pg
 
-    c1 = pg.snspd()
+    c1 = pg.ytron_round()
     c2 = gf.read.from_phidl(component=c1)
     data_regression.check(c2.to_dict())
     difftest(c2)
@@ -17,6 +17,6 @@ def test_import_from_phidl(data_regression: DataRegressionFixture) -> None:
 if __name__ == "__main__":
     import phidl.geometry as pg
 
-    c1 = pg.snspd()
+    c1 = pg.ytron_round()
     c2 = gf.read.from_phidl(component=c1)
     c2.show(show_ports=True)
