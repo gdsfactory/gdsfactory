3493c03b5270cd8fc3d6b50d404508ed2f61381b
SkandanC <s39chand@edu.uwaterloo.ca>
2022-10-07 16:13:41 -0400

    Set to gpu version by default and remove version number
    Former-commit-id: 6c9bcd4dbbabebb6270f4532308e7bdcd9bfd9f9 [formerly 909827bc6d89efd04641f62a1cfb0b082d4e9500 [formerly 50e7460063089352c0b7306aee5476b12b8771e2] [formerly 5790eec29a0a4f0353915f948a73ed6789bd4d6a [formerly 045fa8964f9c0ad0960f3bc0d9a536a1b1072c11]]]
    Former-commit-id: 5790eec29a0a4f0353915f948a73ed6789bd4d6a
    Former-commit-id: f16eedeeec893956e4a26da90dc9d3281212fb53 [formerly 99bc5de4eb9aa0d756bcf46589f13137d2c0dd07]
    Former-commit-id: 5250baa826680812020bf21d20ba6cd981129322

--- install.bat
+++ install.bat
@@ -1,7 +1,7 @@
 
 set PIP_FIND_LINKS="https://whls.blob.core.windows.net/unstable/index.html"
 pip install lytest simphony sax jax sklearn klayout
-pip install "jax[cpu]===0.3.7" -f https://whls.blob.core.windows.net/unstable/index.html --use-deprecated legacy-resolver
+pip install "jaxlib[cuda111]" -f https://whls.blob.core.windows.net/unstable/index.html --use-deprecated legacy-resolver
 pip install gdsfactory==5.38.0
 gf tool install
 
