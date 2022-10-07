afcaa9b69c888cc4aa39b53ebbee620dac2f7d3e
SkandanC <s39chand@edu.uwaterloo.ca>
2022-10-07 15:15:35 -0400

    Fix jaxlib version
    Former-commit-id: fbb737952afe2babdc1c17e560067482b8118780 [formerly c826c6cfd4fe3e493f207e2ac05be8a0016d96e8]
    Former-commit-id: 5acd5d3c723dfb5a84b0e31f358ee449ae336f70

--- install.bat
+++ install.bat
@@ -1,6 +1,7 @@
 
 set PIP_FIND_LINKS="https://whls.blob.core.windows.net/unstable/index.html"
-pip install lytest simphony sax jax jaxlib sklearn klayout
+pip install lytest simphony sax jax sklearn klayout
+pip install "jax[cpu]===0.3.7" -f https://whls.blob.core.windows.net/unstable/index.html --use-deprecated legacy-resolver
 pip install gdsfactory==5.38.0
 gf tool install
 
