bff6a4d41418995062bf73850033316e426c0190
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-10-10 09:12:13 -0700

    Bump version: 5.42.0 → 5.42.1

--- install.bat
+++ install.bat
@@ -2,7 +2,7 @@
 set PIP_FIND_LINKS="https://whls.blob.core.windows.net/unstable/index.html"
 pip install lytest simphony sax jax sklearn klayout
 pip install "jaxlib[cuda111]" -f https://whls.blob.core.windows.net/unstable/index.html --use-deprecated legacy-resolver
-pip install gdsfactory==5.42.0
+pip install gdsfactory==5.42.1
 gf tool install
 
 if exist "%USERPROFILE%\Desktop\gdsfactory" (goto SKIP_INSTALL)
