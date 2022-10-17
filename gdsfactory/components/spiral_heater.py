c864300f8a6cc85b23a099137923213ed72fc024
HelgeGehring <HelgeGehring@gmail.com>
2022-10-17 16:17:29 -0700

    improved spiral heater
    Former-commit-id: 5b2654fdaa8ba2e59834ff196a7299b81a171993 [formerly 524cbf1db455e1e212ed2cba56e24f45d3acfd80]
    Former-commit-id: 860940538fadb5dba83dc1257f1441d30ca9935f

--- gdsfactory/components/spiral_heater.py
+++ gdsfactory/components/spiral_heater.py
@@ -1,59 +1,35 @@
-import numpy as np
-
 import gdsfactory as gf
+from gdsfactory.types import ComponentFactory
 
 
 @gf.cell
 def spiral_heater(
-    min_radius,
-    straight_length,
-    spacing,
-    num,
-    straight_factory=gf.components.straight,
-    bend_factory=gf.components.bend_euler,
+    min_radius: float,
+    straight_length: float,
+    spacing: float,
+    num: int,
+    straight_factory: ComponentFactory = gf.components.straight,
+    bend_factory: ComponentFactory = gf.components.bend_euler,
+    bend_s_factory: ComponentFactory = gf.components.bend_s,
 ):
     c = gf.Component()
 
-    center_straight = c << straight_factory(straight_length)
-    angle = 187
-    center_straight.rotate(180 - angle)
+    bend_s = c << bend_s_factory((straight_length, -min_radius * 2 - spacing))
 
-    for port in center_straight.ports.values():
-        bend = c << bend_factory(angle=angle, radius=min_radius, p=0)
-        bend.connect("o1", port)
-
-        straight = c << straight_factory(1.5 * straight_length)
-        straight.connect("o1", bend.ports["o2"])
-
-        inner_loop_width = (
-            4 * min_radius
-            - straight_length
-            * np.sin(np.deg2rad(-angle))  # width of the area of the inner straight
-            - 2
-            * min_radius
-            * (
-                1 - np.cos(np.deg2rad(180 - angle))
-            )  # width of the part of the bend >180
-        )
-
-        print(inner_loop_width)
-        print(
-            min_radius * (1 - np.cos(np.deg2rad(180 - angle))),
-            np.cos(np.deg2rad(180 - angle)),
-        )
-
+    for port in bend_s.ports.values():
         for i in range(num):
             bend = c << bend_factory(
                 angle=180, radius=min_radius + (i + 1) * spacing, p=0
             )
-            bend.connect("o1", straight.ports["o2"])
+            bend.connect("o1", port)
 
-            straight = c << straight_factory(2 * straight_length)
+            straight = c << straight_factory(straight_length)
             straight.connect("o1", bend.ports["o2"])
+            port = straight.ports["o2"]
 
     return c
 
 
 if __name__ == "__main__":
-    heater = spiral_heater(10, 100, 5, 10)
+    heater = spiral_heater(3.0, 30.0, 2.0, 10)
     heater.flatten().show()
