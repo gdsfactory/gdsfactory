dfc2d23442170b6cf2d965309b647fa0405f522f
HelgeGehring <HelgeGehring@gmail.com>
2022-10-17 16:32:56 -0700

    improved spiral heater: variable spacing
    Former-commit-id: d6473cfbedf0c9dd06e08120786de92aaf429f17 [formerly 4c4bef647dc4dde38e6e54c1c78fc75ce7b00b43 [formerly 0de9be9fa58c8a074ff990ff08603fcd98213bbf] [formerly d316d8994839117ab0f2e39007181f60f7d3fe39 [formerly eefcc7eebe1fd45701ba64eb093652e73981437b]]]
    Former-commit-id: d316d8994839117ab0f2e39007181f60f7d3fe39
    Former-commit-id: 19c835dcd4e527d628785a80c3cbdf6c8d0c58fd [formerly cc8ea3d6530b457149f0ab52587be91540b97450]
    Former-commit-id: 9e42561373d731e644aaa296a5674969c7f2ae8a

--- gdsfactory/components/spiral_heater.py
+++ gdsfactory/components/spiral_heater.py
@@ -1,3 +1,7 @@
+from typing import Tuple
+
+import numpy as np
+
 import gdsfactory as gf
 from gdsfactory.types import ComponentFactory
 
@@ -6,20 +10,19 @@ from gdsfactory.types import ComponentFactory
 def spiral_heater(
     min_radius: float,
     straight_length: float,
-    spacing: float,
-    num: int,
+    spacings: Tuple[float, ...],
     straight_factory: ComponentFactory = gf.components.straight,
     bend_factory: ComponentFactory = gf.components.bend_euler,
     bend_s_factory: ComponentFactory = gf.components.bend_s,
 ):
     c = gf.Component()
 
-    bend_s = c << bend_s_factory((straight_length, -min_radius * 2 - spacing))
+    bend_s = c << bend_s_factory((straight_length, -min_radius * 2 - spacings[0]))
 
     for port in bend_s.ports.values():
-        for i in range(num):
+        for i in range(len(spacings)):
             bend = c << bend_factory(
-                angle=180, radius=min_radius + (i + 1) * spacing, p=0
+                angle=180, radius=min_radius + np.sum(spacings[: i + 1]), p=0
             )
             bend.connect("o1", port)
 
@@ -31,5 +34,5 @@ def spiral_heater(
 
 
 if __name__ == "__main__":
-    heater = spiral_heater(3.0, 30.0, 2.0, 10)
+    heater = spiral_heater(3.0, 30.0, (2, 2, 3, 3, 2, 2))
     heater.flatten().show()
