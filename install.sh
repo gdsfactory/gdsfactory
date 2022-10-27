57df8214f0902fff064ef77d934e09ff324450dd
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-10-27 09:51:22 -0700

    Bump version: 5.45.1 → 5.46.0
    Former-commit-id: 0aaf997388a71c689ee77639d9aead4135532d34 [formerly f6439b1b47d027a60b97cd3133fb2c0c049414ab [formerly 40bfc763bf0da1f0be7a6dce4bb3fa9cf7e1e423] [formerly f5363e8eb215af1774163b81d60ba2d9c1033685 [formerly 5561f39dc5f545f7d644aa31708e611f7bcdedc5]]]
    Former-commit-id: f5363e8eb215af1774163b81d60ba2d9c1033685
    Former-commit-id: 7045751394e32e904b8dac041c8b139a9b2d2e18 [formerly 8b0df5a39f9270845e4c4150d44bc8a164f5b904]
    Former-commit-id: dbf4d091c9f4fcf220d762c6e5e809b66cac3e03

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 pip install gdsfactory[tidy3d]
 pip install gdsfactory[full] --upgrade
-pip install gdsfactory==5.45.1
+pip install gdsfactory==5.46.0
 gf tool install
 
 [ ! -d $HOME/Desktop/gdsfactory ] && git clone https://github.com/gdsfactory/gdsfactory.git $HOME/Desktop/gdsfactory
