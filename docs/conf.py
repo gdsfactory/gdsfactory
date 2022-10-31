6103d5a909b32efe0563f7c4b070bafa13b27c8f
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-10-31 12:09:23 -0700

    Bump version: 5.47.2 → 5.47.3
    Former-commit-id: 336c5d8d6521fbac05d6e5bc19041c25cb200eda [formerly 47b371dc8edc781999a761992b06b264d9ee6865 [formerly 04c4ff017ab580f92d974b31e126c5a933862c51] [formerly 07f03b73bf8e21f16ce752edfe8329e1fdb530b6 [formerly bc1a255714c70466de53f4ac3108874ffe51dc51]]]
    Former-commit-id: 07f03b73bf8e21f16ce752edfe8329e1fdb530b6
    Former-commit-id: ee25ae9d7fa9664166b0c41505ba436c307168cc [formerly b4e4204ed852b02e597197bf396bf3dddc1a4134]
    Former-commit-id: 24f2aa16ac8f7e89ace8bec0980ec2f5e18c1e64

--- docs/conf.py
+++ docs/conf.py
@@ -1,5 +1,5 @@
 project = "gdsfactory"
-release = "5.47.2"
+release = "5.47.3"
 copyright = "2020, MIT License"
 
 html_theme = "sphinx_book_theme"
--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -11,7 +11,7 @@ priority:
 You can access the config dictionary with `print_config`
 """
 
-__version__ = "5.47.2"
+__version__ = "5.47.3"
 import io
 import json
 import os
