47b371dc8edc781999a761992b06b264d9ee6865
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-10-31 12:09:23 -0700

    Bump version: 5.47.2 → 5.47.3
    Former-commit-id: 07f03b73bf8e21f16ce752edfe8329e1fdb530b6 [formerly bc1a255714c70466de53f4ac3108874ffe51dc51]
    Former-commit-id: 04c4ff017ab580f92d974b31e126c5a933862c51

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 pip install gdsfactory[tidy3d]
 pip install gdsfactory[full] --upgrade
-pip install gdsfactory==5.47.2
+pip install gdsfactory==5.47.3
 gf tool install
 
 [ ! -d $HOME/Desktop/gdsfactory ] && git clone https://github.com/gdsfactory/gdsfactory.git $HOME/Desktop/gdsfactory
