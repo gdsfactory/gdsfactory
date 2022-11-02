8c62234f3efd1feb9359d38f2ac61ae6457a86d5
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-11-02 11:15:32 -0700

    Bump version: 5.50.1 → 5.50.2
    Former-commit-id: f6b164b4c04c0f0918793b924295c1135cb9b1d5 [formerly cd08d8b54b0c6f39d245d2bd18a97b03965cc0ba [formerly 62fc5b89e71f90b5be71ee74c3313754c34067c9] [formerly f9f65d2d78c7713446ed5a9fd0b8b212a0172912 [formerly df6fa71bfb346816b3c18d34854b7f89de4b2244]]]
    Former-commit-id: f9f65d2d78c7713446ed5a9fd0b8b212a0172912
    Former-commit-id: 989acd26c454dd84ffc8f63bc530502eec8f7d13 [formerly b2ed556586625a49b74187e3b4610160d9f2a200]
    Former-commit-id: 56daedd710a175295ecba754fb9a57bf141b4e81

--- install.bat
+++ install.bat
@@ -2,7 +2,7 @@
 set PIP_FIND_LINKS="https://whls.blob.core.windows.net/unstable/index.html"
 pip install lytest simphony sax jax sklearn klayout
 pip install "jaxlib[cuda111]" -f https://whls.blob.core.windows.net/unstable/index.html --use-deprecated legacy-resolver
-pip install gdsfactory==5.50.1
+pip install gdsfactory==5.50.2
 gf tool install
 
 if exist "%USERPROFILE%\Desktop\gdsfactory" (goto SKIP_INSTALL)
