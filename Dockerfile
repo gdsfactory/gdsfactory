d740f99d5d97bd5eb7f2cf81731e8917e41f59cb
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-11-02 15:18:19 -0700

    Bump version: 5.50.2 → 5.50.3

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,1 @@
-FROM joamatab/gdsfactory:5.50.2
+FROM joamatab/gdsfactory:5.50.3
--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -11,7 +11,7 @@ priority:
 You can access the config dictionary with `print_config`
 """
 
-__version__ = "5.50.2"
+__version__ = "5.50.3"
 import io
 import json
 import os
