fc2cc4cc8ebf2628e581e6cb7ea9215744839fc3
SkandanC <s39chand@edu.uwaterloo.ca>
2022-11-04 19:22:15 -0400

    Update install.bat
    Former-commit-id: 338ce146072887b6bb7f038d36d4d92477a8ec72 [formerly 921cf1b6c60be1a565f103a470f9919365a3ea2d]
    Former-commit-id: 25bff5baff7ad46112c1bc6b368100d4042c25ed

--- install.bat
+++ install.bat
@@ -1,16 +1,18 @@
 
+echo %cd%
 set PIP_FIND_LINKS="https://whls.blob.core.windows.net/unstable/index.html"
 pip install lytest simphony sax jax sklearn klayout devsim
 pip install "jaxlib[cuda111]" -f https://whls.blob.core.windows.net/unstable/index.html --use-deprecated legacy-resolver
 pip install gdsfactory==5.53.0
 gf tool install
+cd ..
 set GF_PATH=%cd%
 
-if exist "%USERPROFILE%\Desktop\gdsfactory" (goto SKIP_INSTALL)
+@REM if exist "%USERPROFILE%\Desktop\gdsfactory" (goto SKIP_INSTALL)
 cd %USERPROFILE%\Desktop
-git clone https://github.com/gdsfactory/gdsfactory.git
+git clone https://github.com/gdsfactory/gdsfactory.git -b Add-shortcut-during-installation
 cd gdsfactory
-%GF_PATH%/python shortcuts.py %GF_PATH%
+%GF_PATH\python shortcuts.py %GF_PATH%
 
 :SKIP_INSTALL
 echo gdsfactory installed
