d70bc7c3cfbc187142b5f79ad2220ad8f4597dd6
SkandanC <s39chand@edu.uwaterloo.ca>
2022-11-04 00:49:18 -0400

    Add Spyder
    Former-commit-id: 7ab6648180189be2c4dd3c17d3318a8eefbb3683 [formerly 30cdad1f5a40d8adf74c9f8329c03ea9cdbf2944 [formerly a69fbbb936b19db3145e894c44cbe35882237532] [formerly 31adab773299076b7f69623568f5a70031ed1eed [formerly 5d63c7a456bc3e842693f20f1226a3324a616a4a]]]
    Former-commit-id: 31adab773299076b7f69623568f5a70031ed1eed
    Former-commit-id: ba07ed6342918665a953185eccdd30070e224cc0 [formerly df1ba758fddfc26e1a09e7fd64839a2a3ebdf495]
    Former-commit-id: c0bbd21b7038463cfb775ebae57b6819b6a04388

--- install.bat
+++ install.bat
@@ -4,10 +4,12 @@ pip install lytest simphony sax jax sklearn klayout devsim
 pip install "jaxlib[cuda111]" -f https://whls.blob.core.windows.net/unstable/index.html --use-deprecated legacy-resolver
 pip install gdsfactory==5.53.0
 gf tool install
+python -c "import menuinst; menuinst.install('spyder.json')"
 
 if exist "%USERPROFILE%\Desktop\gdsfactory" (goto SKIP_INSTALL)
 cd %USERPROFILE%\Desktop
 git clone https://github.com/gdsfactory/gdsfactory.git
+cd gdsfactory
 
 :SKIP_INSTALL
 echo gdsfactory installed
