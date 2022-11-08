61d5071f7e0a0ae31fadaff3883d0af0355cd1e2
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-11-08 12:32:18 -0800

    fix grating coupler meep input parameters
    Former-commit-id: 7819379b83c671f87923736bb15727604d71d73a [formerly e9219281a1b7a5b1acd260db1710291b7ba29471 [formerly 68f1d386a15737696ad211e78be0072ac1663dc8] [formerly 1996131b1b4b3501bad95322b2bb01021ebde5c5 [formerly 6224d146af9633a12bcc27e17c51e64d1598258d]]]
    Former-commit-id: 1996131b1b4b3501bad95322b2bb01021ebde5c5
    Former-commit-id: 0f65b7b9f07d988a909589cbe04bb1aa5100a192 [formerly 184e107fcc51fc4de6fd6160018e0b289227f628]
    Former-commit-id: 0250412338de28166e1d85c4fbd16bd6a5bdae0e

--- gdsfactory/simulation/gmeep/get_simulation_grating_fiber.py
+++ gdsfactory/simulation/gmeep/get_simulation_grating_fiber.py
@@ -33,7 +33,6 @@ def get_simulation_grating_fiber(
     n_periods: int = 30,
     widths: Optional[Floats] = None,
     gaps: Optional[Floats] = None,
-    slab_thickness: float = 150 * nm,
     fiber_angle_deg: float = 20.0,
     fiber_xposition: float = 1.0,
     fiber_core_diameter: float = 10.4,
@@ -48,6 +47,7 @@ def get_simulation_grating_fiber(
     substrate_thickness: float = 1.0,
     box_thickness: float = 2.0,
     wg_thickness: float = 220 * nm,
+    slab_thickness: float = 150 * nm,
     top_clad_thickness: float = 2.0,
     air_gap_thickness: float = 1.0,
     fiber_thickness: float = 2.0,
@@ -68,7 +68,7 @@ def get_simulation_grating_fiber(
     ncore = sqrt(na**2 + ncore**2)
 
     Args:
-        period: fiber grating period.
+        period: fiber grating period in um.
         fill_factor: fraction of the grating period filled with the grating material.
         n_periods: number of periods.
         widths: Optional list of widths. Overrides period, fill_factor, n_periods.
@@ -78,8 +78,8 @@ def get_simulation_grating_fiber(
         fiber_core_diameter: fiber diameter.
         fiber_numerical_aperture: NA.
         fiber_nclad: fiber cladding index.
-        fiber_ncore: fiber core index.
         nwg: waveguide index.
+        nslab: slab refractive index.
         nclad: top cladding index.
         nbox: box index bottom.
         nsubstrate: index substrate.
@@ -87,6 +87,7 @@ def get_simulation_grating_fiber(
         substrate_thickness: substrate_thickness (um).
         box_thickness: thickness for bottom cladding (um).
         wg_thickness: wg_thickness (um).
+        slab_thickness: slab thickness (um). etch_depth=wg_thickness-slab_thickness.
         top_clad_thickness: thickness of the top cladding.
         air_gap_thickness: air gap thickness.
         fiber_thickness: fiber_thickness.
