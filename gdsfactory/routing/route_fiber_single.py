ef2c5c0aef3d07d87fdca3762a4f9755c57a0d64
Sourcery AI <>
2022-11-10 09:07:02 +0000

    'Refactored by Sourcery'
    Former-commit-id: 80a4122809ce6ae48065d97983a4759c9c489e1a [formerly 8508a0c34b537e4be2ccbdba0b47c5bf43a8b8bf [formerly 0292666a61fc056ce6509dfa355577892682ea9e] [formerly f0c909e22a4e2c5cb3ba02cb1ef504cf47e5118d [formerly 25fd68c18126bad292f3c0d9fd76a0ea16d95545]]]
    Former-commit-id: f0c909e22a4e2c5cb3ba02cb1ef504cf47e5118d
    Former-commit-id: 8ee51faa2755288e92dfdca80b22e103eba87976 [formerly c1b91ec1ef5632b3a7209d455acdfc818a1da515]
    Former-commit-id: 3f83b49d9c5d78143c200f386d345f73f9d16e72

--- gdsfactory/routing/route_fiber_single.py
+++ gdsfactory/routing/route_fiber_single.py
@@ -92,13 +92,13 @@ def route_fiber_single(
     optical_ports = {
         p.name: p for p in optical_ports.values() if p.name not in excluded_ports
     }
-    N = len(optical_ports)
-
     if isinstance(grating_coupler, list):
         grating_couplers = [gf.call_if_func(g) for g in grating_coupler]
         grating_coupler = grating_couplers[0]
     else:
         grating_coupler = gf.call_if_func(grating_coupler)
+        N = len(optical_ports)
+
         grating_couplers = [grating_coupler] * N
 
     gc_port2center = getattr(grating_coupler, "port2center", grating_coupler.xsize / 2)
@@ -169,17 +169,16 @@ def route_fiber_single(
                     elements_south.append(ei)
                 else:
                     elements_south.append(ei.rotate(180))
+        elif isinstance(e, gdstk.Label):
+            ei = e
+            ei.rotation = np.mod(ei.rotation + np.pi, 2 * np.pi)
+            ei.origin = _rotate_points(
+                ei.origin,
+                angle=np.rad2deg(ei.rotation),
+            )
+            elements_south.append(e)
         else:
-            if isinstance(e, gdstk.Label):
-                ei = e
-                ei.rotation = np.mod(ei.rotation + np.pi, 2 * np.pi)
-                ei.origin = _rotate_points(
-                    ei.origin,
-                    angle=np.rad2deg(ei.rotation),
-                )
-                elements_south.append(e)
-            else:
-                elements_south.append(e.rotate(180))
+            elements_south.append(e.rotate(180))
 
     if len(gratings_north) > 0:
         for io in gratings_north[0]:
