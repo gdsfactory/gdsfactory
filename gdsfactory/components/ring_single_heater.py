9314684a30730f553e3e5dbf492e5c84881f485d
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-11-09 19:49:48 -0800

    fix more tests
    Former-commit-id: 651abc20a12569f2eb6af6ec85f87da98f08e308 [formerly 1835a5913fe9ad1c1913eeac83c42894b28ce359 [formerly 32326e6a11fe9225584ad4daee8901c6afa4264f] [formerly 2c168a542af15f7e7a7ffcc04de6caefdb12f446 [formerly 0291db143741bb6699d37744e4f19e2d2db5ce5f]]]
    Former-commit-id: 2c168a542af15f7e7a7ffcc04de6caefdb12f446
    Former-commit-id: dc2f9288b8462ee6323f27c1bf7fbaba289f28bd [formerly 7f38d0f2477a439d42d3fd250e7318bea2a1f9f5]
    Former-commit-id: eba713aa5d85336960451c7b4f0a15d6b86b694f

--- gdsfactory/components/ring_single_heater.py
+++ gdsfactory/components/ring_single_heater.py
@@ -121,8 +121,9 @@ def ring_single_heater(
 
 if __name__ == "__main__":
     c = ring_single_heater(width=0.5, gap=1, layer=(2, 0), radius=10, length_y=1)
+    print(c.to_yaml())
     c.show(show_subports=False)
-    c.pprint_ports()
+    # c.pprint_ports()
 
     # cc = gf.add_pins(c)
     # print(c.settings)
