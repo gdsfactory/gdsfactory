952d0a688922f6b4669d9ec733b76f6e8ee909f5
SkandanC <s39chand@edu.uwaterloo.ca>
2022-11-11 16:53:27 -0500

    Fix installer
    Former-commit-id: e2f29d7a9c41b68bc7fdeadb5691f72da75397da [formerly 9b43d854578bcc5d3e2e13497e5846cfc2c02f48 [formerly cfe07ca4b45c9d6973a512694953dda564d30c54] [formerly 5e90d4b2897632045b2506f0b3ed4a62c8d06ffa [formerly 8ba24c56c2dc63b440fd88b9057b13e84f9f878c]]]
    Former-commit-id: 5e90d4b2897632045b2506f0b3ed4a62c8d06ffa
    Former-commit-id: be745b0f51302d3a2965601944cb69fffeab6182 [formerly 4bfe70efeb8d92f96ead880e4ff46ab31201ce5a]
    Former-commit-id: 4011636437d2047eabba9e4178ff91393a147e7f

--- install.bat
+++ install.bat
@@ -1,15 +1,23 @@
 
-set PIP_FIND_LINKS="https://whls.blob.core.windows.net/unstable/index.html"
-pip install lytest simphony sax jax sklearn klayout devsim
-pip install "jaxlib[cuda111]" -f https://whls.blob.core.windows.net/unstable/index.html --use-deprecated legacy-resolver
-pip install gdsfactory==5.54.0
-gf tool install
-cd ..
+@REM set PIP_FIND_LINKS="https://whls.blob.core.windows.net/unstable/index.html"
+@REM pip install lytest simphony sax jax sklearn klayout devsim
+@REM pip install "jaxlib[cuda111]" -f https://whls.blob.core.windows.net/unstable/index.html --use-deprecated legacy-resolver
+@REM pip install gdsfactory==5.54.0
+@REM gf tool install
+@REM cd ..
 set GF_PATH=%cd%
+echo %GF_PATH%
 
-if exist "%USERPROFILE%\Desktop\gdsfactory" (goto SKIP_INSTALL)
+@REM if exist "%USERPROFILE%\Desktop\gdsfactory" (goto SKIP_INSTALL)
 cd %USERPROFILE%\Desktop
-git clone https://github.com/gdsfactory/gdsfactory.git
+@REM pause
+call %GF_PATH%\condabin\conda activate
+@REM pause
+call conda install -c conda-forge git -y
+@REM pause
+call git clone https://github.com/gdsfactory/gdsfactory.git
+@REM pause
+echo %GF_PATH%
 
 cd gdsfactory
 %GF_PATH%\python shortcuts.py %GF_PATH%
