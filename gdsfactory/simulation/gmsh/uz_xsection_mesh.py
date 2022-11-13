f9e4a7adfbfb26ae17e6098bf9be649c9e8f96e3
simbilod <sb30@princeton.edu>
2022-11-13 12:19:36 -0800

    update uz_xsection with shapely
    Former-commit-id: ba99fd8a53a1499feefa7cec28cf519980f28a36 [formerly 27e1ffd5e0620b6c67bd5a467e0342a5fa69bff9]
    Former-commit-id: d56fae9c913030c8aed9463c973d52f0a697da2a

--- gdsfactory/simulation/gmsh/uz_xsection_mesh.py
+++ gdsfactory/simulation/gmsh/uz_xsection_mesh.py
@@ -2,80 +2,55 @@ from collections import OrderedDict
 from typing import Dict, Optional, Tuple
 
 import numpy as np
-from shapely.geometry import MultiPolygon, Polygon
+from shapely.geometry import LineString, MultiPolygon, Point, Polygon
 from shapely.ops import unary_union
 
 import gdsfactory as gf
-from gdsfactory.geometry.boolean import boolean
 from gdsfactory.simulation.gmsh.mesh import mesh_from_polygons
-from gdsfactory.simulation.gmsh.parse_gds import to_polygons
+from gdsfactory.simulation.gmsh.parse_gds import fuse_component_layer, to_polygons
 from gdsfactory.simulation.gmsh.parse_layerstack import order_layerstack
 from gdsfactory.tech import LayerStack
-from gdsfactory.types import ComponentOrReference, Layer
+from gdsfactory.types import ComponentOrReference
 
 
 def get_xsection_bounds_inplane(
-    component: ComponentOrReference,
+    layer_polygons_dict: Dict[str, MultiPolygon],
     xsection_bounds: Tuple[Tuple[float, float], Tuple[float, float]],
-    layer_stack: Optional[LayerStack] = None,
-    line_layer: Optional[Layer] = 99,
-    line_width: Optional[float] = 0.01,
 ):
     """Given a component c and two coordinates (x1,y1), (x2,y2), computes the \
         bounding box(es) of each layer in the xsection coordinate system (u).
 
-    Uses gdsfactory boolean and component.extract
-
     Args:
-        component: Component or ComponentReference.
+        layer_polygons_dict: dict containing layernames: shapely polygons pairs
         xsection_bounds: ( (x1,y1), (x2,y2) ), with x1,y1 beginning point of cross-sectional line and x2,y2 the end.
-        line_layer: (dummy) layer to put the extraction line on.
-        line_width: (dummy) thickness of extraction line. Cannot be 0, should be small (near dbu) for accuracy.
 
     Returns: Dict containing layer(list pairs, with list a list of bounding box coordinates (u1,u2))
         in xsection line coordinates.
     """
     # Create line component for bool
-    P = gf.Path(xsection_bounds)
-    X = gf.CrossSection(width=line_width, layer=line_layer)
-    line = gf.path.extrude(P, X)
-
-    # Ref line vector
-    ref_vector_origin = np.array(xsection_bounds[0])
+    line = LineString(xsection_bounds)
+    linestart = Point(xsection_bounds[0])
 
     # For each layer in component, extract point where line intersects
-    # Choose intersecting polygon edge with maximal (including sign) dot product to reference line
     bounds_dict = {}
-    layer_dict = layer_stack.to_dict()
-    for layername, layer in layer_dict.items():
-        bounds_layer = []
-        c_layer = component.extract(layer["layer"])
-        layer_boolean = boolean(c_layer, line, "and")
-        polygons = layer_boolean.get_polygons()
-        for polygon in polygons:
-            # Min point is midpoint between two closest vertices to origin
-            distances_origin = np.linalg.norm(polygon - ref_vector_origin, axis=1)
-            distances_sort_index = np.argsort(distances_origin)
-            min_point = (
-                polygon[distances_sort_index[0]] + polygon[distances_sort_index[1]]
-            ) / 2
-            max_point = (
-                polygon[distances_sort_index[2]] + polygon[distances_sort_index[3]]
-            ) / 2
-
-            umin = np.linalg.norm(min_point - ref_vector_origin)
-            umax = np.linalg.norm(max_point - ref_vector_origin)
-
-            bounds_layer.append((umin, umax))
-        bounds_dict[layername] = bounds_layer
+    for layername, polygons in layer_polygons_dict.items():
+        bounds_dict[layername] = []
+        for polygon in polygons if hasattr(polygons, "geoms") else [polygons]:
+            intersection = polygon.intersection(line).bounds
+            if intersection:
+                p1 = Point([intersection[0], intersection[1]])
+                p2 = Point([intersection[2], intersection[3]])
+                bounds_dict[layername].append(
+                    [linestart.distance(p1), linestart.distance(p2)]
+                )
 
     return bounds_dict
 
 
 def get_xsection_bound_polygons(
-    component: ComponentOrReference,
+    layer_polygons_dict: Dict[str, MultiPolygon],
     xsection_bounds: Tuple[Tuple[float, float], Tuple[float, float]],
-    layer_stack: Optional[LayerStack] = None,
+    layerstack: LayerStack,
 ):
     """Given a component and layer stack, computes the bounding box(es) of each \
             layer in the xsection coordinate system (u,z).
@@ -83,21 +58,20 @@ def get_xsection_bound_polygons(
     Args:
         component: Component or ComponentReference.
         xsection_bounds: ( (x1,y1), (x2,y2) ), with x1,y1 beginning point of cross-sectional line and x2,y2 the end
-        line_layer: (dummy) layer to put the extraction line on.
-        line_width: (dummy) thickness of extraction line. Cannot be 0, should be small (near dbu) for accuracy.
 
     Returns: Dict containing layer: polygon pairs, with (u1,u2) in xsection line coordinates
     """
     # Get in-plane cross-sections
     inplane_bounds_dict = get_xsection_bounds_inplane(
-        component, xsection_bounds, layer_stack
+        layer_polygons_dict, xsection_bounds
     )
-
-    # Generate full bounding boxes
-    layer_dict = layer_stack.to_dict()
 
     outplane_bounds_dict = {}
 
+    for key, value in inplane_bounds_dict.items():
+        print(key, value)
+
+    layer_dict = layerstack.to_dict()
     for layername, inplane_bounds_list in inplane_bounds_dict.items():
         outplane_polygons_list = []
         for inplane_bounds in inplane_bounds_list:
@@ -134,9 +108,18 @@ def uz_xsection_mesh(
     background_padding: Tuple[float, float, float, float] = (2.0, 2.0, 2.0, 2.0),
     filename: Optional[str] = None,
 ):
+    # Fuse and cleanup polygons of same layer in case user overlapped them
+    layer_dict = layerstack.to_dict()
+    layer_polygons_dict = {}
+    for layername in layer_dict.keys():  # filtered_layerdict.items():
+        layer_polygons_dict[layername] = fuse_component_layer(
+            component, layername, layer_dict[layername]
+        )
 
     # Find coordinates
-    bounds_dict = get_xsection_bound_polygons(component, xsection_bounds, layerstack)
+    bounds_dict = get_xsection_bound_polygons(
+        layer_polygons_dict, xsection_bounds, layerstack
+    )
 
     # Create polygons from bounds and layers
     layer_order = order_layerstack(layerstack)
@@ -200,6 +183,7 @@ if __name__ == "__main__":
         filtered_layerstack,
         resolutions=resolutions,
         background_tag="Oxide",
+        filename="mesh.msh",
     )
 
     import meshio
