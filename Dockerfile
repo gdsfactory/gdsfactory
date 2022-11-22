d4c47e3062159afeb3a7e8336085bd2a1fb9a8af
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-11-22 06:13:25 -0800

    Bump version: 6.2.2 → 6.2.3
    Former-commit-id: 56f421cf65ef6df0793cd4d9b4e72d882f6ade39 [formerly 50300ff36fc20ba1e93c17b7ff4afb9552fcdac0 [formerly 159530d989b80936aa513d59377984ba81b662d2] [formerly 67984b61bb1f9b1f5a05da52c268758c5854e587 [formerly 12c794ebd59f37b2438cbfec293e4dec0e7814d5]]]
    Former-commit-id: 67984b61bb1f9b1f5a05da52c268758c5854e587
    Former-commit-id: fac87801eff0f75c0e7d181c097e5a2803ec49ac [formerly 08dcebcae316e8895e9193b923fddc99ae9ddc37]
    Former-commit-id: 6e281caaa87fea1e0256a5047327daf8c6024882

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,1 @@
-FROM joamatab/gdsfactory:6.2.2
+FROM joamatab/gdsfactory:6.2.3
--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -11,7 +11,7 @@ priority:
 You can access the config dictionary with `print_config`
 """
 
-__version__ = "6.2.2"
+__version__ = "6.2.3"
 import io
 import json
 import os
