1a77b1547ac709d2ae5de39f89cc769f4247f3dd
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-11-21 17:39:00 -0800

    add Section to types
    Former-commit-id: c1d7bff3ae6d5cfe893ce863be7fc217ea9ca760 [formerly 2aeaf6e3d9a317df776c8ea7d48835aef5a9421c [formerly 3beb63a9feb9a0a33f1efc5f3260bd7d03b8547f] [formerly 7ba952e238e56f14907d8e7be13bc5c7d8ceca89 [formerly 755f0cc0b97f820bc7dd58c50f0a9d862c8a671a]]]
    Former-commit-id: 7ba952e238e56f14907d8e7be13bc5c7d8ceca89
    Former-commit-id: dd9dfba72b2eedc43c65cc71eff583ab79ceceb9 [formerly 815b5ead61e63f329e0b3754d5943f36f08d2d2c]
    Former-commit-id: 8e6c27a467dcdc57af63f87b611311b85b4cabb0

--- gdsfactory/samples/demo/pcell.py
+++ gdsfactory/samples/demo/pcell.py
@@ -16,6 +16,6 @@ def mzi_with_bend(radius: float = 10):
 
 
 if __name__ == "__main__":
-    c = mzi_with_bend(radius=5)
-    cc = gf.routing.add_fiber_single(c)
-    cc.show(show_ports=True)
+    c = mzi_with_bend(radius=25)
+    # c = gf.routing.add_fiber_single(c)
+    c.show(show_ports=True)
