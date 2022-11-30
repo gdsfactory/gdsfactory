42d394ea2715b8f9191f36f047b37308e32002d3
simbilod <sb30@princeton.edu>
2022-11-30 14:34:03 -0800

    refactoring of meshing code, mesh callback for refinement, example with devsim

--- gdsfactory/simulation/gmsh/mesh.py
+++ gdsfactory/simulation/gmsh/mesh.py
@@ -1,216 +1,183 @@
 from __future__ import annotations
 
 from collections import OrderedDict
-from itertools import combinations
+from itertools import combinations, product
 from typing import Dict, Optional
 
 import gmsh
+import numpy as np
 import pygmsh
-from shapely.geometry import LineString, MultiLineString, MultiPolygon, Point, Polygon
+from scipy.interpolate import NearestNDInterpolator
+from shapely.geometry import LineString, Point, Polygon
 
+from gdsfactory.simulation.gmsh.break_geometry import break_geometry
 from gdsfactory.simulation.gmsh.meshtracker import MeshTracker
-from gdsfactory.simulation.gmsh.parse_gds import break_line, tile_shapes
+from gdsfactory.simulation.gmsh.refine import (
+    global_callback_refinement,
+    surface_interface_refinement,
+)
+
+
+def define_entities(model, shapes_dict: OrderedDict):
+    """Adds the polygons and lines from a "shapes_dict" as physical entities in the pygmsh model "model".
+
+    Args:
+        model: pygmsh model to define the entities into.
+        shapes_dict: OrderedDict containing shapes to mesh as values, and keys as physical tags.
+
+    Returns:
+        model: updated pygmsh model.
+        meshtracker: meshtracker object containing the mapping between labels, shapely objects, and gmsh objects.
+    """
+    # Break up lines and polygon edges so that plane is tiled with no partially overlapping line segments
+    polygons_broken_dict, lines_broken_dict = break_geometry(shapes_dict)
+    # Instantiate shapely to gmsh map
+    meshtracker = MeshTracker(model=model, atol=1e-3)
+    # Add lines, reusing line segments
+    model, meshtracker = add_lines(model, meshtracker, lines_broken_dict)
+    # Add surfaces, reusing lines
+    model, meshtracker = add_surfaces(model, meshtracker, polygons_broken_dict)
+    # Add interfaces
+    model, meshtracker = tag_interfaces(model, meshtracker, polygons_broken_dict)
+    # Synchronize
+    model.synchronize()
+
+    return model, meshtracker
+
+
+def add_lines(model, meshtracker, lines_broken_dict):
+    """Add lines, reusing segments via meshtracker.
+
+    Args:
+        model: pygmsh model
+        meshtracker: meshtracker object
+        lines_broken_dict: dict
+
+    Returns:
+        model: updated pygmsh model
+        meshtracker: updated meshtracker object
+    """
+    for line_name, line in lines_broken_dict.items():
+        meshtracker.add_get_xy_line(line, line_name)
+
+    return model, meshtracker
+
+
+def add_surfaces(model, meshtracker, polygons_broken_dict):
+    """Add surfaces of polygons_broken_dict to model, reusing lines via meshtracker.
+
+    Args:
+        model: pygmsh model
+        meshtracker: meshtracker object
+        polygons_broken_dict: dict of
+
+    Returns:
+        model: updated pygmsh model
+        meshtracker: updated meshtracker object
+    """
+    for polygon_name, polygons in polygons_broken_dict.items():
+        gmsh_surfaces = []
+        for polygon in polygons if hasattr(polygons, "geoms") else [polygons]:
+            gmsh_surface = meshtracker.add_xy_surface(polygon, f"{polygon_name}")
+            gmsh_surfaces.append(gmsh_surface)
+        meshtracker.model.add_physical(gmsh_surfaces, f"{polygon_name}")
+
+    return model, meshtracker
+
+
+def tag_interfaces(model, meshtracker, polygons_broken_dict):
+    """Tag all interfacial lines between polygons as logged in meshtracker.
+
+    Args:
+        model: pygmsh model
+        meshtracker: meshtracker object
+        polygons_broken_dict: dict of
+
+    Returns:
+        model: updated pygmsh model
+        meshtracker: updated meshtracker object
+    """
+    for surface1, surface2 in combinations(polygons_broken_dict.keys(), 2):
+        if interfaces := [
+            line
+            for index, line in enumerate(meshtracker.gmsh_xy_segments)
+            if (
+                meshtracker.xy_segments_main_labels[index] == surface1
+                and meshtracker.xy_segments_secondary_labels[index] == surface2
+            )
+            or (
+                meshtracker.xy_segments_main_labels[index] == surface2
+                and meshtracker.xy_segments_secondary_labels[index] == surface1
+            )
+        ]:
+            model.add_physical(interfaces, f"{surface1}___{surface2}")
+
+    return model, meshtracker
 
 
 def mesh_from_polygons(
     shapes_dict: OrderedDict,
     resolutions: Optional[Dict[str, float]] = None,
+    mesh_scaling_factor: float = 1.0,
     default_resolution_min: float = 0.01,
     default_resolution_max: float = 0.5,
     filename: Optional[str] = None,
+    global_meshsize_array: Optional[np.array] = None,
+    global_meshsize_interpolant_func: Optional[callable] = NearestNDInterpolator,
+    verbosity: Optional[bool] = False,
 ):
-    """Return a 2D mesh from an ordered dict of shapely polygons."""
-    with pygmsh.occ.geometry.Geometry() as geometry:
+    """Return a 2D mesh from an ordered dict of shapely polygons.
 
-        gmsh.initialize()
+    Args:
+        shapes_dict: OrderedDict containing shapes to mesh.
+        resolutions: Dict with same keys as shapes_dict. containing another dict with resolution settings:
+            resolution: density of the mesh
+            distance: "dropoff" distance of this resolution away from the surface
+        default_resolution_min: gmsh default smallest characteristic length
+        default_resolution_max: gmsh default largest characteristic length
+        filename: to save the mesh
+        global_meshsize_array: array [x,y,z,lc] defining local mesh sizes. Not used if None
+        global_meshsize_interpolant: interpolation function for array [x,y,z,lc]. Default scipy.interpolate.NearestNDInterpolator
+        verbosity: boolean, gmsh sdout as it meshes
+    """
+    global_meshsize_callback_bool = global_meshsize_array is not None
+
+    with pygmsh.occ.geometry.Geometry() as geometry:
 
         geometry.characteristic_length_min = default_resolution_min
         geometry.characteristic_length_max = default_resolution_max
 
         model = geometry.__enter__()
 
-        # Break up shapes in order so that plane is tiled with non-overlapping layers
-        shapes_tiled_dict = tile_shapes(shapes_dict)
+        # Define geometry
+        (
+            model,
+            meshtracker,
+        ) = define_entities(model, shapes_dict)
 
-        # Break up lines and polygon edges so that plane is tiled with no partially overlapping line segments
-        polygons_broken_dict = OrderedDict()
-        lines_broken_dict = OrderedDict()
-        for _first_index, (first_name, _init_first_shapes) in enumerate(
-            shapes_dict.items()
-        ):
-            first_shapes = shapes_tiled_dict[first_name]
-            broken_shapes = []
-            for first_shape in (
-                first_shapes.geoms if hasattr(first_shapes, "geoms") else [first_shapes]
-            ):
-                # First line exterior
-                first_exterior_line = (
-                    LineString(first_shape.exterior)
-                    if first_shape.type == "Polygon"
-                    else first_shape
-                )
-                for _second_index, (second_name, second_shapes) in enumerate(
-                    shapes_dict.items()
-                ):
-                    # Do not compare to itself
-                    if second_name == first_name:
-                        continue
-                    else:
-                        second_shapes = shapes_tiled_dict[second_name]
-                        for second_shape in (
-                            second_shapes.geoms
-                            if hasattr(second_shapes, "geoms")
-                            else [second_shapes]
-                        ):
-                            # Second line exterior
-                            second_exterior_line = (
-                                LineString(second_shape.exterior)
-                                if second_shape.type == "Polygon"
-                                else second_shape
-                            )
-                            first_exterior_line = break_line(
-                                first_exterior_line, second_exterior_line
-                            )
-                            # Second line interiors
-                            for second_interior_line in (
-                                second_shape.interiors
-                                if second_shape.type == "Polygon"
-                                else []
-                            ):
-                                second_interior_line = LineString(second_interior_line)
-                                first_exterior_line = break_line(
-                                    first_exterior_line, second_interior_line
-                                )
-                # First line interiors
-                if first_shape.type == "Polygon" or first_shape.type == "MultiPolygon":
-                    first_shape_interiors = []
-                    for first_interior_line in first_shape.interiors:
-                        first_interior_line = LineString(first_interior_line)
-                        for _second_index, (second_name, second_shapes) in enumerate(
-                            shapes_dict.items()
-                        ):
-                            if second_name == first_name:
-                                continue
-                            else:
-                                second_shapes = shapes_tiled_dict[second_name]
-                                for second_shape in (
-                                    second_shapes.geoms
-                                    if hasattr(second_shapes, "geoms")
-                                    else [second_shapes]
-                                ):
-                                    # Exterior
-                                    second_exterior_line = (
-                                        LineString(second_shape.exterior)
-                                        if second_shape.type == "Polygon"
-                                        else second_shape
-                                    )
-                                    first_interior_line = break_line(
-                                        first_interior_line, second_exterior_line
-                                    )
-                                    # Interiors
-                                    for second_interior_line in (
-                                        second_shape.interiors
-                                        if second_shape.type == "Polygon"
-                                        else []
-                                    ):
-                                        second_interior_line = LineString(
-                                            second_interior_line
-                                        )
-                                        first_interior_line = break_line(
-                                            first_interior_line, second_interior_line
-                                        )
-                        first_shape_interiors.append(first_interior_line)
-                if first_shape.type == "Polygon" or first_shape.type == "MultiPolygon":
-                    broken_shapes.append(
-                        Polygon(first_exterior_line, holes=first_shape_interiors)
-                    )
-                else:
-                    broken_shapes.append(LineString(first_exterior_line))
-            if broken_shapes:
-                if first_shape.type == "Polygon" or first_shape.type == "MultiPolygon":
-                    polygons_broken_dict[first_name] = (
-                        MultiPolygon(broken_shapes)
-                        if len(broken_shapes) > 1
-                        else broken_shapes[0]
-                    )
-                else:
-                    lines_broken_dict[first_name] = (
-                        MultiLineString(broken_shapes)
-                        if len(broken_shapes) > 1
-                        else broken_shapes[0]
-                    )
+        # Synchronize
+        model.synchronize()
 
-        # Add lines, reusing line segments
-        meshtracker = MeshTracker(model=model, atol=1e-3)
-        for line_name, line in lines_broken_dict.items():
-            meshtracker.add_get_xy_line(line, line_name)
-
-        # Add surfaces, reusing lines to simplify at early stage
-        for polygon_name, polygons in polygons_broken_dict.items():
-            gmsh_surfaces = []
-            for polygon in polygons if hasattr(polygons, "geoms") else [polygons]:
-                gmsh_surface = meshtracker.add_xy_surface(polygon, f"{polygon_name}")
-                gmsh_surfaces.append(gmsh_surface)
-            meshtracker.model.add_physical(gmsh_surfaces, f"{polygon_name}")
-
-        # Refinement in surfaces
-        n = 0
-        refinement_fields = []
-        for label, resolution in resolutions.items():
-            # Inside surface
-            mesh_resolution = resolution["resolution"]
-            gmsh.model.mesh.field.add("MathEval", n)
-            gmsh.model.mesh.field.setString(n, "F", f"{mesh_resolution}")
-            gmsh.model.mesh.field.add("Restrict", n + 1)
-            gmsh.model.mesh.field.setNumber(n + 1, "InField", n)
-            gmsh.model.mesh.field.setNumbers(
-                n + 1,
-                "SurfacesList",
-                meshtracker.get_gmsh_xy_surfaces_from_label(label),
+        # Refinement
+        if not global_meshsize_callback_bool and resolutions:
+            surface_interface_refinement(
+                model,
+                meshtracker,
+                resolutions,
+                default_resolution_min,
+                default_resolution_max,
             )
-            # Around surface
-            mesh_distance = resolution["distance"]
-            gmsh.model.mesh.field.add("Distance", n + 2)
-            gmsh.model.mesh.field.setNumbers(
-                n + 2, "CurvesList", meshtracker.get_gmsh_xy_lines_from_label(label)
+
+        elif global_meshsize_callback_bool:
+            global_callback_refinement(
+                model,
+                global_meshsize_array,
+                global_meshsize_interpolant_func,
             )
-            gmsh.model.mesh.field.setNumber(n + 2, "Sampling", 100)
-            gmsh.model.mesh.field.add("Threshold", n + 3)
-            gmsh.model.mesh.field.setNumber(n + 3, "InField", n + 2)
-            gmsh.model.mesh.field.setNumber(n + 3, "SizeMin", mesh_resolution)
-            gmsh.model.mesh.field.setNumber(n + 3, "SizeMax", default_resolution_max)
-            gmsh.model.mesh.field.setNumber(n + 3, "DistMin", 0)
-            gmsh.model.mesh.field.setNumber(n + 3, "DistMax", mesh_distance)
-            # Save and increment
-            refinement_fields.append(n + 1)
-            refinement_fields.append(n + 3)
-            n += 4
 
-        # Use the smallest element size overall
-        gmsh.model.mesh.field.add("Min", n)
-        gmsh.model.mesh.field.setNumbers(n, "FieldsList", refinement_fields)
-        gmsh.model.mesh.field.setAsBackgroundMesh(n)
-
-        gmsh.model.mesh.MeshSizeFromPoints = 0
-        gmsh.model.mesh.MeshSizeFromCurvature = 0
-        gmsh.model.mesh.MeshSizeExtendFromBoundary = 0
-
-        # Tag all interfacial lines
-        for surface1, surface2 in combinations(polygons_broken_dict.keys(), 2):
-            interfaces = []
-            for index, line in enumerate(meshtracker.gmsh_xy_segments):
-                if (
-                    meshtracker.xy_segments_main_labels[index] == surface1
-                    and meshtracker.xy_segments_secondary_labels[index] == surface2
-                ) or (
-                    meshtracker.xy_segments_main_labels[index] == surface2
-                    and meshtracker.xy_segments_secondary_labels[index] == surface1
-                ):
-                    interfaces.append(line)
-            if interfaces:
-                model.add_physical(interfaces, f"{surface1}___{surface2}")
-
-        mesh = geometry.generate_mesh(dim=2, verbose=True)
+        # Perform meshing
+        gmsh.option.setNumber("Mesh.ScalingFactor", mesh_scaling_factor)
+        mesh = geometry.generate_mesh(dim=2, verbose=verbosity)
 
         if filename:
             gmsh.write(f"{filename}")
@@ -292,13 +259,39 @@ if __name__ == "__main__":
     # The edge of a polygon and another polygon (or entire simulation domain) will form a line object that can be refined independently
     resolutions = {}
     resolutions["core"] = {"resolution": 0.05, "distance": 0}
-    resolutions["core_clad"] = {"resolution": 0.01, "distance": 0.5}
-    resolutions["clad_box"] = {"resolution": 0.01, "distance": 0.5}
+    resolutions["core___clad"] = {"resolution": 0.01, "distance": 0.5}
+    resolutions["clad___box"] = {"resolution": 0.01, "distance": 0.5}
     resolutions["bottom_edge"] = {"resolution": 0.05, "distance": 0.5}
     resolutions["left_edge"] = {"resolution": 0.05, "distance": 0.5}
     # resolutions["clad"] = {"resolution": 0.1, "dist_min": 0.01, "dist_max": 0.3}
 
-    mesh = mesh_from_polygons(shapes, resolutions, filename="mesh.msh")
+    xs = np.linspace(-wsim / 2, wsim / 2, 500)
+    ys = np.linspace(-hcore / 2 - hbox, -hcore / 2 + hclad, 500)
+    global_meshsize_array = []
+
+    ls0 = 0.02
+    ls1 = 0.05
+    ls2 = 0.005
+
+    r = 0.5
+    for x, y in product(xs, ys):
+        if np.abs(x) ** 2 + np.abs(y) ** 2 <= r**2:
+            global_meshsize_array.append([x, y, 0, ls2])
+        elif y > 0:
+            global_meshsize_array.append([x, y, 0, ls1])
+        else:
+            global_meshsize_array.append([x, y, 0, ls0])
+
+    global_meshsize_array = np.array(global_meshsize_array)
+
+    mesh = mesh_from_polygons(
+        shapes,
+        resolutions,
+        mesh_scaling_factor=0.1,
+        filename="mesh.msh",
+        global_meshsize_array=global_meshsize_array,
+        verbosity=True,
+    )
 
     # gmsh.write("mesh.msh")
     # gmsh.clear()
