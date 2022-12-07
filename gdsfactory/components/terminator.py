ce2c39bf607a3cf5f44662ce737e3d8b6b057ade
Simon Bilodeau <sb30@princeton.edu>
2022-12-07 05:16:26 +0000

    waveguide termination component
    Former-commit-id: 1a3cde5a6b035a275e68711625de2b755dc85a57 [formerly b837674c74ba23751ce12a648ce9e477977f5e77 [formerly 589ddaa2924ac07bf7c631a7bfc2c89e09241cd3] [formerly 5377f75a9ee8d92f165d7a1c9837459e3de61f00 [formerly 536281b50d538615b671be79310a5bef211bbd61]]]
    Former-commit-id: 5377f75a9ee8d92f165d7a1c9837459e3de61f00
    Former-commit-id: 9f4af757784416034fe901d1930654e39f3cf976 [formerly cdae0e2c35deb2e8bf6c4761ed0e16e7ce6dd225]
    Former-commit-id: 1dd88c1153826389b55a3c32d95e718e3f0fffc3

---
+++ gdsfactory/components/terminator.py
@@ -0,0 +1,48 @@
+from __future__ import annotations
+
+from typing import List, Optional
+
+import gdsfactory as gf
+from gdsfactory.component import Component
+from gdsfactory.components.taper_cross_section import taper_cross_section
+from gdsfactory.cross_section import CrossSection, strip
+from gdsfactory.types import LayerSpec
+
+
+@gf.cell
+def terminator(
+    length: Optional[float] = 50,
+    input_xs: Optional[CrossSection] = strip,
+    tapered_xs: Optional[CrossSection] = None,
+    doping_layers: List[LayerSpec] = ["NPP"],
+    **kwargs,
+) -> gf.Component:
+    """Doped narrow taper to terminate waveguides.
+
+    Args:
+        input_xs: input cross-section.
+        tapered_xs: cross-section at the end of the termination (by default, input_xs with width 200 nm)
+        doping_layers: doping layers to superimpose on the taper. Default N++.
+        **kwargs: taper arguments
+    """
+    c = Component()
+
+    tapered_xs = tapered_xs if tapered_xs else gf.partial(input_xs, width=0.2)
+
+    taper = c << gf.get_component(
+        taper_cross_section,
+        length=length,
+        cross_section1=input_xs,
+        cross_section2=tapered_xs,
+    )
+
+    for layer in doping_layers:
+        c << gf.components.bbox(bbox=taper.bbox, layer=layer)
+
+    return c
+
+
+if __name__ == "__main__":
+
+    c = terminator()
+    c.show(show_ports=True)
