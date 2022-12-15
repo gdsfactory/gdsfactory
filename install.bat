639515aff65326023b63839507e963aa69a02ab7
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2022-12-15 11:25:26 -0800

    Bump version: 6.9.0 → 6.10.0
    Former-commit-id: 947d088ac0b214c1ae1d8c1250e49fcfb98a7061 [formerly e8b662cdf186fa8a66550185b2fc786c9d2a6b93]
    Former-commit-id: 8b7acd18ca1226f530c89aeda8923b4d557294e9

--- install.bat
+++ install.bat
@@ -2,7 +2,7 @@
 set PIP_FIND_LINKS="https://whls.blob.core.windows.net/unstable/index.html"
 pip install lytest simphony sax jax sklearn klayout devsim
 pip install "jaxlib[cuda111]" -f https://whls.blob.core.windows.net/unstable/index.html --use-deprecated legacy-resolver
-pip install gdsfactory==6.9.0
+pip install gdsfactory==6.10.0
 gf tool install
 
 cd ..\condabin
