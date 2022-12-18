a94be5c16fb4367d5377db8112ce7a395188de13
simbilod <sb30@princeton.edu>
2022-12-18 15:58:15 -0800

    EME works

--- gdsfactory/simulation/eme/get_meow_simulation.py
+++ gdsfactory/simulation/eme/get_meow_simulation.py
@@ -1,6 +1,9 @@
 import meow as mw
+import numpy as np
 
+from gdsfactory.simulation.gmsh.parse_layerstack import list_unique_layerstack_z
 from gdsfactory.tech import LAYER, LayerStack
+from gdsfactory.types import ComponentSpec
 
 """Conversion between gdsfactory material names and meow materials class."""
 gdsfactory_to_meow_materials = {
@@ -13,7 +16,7 @@ def add_global_layers(component, layerstack):
     """Adds bbox polygons for global layers."""
     bbox = component.bbox
     for layername, layer in layerstack.layers.items():
-        if layer["layer"] == LAYER.WAFER:
+        if layer.layer == LAYER.WAFER:
             component.add_ref(
                 gf.components.box(bbox[0, 0], bbox[0, 1], bbox[1, 0], bbox[1, 1])
             )
@@ -39,6 +42,127 @@ def layerstack_to_extrusion(layerstack: LayerStack):
     return extrusions
 
 
+def get_eme_cells(
+    component,
+    extrusion_rules,
+    num_cells: int = 10,
+    xspan: float = 2.0,
+    xoffset: float = 0,
+    xres: int = 100,
+    yspan: float = 2.0,
+    yoffset: float = 0,
+    yres: int = 100,
+):
+    """Get meow cells from extruded component.
+
+    Arguments:
+        structs: meow structs
+        extrusion rules: from layerstack_to_extrusion
+        num_cells: number of slices
+        xspan: size of the horizontal cross-sectional simulation region
+        xoffset: center of the horizontal cross-sectional simulation region
+        xres: number of mesh points for the horizontal cross-sectional simulation region
+        yspan: size of the vertical cross-sectional simulation region
+        yoffset: center of the vertical cross-sectional simulation region
+        yres: number of mesh points for the vertical cross-sectional simulation region
+    """
+    structs = mw.extrude_gds(component, extrusion_rules)
+    bbox = component.bbox
+    Ls = [np.diff(bbox[:, 0]).item() / num_cells for _ in range(num_cells)]
+    return mw.create_cells(
+        structures=structs,
+        mesh=mw.Mesh2d(
+            x=np.linspace(xoffset - xspan / 2, xoffset + xspan / 2, xres),
+            y=np.linspace(yoffset - yspan / 2, yoffset + yspan / 2, yres),
+        ),
+        Ls=Ls,
+    )
+
+
+def eme_calculation(
+    component: ComponentSpec,
+    layerstack: LayerStack,
+    num_modes: int = 4,
+    wavelength: float = 1.55,
+    temperature: float = 25.0,
+    num_cells: int = 10,
+    xspan: float = None,
+    xoffset: float = 0.0,
+    xres: int = 100,
+    yspan: float = None,
+    yoffset: float = 0.0,
+    yres: int = 100,
+    validate_component: bool = True,
+):
+    """Computes multimode 2-port S-parameters for a gdsfactory component, assuming port 1 is at the left boundary and port 2 at the right boundary.
+
+    Arguments:
+        component: gdsfactory component
+        layerstack: gdsfactory layerstack
+        num_modes: number of modes to compute for the eigenmode expansion
+        wavelength: wavelength in microns (for FDE, and for material properties)
+        temperature: temperature in C (for material properties)
+        num_cells: number of component slices along the propagation direction for the EME
+        xspan: size of the horizontal cross-sectional simulation region
+        xoffset: center of the horizontal cross-sectional simulation region
+        xres: number of mesh points for the horizontal cross-sectional simulation region
+        yspan: size of the vertical cross-sectional simulation region
+        yoffset: center of the vertical cross-sectional simulation region
+        yres: number of mesh points for the vertical cross-sectional simulation region
+        validate_component: whether raise errors if the component ports are of the wrong number and orientation
+
+    Returns:
+        S-parameters in form o1@0:o2@0, etc.
+    """
+    # Check component validity
+    if validate_component:
+        optical_ports = [
+            port
+            for portname, port in component.ports.items()
+            if port.port_type == "optical"
+        ]
+        if len(optical_ports) != 2:
+            raise ValueError(
+                "Component provided to MEOW does not have exactly 2 optical ports."
+            )
+        elif component.ports["o1"].orientation != 180:
+            raise ValueError("Component port o1 does not face westward (180 deg).")
+        elif component.ports["o2"].orientation != 0:
+            raise ValueError("Component port o2 does not face eastward (0 deg).")
+
+    # Preprocess component and layerstack
+    component = add_global_layers(component, layerstack)
+    extrusion_rules = layerstack_to_extrusion(layerstack)
+    xspan = xspan or component.bbox[0, 1] + 2
+    zs = list_unique_layerstack_z(layerstack)
+    yspan = yspan or np.max(zs) - np.min(zs)
+
+    # Get EME cells
+    cells = get_eme_cells(
+        component,
+        extrusion_rules,
+        num_cells=num_cells,
+        xspan=xspan,
+        xoffset=xoffset,
+        xres=xres,
+        yspan=yspan,
+        yoffset=yoffset,
+        yres=yres,
+    )
+
+    # Get EME cross-sections
+    env = mw.Environment(wl=wavelength, T=temperature)
+    css = [mw.CrossSection(cell=cell, env=env) for cell in cells]
+
+    # Compute modes
+    modes = [mw.compute_modes(cs, num_modes=num_modes) for cs in css]
+
+    # Compute EME
+    S, port_map = mw.compute_s_matrix(modes)
+
+    return S, port_map
+
+
 if __name__ == "__main__":
 
     import gdsfactory as gf
@@ -60,4 +184,4 @@ if __name__ == "__main__":
         }
     )
 
-    print(layerstack_to_extrusion(filtered_layerstack))
+    print(eme_calculation(component=c, layerstack=filtered_layerstack))
