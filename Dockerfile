b94069b28994e3bd69236763c6daf51c4ff6845e
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-01-04 14:40:41 -0800

    Bump version: 6.18.0 → 6.18.1
    Former-commit-id: ccf15abbc0a97915c24e834d6517592a9031ec69 [formerly 5398d681fd3e40763a1a85c98097699e307f95e2 [formerly 98687c0f7434dfc0a23c6fcd879557fa7168a068] [formerly ac50525cb330f5051b72119d27e053072e9fd611 [formerly 1cd12bc641d5fb03c4cd8074103f92a2c69e3667]]]
    Former-commit-id: ac50525cb330f5051b72119d27e053072e9fd611
    Former-commit-id: 52890d868eb841217fcd05bc414d556b39bf6ef0 [formerly 1f9bc1f077b23c7d9fee67bf8cf3c3074c2125c7]
    Former-commit-id: 189cae3b62969fe8e77348f38d340a37edadca75

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,1 @@
-FROM joamatab/gdsfactory:6.18.0
+FROM joamatab/gdsfactory:6.18.1
--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -27,7 +27,7 @@ import omegaconf
 from loguru import logger
 from omegaconf import OmegaConf
 
-__version__ = "6.18.0"
+__version__ = "6.18.1"
 PathType = Union[str, pathlib.Path]
 
 home = pathlib.Path.home()
