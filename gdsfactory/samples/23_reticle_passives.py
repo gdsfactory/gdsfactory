8a98013d0e3e585684dae5351a0ec866e82c7a9f
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-01-06 12:16:06 -0800

    fix tests
    Former-commit-id: cbaf79b5c161236c1c6e594c7a1d55b5a68bad4e [formerly d1b40e3f26e4f835dd5d307322e2dae6c958cb4b]
    Former-commit-id: b4380ae82f33a16183f8e7161480cdb5b0da695c

--- gdsfactory/samples/23_reticle_passives.py
+++ gdsfactory/samples/23_reticle_passives.py
@@ -2,20 +2,16 @@
 
 from __future__ import annotations
 
+import pandas as pd
+
 import gdsfactory as gf
-from gdsfactory.labels import add_label_yaml
-from gdsfactory.read.labels import add_port_markers
 from gdsfactory.types import Component
 
 
 def mzi_te(**kwargs) -> Component:
     gc = gf.c.grating_coupler_elliptical_tm()
     c = gf.c.mzi_phase_shifter_top_heater_metal(delta_length=40)
-    c = gf.routing.add_fiber_single(
-        c, get_input_label_text_function=None, grating_coupler=gc
-    )
-    add_label_yaml(c)
-    c = c.rotate(-90)
+    c = gf.routing.add_fiber_single(c, grating_coupler=gc)
     return c
 
 
@@ -25,16 +21,13 @@ def test_mask() -> Component:
             mzi_te(),
             mzi_te(),
             gf.functions.rotate(mzi_te),
-            add_label_yaml(
-                gf.functions.mirror(gf.routing.add_fiber_single(gf.components.mmi1x2))
-            ),
         ]
     )
     gdspath = c.write_gds("mask.gds")
-    csvpath = gf.labels.write_labels.write_labels_gdstk(
-        gdspath, prefix="component_name"
-    )
-    return add_port_markers(gdspath=gdspath, csvpath=csvpath, marker_size=40)
+    csvpath = gf.labels.write_labels.write_labels_gdstk(gdspath)
+    labels = pd.read_csv(csvpath)
+    assert len(labels) == 11, len(labels)
+    return c
 
 
 if __name__ == "__main__":
