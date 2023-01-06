c4c6a5b2484d0dbac18be610bd59d3f0b67a4960
Alex Sludds <asludds@mit.edu>
2023-01-06 12:47:42 -0500

    Function for layer_colors -> .lyp file
    I've added a function (which I mostly stole from PHIDL), which takes in a gdsfactory layer_colors object and converts it to a .lyp file.
    I find .lyp files really useful for sharing gds files with folks who don't use gdsfactory.

--- gdsfactory/layers.py
+++ gdsfactory/layers.py
@@ -350,6 +350,91 @@ def load_lyp(filepath: Path) -> LayerColors:
                     _add_layer(member, lys, shorten_names=False)
     return lys
 
+def write_lyp(filename, layer_colors):
+    """Creates a KLayout .lyp Layer Properties file from a gdsfactory LayerMap"""
+    stipple_default = ["I2", "I5", "I9", "I17", "I19", "I22", "I33", "I38"]
+    stipple_count = 0
+
+    if filename[-4:] != ".lyp":
+        filename = filename + ".lyp"
+
+    # Opening file for writing
+    with open("%s" % filename, "w+") as f:
+
+        # Writing header string
+        f.write('<?xml version="1.0" encoding="utf-8"?>\n')
+
+        # Writing layer properties opener
+        f.write("<layer-properties>\n")
+
+        for layer in layer_colors.keys():
+            print(layer)
+            print(layer_colors[layer])
+            # Extracting information from dictionary layer by layer
+            gds_layer = layer_colors[layer].gds_layer
+            gds_datatype = layer_colors[layer].gds_datatype
+            color = layer_colors[layer].color
+
+            name = f"{str(gds_layer)}/{str(gds_datatype)} - " + layer
+
+            # Setting stipple or 'dither'
+            try:
+                dither = layer.dither
+            except:
+                dither = None
+            if dither is None:
+                dither = stipple_default[stipple_count]
+                stipple_count = (stipple_count + 1) % len(stipple_default)
+            elif dither[0] != "I":
+                raise ValueError("""Stipple must begin with an I""")
+            elif int(dither[1 : len(dither)]) < 0:
+                raise ValueError("""Stipple index cannot be less than 0""")
+            elif int(dither[1 : len(dither)]) > 46:
+                raise ValueError("""Stipple index cannot be greater than 46""")
+            else:
+                pass
+
+            # Writing properties header for speciic layer
+            f.write(" <properties>\n")
+            # Writing line to specify frame colour
+            f.write("  <frame-color>%s</frame-color>\n" % color)
+            # Writing line to specify fill colour
+            f.write("  <fill-color>%s</fill-color>\n" % color)
+            #            # Writing line to specify brightness (value between [-255, 255])
+            #            f.write('  <frame-brightness>%s</frame-brightness>\n  <fill-brightness>%s</fill-brightness>\n' % (int(brightness), int(brightness)))
+            frame_brightness = -25
+            f.write(
+                "  <frame-brightness>%s</frame-brightness>\n" % (int(frame_brightness))
+            )
+            # Writing line to specify dither pattern
+            f.write("  <dither-pattern>%s</dither-pattern>\n" % dither)
+            # Writing lines to specify line style
+            f.write("  <line-style/>\n")
+            # Writing line to specify validity
+            f.write("  <valid>true</valid>\n")
+            # Writing line to specify visibility
+            f.write("  <visible>true</visible>\n")
+            # Writing line to specify transparency
+            f.write("  <transparent>false</transparent>\n")
+            # Writing line to specify width
+            f.write("  <width/>\n")
+            # Writing line to specity markedness
+            f.write("  <marked>false</marked>\n")
+            # Writing line to specify xfill
+            f.write("  <xfill>false</xfill>\n")
+            # Writing line to specify animation
+            f.write("  <animation>0</animation>\n")
+            # Writing line to specify layer name
+            f.write("  <name>%s</name>\n" % name)
+            # Writing line to specify source
+            f.write(f"  <source>{str(gds_layer)}/{str(gds_datatype)}@1</source>\n")
+            # Writing properties closer for specific layer
+            f.write(" </properties>\n")
+
+        # Writing layer properties trailer
+        f.write("</layer-properties>\n")
+
+
 
 load_lyp_generic = partial(load_lyp, filepath=layer_path)
 
