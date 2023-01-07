5fb47a9f7736d65a6bd87bbd0f6730a5d8f9ac47
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-01-06 16:37:48 -0800

    move geometry.get_xsection_script to generic_tech.get_klayout_pyxs
    Former-commit-id: 6ce7d0411c0f0ad7870369a3f38410cb173ebf2b [formerly 7fad5b1adb1fab467c54ea60764673b187d976b8]
    Former-commit-id: df6a5b2784921a0c879eaec9cffe4681a5f5b8fc

--- gdsfactory/generic_tech/get_klayout_pyxs.py
+++ gdsfactory/generic_tech/get_klayout_pyxs.py
@@ -1,5 +1,12 @@
+"""write xsection script for KLayout plugin.
+
+https://gdsfactory.github.io/klayout_pyxs/DocGrow.html
+
+"""
+
 from __future__ import annotations
 
+import pathlib
 from typing import Tuple
 
 from gdsfactory.generic_tech import LAYER
@@ -11,7 +18,7 @@ def layer_to_string(layer: Tuple[int, int]) -> str:
     return f"{layer[0]}/{layer[1]}"
 
 
-def get_xsection_script(
+def get_klayout_pyxs(
     t_box=1.0,
     t_slab=90 * nm,
     t_si=0.22,
@@ -55,7 +62,7 @@ def get_xsection_script(
     layer_m3=LAYER.M3,
     layer_open=LAYER.PADOPEN,
 ) -> str:
-    """Returns xsection script for klayout_pyxs plugin.
+    """Returns klayout_pyxs plugin script to show chip cross-section in klayout.
 
     https://gdsfactory.github.io/klayout_pyxs/DocGrow.html
 
@@ -238,5 +245,50 @@ output("311/0", ox_m3)
 
 
 if __name__ == "__main__":
-    s = get_xsection_script()
-    print(s)
+    script = get_klayout_pyxs(
+        t_box=2.0,
+        t_slab=110 * nm,
+        t_si=220 * nm,
+        t_ge=400 * nm,
+        t_nitride=400 * nm,
+        h_etch1=0.07,
+        h_etch2=0.06,
+        h_etch3=0.09,
+        t_clad=0.6,
+        t_m1=0.5,
+        t_m2=0.5,
+        t_m3=2.0,
+        gap_m1_m2=0.6,
+        gap_m2_m3=0.3,
+        t_heater=0.1,
+        gap_oxide_nitride=0.82,
+        t_m1_oxide=0.6,
+        t_m2_oxide=2.0,
+        t_m3_oxide=0.5,
+        layer_wg=LAYER.WG,
+        layer_fc=LAYER.SLAB150,
+        layer_rib=LAYER.SLAB90,
+        layer_n=LAYER.N,
+        layer_np=LAYER.NP,
+        layer_npp=LAYER.NPP,
+        layer_p=LAYER.P,
+        layer_pp=LAYER.PP,
+        layer_ppp=LAYER.PPP,
+        layer_PDPP=LAYER.GEP,
+        layer_nitride=LAYER.WGN,
+        layer_Ge=LAYER.GE,
+        layer_GePPp=LAYER.GEP,
+        layer_GeNPP=LAYER.GEN,
+        layer_viac=LAYER.VIAC,
+        layer_viac_slot=LAYER.VIAC,
+        layer_m1=LAYER.M1,
+        layer_mh=LAYER.HEATER,
+        layer_via1=LAYER.VIA1,
+        layer_m2=LAYER.M2,
+        layer_via2=LAYER.VIA2,
+        layer_m3=LAYER.M3,
+        layer_open=LAYER.PADOPEN,
+    )
+
+    script_path = pathlib.Path(__file__).parent.absolute() / "xsection_planarized.pyxs"
+    script_path.write_text(script)
