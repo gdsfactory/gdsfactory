f2d0f20f96cc1f5b4328bb2948670af082bad516
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-01-06 16:50:51 -0800

    fix lumerical script

--- gdsfactory/technology/__init__.py
+++ gdsfactory/technology/__init__.py
@@ -1,7 +1,10 @@
 from gdsfactory.technology.layer_map import lyp_to_dataclass
 from gdsfactory.technology.layer_stack import LayerLevel, LayerStack
 from gdsfactory.technology.layer_views import LayerView, LayerViews
-from gdsfactory.technology.simulation_settings import SimulationSettingsLumericalFdtd
+from gdsfactory.technology.simulation_settings import (
+    SIMULATION_SETTINGS_LUMERICAL_FDTD,
+    SimulationSettingsLumericalFdtd,
+)
 
 __all__ = [
     "LayerView",
@@ -10,4 +13,5 @@ __all__ = [
     "LayerStack",
     "lyp_to_dataclass",
     "SimulationSettingsLumericalFdtd",
+    "SIMULATION_SETTINGS_LUMERICAL_FDTD",
 ]
