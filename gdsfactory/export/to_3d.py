0c66e0280794d62c19be13bc79e97c6f8857643e
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-01-08 20:52:14 -0800

    fix docs
    Former-commit-id: 74baef1ec084001861bb947398280817a0af12ff [formerly 9ebb0a601d20c4dad81be75bd8ec6bc8ba92022b]
    Former-commit-id: bfffeeead006fc0a96768697bce414e62957b490

--- gdsfactory/export/to_3d.py
+++ gdsfactory/export/to_3d.py
@@ -26,7 +26,7 @@ def to_3d(
         exclude_layers: layers to exclude.
 
     """
-    from gdsfactory.pdk import get_layer_stack, get_layer_views
+    from gdsfactory.pdk import get_active_pdk, get_layer_stack, get_layer_views
 
     try:
         from trimesh.creation import extrude_polygon
@@ -42,6 +42,7 @@ def to_3d(
     layer_to_thickness = layer_stack.get_layer_to_thickness()
     layer_to_zmin = layer_stack.get_layer_to_zmin()
     exclude_layers = exclude_layers or ()
+    # layers = layer_views.layer_map.values()
 
     has_polygons = False
 
@@ -57,8 +58,7 @@ def to_3d(
             color_rgb = [
                 c / 255 for c in layer_view.fill_color.as_rgb_tuple(alpha=False)
             ]
-            opacity = layer_view.get_alpha()
-
+            # opacity = layer_view.get_alpha()
             # print(layer, height, zmin, opacity, layer_view.visible)
 
             if zmin is not None and layer_view.visible:
@@ -66,14 +66,14 @@ def to_3d(
                     p = shapely.geometry.Polygon(polygon.points)
                     mesh = extrude_polygon(p, height=height)
                     mesh.apply_translation((0, 0, zmin))
-                    mesh.visual.face_colors = (*color_rgb, opacity)
+                    mesh.visual.face_colors = (*color_rgb, 0.5)
                     scene.add_geometry(mesh)
                     has_polygons = True
 
     if not has_polygons:
         raise ValueError(
             f"{component.name!r} does not have polygons defined in the "
-            "layer_stack or layer_views for the active Pdk {get_active_pdk().name!r}"
+            f"layer_stack or layer_views for the active Pdk {get_active_pdk().name!r}"
         )
     return scene
 
@@ -82,7 +82,10 @@ if __name__ == "__main__":
     import gdsfactory as gf
 
     # c = gf.components.taper_strip_to_ridge()
-    c = gf.components.straight_heater_metal(length=40)
+    c = gf.Component()
+    c << gf.components.straight_heater_metal(length=40)
+    c << gf.c.rectangle(layer=(113, 0))
+
     c.show()
-    s = to_3d(c)
-    # s.show()
+    s = c.to_3d()
+    s.show()
