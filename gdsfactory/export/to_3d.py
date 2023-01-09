5a80a95bcdad2b6141a61a415cf0a3dd931502cb
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-01-08 16:27:13 -0800

    fix pre-commit
    Former-commit-id: d7de86ce45681754ac310dfee66a14df3e39c50f [formerly db22cf0fcc72d32c06ef7b8194a99f6aa505caae [formerly c31495bedf212880c2a881ecea6058c05a9aa4d4] [formerly 6da0f76f1c976f1129c1dae05e0f59818dba968d [formerly 922ba7e66bf16370a28de559cc204d84e33c5de1]]]
    Former-commit-id: 6da0f76f1c976f1129c1dae05e0f59818dba968d
    Former-commit-id: ba08dc1e0c970cafa698fbe8b60fc58a235202c0 [formerly c6af10ca812a1c610a4f04556c8027394618cc3e]
    Former-commit-id: 9a5e705b8fcb371d4829331d29158da082245efe

--- gdsfactory/export/to_3d.py
+++ gdsfactory/export/to_3d.py
@@ -54,7 +54,9 @@ def to_3d(
             height = layer_to_thickness[layer]
             zmin = layer_to_zmin[layer]
             layer_view = layer_views.get_from_tuple(layer)
-            color_rgb = [c / 255 for c in layer_view.fill_color.as_rgb_tuple(alpha=False)]
+            color_rgb = [
+                c / 255 for c in layer_view.fill_color.as_rgb_tuple(alpha=False)
+            ]
             opacity = layer_view.get_alpha()
 
             # print(layer, height, zmin, opacity, layer_view.visible)
