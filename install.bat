bf175e21240c99ef56518d9dde4853fe78a2a117
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-01-14 11:26:39 -0800

    install database
    Former-commit-id: fa1428991e9ff97621124ab784eebedb78f66700 [formerly 6aa73b427770d713e617044db82faa1a03333aae]
    Former-commit-id: 07360df58d40901d6e8ba8c2481abfbb70adc65a

--- install.bat
+++ install.bat
@@ -2,11 +2,12 @@
 set PIP_FIND_LINKS="https://whls.blob.core.windows.net/unstable/index.html"
 pip install sax jax sklearn
 pip install "jaxlib[cuda111]" -f https://whls.blob.core.windows.net/unstable/index.html --use-deprecated legacy-resolver
-pip install gdsfactory[full,gmsh,tidy3d,devsim,meow,ray]==6.19.4
+pip install gdsfactory[full,gmsh,tidy3d,devsim,meow,ray,database]==6.19.4
 gf tool install
 
 cd ..\condabin
 call conda activate
+call conda install -c conda-forge slepc4py=*=complex* -y
 call conda install -c conda-forge git spyder -y
 if exist "%USERPROFILE%\Desktop\gdsfactory" (goto SKIP_INSTALL)
 cd %USERPROFILE%\Desktop
