19fd77b0aec481b97d2ecae837b66f2f5ac1a04e
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-01-17 17:11:20 -0800

    delete fixme and move conda specific files to conda folder
    Former-commit-id: 4047db9a77140903f9f0553a034177f7b907148f [formerly e6a0c4b7a8cd4fa76e80906d46145acd94a2f836]
    Former-commit-id: 0423ea8252eb2691baaaec9d20930a76c46bdd3c

--- fixme/fixed/path_smooth.py
+++
@@ -1,7 +0,0 @@
-import gdsfactory as gf
-
-p = gf.path.smooth(points=[(0, 0), (0, 1000), (1, 10000)])
-
-# IndexError: index -1 is out of bounds for axis 0 with size 0
-c = p.extrude(cross_section="strip")
-c.show()
