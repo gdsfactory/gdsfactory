5499f6212d41dd85a653fcab083bae7ed5431c87
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-01-18 12:08:23 -0800

    Bump version: 6.23.1 → 6.23.2

--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -27,7 +27,7 @@ import omegaconf
 from loguru import logger
 from omegaconf import OmegaConf
 
-__version__ = "6.23.1"
+__version__ = "6.23.2"
 PathType = Union[str, pathlib.Path]
 
 home = pathlib.Path.home()
