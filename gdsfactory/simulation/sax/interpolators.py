7285313a22311bc363d8cd571a3172dfac8655cf
simbilod <sb30@princeton.edu>
2023-01-23 09:54:44 -0800

    push work to remote
    Former-commit-id: f4e9df4d12edcbf4d079dc8409b188444c7af807 [formerly a05c3c8e3e78d6308136c60688d1e97e49605276 [formerly e2289fb8951421fdff78dcd02c13c5efdf677542] [formerly f7c8981566becba1174e24bf9a45926cc469faf9 [formerly 1a358411d70c81060d657b80ce4083dffd2dce92]]]
    Former-commit-id: f7c8981566becba1174e24bf9a45926cc469faf9
    Former-commit-id: 14659c5990736f74b21263e3b8a16e8bd0439e44 [formerly 7f1ccf0c2f38600031d32dc470a507464e42a6a6]
    Former-commit-id: 0b6ed12a8903d6fc2b11bc2d861b0461db4ffe43

--- gdsfactory/simulation/sax/interpolators.py
+++ gdsfactory/simulation/sax/interpolators.py
@@ -5,31 +5,38 @@ import jax.numpy as jnp
 
 
 def nd_interpolation(_grid, output_vector):
-    """Return JAX N-D interpolator given a N-D input and 1-D output vector."""
+    """Return JAX N-D interpolator given a M-D input and 1-D output vector."""
     _data = jnp.asarray(output_vector).reshape(len(_grid[i]) for i in range(len(_grid)))
 
-    @jax.jit
+    # @jax.jit
     def _get_coordinate(arr1d: jnp.ndarray, value: jnp.ndarray):
         return jnp.interp(value, arr1d, jnp.arange(arr1d.shape[0]))
 
-    @jax.jit
+    # @jax.jit
     def _get_coordinates(arrs1d: List[jnp.ndarray], values: jnp.ndarray):
-        # don't use vmap as arrays in arrs1d could have different shapes...
         return jnp.array([_get_coordinate(a, v) for a, v in zip(arrs1d, values)])
 
-    @jax.jit
+    # @jax.jit
     def interp_output(params):
         coords = _get_coordinates(_grid, params)
-        print(jnp.shape(coords), jnp.shape(_data))
         return jax.scipy.ndimage.map_coordinates(_data, coords, 1, mode="nearest")
 
     return interp_output
 
 
-def nd_nd_interpolation(input_vectors, output_labels, output_vectors):
-    """Return JAX N-D interpolator given a N-D input and N-D output vector."""
+def nd_nd_interpolation(input_vectors, output_vectors):
+    """Return JAX N-D interpolator given a N-D input and M-D output vector."""
     _grid = [jnp.sort(jnp.unique(input_vector)) for input_vector in input_vectors.T]
-    return {
-        output_label: nd_interpolation(_grid, jnp.asarray(output_vector))
-        for output_label, output_vector in zip(output_labels, output_vectors.T)
-    }
+    return [
+        nd_interpolation(_grid, jnp.asarray(output_vector))
+        for output_vector in output_vectors.T
+    ]
+
+
+# def nd_nd_interpolation(input_vectors, output_labels, output_vectors):
+#     """Return JAX N-D interpolator given a N-D input and M-D output vector."""
+#     _grid = [jnp.sort(jnp.unique(input_vector)) for input_vector in input_vectors.T]
+#     return {
+#         output_label: nd_interpolation(_grid, jnp.asarray(output_vector))
+#         for output_label, output_vector in zip(output_labels, output_vectors.T)
+#     }
