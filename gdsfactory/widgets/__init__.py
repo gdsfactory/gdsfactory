f402072de17dcaef01d128d9023c90ba80697d75
Troy Tamas <troy.tamas@rockleyphotonics.com>
2023-01-28 10:06:50 -0800

    removing copyright notice

--- gdsfactory/widgets/__init__.py
+++
@@ -1,6 +0,0 @@
-###################################################################################################################
-# PROPRIETARY AND CONFIDENTIAL
-# THIS SOFTWARE IS THE SOLE PROPERTY AND COPYRIGHT (c) 2022 OF ROCKLEY PHOTONICS LTD.
-# USE OR REPRODUCTION IN PART OR AS A WHOLE WITHOUT THE WRITTEN AGREEMENT OF ROCKLEY PHOTONICS LTD IS PROHIBITED.
-# RPLTD NOTICE VERSION: 1.1.1
-###################################################################################################################
