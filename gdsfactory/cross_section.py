71bf19f7234dd54adbb321ebe04304076ca3147e
Troy Tamas <troy.tamas@rockleyphotonics.com>
2023-02-02 16:49:38 -0800

    getting all-angle routing running fully and more generally
    Former-commit-id: b6cbc4eff5e1f372b77600e2681d1971b76d2c1c [formerly a8ee15027fdab7f7f09af8536df629c2ab6c069d]
    Former-commit-id: e84e2ea79ecae9a3be327c0e52e3bec066a20a67

--- gdsfactory/cross_section.py
+++ gdsfactory/cross_section.py
@@ -382,7 +382,16 @@ def cross_section(
     )
 
 
-strip = partial(cross_section, info={"separation": 3.0})
+def strip(width: Union[Callable, float] = 0.5, **kwargs) -> CrossSection:
+    """
+    Strip cross-section.
+
+    """
+    cs = cross_section(width=width, **kwargs)
+    cs.info["separation"] = width + 3.0
+    return cs
+
+
 strip_auto_widen = partial(strip, width_wide=0.9, auto_widen=True)
 strip_no_pins = partial(
     strip, add_pins=None, add_bbox=None, cladding_layers=None, cladding_offsets=None
