f1a4fa50b7358dba2a9b3b466f0cd60e6b49b8d5
simbilod <sb30@princeton.edu>
2023-02-04 17:57:43 -0800

    model building backend
    Former-commit-id: 877d7eea2e2555bb25a37ba6c0a8e50e247805b6 [formerly 04e43b5cc2419efd09ceeb4d02c4a952e32f69ef]
    Former-commit-id: 7824c1a8c97758d645e2a6feb1dac280beab76ce

--- gdsfactory/simulation/sax/build_model.py
+++ gdsfactory/simulation/sax/build_model.py
@@ -14,7 +14,7 @@ from gdsfactory.types import PortSymmetries
 class Model:
     def __init__(
         self,
-        component: callable,
+        trainable_component: callable,
         layerstack: LayerStack,
         trainable_parameters: Dict[
             str, Union[LayerStackThickness, NamedParameter]
@@ -39,7 +39,7 @@ class Model:
         is solver-dependent, and hence resides in child classes.
 
         Attributes:
-            component: component associated with model
+            trainable_component: callable wrapping component associated with model
             layerstack: complete layerstack associated with model
             trainable parameters: parameters that are model features that need to be learned (e.g. width, layer thicknesses, wavelength, materials properties)
             non_trainable_parameters: parameters for input_dict whose effect on S-parameters is known and does not need to be trained (e.g. length for a waveguide)
@@ -50,7 +50,7 @@ class Model:
                 e.g. {"o1@0,o1@0": ["o2@0,o2@0", "o3@0,o3@0", "o4@0,o4@0"]} means that S22 = S33 = S44 are the same as S11,
                 and hence the output vector for model training will only contain the key S11 to reduce the number of variables to fit.
         """
-        self.component = component
+        self.component = trainable_component
         self.layerstack = layerstack
         self.trainable_parameters = trainable_parameters or {}
         self.non_trainable_parameters = non_trainable_parameters or {}
@@ -60,7 +60,9 @@ class Model:
         # Extract inputs and outputs vector size data
         self.size_inputs = len(self.trainable_parameters)
         self.num_ports = len(
-            component(self.get_nominal_dict()).get_ports_list(port_type="optical")
+            trainable_component(self.get_nominal_dict()).get_ports_list(
+                port_type="optical"
+            )
         )
 
         # Extract input and output vector label data
@@ -124,38 +126,8 @@ class Model:
     Generate training data
     """
 
-    # def define_output_vector_labels(self):
-    #     """Uses number of component ports, number of modes solved for, and port_symmetries to define smallest output vector."""
-    #     output_vector_labels_iter = itertools.product(
-    #         range(1, self.num_ports + 1),
-    #         range(self.num_modes),
-    #         range(1, self.num_ports + 1),
-    #         range(self.num_modes),
-    #     )
-    #     output_vector_labels = []
-    #     for output_label in output_vector_labels_iter:
-    #         output_key1 = f"o{output_label[0]}@{output_label[1]}"
-    #         output_key2 = f"o{output_label[2]}@{output_label[3]}"
-    #         if output_key1 != output_key2:
-    #             output_key = f"{output_key1},{output_key2}"
-    #             output_vector_labels.append(output_key)
-
-    #     if self.port_symmetries:
-    #         for value_list in self.port_symmetries.values():
-    #             for value in value_list:
-    #                 output_vector_labels.remove(value)
-
-    #     return output_vector_labels
-
-    def get_results(self, input_dict):
-        """To be overridden by child classes.
-
-        Returns output vector requiring a compact model that is then used by get_Sparameters.
-        """
-        return None
-
-    def get_data(self):
-        """Retrieve the input and output data for training the model by getting results on all parameter input combinations."""
+    def get_model_input_output(self):
+        """Retrieve the input and output data for training the model by getting results on all trainable parameter input combinations."""
         ranges_dict = {
             name: parameter.arange()
             for name, parameter in self.trainable_parameters.items()
@@ -168,7 +140,7 @@ class Model:
             input_dict = dict(
                 zip(ranges_dict.keys(), [float(value) for value in values])
             )
-            new_inputs, output_vector = self.get_results(input_dict)
+            new_inputs, output_vector = self.outputs_from_inputs(input_dict)
             input_vector = list(values)
             input_vector.extend(iter(new_inputs))
             input_vectors.append(input_vector)
@@ -185,14 +157,29 @@ class Model:
 
     def set_nd_nd_interp(self):
         """Returns ND-ND interpolator."""
-        input_vectors, output_vectors = self.get_data()
+        input_vectors, output_vectors = self.get_model_input_output()
         self.inference = nd_nd_interpolation(input_vectors, output_vectors)
 
-    # """
-    # Compute S-parameters from the fits.
-    # """
+    """
+    Validating data.
+    """
 
-    # def inference(self, input_dict):
-    #     """Returns results from interpolator."""
+    def validate(self, num_samples=1):
+        """Validates the model by calculating random points within the interpolation range, and comparing to predictions."""
+        calculated_outputs = []
+        inferred_outputs = []
+        validation_inputs = []
+        for _index in range(num_samples):
+            validation_inputs_local = self.get_random_dict()
+            validation_inputs.append(validation_inputs_local)
+            calculated_outputs.append(
+                self.outputs_from_inputs(validation_inputs_local)[1]
+            )
+            inferred_outputs_local = []
+            for mode in range(self.num_modes):
+                inferred_outputs_local.append(
+                    self.inference[mode](validation_inputs_local.values())
+                )
+            inferred_outputs.append(inferred_outputs_local)
 
-    #     return None
+        return validation_inputs, calculated_outputs, inferred_outputs
