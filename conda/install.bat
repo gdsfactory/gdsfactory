760d9cda2ab6e38fb6d0a54581e0e1eb5ac7a359
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-02-09 14:08:16 -0800

    fix windows installer
    Former-commit-id: fefc90b90c9d650ed68b7179ec0e1f30452946c3 [formerly eb061be3641d4827a2f8aa1d53d19cfdca2536ec]
    Former-commit-id: b12998dc9df9a9771d16e6ae2a69bf81bc154731

--- conda/install.bat
+++ conda/install.bat
@@ -1,14 +1,19 @@
 
 set PIP_FIND_LINKS="https://whls.blob.core.windows.net/unstable/index.html"
-pip install sax jax sklearn
-pip install "jaxlib[cuda111]" -f https://whls.blob.core.windows.net/unstable/index.html --use-deprecated legacy-resolver
-pip install gdsfactory[full,gmsh,tidy3d,devsim,meow,ray,database]==6.36.0
-gf tool install
 
 cd ..\condabin
 call conda activate
+
+call pip install sax jax sklearn
+call pip install "jaxlib[cuda111]" -f https://whls.blob.core.windows.net/unstable/index.html --use-deprecated legacy-resolver
+call pip install gdsfactory[full,gmsh,tidy3d,devsim,meow,ray,database]==6.36.0
+call gf tool install
+
 call conda install -c conda-forge slepc4py=*=complex* -y
 call conda install -c conda-forge git spyder -y
+
+call python shortcuts.py
+
 if exist "%USERPROFILE%\Desktop\gdsfactory" (goto SKIP_INSTALL)
 cd %USERPROFILE%\Desktop
 call git clone https://github.com/gdsfactory/gdsfactory.git
@@ -17,4 +22,3 @@ call git clone https://github.com/gdsfactory/gdsfactory.git
 echo gdsfactory installed
 
 cd gdsfactory
-python shortcuts.py
