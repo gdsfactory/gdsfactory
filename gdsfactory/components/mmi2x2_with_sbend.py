09fce82ca197e40d2bafa17617555793c929be73
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-02-16 08:34:41 -0800

    add cross_section to mmi1x2_with_sbend
    Former-commit-id: dad933261ac9998f2b9b6148df4f33a9493fb763 [formerly cd5f30022ccd8cf6cefa88c81132a7145cf1352f]
    Former-commit-id: e262ac5a926aa9f8c11c9b2a90159d62b1421b77

--- gdsfactory/components/mmi2x2_with_sbend.py
+++ gdsfactory/components/mmi2x2_with_sbend.py
@@ -1,19 +1,25 @@
 import numpy as np
 import gdsfactory as gf
 from gdsfactory.component import Component
-from gdsfactory.typings import ComponentSpec
+from gdsfactory.typings import ComponentSpec, CrossSectionSpec
 from gdsfactory.components.bend_s import bend_s
 
 
 @gf.cell
 def mmi2x2_with_sbend(
-    with_sbend: bool = False,
+    with_sbend: bool = True,
     s_bend: ComponentSpec = bend_s,
+    cross_section: CrossSectionSpec = "strip",
 ) -> Component:
     """Returns mmi2x2 for Cband.
 
-    A C_band 2x2MMI in 220nm thick silicon from
+    A C_band 2x2MMI in 220nm thick silicon
     https://opg.optica.org/oe/fulltext.cfm?uri=oe-25-23-28957&id=376719
+
+    Args:
+        with_sbend: add sbend.
+        s_bend: S-bend spec.
+        cross_section: spec.
     """
 
     def mmi_widths(t):
@@ -25,30 +31,34 @@ def mmi2x2_with_sbend(
     c = gf.Component()
 
     P = gf.path.straight(length=2 * 2.4 + 2 * 1.6, npoints=5)
-    X = gf.CrossSection(width=mmi_widths, offset=0, layer="WG")
-    c << gf.path.extrude(P, cross_section=X)
+
+    xs = gf.get_cross_section(cross_section)
+    xs.width = mmi_widths
+    c << gf.path.extrude(P, cross_section=xs)
 
     # Add input and output tapers
     top_input_block = c << gf.components.taper(
-        length=1, width1=0.5, width2=0.7, layer="WG"
+        length=1, width1=0.5, width2=0.7, cross_section=cross_section
     ).move((-1, 0.45))
     bottom_input_block = c << gf.components.taper(
-        length=1, width1=0.5, width2=0.7, layer="WG"
+        length=1, width1=0.5, width2=0.7, cross_section=cross_section
     ).move((-1, -0.45))
 
     top_output_block = c << gf.components.taper(
-        length=1, width1=0.7, width2=0.5, layer="WG"
+        length=1, width1=0.7, width2=0.5, cross_section=cross_section
     ).move((8, 0.45))
     bottom_output_block = c << gf.components.taper(
-        length=1, width1=0.7, width2=0.5, layer="WG"
+        length=1, width1=0.7, width2=0.5, cross_section=cross_section
     ).move((8, -0.45))
 
     if with_sbend:
-        top_input_sbend_ref = c << s_bend()
+        sbend = gf.get_component(s_bend, cross_section=cross_section)
+
+        top_input_sbend_ref = c << sbend
         top_input_sbend_ref.mirror([0, 1])
-        bottom_input_sbend_ref = c << s_bend()
-        top_output_sbend_ref = c << s_bend()
-        bottom_output_sbend_ref = c << s_bend()
+        bottom_input_sbend_ref = c << sbend
+        top_output_sbend_ref = c << sbend
+        bottom_output_sbend_ref = c << sbend
         bottom_output_sbend_ref.mirror([0, 1])
 
         top_input_sbend_ref.connect("o1", destination=top_input_block.ports["o1"])
@@ -72,6 +82,9 @@ def mmi2x2_with_sbend(
 
 
 if __name__ == "__main__":
-    # c = mmi2x2_with_sbend(with_sbend=True)
-    c = mmi2x2_with_sbend(with_sbend=False)
+    c = mmi2x2_with_sbend(
+        with_sbend=True,
+        cross_section=dict(cross_section="strip", settings=dict(layer=(2, 0))),
+    )
+    # c = mmi2x2_with_sbend(with_sbend=False)
     c.show(show_ports=True)
