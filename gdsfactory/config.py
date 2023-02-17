ba31e9ea516b4b791f4e967336ff1b2ffba13ebc
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-02-17 12:19:29 -0800

    Bump version: 6.41.1 → 6.42.0
    Former-commit-id: 6b231f045c459bffac65a09734a91443065dca95 [formerly c0416fc10dacd34c3a68d573445cd3b9f7792804 [formerly 5e27445cc9dbf77f1024f8f67f96afb682a75787] [formerly 990b9c1efaabf490c02985e44b4e7e338c30fc76 [formerly 1f9ba636f1ab2ff5d2e46d97fdef0a555c3b50f6]]]
    Former-commit-id: 990b9c1efaabf490c02985e44b4e7e338c30fc76
    Former-commit-id: b677119b2982acf71abba49efb09337aa30b433a [formerly 335a40cc46f9f15f41ceb76c8f36c6fc4a04c952]
    Former-commit-id: 6268eaabca8a799c8b817de45698ce14da3d81a5

--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -28,7 +28,7 @@ import omegaconf
 from loguru import logger
 from omegaconf import OmegaConf
 
-__version__ = "6.41.1"
+__version__ = "6.42.0"
 PathType = Union[str, pathlib.Path]
 
 home = pathlib.Path.home()
