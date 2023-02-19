b0724291cc47512b2dcfff757695f766a850c51b
simbilod <sb30@princeton.edu>
2023-02-19 14:21:02 -0500

    moving off VM

--- gdsfactory/simulation/sax/parameter.py
+++ gdsfactory/simulation/sax/parameter.py
@@ -4,6 +4,10 @@ import numpy as np
 
 from gdsfactory.pdk import get_layer_stack
 from gdsfactory.technology import LayerStack
+import gdsfactory as gf
+import shapely
+from shapely.affinity import translate
+from shapely.ops import unary_union
 
 
 class Parameter:
@@ -17,10 +21,10 @@ class Parameter:
         """Generic parameter class for training Component models.
 
         Arguments:
-            min_value: minimum value of the parameter. Default to layer thickness minus tolerance.
-            max_value: maximum value of the parameter. Default to layer thickness plus tolerance.
-            nominal_value: nominal value of the parameter. Default to layer thickness.
-            step: size of the step going from min_value to max_value when generating data. Default to 3 steps between min and max.
+            min_value: minimum value of the parameter.
+            max_value: maximum value of the parameter.
+            nominal_value: nominal value of the parameter.
+            step: size of the step going from min_value to max_value when generating data.
         """
 
     def sample(self, rand_val: float = None):
@@ -60,6 +64,10 @@ class LayerStackThickness(Parameter):
         Arguments:
             layerstack: LayerStack
             layername: Name of the layer in the layerstack
+            min_value: minimum value of the parameter. Default to layer thickness minus tolerance.
+            max_value: maximum value of the parameter. Default to layer thickness plus tolerance.
+            nominal_value: nominal value of the parameter. Default to layer thickness.
+            step: size of the step going from min_value to max_value when generating data. Default to 3 steps between min and max.
         """
         super().__init__(**kwargs)
         self.layerstack = layerstack or get_layer_stack()
@@ -82,10 +90,175 @@ class LayerStackThickness(Parameter):
 
 class NamedParameter(Parameter):
     def __init__(self, **kwargs) -> None:
-        """Parameter associated with the Component or simulation (e.g. wavelength)."""
+        """Parameter associated with the Component function signature or simulation (e.g. some physical dimension, wavelength)."""
         super().__init__(**kwargs)
         self.min_value = kwargs.get("min_value")
         self.max_value = kwargs.get("max_value")
         self.nominal_value = kwargs.get("nominal_value")
         self.step = kwargs.get("step")
         return None
+
+
+class LithoParameter(Parameter):
+    def __init__(
+        self,
+        type: str = "layer_dilation_erosion",
+        layerstack: Optional[LayerStack] = None,
+        layername: Optional[str] = "core",
+        **kwargs,
+    ) -> None:
+        """Parameter associated with a morphological transformation of the Component.
+
+        Currently implemented transformations:
+            * Erosion and dilation (type = "layer_dilation_erosion")
+            * Layer translation offset (type = "layer_x_offset" and "layer_y_offset")
+            * Corner rounding (type = "layer_round_corners")
+        """
+        self.min_value = kwargs.get("min_value")
+        self.max_value = kwargs.get("max_value")
+        self.nominal_value = kwargs.get("nominal_value")
+        self.step = kwargs.get("step")
+        layerstack = layerstack or get_layer_stack()
+
+        self.layer = layerstack[layername].layer
+
+        if type == "layer_dilation_erosion":
+            self.transformation = self.layer_dilation_erosion
+        elif type == "layer_x_offset":
+            self.transformation = self.layer_x_offset
+        elif type == "layer_y_offset":
+            self.transformation = self.layer_y_offset
+        elif type == "layer_round_corners":
+            self.transformation = self.layer_round_corners
+
+        return None
+
+    def layer_dilation_erosion(self, component, dilation_value):
+        temp_component = gf.Component()
+        for layer, layer_polygons in component.get_polygons(by_spec=True).items():
+            if layer == self.layer:
+                # Make sure all layer polygons are fused properly
+                shapely_polygons = [
+                    shapely.geometry.Polygon(polygon) for polygon in layer_polygons
+                ]
+                shapely_polygons = unary_union(shapely_polygons)
+                for shapely_polygon in (
+                    shapely_polygons.geoms
+                    if hasattr(shapely_polygons, "geoms")
+                    else [shapely_polygons]
+                ):
+                    buffered_polygon = shapely_polygon.buffer(dilation_value)
+                    temp_component.add_polygon(
+                        buffered_polygon.exterior.coords, layer=layer
+                    )
+            else:
+                for layer_polygon in layer_polygons:
+                    temp_component.add_polygon(layer_polygon, layer=layer)
+        temp_component.add_ports(ports=component.get_ports())
+        return temp_component
+
+    def layer_x_offset(self, component, offset_value):
+        temp_component = gf.Component()
+        for layer, layer_polygons in component.get_polygons(by_spec=True).items():
+            for layer_polygon in layer_polygons:
+                if layer == self.layer:
+                    shapely_polygon = shapely.geometry.Polygon(layer_polygon)
+                    translated_polygon = translate(
+                        shapely_polygon, xoff=offset_value, yoff=0.0
+                    )
+                    temp_component.add_polygon(
+                        translated_polygon.exterior.coords, layer=layer
+                    )
+                else:
+                    temp_component.add_polygon(layer_polygon, layer=layer)
+        temp_component.add_ports(ports=component.get_ports())
+        return temp_component
+
+    def layer_y_offset(self, component, offset_value):
+        temp_component = gf.Component()
+        for layer, layer_polygons in component.get_polygons(by_spec=True).items():
+            for layer_polygon in layer_polygons:
+                if layer == self.layer:
+                    shapely_polygon = shapely.geometry.Polygon(layer_polygon)
+                    translated_polygon = translate(
+                        shapely_polygon, xoff=0.0, yoff=offset_value
+                    )
+                    temp_component.add_polygon(
+                        translated_polygon.exterior.coords, layer=layer
+                    )
+                else:
+                    temp_component.add_polygon(layer_polygon, layer=layer)
+        temp_component.add_ports(ports=component.get_ports())
+        return temp_component
+
+    def layer_round_corners(self, component, round_value):
+        temp_component = gf.Component()
+        for layer, layer_polygons in component.get_polygons(by_spec=True).items():
+            for layer_polygon in layer_polygons:
+                if layer == self.layer:
+                    shapely_polygon = shapely.geometry.Polygon(layer_polygon)
+                    buffered_polygon = (
+                        shapely_polygon.buffer(round_value, join_style=1)
+                        .buffer(-2 * round_value, join_style=1)
+                        .buffer(round_value, join_style=1)
+                    )
+                    temp_component.add_polygon(
+                        buffered_polygon.exterior.coords, layer=layer
+                    )
+                else:
+                    temp_component.add_polygon(layer_polygon, layer=layer)
+        temp_component.add_ports(component.ports)
+        return temp_component
+
+
+if __name__ == "__main__":
+    c = gf.Component("myComponent")
+    poly1 = c.add_polygon(
+        [
+            [4.0, -2.5],
+            [3.0, -2.5],
+            [2.0, -2.5],
+            [1.5, -3.0],
+            [2.0, -3.5],
+            [2.5, -4.0],
+            [2.0, -4.5],
+            [1.0, -4.5],
+            [0.5, -4.0],
+            [0.5, -3.0],
+            [0.5, -2.0],
+            [1.0, -1.5],
+            [1.5, -1.0],
+            [1.5, -0.0],
+        ],
+        layer=1,
+    )
+    poly2 = c.add_polygon(
+        [
+            [0.5, -4.5],
+            [4, -4.5],
+            [4, 0],
+            [0.5, 0],
+        ],
+        layer=2,
+    )
+    c.show()
+
+    # param = LithoParameter(layername="core")
+    # eroded_c = param.layer_dilation_erosion(c, 0.2)
+    # eroded_c.show()
+
+    # param = LithoParameter(layername=(1,0))
+    # eroded_c = param.layer_dilation_erosion(c, -0.2)
+    # eroded_c.show()
+
+    # param = LithoParameter(layername=(1,0))
+    # eroded_c = param.layer_x_offset(c, 0.5)
+    # eroded_c.show()
+
+    # param = LithoParameter(layername=(1,0))
+    # eroded_c = param.layer_y_offset(c, 0.5)
+    # eroded_c.show()
+
+    # param = LithoParameter(layername=(1,0))
+    # eroded_c = param.layer_round_corners(c, 0.2)
+    # eroded_c.show()
