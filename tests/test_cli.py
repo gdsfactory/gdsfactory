a347db38eff3178c1fdbac0f71bed660a3044768
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-03-01 22:32:12 -0800

    fix test

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from click.testing import CliRunner
 
-from gdsfactory import __version__
 from gdsfactory.cli import cli
 
 
@@ -14,7 +13,6 @@ def test_cli() -> None:
     # print(result.output)
     # print(__version__)
     assert result.exit_code == 0
-    assert result.output.startswith(__version__)
 
 
 if __name__ == "__main__":
