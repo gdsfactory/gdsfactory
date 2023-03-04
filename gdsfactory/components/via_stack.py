9947c608c8235d62eef6e87f901ca0e0ece31480
simbilod <sb30@princeton.edu>
2023-03-03 22:57:12 -0800

    via optimization draft

--- gdsfactory/components/via_stack.py
+++ gdsfactory/components/via_stack.py
@@ -7,7 +7,7 @@ from numpy import floor
 import gdsfactory as gf
 from gdsfactory.component import Component
 from gdsfactory.components.compass import compass
-from gdsfactory.components.via import via1, via2, viac
+from gdsfactory.components.via import via, via1, via2, viac
 from gdsfactory.typings import ComponentSpec, LayerSpec, LayerSpecs
 
 
@@ -64,13 +64,13 @@ def via_stack(
             ref = c << compass(size=size, layer=layer, port_type="placement")
 
     vias = vias or []
-    for via in vias:
-        if via is not None:
-            via = gf.get_component(via)
+    for via_type in vias:
+        if via_type is not None:
+            via_type = gf.get_component(via_type)
 
-            w, h = via.info["size"]
-            g = via.info["enclosure"]
-            pitch_x, pitch_y = via.info["spacing"]
+            w, h = via_type.info["size"]
+            g = via_type.info["enclosure"]
+            pitch_x, pitch_y = via_type.info["spacing"]
 
             nb_vias_x = (width - w - 2 * g) / pitch_x + 1
             nb_vias_y = (height - h - 2 * g) / pitch_y + 1
@@ -78,7 +78,7 @@ def via_stack(
             nb_vias_x = int(floor(nb_vias_x)) or 1
             nb_vias_y = int(floor(nb_vias_y)) or 1
             ref = c.add_array(
-                via, columns=nb_vias_x, rows=nb_vias_y, spacing=(pitch_x, pitch_y)
+                via_type, columns=nb_vias_x, rows=nb_vias_y, spacing=(pitch_x, pitch_y)
             )
 
             cw = (width - (nb_vias_x - 1) * pitch_x - w) / 2
@@ -88,6 +88,50 @@ def via_stack(
             ref.move((x0, y0))
 
     return c
+
+
+def optimized_via(
+    size: Tuple[float, float] = (11.0, 11.0),
+    min_via_size: Tuple[float, float] = (0.3, 0.3),
+    min_via_gap: Tuple[float, float] = (0.1, 0.1),
+    min_via_enclosure: float = 0.2,
+    layer: LayerSpecs = "VIA1",
+    bbox_layers: Optional[LayerSpecs] = None,
+    bbox_offset: float = 0,
+) -> Component:
+    """Given a target total inclusion size, returns an optimized dimension for the via.
+
+    Uses inclusion, minimum width, and minimum spacing rules to place the maximum number of individual vias, with maximum via area.
+
+    Arguments:
+        size: of the target enclosing medium
+        min_via_size: minimum size the vias can take
+        min_via_gap: minimum distance between vias
+        min_via_enclosure: minimum distance between edge of enclosing medium and nearest via edge
+        layer: of the returned via component
+        bbox_layers: of the returned via component
+        bbox_offset: of the returned via component
+    """
+    via_size = [0, 0]
+    for dim in [0, 1]:
+        via_area = size[dim] - 2 * min_via_enclosure + min_via_gap[dim]
+        num_vias = int(via_area / (min_via_size[dim] + min_via_gap[dim]))
+        try:
+            via_size[dim] = float(via_area / num_vias) - min_via_gap[dim]
+        except ZeroDivisionError as e:
+            raise RuntimeError(
+                "Cannot fit vias with specific minimum dimensions in provided space."
+            ) from e
+
+    return via(
+        size=via_size,
+        gap=min_via_gap,
+        spacing=None,
+        enclosure=min_via_enclosure,
+        layer=layer,
+        bbox_layers=bbox_layers,
+        bbox_offset=min_via_enclosure,
+    )
 
 
 via_stack_m1_m3 = gf.partial(
@@ -117,6 +161,13 @@ via_stack_heater_mtop = via_stack_heater_m3 = gf.partial(
 
 
 if __name__ == "__main__":
-    c = via_stack_m1_m3()
-    print(c.to_dict())
+    # c = via_stack_m1_m3()
+    # print(c.to_dict())
+    # c.show(show_ports=True)
+
+    c = via_stack(
+        size=(2.0, 2.0),
+        layers=("M1", "M2", "M3"),
+        vias=(optimized_via(layer="VIA1"), optimized_via(layer="VIA2")),
+    )
     c.show(show_ports=True)
