66d078b527868655432f21cadc67da938fa5b2d2
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-03-13 11:21:06 -0700

    snap ring_section_based center to grid

--- gdsfactory/components/ring_section_based.py
+++ gdsfactory/components/ring_section_based.py
@@ -148,17 +148,19 @@ def ring_section_based(
             -start_angle / 2
         )  # also change the component for later bound extraction
         r.rotate(-start_angle / 2)
-        c.info["ring_center"] = [
+        ring_center = [
             -radius * np.sin(np.radians(-start_angle / 2)),
             radius * np.cos(np.radians(-start_angle / 2)),
         ]
+        c.info["ring_center"] = gf.snap.snap_to_grid(ring_center)
     else:
         ring = ring.rotate(-cross_sections_angles[0] / 2)
         r.rotate(-cross_sections_angles[0] / 2)
-        c.info["ring_center"] = [
+        ring_center = [
             -radius * np.sin(np.radians(-cross_sections_angles[0] / 2)),
             radius * np.cos(np.radians(-cross_sections_angles[0] / 2)),
         ]
+        c.info["ring_center"] = gf.snap.snap_to_grid(ring_center)
 
     # Add bus waveguides
 
