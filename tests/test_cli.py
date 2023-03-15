1777c3cb406a1836ce66083ef5f6e5034da25e59
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-03-15 08:54:30 -0700

    remove test_cli

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,16 +1,16 @@
-from __future__ import annotations
+# from __future__ import annotations
 
-from click.testing import CliRunner
+# from click.testing import CliRunner
 
-from gdsfactory.cli import cli
-
-
-def test_cli() -> None:
-    """checks that the CLI returns the correct version."""
-    runner = CliRunner()
-    result = runner.invoke(cli, ["--version"])
-    assert result.exit_code == 0
+# from gdsfactory.cli import cli
 
 
-if __name__ == "__main__":
-    test_cli()
+# def test_cli() -> None:
+#     """checks that the CLI returns the correct version."""
+#     runner = CliRunner()
+#     result = runner.invoke(cli, ["--version"])
+#     assert result.exit_code == 0
+
+
+# if __name__ == "__main__":
+#     test_cli()
