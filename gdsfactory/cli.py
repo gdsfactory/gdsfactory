fb83db6efa88f7052e5dd91d700aea12214203d9
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-03-16 20:26:20 -0700

    Bump version: 6.61.1 → 6.61.2

--- gdsfactory/cli.py
+++ gdsfactory/cli.py
@@ -17,7 +17,7 @@ from gdsfactory.technology import lyp_to_dataclass
 from gdsfactory.typings import PathType
 from gdsfactory.write_cells import write_cells as write_cells_to_separate_gds
 
-VERSION = "6.61.1"
+VERSION = "6.61.2"
 LAYER_LABEL = LAYER.LABEL
 
 
