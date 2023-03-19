017828b129863b36e1c54b39facc01689efe9014
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-03-19 16:07:30 -0700

    Bump version: 6.64.1 → 6.64.2

--- gdsfactory/__init__.py
+++ gdsfactory/__init__.py
@@ -145,4 +145,4 @@ __all__ = (
     "write_cells",
     "PATH",
 )
-__version__ = "6.64.1"
+__version__ = "6.64.2"
--- gdsfactory/cli.py
+++ gdsfactory/cli.py
@@ -23,7 +23,7 @@ try:
 except ImportError:
     import click
 
-VERSION = "6.64.1"
+VERSION = "6.64.2"
 LAYER_LABEL = LAYER.LABEL
 
 
