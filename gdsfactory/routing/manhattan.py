1fbd1b9bd6145f57754e8347fb50c1b4200b17cf
Sourcery AI <>
2023-03-24 19:27:40 +0000

    'Refactored by Sourcery'

--- gdsfactory/routing/manhattan.py
+++ gdsfactory/routing/manhattan.py
@@ -557,11 +557,10 @@ def get_route_error(
 
     if with_sbend:
         raise RouteError(f"route error for points {points}")
-    else:
-        import traceback
+    import traceback
 
-        traceback.print_stack()
-        warnings.warn(f"Route error for points {points}", RouteWarning)
+    traceback.print_stack()
+    warnings.warn(f"Route error for points {points}", RouteWarning)
 
     c = Component(f"route_{uuid.uuid4()}"[:16])
     path = gdstk.FlexPath(
