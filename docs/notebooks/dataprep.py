98f61e7aa71a1c0efcc8a72c25edd59f33d6e932
simbilod <sb30@princeton.edu>
2023-03-24 15:33:55 -0700

    dataprep arbitrary klayout example

--- docs/notebooks/dataprep.py
+++ docs/notebooks/dataprep.py
@@ -4,22 +4,20 @@
 #     custom_cell_magics: kql
 #     text_representation:
 #       extension: .py
-#       format_name: percent
-#       format_version: '1.3'
-#       jupytext_version: 1.11.2
+#       format_name: light
+#       format_version: '1.5'
+#       jupytext_version: 1.14.5
 #   kernelspec:
 #     display_name: Python 3 (ipykernel)
 #     language: python
 #     name: python3
 # ---
 
-# %% [markdown]
 # # Dataprep
 #
 # When building a reticle sometimes you want to do boolean operations
 #
 
-# %% tags=[]
 from gdsfactory.generic_tech.layer_map import LAYER as l
 import gdsfactory.dataprep as dp
 import gdsfactory as gf
@@ -27,12 +25,10 @@ import gdsfactory as gf
 c = gf.components.coupler_ring()
 c.write_gds("src.gds")
 c
-# %% [markdown]
 # ## Size
 #
 # You can copy/size layers
 
-# %%
 d = dp.Layout(filepath="src.gds", layermap=dict(l))
 d.SLAB150 = d.WG.copy()  # copy layer
 d.SLAB150 += 4  # size layer by 4 um
@@ -40,13 +36,11 @@ d.SLAB150 -= 2  # size layer by 2 um
 c = d.write("dst.gds")
 c
 
-# %% [markdown]
 # ## Booleans
 #
 # You can derive layers and do boolean operations.
 
 
-# %%
 d = dp.Layout(filepath="src.gds", layermap=dict(l))
 d.SLAB150 = d.WG.copy()
 d.SLAB150 += 3  # size layer by 3 um
@@ -55,13 +49,22 @@ c = d.write("dst.gds")
 c
 
 
-# %% [markdown]
+# ## KLayout operations
+#
+# Any operation from Klayout Region can be called directly:
+
+d = dp.Layout(filepath="src.gds", layermap=dict(l))
+d.SLAB150 = d.WG.copy()
+d.SLAB150.round_corners(1 * 1e3, 1 * 1e3, 100)  # round corners by 1um
+c = d.write("dst.gds")
+c
+
 # ## Parallel processing
 #
 # You can use dask for parallel processing
 
 
-# %%
+# +
 import dask
 from IPython.display import HTML
 
@@ -71,7 +74,7 @@ c = gf.components.coupler_ring()
 c.write_gds("src.gds")
 c
 
-# %%
+# +
 d = dp.Layout(filepath="src.gds", layermap=dict(l))
 # you can do a bunch of derivations just to get a more interesting task graph
 d.SLAB150 = d.WG + 3
@@ -83,12 +86,10 @@ d.M2 = d.DEEP_ETCH - d.SHALLOW_ETCH
 # visualize the taskgraph and save as 'tasks.html'
 d.visualize("tasks")
 HTML(filename="tasks.html")
+# -
 
 
-# %%
 # evaluation of the task graph is lazy
 d.calculate()
 c = d.write("dst.gds")
 c
-
-# %%
