ebd988f43c8e648eb3e10d6012fc45e042858e88
mdecea <mdecea@mit.edu>
2023-03-24 13:17:13 -0700

    Section based rings can have a different cross section for add and drop, and different gaps

--- gdsfactory/components/ring_section_based.py
+++ gdsfactory/components/ring_section_based.py
@@ -10,7 +10,7 @@ from typing import Dict, Optional
 
 import numpy as np
 import gdsfactory as gf
-from gdsfactory.typings import CrossSectionSpec
+from gdsfactory.typings import CrossSectionSpec, Union, List, Tuple, Floats
 from gdsfactory.components.bend_circular import bend_circular
 from gdsfactory.components.straight import straight
 
@@ -20,15 +20,15 @@ def_ang_dict = {"A": 6, "B": 6}
 
 @gf.cell
 def ring_section_based(
-    gap: float = 0.3,
+    gap: Union[float, Floats] = 0.3,
     radius: float = 5.0,
     add_drop: bool = False,
     cross_sections: Dict = def_dict,
-    cross_sections_sequence: str = "AB",
+    cross_sections_sequence: Union[str, List[str], Tuple[str]] = "AB",
     cross_sections_angles: Optional[Dict] = def_ang_dict,
     start_cross_section: Optional[CrossSectionSpec] = None,
     start_angle: Optional[float] = 10.0,
-    start_section_at_drop: bool = True,
+    drop_cross_section: Optional[CrossSectionSpec] = None,
     bus_cross_section: CrossSectionSpec = "strip",
 ) -> gf.Component:
     """Returns a ring made of the specified cross sections.
@@ -45,6 +45,9 @@ def ring_section_based(
         cross_sections_sequence: sequence to follow filling the ring.
             Ex: "AB" means we will put first section A, then section B,
             then section A again... until the ring is filled
+            If we have a drop, then this can be a list of two strings,
+            where the first sequence is for one side of the ring
+            and the second for the other side of the ring
         cross_sections_angles: angular extent of each cross section in the
             cross_sections dictionary (deg). If not indicated, then we assume that
             the sequence is only repeated once and calculate the necessary angular
@@ -53,8 +56,8 @@ def ring_section_based(
             is different than the sequence we want to repeat. If that's the case, then
             here you indicate the initial cross section.
         start_angle: angular extent of the initial cross section (deg)
-        start_section_at_drop: if True, the initial section is placed at both ring-bus
-            junctions (only applies if add_drop = True)
+        drop_cross_section: cross section for the drop port. If not indicated, we assume
+            it is the same as init_cross_section.
         bus_cross_section: cross section for the bus waveguide.
     """
 
@@ -63,45 +66,82 @@ def ring_section_based(
     # First of all we need to do a bunch of checks
     angular_extent_sequence = 360
 
+    if not isinstance(gap, Union[List, Tuple]):
+        gap = [gap] * 2
+
     # See if we need to add initial cross sections
     if start_cross_section is not None:
         start_xs = gf.get_cross_section(start_cross_section)
         angular_extent_sequence -= start_angle
 
-        if add_drop and start_section_at_drop:
+        if add_drop:
             angular_extent_sequence -= start_angle
 
+            if not isinstance(cross_sections_sequence, Union[List, Tuple]):
+                cross_sections_sequence = [cross_sections_sequence] * 2
+
     if cross_sections_angles is None:
-        n_sections = len(cross_sections_sequence)
+        if add_drop:
+            n_sections_0 = len(cross_sections_sequence[0])
+            n_sections_1 = len(cross_sections_sequence[1])
+            sing_sec_ang_1 = angular_extent_sequence / (2 * n_sections_0)
+            sing_sec_ang_2 = angular_extent_sequence / (2 * n_sections_1)
+            cross_sections_angles = {
+                elem: sing_sec_ang_1 for elem in cross_sections_sequence[0]
+            }
+            for elem in cross_sections_sequence[1]:
+                cross_sections_angles[elem] = sing_sec_ang_2
 
-        if add_drop and start_section_at_drop:
-            sing_sec_ang = angular_extent_sequence / (2 * n_sections)
         else:
+            n_sections = len(cross_sections_sequence)
             sing_sec_ang = angular_extent_sequence / n_sections
+            cross_sections_angles = {
+                elem: sing_sec_ang for elem in cross_sections_sequence
+            }
 
-        cross_sections_angles = {elem: sing_sec_ang for elem in cross_sections_sequence}
     # Now make sure that the specified angular extents of the sections
     # are compatible with the ring extent (360 degree)
-    sing_seq_angular_extent = np.sum(
-        [cross_sections_angles[sec] for sec in cross_sections_sequence]
-    )
-    # Make sure we can fit an integer number of sequences into the
-    # ring circumference
-    if add_drop and start_section_at_drop:
-        if (angular_extent_sequence / (sing_seq_angular_extent / 2)).is_integer():
-            n_repeats_seq = int(angular_extent_sequence / (2 * sing_seq_angular_extent))
+    if not add_drop:
+        sing_seq_angular_extent = np.sum(
+            [cross_sections_angles[sec] for sec in cross_sections_sequence]
+        )
+        if not (angular_extent_sequence / sing_seq_angular_extent).is_integer():
+            raise ValueError(
+                "The specified sequence angles do not result in an integer number "
+                "of sequences fitting in the ring."
+            )
+        else:
+            n_repeats_seq = int(angular_extent_sequence / sing_seq_angular_extent)
+
+    else:
+        sing_seq_angular_extent_0 = np.sum(
+            [cross_sections_angles[sec] for sec in cross_sections_sequence[0]]
+        )
+        sing_seq_angular_extent_1 = np.sum(
+            [cross_sections_angles[sec] for sec in cross_sections_sequence[1]]
+        )
+
+        if (angular_extent_sequence / (sing_seq_angular_extent_0 * 2)).is_integer():
+            n_repeats_seq_0 = int(
+                angular_extent_sequence / (sing_seq_angular_extent_0 * 2)
+            )
         else:
             raise ValueError(
-                "The specified sequence angles do not result in an integer "
+                "The specified sequence 1 angles do not result in an integer "
                 "number of sequences fitting in the ring."
             )
-    elif not (angular_extent_sequence / sing_seq_angular_extent).is_integer():
-        raise ValueError(
-            "The specified sequence angles do not result in an integer number "
-            "of sequences fitting in the ring."
-        )
-    else:
-        n_repeats_seq = int(angular_extent_sequence / sing_seq_angular_extent)
+
+        if (angular_extent_sequence / (sing_seq_angular_extent_1 * 2)).is_integer():
+            n_repeats_seq_1 = int(
+                angular_extent_sequence / (sing_seq_angular_extent_1 * 2)
+            )
+        else:
+            raise ValueError(
+                "The specified sequence 2 angles do not result in an integer "
+                "number of sequences fitting in the ring."
+            )
+
+        print(n_repeats_seq_0)
 
     # Now we are ready to construct the ring
 
@@ -126,6 +166,19 @@ def ring_section_based(
             )
         sections_dict["0"] = (b, "o1", "o2")
 
+    if drop_cross_section is not None:
+        b = bend_circular(
+            angle=start_angle,
+            with_bbox=False,
+            cross_section=gf.get_cross_section(drop_cross_section),
+            radius=radius,
+        )
+        if "1" in sections_dict:
+            raise ValueError(
+                "Please do not have '1' as a key for the cross_sections dict"
+            )
+        sections_dict["1"] = (b, "o1", "o2")
+
     # Now we just need to generate a chain of characters
     # to creae the sequence
 
@@ -134,10 +187,19 @@ def ring_section_based(
     if start_cross_section is not None:
         sequence += "0"
 
-    sequence += cross_sections_sequence * n_repeats_seq
+    if not add_drop:
+        sequence += cross_sections_sequence * n_repeats_seq
 
-    if add_drop and start_section_at_drop:
-        sequence *= 2
+    else:
+        sequence += cross_sections_sequence[0] * n_repeats_seq_0
+
+        if drop_cross_section is None and start_cross_section is not None:
+            sequence += "0"
+        elif drop_cross_section is not None:
+            sequence += "1"
+
+        seq_2 = cross_sections_sequence[1] * n_repeats_seq_1
+        sequence = sequence + seq_2
 
     ring = gf.components.component_sequence(
         sequence=sequence, symbol_to_component=sections_dict
@@ -163,6 +225,7 @@ def ring_section_based(
             radius * np.cos(np.radians(-cross_sections_angles[sequence[0]] / 2)),
         ]
     c.info["ring_center"] = gf.snap.snap_to_grid(ring_center)
+
     # Add bus waveguides
 
     # Figure out main waveguiding layer of the ring at the ring-bus interface
@@ -181,12 +244,12 @@ def ring_section_based(
 
     s_add = c << s
     s_add.x = r.x
-    s_add.ymax = ring_guide.ymin - gap + s.ysize / 2 - input_xs_width / 2
+    s_add.ymax = ring_guide.ymin - gap[0] + s.ysize / 2 - input_xs_width / 2
 
     if add_drop:
         s_drop = c << s
         s_drop.x = r.x
-        s_drop.ymin = ring_guide.ymax + gap - s.ysize / 2 + input_xs_width / 2
+        s_drop.ymin = ring_guide.ymax + gap[1] - s.ysize / 2 + input_xs_width / 2
 
     # Add ports
     c.add_port("o1", port=s_add.ports["o1"])
@@ -214,11 +277,11 @@ if __name__ == "__main__":
         radius=5.0,
         add_drop=True,
         cross_sections={"A": "rib", "B": "slot"},
-        cross_sections_sequence="AB",
+        cross_sections_sequence=["AB", "BA"],
         cross_sections_angles={"A": 17, "B": 17},
         start_cross_section=partial(rib, width=0.65),
         start_angle=10.0,
-        start_section_at_drop=True,
+        drop_cross_section=partial(rib, width=0.55),
         bus_cross_section="strip",
     )
 
@@ -227,7 +290,7 @@ if __name__ == "__main__":
     #                     cross_section = "strip",)
     # print(b.ports)
     # b.rotate(-7.5)
-    c = ring_section_based(add_drop=True)
+    # c = ring_section_based(add_drop=True)
 
     c.show(show_ports=True)
     print(c.info["ring_center"])
