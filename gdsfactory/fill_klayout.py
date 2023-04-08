367a35c6996acd4adccbfaebd606ac8710af32bd
joamatab <4514346+joamatab@users.noreply.github.com>
2023-04-08 16:23:39 -0700

    add klayout fill

--- gdsfactory/fill_klayout.py
+++ gdsfactory/fill_klayout.py
@@ -3,7 +3,6 @@ from __future__ import annotations
 
 from typing import Optional, Tuple
 
-
 import gdsfactory as gf
 from gdsfactory.typings import PathType, LayerSpecs, Layer
 
@@ -60,27 +59,51 @@ def fill(
     )
     fill_margin = kf.kdb.Point(0, 0)
 
+    layer_to_fill = gf.get_layer(layer_to_fill)
+    layer_to_fill = cell.klib.layer(*layer_to_fill)
     region = kdb.Region()
-    layer = gf.get_layer(layer_to_fill)
-    layer = kf.klib.layer(*layer)
-    region.insert(cell.begin_shapes_rec(layer))
-    region.merge()
-    # region.size(layer_to_fill_margin)
+    region_ = kdb.Region()
+    for layer in cell.klib.layer_indices():
+        region_.insert(cell.begin_shapes_rec(layer)) if layer != layer_to_fill else None
+
+    region.insert(cell.begin_shapes_rec(layer_to_fill))
+    region = region - region_
 
     fill = kf.KCell(fill_name)
     fill.fill_region(
-        region, fill_cell_index, fill_cell_box, None, region, fill_margin, None
+        region,
+        fill_cell_index,
+        fill_cell_box,
+        fill_margin,
     )
     gdspath_out = gdspath_out or gdspath
+    for layer in cell.klib.layer_infos():
+        fill.shapes(fill.klib.layer(layer)).insert(
+            cell.begin_shapes_rec(cell.klib.layer(layer))
+        )
+    # fill.shapes(layer_to_fill).insert(cell.shapes(layer_to_fill))
     fill.write(str(gdspath_out))
 
 
+@gf.cell
+def cell_with_pad():
+    c = gf.Component()
+    c << gf.components.mzi(decorator=gf.add_padding)
+    pad = c << gf.components.pad(size=(2, 2))
+    pad.movey(10)
+    return c
+
+
 if __name__ == "__main__":
-    c = gf.components.mzi(decorator=gf.add_padding)
+    c = cell_with_pad()
+    c.show()
     gdspath = c.write_gds("mzi_fill.gds")
     fill(
         gdspath,
         fill_layers=("WG",),
         layer_to_fill=gf.LAYER.PADDING,
+        fill_cell_name="pad_size2__2",
+        fill_spacing=(1, 1),
+        fill_size=(1, 1),
     )
     gf.show(gdspath)
