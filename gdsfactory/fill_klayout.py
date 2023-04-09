85861b469310615238012bf69b2ce399883e2826
joamatab <4514346+joamatab@users.noreply.github.com>
2023-04-09 08:53:13 -0700

    add examples

--- gdsfactory/fill_klayout.py
+++ gdsfactory/fill_klayout.py
@@ -110,11 +110,53 @@ if __name__ == "__main__":
         gdspath,
         fill_layers=("WG",),
         layer_to_fill=gf.LAYER.PADDING,
-        # fill_cell_name="pad_size2__2",
-        fill_cell_name="demo",
-        # cell_name='cell_with_pad',
+        fill_cell_name="fill_cell",
         with_new_fill_cell=True,
         fill_spacing=(1, 1),
         fill_size=(1, 1),
     )
+    # fill(
+    #     gdspath,
+    #     fill_layers=("WG",),
+    #     layer_to_fill=gf.LAYER.PADDING,
+    #     fill_cell_name="pad_size2__2",
+    #     with_new_fill_cell=False,
+    #     fill_spacing=(1, 1),
+    #     fill_size=(1, 1),
+    # )
     gf.show(gdspath)
+
+    # # import gdsfactory.fill_processor as fill
+    # import kfactory.utils.geo.fill as fill
+
+    # c = kf.KCell("ToFill")
+    # c.shapes(kf.klib.layer(1, 0)).insert(
+    #     kf.kdb.DPolygon.ellipse(kf.kdb.DBox(5000, 3000), 512)
+    # )
+    # c.shapes(kf.klib.layer(10, 0)).insert(
+    #     kf.kdb.DPolygon(
+    #         [kf.kdb.DPoint(0, 0), kf.kdb.DPoint(5000, 0), kf.kdb.DPoint(5000, 3000)]
+    #     )
+    # )
+
+    # fc = kf.KCell("fill")
+    # fc.shapes(fc.klib.layer(2, 0)).insert(kf.kdb.DBox(20, 40))
+    # fc.shapes(fc.klib.layer(3, 0)).insert(kf.kdb.DBox(30, 15))
+
+    # # fill.fill_tiled(c, fc, [(kf.klib.layer(1,0), 0)], exclude_layers = [(kf.klib.layer(10,0), 100), (kf.klib.layer(2,0), 0), (kf.klib.layer(3,0),0)], x_space=5, y_space=5)
+    # fill.fill_tiled(
+    #     c,
+    #     fc,
+    #     [(kf.klib.layer(1, 0), 0)],
+    #     exclude_layers=[
+    #         (kf.klib.layer(10, 0), 100),
+    #         (kf.klib.layer(2, 0), 0),
+    #         (kf.klib.layer(3, 0), 0),
+    #     ],
+    #     x_space=5,
+    #     y_space=5,
+    # )
+
+    # gdspath = "mzi_fill.gds"
+    # c.write(gdspath)
+    # gf.show(gdspath)
