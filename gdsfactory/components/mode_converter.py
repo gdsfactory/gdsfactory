f03f8f00ed5e04c92a9baf623be4ae013b7073ad
Edward Deacon <ecr.deacon@gmail.com>
2023-04-12 15:43:50 +0100

    Added taper length and mc_mm_width as an argument

--- gdsfactory/components/mode_converter.py
+++ gdsfactory/components/mode_converter.py
@@ -5,7 +5,7 @@ from gdsfactory.component import Component
 from gdsfactory.components.coupler_straight_asymmetric import (
     coupler_straight_asymmetric as coupler_straight_asymmetric_function,
 )
-from gdsfactory.components.bend_s import bend_s
+from gdsfactory.components.bend_euler import bend_euler
 from gdsfactory.components.taper import taper as taper_function
 from gdsfactory.typings import ComponentSpec, CrossSectionSpec
 
@@ -15,11 +15,12 @@ def mode_converter(
     gap: float = 0.3,
     length: float = 10,
     coupler_straight_asymmetric: ComponentSpec = coupler_straight_asymmetric_function,
-    bend: ComponentSpec = bend_s,
+    bend: ComponentSpec = bend_euler,
     taper: ComponentSpec = taper_function,
-    mc_mm_width: float = 1,
     mm_width: float = 1.2,
+    mc_mm_width: float = 1,
     sm_width: float = 0.5,
+    taper_length: float = 50,
     cross_section: CrossSectionSpec = "strip",
     **kwargs,
 ) -> Component:
@@ -33,7 +34,8 @@ def mode_converter(
         gap: directional coupler gap.
         length: coupler length interaction.
         coupler_straight_asymmetric: spec.
-        mm_width: multimode waveguide width.
+        mm_width: input/output multimode waveguide width.
+        mc_mm_width: mode converter multimode waveguide width
         sm_width: single mode waveguide width.
         cross_section: cross_section spec.
         kwargs: cross_section settings.
@@ -69,7 +71,7 @@ def mode_converter(
         taper,
         width1=mc_mm_width,
         width2=mm_width,
-        length=100*abs(mc_mm_width-mm_width),
+        length=taper_length,
         **kwargs,
     )
 
