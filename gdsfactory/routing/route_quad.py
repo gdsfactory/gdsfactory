6991adc736bfaaa3797c166d471046e0c4801a05
simbilod <sb30@princeton.edu>
2023-04-24 20:54:20 -0700

    fix manhattanize test

--- gdsfactory/routing/route_quad.py
+++ gdsfactory/routing/route_quad.py
@@ -116,7 +116,7 @@ def test_manhattan_route_quad():
         manhattan_target_step=0.1,
     )
 
-    assert np.shape(route_gnd.get_polygons()) == (1, 807, 2)
+    assert np.shape(route_gnd.get_polygons()) == (1, 1210, 2)
 
 
 if __name__ == "__main__":
