5437b1496b181b0260d4d5fe955c827db95a6f72
joamatab <4514346+joamatab@users.noreply.github.com>
2023-05-21 10:10:05 -0700

    add cache

--- docs/notebooks/declarative_cell.py
+++ docs/notebooks/declarative_cell.py
@@ -1,4 +1,23 @@
-import gdstk
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: base
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
+# # Declarative Cell
+
+# %%
 import gdsfactory as gf
 
 
@@ -44,3 +63,8 @@ class mzi:
 
 
 mzi()
+
+# %%
+mzi(delta_length=100)
+
+# %%
