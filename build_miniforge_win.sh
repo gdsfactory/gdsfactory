7a99bc397dbadd97dfa1650c0a1c3f785c517aa9
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-06-08 10:50:58 +0200

    windows build

--- build_miniforge_win.sh
+++ build_miniforge_win.sh
@@ -7,4 +7,4 @@ source scripts/build.sh
 #source scripts/test.sh
 set PIP_FIND_LINKS="https://whls.blob.core.windows.net/unstable/index.html"
 pip install sax[nojax]
-pip install .[docs,dev,ray,full,gmsh,tidy3d,devsim,meow,sax,database,kfactory,femwell]
+pip install .[docs,dev,ray,full,gmsh,tidy3d,devsim,meow,database,kfactory,femwell]
