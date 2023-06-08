9c9783a84aa921f6a88836d0cef5edfabd38969f
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-06-08 10:14:32 +0200

    fix windows installer

--- build_miniforge_win.sh
+++ build_miniforge_win.sh
@@ -7,5 +7,4 @@ source scripts/build.sh
 #source scripts/test.sh
 set PIP_FIND_LINKS="https://whls.blob.core.windows.net/unstable/index.html"
 pip install sax[nojax]
-make full
-pip install .
+pip install .[docs,dev,ray,full,gmsh,tidy3d,devsim,meow,sax,database,kfactory,femwell]
