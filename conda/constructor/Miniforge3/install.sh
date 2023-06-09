2662c1d036f67becea6430209968eecb5f62b34b
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-06-09 07:34:31 +0200

    post install

--- conda/constructor/Miniforge3/install.sh
+++ conda/constructor/Miniforge3/install.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-conda install -c conda-forge slepc4py=*=complex* -y
-pip install sklearn gdsfactory[full,dev,gmsh,tidy3d,devsim,meow,sax,ray,database,femwell,kfactory]==6.103.6
+source "$PREFIX/etc/profile.d/conda.sh"
+conda activate "$PREFIX"
+
+python -m pip install sklearn gdsfactory[full,dev,gmsh,tidy3d,devsim,meow,sax,ray,database,femwell,kfactory]==6.103.6
+
+# conda install -c conda-forge slepc4py=*=complex* -y
 
 # if [[ $(uname -s) == Linux ]]; then
 #     if [[ ${INSTALLER_PLAT} != linux-* ]]; then
