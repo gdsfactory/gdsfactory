bc84058ddfc8fb8c734b751f52579bc47c1c1709
Troy Tamas <troy.tamas@rockleyphotonics.com>
2023-06-16 13:51:28 +0900

    reformatting code

--- tests/test_paths_extrude.py
+++ tests/test_paths_extrude.py
@@ -65,12 +65,12 @@ def test_transition_cross_section() -> None:
 
 def dummy_cladded_wg_cs(intent_layer, core_layer, core_width, clad_layer, clad_width):
     sections = (
-        Section(width=core_width, offset=0, layer=core_layer, name='core'),
-        Section(width=clad_width, offset=0, layer=clad_layer, name='clad'),
+        Section(width=core_width, offset=0, layer=core_layer, name="core"),
+        Section(width=clad_width, offset=0, layer=clad_layer, name="clad"),
     )
-    return gf.cross_section.cross_section(width=core_width,
-                                          sections=sections,
-                                          layer=intent_layer)
+    return gf.cross_section.cross_section(
+        width=core_width, sections=sections, layer=intent_layer
+    )
 
 
 def test_transition_cross_section_different_layers() -> None:
@@ -85,8 +85,20 @@ def test_transition_cross_section_different_layers() -> None:
     # in platforms with multiple waveguide types, it is useful to use separate intent layers for the different cross sections
     # this will simulate a transition between waveguides with different intent layers (which i just made up arbitrarily for this test)
     # but shared physical layers
-    cs1 = dummy_cladded_wg_cs(intent_layer=intent_layer_1, core_layer="WG", core_width=core_width, clad_layer="WGCLAD", clad_width=w1)
-    cs2 = dummy_cladded_wg_cs(intent_layer=intent_layer_2, core_layer="WG", core_width=core_width, clad_layer="WGCLAD", clad_width=w2)
+    cs1 = dummy_cladded_wg_cs(
+        intent_layer=intent_layer_1,
+        core_layer="WG",
+        core_width=core_width,
+        clad_layer="WGCLAD",
+        clad_width=w1,
+    )
+    cs2 = dummy_cladded_wg_cs(
+        intent_layer=intent_layer_2,
+        core_layer="WG",
+        core_width=core_width,
+        clad_layer="WGCLAD",
+        clad_width=w2,
+    )
     transition = gf.path.transition(cs1, cs2, port_names=(None, None))
 
     c = gf.components.straight(length=length, cross_section=transition)
@@ -101,6 +113,7 @@ def test_transition_cross_section_different_layers() -> None:
     # TODO: restore and replace after area() function is fixed
     # assert c.area() == expected_area
     assert c._cell.area(True)[gf.get_layer("WGCLAD")] == expected_area
+
 
 if __name__ == "__main__":
     test_transition_cross_section()
