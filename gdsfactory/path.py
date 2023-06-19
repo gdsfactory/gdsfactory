54dabe9631a025c3811bb9a21a4748ce2ad2e0c5
Troy Tamas <troy.tamas@rockleyphotonics.com>
2023-06-19 11:41:10 +0900

    black formatting

--- gdsfactory/path.py
+++ gdsfactory/path.py
@@ -378,12 +378,24 @@ class Path(_GeometryHelper):
         magic_offset = 0.17048614
 
         final_hash = hashlib.sha1()
-        points = np.ascontiguousarray(
-            (self.points / precision) + magic_offset, dtype=np.float64
-        ).round().astype(np.int64)
+        points = (
+            np.ascontiguousarray(
+                (self.points / precision) + magic_offset, dtype=np.float64
+            )
+            .round()
+            .astype(np.int64)
+        )
         final_hash.update(points)
-        angles = (np.ascontiguousarray((self.start_angle, self.end_angle), dtype=np.float64) / precision
-                  ).round().astype(np.int64)
+        angles = (
+            (
+                np.ascontiguousarray(
+                    (self.start_angle, self.end_angle), dtype=np.float64
+                )
+                / precision
+            )
+            .round()
+            .astype(np.int64)
+        )
         final_hash.update(angles)
         return final_hash.hexdigest()
 
