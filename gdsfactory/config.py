9a16faa9433795af450d8a4350422c6ddb105a7d
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-06-19 17:41:42 +0200

    Bump version: 6.107.2 → 6.107.3

--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -30,7 +30,7 @@ from rich.table import Table
 if TYPE_CHECKING:
     from loguru import Logger
 
-__version__ = "6.107.2"
+__version__ = "6.107.3"
 PathType = Union[str, pathlib.Path]
 
 home = pathlib.Path.home()
