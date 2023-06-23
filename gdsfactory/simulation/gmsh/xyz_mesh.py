1b84506289ce419869100662457cd4c881c1ce36
simbilod <sb30@princeton.edu>
2023-06-23 14:22:13 -0400

    update file test

--- gdsfactory/simulation/gmsh/xyz_mesh.py
+++ gdsfactory/simulation/gmsh/xyz_mesh.py
@@ -92,17 +92,9 @@ if __name__ == "__main__":
     from gdsfactory.generic_tech import LAYER
 
     c = gf.component.Component()
-    waveguide = c << gf.get_component(
-        gf.components.straight(length=5, cross_section="rib")
-    )
-    # Add wafer information
-    wafer = c << gf.components.bbox(bbox=waveguide.bbox, layer=LAYER.WAFER)
 
-    # ring = c << gf.get_component(gf.components.ring_crow)
-    # c << gf.components.spiral_racetrack(cross_section = "strip",
-    #                                         cross_section_s = "strip",)
-    # c << gf.components.coupler_straight()
-    c.show()
+    waveguide = c << gf.get_component(gf.components.straight_pin(length=5, taper=None))
+    wafer = c << gf.components.bbox(bbox=waveguide.bbox, layer=LAYER.WAFER)
 
     filtered_layerstack = LayerStack(
         layers={
@@ -110,28 +102,28 @@ if __name__ == "__main__":
             for k in (
                 "slab90",
                 "core",
-                # "via_contact",
-                # "undercut",
+                "via_contact",
                 "box",
-                # "substrate",
                 "clad",
-                # "metal1",
             )
         }
     )
 
-    # # filtered_layerstack.layers["via_contact"].info["mesh_order"] = 4
-    # # filtered_layerstack.layers["clad"].info["mesh_order"] = 5
+    filtered_layerstack.layers["core"].info["mesh_order"] = 1
+    filtered_layerstack.layers["slab90"].info["mesh_order"] = 2
+    filtered_layerstack.layers["via_contact"].info["mesh_order"] = 3
+    filtered_layerstack.layers["box"].info["mesh_order"] = 4
+    filtered_layerstack.layers["clad"].info["mesh_order"] = 5
 
     resolutions = {
         "core": {"resolution": 0.1},
         "slab90": {"resolution": 0.4},
-        # "via_contact": {"resolution": 0.4},
+        "via_contact": {"resolution": 0.4},
     }
     geometry = xyz_mesh(
         component=c,
         layerstack=filtered_layerstack,
         resolutions=resolutions,
         filename="mesh.msh",
-        verbosity=99,
+        verbosity=0,
     )
