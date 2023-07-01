69909ce47d92acce798f27227975ab3cf6cf726a
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-07-01 10:53:46 +0200

    fix yaml component docs

--- gdsfactory/samples/notebooks/10_yaml_component.py
+++ gdsfactory/samples/notebooks/10_yaml_component.py
@@ -1,3 +1,20 @@
+# ---
+# jupyter:
+#   jupytext:
+#     cell_metadata_filter: -all
+#     custom_cell_magics: kql
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.3'
+#       jupytext_version: 1.11.2
+#   kernelspec:
+#     display_name: base
+#     language: python
+#     name: python3
+# ---
+
+# %% [markdown]
 # # YAML Place and AutoRoute
 #
 # You have two options for working with gdsfactory:
@@ -24,10 +41,10 @@
 #
 # `# %matplotlib widget`  -> `%matplotlib widget`
 
-# +
+# %%
 # # %matplotlib widget
 
-# +
+# %%
 import ipywidgets
 from IPython.display import display
 from functools import partial
@@ -72,7 +89,7 @@ routes:
     electrical:
         settings:
             separation: 20
-            layer: [31, 0]
+            layer: [41, 0]
             width: 10
         links:
             tl,e3: tr,e1
@@ -91,9 +108,8 @@ display(x, out)
 def f(change, out=out):
     try:
         c = gf.read.from_yaml(change["new"])
-        # clear_output()
-        c.plot()
         c.show(show_ports=True)
+        c.plot_klayout()
         out.clear_output()
     except Exception as e:
         out.clear_output()
@@ -103,12 +119,13 @@ def f(change, out=out):
 
 x.observe(f, "value")
 f({"new": x.value})
-# -
 
+# %% [markdown]
 # Lets start by defining the `instances` and `placements` section in YAML
 #
 # Lets place an `mmi_long` where you can place the `W0` port at `x=20, y=10`
 
+# %%
 x.value = """
 name: mmis
 instances:
@@ -132,6 +149,7 @@ placements:
 """
 display(x, out)
 
+# %%
 x.value = """
 name: mmi_mirror
 instances:
@@ -155,6 +173,7 @@ placements:
 """
 display(x, out)
 
+# %% [markdown]
 # ## ports
 #
 # You can expose any ports of any instance to the new Component with a `ports` section in YAML
@@ -165,7 +184,7 @@ display(x, out)
 #
 # you can see the ports in `red` and subports in `blue`
 
-# +
+# %%
 x.value = """
 name: ports_demo
 instances:
@@ -188,13 +207,13 @@ ports:
 """
 
 display(x, out)
-# -
 
+# %% [markdown]
 # You can also define a mirror placement using a port
 #
 # Try mirroring with other ports `o2`, `o3` or with a number as well as with a rotation `90`, `180`, `270`
 
-# +
+# %%
 x.value = """
 name: mirror_demo
 instances:
@@ -212,8 +231,8 @@ placements:
 """
 
 display(x, out)
-# -
 
+# %% [markdown]
 # ## connections
 #
 # You can connect any two instances by defining a `connections` section in the YAML file.
@@ -222,7 +241,7 @@ display(x, out)
 #
 # `instance_source,port : instance_destination,port`
 
-# +
+# %%
 x.value = """
 name: connections_demo
 instances:
@@ -254,15 +273,15 @@ ports:
 """
 
 display(x, out)
-# -
 
+# %% [markdown]
 # **Relative port placing**
 #
 # You can also place a component with respect to another instance port
 #
 # You can also define an x and y offset with `dx` and `dy`
 
-# +
+# %%
 x.value = """
 name: rel_port_placing
 instances:
@@ -292,8 +311,8 @@ placements:
 
 
 display(x, out)
-# -
 
+# %% [markdown]
 # ## routes
 #
 # You can define routes between two instances by defining a `routes` section in YAML
@@ -312,7 +331,7 @@ display(x, out)
 #
 # ```
 
-# +
+# %%
 x.value = """
 name: with_routes
 instances:
@@ -342,12 +361,14 @@ routes:
 """
 
 display(x, out)
-# -
 
+# %% [markdown]
 # You can **rotate** and instance specifying the angle in degrees
 
+# %% [markdown]
 # You can also access the routes in the newly created component
 
+# %% [markdown]
 # ## instances, placements, connections, ports, routes
 #
 # Lets combine all you learned so far.
@@ -358,9 +379,10 @@ display(x, out)
 # instance_destination.port` so the order is important and therefore you can only
 # change the position of the `instance_destination`
 
+# %% [markdown]
 # You can define several routes that will be connected using `gf.routing.get_bundle`
 
-# +
+# %%
 x.value = """
 name: connections_2x2_problem
 
@@ -384,13 +406,13 @@ routes:
 """
 
 display(x, out)
-# -
 
+# %% [markdown]
 # You can also add custom component_factories to `gf.read.from_yaml`
 #
 
 
-# +
+# %%
 @gf.cell
 def pad_new(size=(100, 100), layer=gf.LAYER.WG):
     c = gf.Component()
@@ -403,7 +425,7 @@ gf.get_active_pdk().register_cells(pad_new=pad_new)
 c = pad_new(cache=False)
 f = c.plot()
 
-# +
+# %%
 x.value = """
 name: connections_2x2_problem
 
@@ -421,7 +443,7 @@ placements:
 
 display(x, out)
 
-# +
+# %%
 x.value = """
 name: custom_routes
 
@@ -453,11 +475,11 @@ routes:
 """
 
 display(x, out)
-# -
 
+# %% [markdown]
 # Also, you can define route aliases, that have different settings and specify the route `factory` as a parameter as well as the `settings` for that particular route alias.
 
-# +
+# %%
 x.value = """
 name: sample_settings
 
@@ -503,7 +525,7 @@ routes:
 
 display(x, out)
 
-# +
+# %%
 x.value = """
 instances:
     t:
@@ -537,7 +559,7 @@ routes:
 
 display(x, out)
 
-# +
+# %%
 x.value = """
 
 instances:
@@ -571,7 +593,7 @@ routes:
 
 display(x, out)
 
-# +
+# %%
 x.value = """
 instances:
     t:
@@ -606,15 +628,16 @@ routes:
 """
 
 display(x, out)
-# -
 
+# %% [markdown]
 # Note that you define the connections as `instance_source.port -> instance_destination.port` so the order is important and therefore you can only change the position of the `instance_destination`
 
+# %% [markdown]
 # ## Custom factories
 #
 # You can leverage netlist defined components to define more complex circuits
 
-# +
+# %%
 mmi1x2_faba = partial(gf.components.mmi1x2, length_mmi=30)
 mmi2x2_faba = partial(gf.components.mmi2x2, length_mmi=30)
 gf.get_active_pdk().register_cells(mmi1x2_faba=mmi1x2_faba, mmi2x2_faba=mmi2x2_faba)
@@ -645,23 +668,27 @@ ports:
 """
 
 display(x, out)
-# -
 
+# %%
 c = gf.components.mzi()
 c.plot()
 
+# %%
 c.plot_netlist()
 
+# %%
 n = c.get_netlist()
 
+# %%
 print(c.get_netlist().keys())
 
+# %% [markdown]
 # ## variables
 #
 #
 # You can define a global variables `settings` in your YAML file, and use the variable in the other YAML settings by using `${settings.length_mmi}`
 
-# +
+# %%
 x.value = """
 settings:
     length_mmi: 10
@@ -680,12 +707,12 @@ instances:
 """
 
 display(x, out)
-# -
 
+# %% [markdown]
 # ## `cell_from_yaml_template`: Jinja-template-based Parser
 # An optional parser variant is also available which is capable of parsing jinja templating directives within the yaml-based cells. This can give python-like flexibility inside the otherwise declaratively-defined yaml circuit syntax.
 
-# +
+# %%
 from gdsfactory.read import cell_from_yaml_template
 from IPython.display import Code
 
@@ -729,25 +756,29 @@ gf.get_active_pdk().register_cells(
     demo_jinja=pic_cell
 )  # let's register this cell so we can use it later
 Code(filename=pic_filename, language="yaml+jinja")
-# -
 
+# %% [markdown]
 # You'll see that this generated a python function, with a real signature, default arguments, docstring and all!
 
-# +
+# %%
 # pic_cell?
-# -
 
+# %% [markdown]
 # You can invoke this cell without arguments to see the default implementation
 
+# %%
 pic_cell()
 
+# %% [markdown]
 # Or you can provide arguments explicitly, like a normal cell. Note however that yaml-based cells **only accept keyword arguments**, since yaml dictionaries are inherently unordered.
 
+# %%
 pic_cell(length_mmi=100)
 
+# %% [markdown]
 # The power of jinja-templated cells become more apparent with more complex cells, like the following.
 
-# +
+# %%
 gf.clear_cache()
 
 jinja_yaml = """
@@ -800,11 +831,12 @@ with open(pic_filename, mode="w") as f:
 
 big_cell = cell_from_yaml_template(pic_filename, name="demo_jinja_loops")
 Code(filename=pic_filename, language="yaml+jinja")
-# -
 
+# %%
 bc = big_cell()
 bc
 
+# %%
 bc2 = big_cell(
     length_mmis=[10, 20, 40, 100, 200, 150, 10, 40],
     spacing_mmi=60,
@@ -812,12 +844,14 @@ bc2 = big_cell(
 )
 bc2
 
+# %% [markdown]
 # ## Choosing your preferred yaml parser
 #
 
+# %% [markdown]
 # In general, the jinja-yaml parser has a superset of the functionalities and syntax of the standard yaml parser. The one notable exception is with `settings`. When reading any yaml files with `settings` blocks, the default settings will be read and applied, but they will not be settable, as the jinja parser has a different mechanism for setting injection with the `default_settings` block and jinja2.
 
-# +
+# %%
 pic_filename = "demo_backwards_compatibility.pic.yml"
 
 with open(pic_filename, mode="w") as f:
@@ -827,11 +861,13 @@ retro_cell = cell_from_yaml_template(
     pic_filename, name="demo_jinja_backwards_compatible"
 )
 Code(filename=pic_filename, language="yaml")
-# -
 
+# %%
 retro_cell()  # this is fine-- because cell_from_yaml_template internally calls from_yaml, cells should work from their default state
 # retro_cell(length_mmi=15) # this fails-- you must use "default_settings" and jinja syntax with the yaml-jinja parser for settings to be settable
 
+# %% [markdown]
 # Because of this incompatibility, you must choose one parser or another to be used by default at the scope of the PDK.
 
+# %%
 gf.get_active_pdk().circuit_yaml_parser = cell_from_yaml_template
