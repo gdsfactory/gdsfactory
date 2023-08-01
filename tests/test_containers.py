1c833218074a1bf5e398751e58086df4f9074268
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-08-01 15:10:01 -0700

    test containers

--- tests/test_containers.py
+++ tests/test_containers.py
@@ -5,25 +5,14 @@ import pytest
 from pytest_regressions.data_regression import DataRegressionFixture
 
 import gdsfactory as gf
-from gdsfactory.containers import containers
 from gdsfactory.difftest import difftest
+from gdsfactory.generic_tech.containers import containers
+
 
 component = gf.components.mzi2x2_2x2(straight_x_top="straight_heater_metal")
 
-skip_test = {
-    "add_grating_couplers",
-    "add_grating_couplers_with_loopback_fiber_single",
-    "array",
-    "cavity",
-    "extend_ports",
-    "pack_doe",
-    "pack_doe_grid",
-    "ring_single_dut",
-}
-skip_test_gds = {
-    "add_pads_top",
-    "add_pads_bot",
-}
+skip_test = {"add_fiber_array", "add_termination"}
+skip_test_gds = {"add_pads_top", "add_pads_bot"}
 container_names = set(containers.keys()) - skip_test
 container_names_gds = container_names - skip_test_gds
 
