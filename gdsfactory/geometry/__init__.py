99cfba0a61c0f64d89d822166a6a3f9ed8216081
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-08-08 15:47:39 -0700

    move klayout plugins to gplugins

--- gdsfactory/geometry/__init__.py
+++ gdsfactory/geometry/__init__.py
@@ -6,11 +6,6 @@ from gdsfactory.geometry import functions
 from gdsfactory.geometry.boolean import boolean
 from gdsfactory.geometry.boolean_klayout import boolean_klayout
 from gdsfactory.geometry.boolean_polygons import boolean_polygons
-from gdsfactory.geometry.check_duplicated_cells import check_duplicated_cells
-from gdsfactory.geometry.check_exclusion import check_exclusion
-from gdsfactory.geometry.check_inclusion import check_inclusion
-from gdsfactory.geometry.check_space import check_space
-from gdsfactory.geometry.check_width import check_width
 from gdsfactory.geometry.fillet import fillet
 from gdsfactory.geometry.invert import invert
 from gdsfactory.geometry.layer_priority import layer_priority
@@ -25,11 +20,6 @@ __all__ = (
     "boolean",
     "boolean_klayout",
     "boolean_polygons",
-    "check_duplicated_cells",
-    "check_exclusion",
-    "check_inclusion",
-    "check_space",
-    "check_width",
     "fillet",
     "functions",
     "invert",
