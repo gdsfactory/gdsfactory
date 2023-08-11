a3e2e0dea4952bbe1386126045ca20f60a52ade4
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-08-11 08:03:25 -0700

    remove plugins

--- gdsfactory/plugins/web/Makefile
+++
@@ -1,2 +0,0 @@
-run:
-	uvicorn main:app --reload
--- gdsfactory/simulation/sax/__init__.py
+++
@@ -1,6 +0,0 @@
-from __future__ import annotations
-
-from gdsfactory.simulation.sax import models, read
-from gdsfactory.simulation.sax.plot_model import plot_model
-
-__all__ = ["read", "plot_model", "models"]
