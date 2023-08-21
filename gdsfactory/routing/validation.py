2bf5f8e063dc1fe591c68719e8e37cbc2e38c6ad
Troy Tamas <troy.tamas@rockleyphotonics.com>
2023-08-21 11:12:56 +0900

    adding docstrings to validation functions

--- gdsfactory/routing/validation.py
+++ gdsfactory/routing/validation.py
@@ -11,6 +11,17 @@ from gdsfactory.typings import Route
 def validate_connections(
     ports1: list[Port], ports2: list[Port], routes: list[Route]
 ) -> list[Route]:
+    """
+    Validates that a set of Routes indeed connects the port-pairs listed in ports1 and ports2. If the Routes form valid connections between ports1 and ports2, the original Routes will be returned. If not, a RouteWarning will be raised, and a set of error traces will be returned instead.
+
+    Args:
+        ports1: the list of starting ports
+        ports2: the list of ending ports
+        routes: the list of Route objects, purportedly between ports1 and ports2
+
+    Returns:
+        A list of Routes. If the input routes are valid, they will be returned as-is. Otherwise, a list of error traces will be returned and a RouteWarning will be raised.
+    """
     connections_expected = {_connection_tuple(p1, p2) for p1, p2 in zip(ports1, ports2)}
 
     for route in routes:
@@ -27,6 +38,17 @@ def validate_connections(
 def make_error_traces(
     ports1: list[Port], ports2: list[Port], message: str
 ) -> list[Route]:
+    """
+    Creates a set of error traces showing the intended connectivity between ports1 and ports2. The specified message will be included in the RouteWarning that is raised.
+
+    Args:
+        ports1: the list of starting ports
+        ports2: the list of ending ports
+        message: a message to include in the RouteWarning that is raised
+
+    Returns:
+        A list of Routes (the error traces).
+    """
     import gdsfactory as gf
     from gdsfactory.routing.manhattan import RouteWarning
 
