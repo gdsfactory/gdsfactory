2970fa863c7cf2e8622d93fbb50079cf40f2c21d
SkandanC <s39chand@edu.uwaterloo.ca>
2023-09-07 19:22:56 -0400

    Fix precommit

--- gdsfactory/path.py
+++ gdsfactory/path.py
@@ -581,6 +581,7 @@ def transition_adiabatic(
         [2] Fu, Yunfei, et al. "Efficient adiabatic silicon-on-insulator waveguide taper."
             Photonics Res., vol. 2, no. 3, 1 June 2014, pp. A41-A44, doi:10.1364/PRJ.2.000A41.
     """
+
     # Define ODE
     def dWdx(w, x, neff_w, wavelength, alpha):
         return alpha * wavelength / (neff_w(w) * w)
