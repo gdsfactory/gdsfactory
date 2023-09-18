f32133dac668ec973a502d8fd15a721b5ed16a81
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-09-18 10:54:09 -0700

    remove conda folder (#2099)
    * remove conda folder
    * consistent display type in test_docs

--- conda/mamba_activate.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-source "${HOME}/mambaforge/etc/profile.d/conda.sh"
-source "${HOME}/mambaforge/etc/profile.d/mamba.sh"
--- conda/mamba_install.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-curl -L -O "https://github.com/conda-forge/miniforge/releases/latest/download/Mambaforge-$(uname)-$(uname -m).sh"
-bash Mambaforge-$(uname)-$(uname -m).sh -b
