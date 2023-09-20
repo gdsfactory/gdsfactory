69b2ba838c3722e6a7ae57b130ad8b9a1de62b13
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-09-20 09:48:10 -0700

    Bump version: 7.6.0 → 7.6.1

--- gdsfactory/cli.py
+++ gdsfactory/cli.py
@@ -7,7 +7,7 @@ import typer
 
 app = typer.Typer()
 
-VERSION = "7.6.0"
+VERSION = "7.6.1"
 
 
 @app.command()
--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -25,7 +25,7 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 from rich.console import Console
 from rich.table import Table
 
-__version__ = "7.6.0"
+__version__ = "7.6.1"
 PathType = str | pathlib.Path
 
 home = pathlib.Path.home()
