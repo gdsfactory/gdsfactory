71c5aead114cfcbe02526c512bd5f3af511109ca
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-10-08 12:20:54 -0700

    fix dbr

--- gdsfactory/components/dbr.py
+++ gdsfactory/components/dbr.py
@@ -79,7 +79,7 @@ def dbr(
     l2: float = period / 2,
     n: int = 10,
     cross_section: CrossSectionSpec = "xs_sc",
-    straight_length:float=10e-3,
+    straight_length: float = 10e-3,
     **kwargs,
 ) -> Component:
     """Distributed Bragg Reflector.
@@ -112,21 +112,20 @@ def dbr(
     s2 = c << straight(cross_section=xs, length=straight_length)
 
     cell = dbr_cell(w1=w1, w2=w2, l1=l1, l2=l2, cross_section=cross_section)
-    c.add_array(cell, columns=n, rows=1, spacing=(l1 + l2, 100))
+    ref = c.add_array(cell, columns=n, rows=1, spacing=(l1 + l2, 100))
 
     s1.connect(port="o1", other=cell.ports["o1"], allow_width_mismatch=True)
     s2.connect(port="o1", other=cell.ports["o2"], allow_width_mismatch=True)
+    s2.xmin = ref.xmax
 
     c.add_port("o1", port=s1.ports["o2"])
-    c.add_port("o2", port=s2.ports["o2"])
-
     xs.add_pins(c)
     return c
 
 
 if __name__ == "__main__":
     # c = dbr(w1=0.5, w2=0.6, l1=0.2, l2=0.3, n=10)
-    c = dbr(n=1)
+    c = dbr(n=2)
     # c = dbr_cell()
     # c.assert_ports_on_grid()
     c.show()
