917e6fb76cdeb37452084b6eabe117a09ce1f406
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-11-06 15:26:53 -0800

    fix rectangle_with_slits

--- gdsfactory/components/rectangle_with_slits.py
+++ gdsfactory/components/rectangle_with_slits.py
@@ -60,26 +60,17 @@ def rectangle_with_slits(
     c = Component()
     layer = gf.get_layer(layer)
 
-    r = rectangle(size=size, layer=layer, port_type=port_type, centered=centered)
+    r = c << rectangle(size=size, layer=layer, port_type=port_type, centered=centered)
     c.add_ports(r.ports)
     slit = rectangle(size=slit_size, port_type=None, layer=layer_slit or layer)
-
     columns = np.floor((size[0] - 2 * slit_enclosure) / slit_spacing[0])
     rows = np.floor((size[1] - 2 * slit_enclosure) / slit_spacing[1])
-    slits = array(slit, columns=columns, rows=rows, spacing=slit_spacing).ref()
-    slits.xmin = slit_enclosure
-    slits.ymin = slit_enclosure
-
-    if layer_slit:
-        _ = c << r
-        c.add(slits)
-    else:
-        r_with_slits = c << gf.boolean(r, slits, operation="not", layer=layer)
-        c.absorb(r_with_slits)
+    slits = c << array(slit, columns=columns, rows=rows, spacing=slit_spacing)
+    slits.center = r.center
     return c
 
 
 if __name__ == "__main__":
     # c = rectangle_with_slits(layer_slit=None)
-    c = rectangle_with_slits(layer_slit=(2, 0), slit_size=(10, 10))
+    c = rectangle_with_slits(slit_size=(10, 10), centered=True)
     c.show()
