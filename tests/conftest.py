bdc0c14eb802c611eda34fb1d45592f245c13086
Niko Savola <nikosavola@google.com>
2023-12-21 14:03:41 -0800

    Support gdsfactory-employed `loguru` logger in pytests

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,7 +2,7 @@ import pathlib
 
 import pytest
 
-from gdsfactory.config import PATH
+from gdsfactory.config import PATH, logger
 
 
 @pytest.fixture(scope="session")
@@ -13,3 +13,11 @@ def datadir() -> pathlib.Path:
 @pytest.fixture(scope="session")
 def original_datadir() -> pathlib.Path:
     return PATH.repo / "test-data-regression"
+
+
+@pytest.fixture
+def caplog(caplog):
+    """Support `loguru` logger in pytest caplog fixture. See https://github.com/Delgan/loguru/issues/59."""
+    handler_id = logger.add(caplog.handler, format="{message}")
+    yield caplog
+    logger.remove(handler_id)
