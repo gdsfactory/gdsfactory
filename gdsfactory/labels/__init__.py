c4781c3a004d4c10d7c41155074c0cc43ea97507
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2023-12-30 19:53:30 +0900

    remove merge_test_metadata

--- gdsfactory/labels/__init__.py
+++ gdsfactory/labels/__init__.py
@@ -2,11 +2,10 @@
 
 from __future__ import annotations
 
-from gdsfactory.labels import ehva, siepic, write_labels
+from gdsfactory.labels import siepic, write_labels
 from gdsfactory.labels.add_label_yaml import add_label_json, add_label_yaml
 from gdsfactory.labels.ehva import add_label_ehva
 from gdsfactory.labels.get_test_manifest import get_test_manifest
-from gdsfactory.labels.merge_test_metadata import merge_test_metadata
 from gdsfactory.labels.siepic import add_fiber_array_siepic
 
 __all__ = [
@@ -14,9 +13,7 @@ __all__ = [
     "add_label_ehva",
     "add_label_yaml",
     "add_label_json",
-    "ehva",
     "siepic",
     "write_labels",
-    "merge_test_metadata",
     "get_test_manifest",
 ]
