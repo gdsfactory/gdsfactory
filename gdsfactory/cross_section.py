74e5ad81702caad7b167532c96c5dceeac116d42
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-01-13 10:34:21 -0800

    fix serialize_width

--- gdsfactory/cross_section.py
+++ gdsfactory/cross_section.py
@@ -396,12 +396,11 @@ class Transition(CrossSection):
 
     @field_serializer("width_type")
     def serialize_width(self, width_type: WidthTypes | Callable) -> str | None:
-        if isinstance(width_type, str):
+        if isinstance(width_type, str | int | float):
             return width_type
         t_values = np.linspace(0, 1, 10)
-        return ",".join(
-            [str(round(width, 3)) for width in width_type(t_values, *self.width)]
-        )
+        widths = width_type(t_values, *self.width)
+        return ",".join([str(round(width, 3)) for width in widths])
 
     @property
     def width(self) -> tuple[float, float]:
