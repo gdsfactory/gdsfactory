cb987aa759c647a4f383ab4f30653268d919a52c
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-02-10 14:09:08 -0800

    Bump to 7.11.0

--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -32,7 +32,7 @@ from rich.table import Table
 if TYPE_CHECKING:
     from loguru import Logger
 
-__version__ = "7.10.8"
+__version__ = "7.11.0"
 PathType = str | pathlib.Path
 
 home = pathlib.Path.home()
