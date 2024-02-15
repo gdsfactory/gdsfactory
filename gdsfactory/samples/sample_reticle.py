4c371ef4b584457ea9a4cd64a53e350d3fe39654
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-02-15 08:01:51 -0800

    better sample reticle

--- gdsfactory/samples/sample_reticle.py
+++ gdsfactory/samples/sample_reticle.py
@@ -3,6 +3,68 @@ from __future__ import annotations
 import gdsfactory as gf
 
 
+@gf.cell
+def mzi_phase_shifter(length_x: float = 100.0, **kwargs) -> gf.Component:
+    """Returns MZI with TE grating couplers.
+
+    Args:
+        length_x: length of the phase shifter.
+        kwargs: extra arguments passed to the MZI component.
+    """
+    test_info_mzi_heaters = dict(
+        doe="mzis_heaters",
+        analysis="mzi_heater",
+        measurement="optical_loopback4_heater_sweep",
+    )
+    c = gf.components.mzi2x2_2x2_phase_shifter(length_x=length_x, **kwargs)
+    c = gf.components.add_fiber_array_optical_south_electrical_north(
+        c,
+        electrical_port_names=["top_l_e2", "top_r_e2"],
+    )
+    c.info.update(test_info_mzi_heaters)
+    return c
+
+
+@gf.cell
+def ring_single_heater(length_x: float = 10.0, **kwargs) -> gf.Component:
+    """Returns ring with TE grating couplers.
+
+    Args:
+        length_x: length of the ring.
+        kwargs: extra arguments passed to the ring component.
+    """
+    test_info_ring_heaters = dict(
+        doe="ring_heaters",
+        analysis="ring_heater",
+        measurement="optical_loopback2_heater_sweep",
+    )
+    c = gf.components.ring_single_heater(length_x=length_x, **kwargs)
+    c = gf.components.add_fiber_array_optical_south_electrical_north(
+        c,
+        electrical_port_names=["l_e2", "r_e2"],
+    )
+    c.info.update(test_info_ring_heaters)
+    return c
+
+
+@gf.cell
+def spiral_sc(length: float = 20e3, **kwargs) -> gf.Component:
+    """Returns spiral with TE grating couplers.
+
+    Args:
+        length: length of the spiral.
+        kwargs: extra arguments passed to the spiral component.
+    """
+    info = dict(
+        doe="spirals_sc",
+        measurement="optical_loopback4",
+        analysis="optical_loopback4_spirals",
+    )
+    c = gf.components.spiral_inner_io_fiber_array(length=length, **kwargs)
+    c.info.update(info)
+    return c
+
+
 def sample_reticle(grid: bool = True, **kwargs) -> gf.Component:
     """Returns MZI with TE grating couplers.
 
@@ -10,63 +72,12 @@ def sample_reticle(grid: bool = True, **kwargs) -> gf.Component:
         grid: if True returns components on a regular grid. False packs them as close as possible.
         kwargs: passed to pack or grid.
     """
-    test_info_mzi_heaters = dict(
-        doe="mzis_heaters",
-        analysis="mzi_heater",
-        measurement="optical_loopback4_heater_sweep",
-    )
-    test_info_ring_heaters = dict(
-        doe="ring_heaters",
-        analysis="ring_heater",
-        measurement="optical_loopback2_heater_sweep",
-    )
 
-    mzis = [
-        gf.components.mzi2x2_2x2_phase_shifter(
-            length_x=length, name=f"mzi_heater_{length}"
-        )
-        for length in [100, 200, 300]
-    ]
-    rings = [
-        gf.components.ring_single_heater(
-            length_x=length_x, name=f"ring_single_heater_{length_x}"
-        )
-        for length_x in [10, 20, 30]
-    ]
+    mzis_te = [mzi_phase_shifter(length_x=length) for length in [100, 200, 300]]
+    rings_te = [ring_single_heater(length_x=length_x) for length_x in [10, 20, 30]]
+    spirals_te = [spiral_sc(length=length) for length in [20e3, 40e3, 60e3]]
 
-    spirals_sc = [
-        gf.components.spiral_inner_io_fiber_array(
-            name=f"spiral_sc_{int(length/1e3)}mm",
-            length=length,
-            info=dict(
-                doe="spirals_sc",
-                measurement="optical_loopback4",
-                analysis="optical_loopback4_spirals",
-            ),
-        )
-        for length in [20e3, 40e3, 60e3]
-    ]
-
-    mzis_te = [
-        gf.components.add_fiber_array_optical_south_electrical_north(
-            mzi,
-            electrical_port_names=["top_l_e2", "top_r_e2"],
-            info=test_info_mzi_heaters,
-            name=f"{mzi.name}_te",
-        )
-        for mzi in mzis
-    ]
-    rings_te = [
-        gf.components.add_fiber_array_optical_south_electrical_north(
-            ring,
-            electrical_port_names=["l_e2", "r_e2"],
-            info=test_info_ring_heaters,
-            name=f"{ring.name}_te",
-        )
-        for ring in rings
-    ]
-
-    components = mzis_te + rings_te + spirals_sc
+    components = mzis_te + rings_te + spirals_te
 
     if grid:
         return gf.grid(components, name_ports_with_component_name=True, **kwargs)
@@ -78,5 +89,7 @@ def sample_reticle(grid: bool = True, **kwargs) -> gf.Component:
 
 if __name__ == "__main__":
     c = sample_reticle(grid=False)
+    # c = mzi_phase_shifter()
+    # c = spiral_sc()
     c.show(show_ports=True)
-    c.pprint_ports()
+    # c.pprint_ports()
