e4820db62636baafe1c9f6fbac8c7403c9ed3188
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-02-18 11:52:24 -0800

    allow recursively convert to kfactory

--- gdsfactory/export/to_kfactory.py
+++ gdsfactory/export/to_kfactory.py
@@ -23,22 +23,19 @@ def import_gds(gdspath: str | Path) -> kf.KCell:
     return kf.kcl[new_top_cells.pop().name]
 
 
-def to_kfactory(component) -> kf.KCell:
-    """Converts a gdsfactory component to a kfactory component."""
-    c0 = component.write_gds(with_metadata=True)
-    c = import_gds(c0)
+def add_ports(c: kf.KCell, gf_component: gf.Component):
+    """Adds ports to a kfactory component.
 
-    for k, v in dict(component.info).items():
-        c.info[k] = v
-    for k, v in dict(component.settings).items():
-        if isinstance(v, str | int | float | bool):
-            c.info[k] = v
-    for port in component.ports.values():
+    Args:
+        c: kfactory component.
+        gf_component: gdsfactory component.
+    """
+    for port in gf_component.ports.values():
         center = port.center
-        orientation = port.orientation
+        orientation = port.orientation or 0
         layer = gf.get_layer(port.layer)
         layer = kf.kcl.layer(*layer)
-        trans = kf.kdb.DCplxTrans(1, orientation, False, center[0], center[1])
+        trans = kf.kdb.DCplxTrans(1, float(orientation), False, center[0], center[1])
         width = port.width
         c.create_port(
             name=port.name,
@@ -47,11 +44,51 @@ def to_kfactory(component) -> kf.KCell:
             port_type=port.port_type,
             dcplx_trans=trans,
         )
+
+
+def to_kfactory(component: gf.Component, recursive: bool = True) -> kf.KCell:
+    """Converts a gdsfactory component to a kfactory component.
+
+    Args:
+        component: gdsfactory component.
+        recursive: if True, recursively add ports and info to all subcomponents.
+
+    """
+    c0 = component.write_gds(with_metadata=True)
+    c = import_gds(c0)
+
+    for k, v in sorted(dict(component.info).items()):
+        c.info[k] = v
+
+    settings = {}
+    for k, v in sorted(dict(component.settings).items()):
+        if isinstance(v, str | int | float | bool):
+            settings[k] = v
+
+    c._settings = kf.kcell.KCellSettings(**settings)
+
+    if recursive:
+        for child in component.references:
+            component = child.parent
+            cell_name = component.name
+            kcell_child = kf.kcl[cell_name]
+            add_ports(kcell_child, component)
+
+            for k, v in sorted(dict(component.info).items()):
+                kcell_child.info[k] = v
+
+            settings = {}
+            for k, v in sorted(dict(component.settings).items()):
+                if isinstance(v, str | int | float | bool):
+                    settings[k] = v
+            kcell_child._settings = kf.kcell.KCellSettings(**settings)
+
+    add_ports(c, component)
     return c
 
 
 if __name__ == "__main__":
-    c0 = gf.components.mzi()
+    # c0 = gf.components.mzi()
     c0 = gf.pack([gf.c.mzi(), gf.c.straight()])[0]
     c = to_kfactory(c0)
     c.show()
