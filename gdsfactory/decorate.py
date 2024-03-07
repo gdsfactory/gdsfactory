15003a9220eafeb5f93ae2bf77c5ef0746031231
Troy Tamas <tvtamas@gmail.com>
2024-03-07 17:30:08 +0900

    better caching behavior

--- gdsfactory/decorate.py
+++ gdsfactory/decorate.py
@@ -1,23 +1,28 @@
-import functools
 import hashlib
+from functools import wraps
 
 from gdsfactory.name import get_name_short
 from gdsfactory.serialization import clean_value_name
 
 
 def decorate_cell(cell_function: callable, *decorators: callable):
-    @functools.wraps(cell_function)
+    @wraps(cell_function)
     def wrapper(*args, **kwargs):
+        from gdsfactory.cell import CACHE
+
         c_orig = cell_function(*args, **kwargs)
-        c = c_orig.copy()
-        suffix = ""
-        for decorator in decorators:
-            c = decorator(c)
-            suffix += f"_{clean_value_name(decorator)}"
+        suffix_components = [clean_value_name(decorator) for decorator in decorators]
+        suffix = "_".join(suffix_components)
         suffix_hash = hashlib.md5(suffix.encode()).hexdigest()[:8]
         new_name = get_name_short(f"{c_orig.name}__D{suffix_hash}")
-        c.rename(new_name)
-        return c
+        if new_name in CACHE:
+            return CACHE[new_name]
+        else:
+            c = c_orig.copy()
+            for decorator in decorators:
+                c = decorator(c)
+            c.rename(new_name)
+            return c
 
     return wrapper
 
@@ -30,6 +35,14 @@ if __name__ == "__main__":
         gf.c.straight, gf.add_pins.add_pins, gf.add_pins.add_bbox_siepic
     )
 
+    # gives the docs for the wrapped function, as expected
+    help(straight_with_pins)
+
     # call the decorated function and show it
     c = straight_with_pins(length=25)
+
+    # caching works, as expected
+    c2 = straight_with_pins(length=25)
+    assert c2 is c
+
     c.show()
