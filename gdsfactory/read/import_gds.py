1bc64e9d7e2d5bf7abbed17f09fc95ff3fb91dd9
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-04-09 15:09:55 -0700

    add import_gds_with_conflicts

--- gdsfactory/read/import_gds.py
+++ gdsfactory/read/import_gds.py
@@ -3,6 +3,7 @@ from __future__ import annotations
 import warnings
 from pathlib import Path
 
+import kfactory as kf
 from kfactory import KCLayout
 
 from gdsfactory.component import Component
@@ -21,7 +22,8 @@ def import_gds(
     """
 
     if kwargs:
-        warnings.warn(f"kwargs {kwargs} are not used")
+        for k in kwargs:
+            warnings.warn(f"kwargs {k!r} is deprecated and ignored")
 
     temp_kcl = KCLayout(name=str(gdspath))
     temp_kcl.read(gdspath)
@@ -31,10 +33,60 @@ def import_gds(
     c = Component()
     c.name = cellname
     c._kdb_cell.copy_tree(kcell._kdb_cell)
-
     c.ports = kcell.ports
     c._settings = kcell.settings.model_copy()
     c.info = kcell.info.model_copy()
+    return c
+
+
+def import_gds_with_conflicts(
+    gdspath: str | Path,
+    cellname: str | None = None,
+    name: str | None = None,
+    **kwargs,
+) -> Component:
+    """Reads a GDS file and returns a Component.
+
+    Args:
+        gdspath: path to GDS file.
+        cellname: name of the cell to return. Defaults to top cell.
+        name: optional name.
+
+    Modes:
+        AddToCell: Add content to existing cell. Content of new cells is simply added to existing cells with the same name.
+        OverwriteCell: The old cell is overwritten entirely (including child cells which are not used otherwise)
+        RenameCell: The new cell will be renamed to become unique
+        SkipNewCell: The new cell is skipped entirely (including child cells which are not used otherwise)
+    """
+
+    if kwargs:
+        for k in kwargs:
+            warnings.warn(f"kwargs {k!r} is deprecated and ignored")
+
+    read_options = kf.kcell.load_layout_options()
+    read_options.cell_conflict_resolution = (
+        kf.kdb.LoadLayoutOptions.CellConflictResolution.RenameCell
+    )
+    top_cells = set(kf.kcl.top_cells())
+    kf.kcl.read(gdspath, read_options, test_merge=True)
+    new_top_cells = set(kf.kcl.top_cells()) - top_cells
+    if len(new_top_cells) != 1:
+        raise ValueError(f"Expected 1 new top cell, got {len(new_top_cells)}")
+
+    if cellname is None:
+        if new_top_cells:
+            cellname = new_top_cells.pop().name
+        else:
+            cellname = kf.kcl.top_kcell.name
+
+    kcell = kf.kcl[cellname]
+    c = Component()
+    c._kdb_cell.copy_tree(kcell._kdb_cell)
+    c.ports = kcell.ports
+    c._settings = kcell.settings.model_copy()
+    c.info = kcell.info.model_copy()
+    name = name or kcell.name
+    c.name = name
     return c
 
 
