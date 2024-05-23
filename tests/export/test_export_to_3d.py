a785c1567727bfadaf32cbdea9930438249a11e1
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-05-23 13:05:07 -0400

    fix more tests

--- tests/export/test_export_to_3d.py
+++ tests/export/test_export_to_3d.py
@@ -3,6 +3,7 @@ import trimesh
 
 import gdsfactory as gf
 from gdsfactory.export.to_3d import to_3d
+from gdsfactory.generic_tech import LAYER
 from gdsfactory.technology import LayerLevel, LayerStack
 
 
@@ -12,7 +13,7 @@ def get_layer_stack() -> LayerStack:
     return LayerStack(
         layers=dict(
             substrate=LayerLevel(
-                layer=(0, 0),
+                layer=(1, 0),
                 thickness=1,
                 zmin=0,
                 material="si",
@@ -28,44 +29,41 @@ def test_valid_component() -> None:
     assert isinstance(scene, trimesh.Scene)
 
 
-# Tests that the function raises a ValueError if no polygons are defined in the layer_stack or layer_views
 def test_no_polygons_defined() -> None:
+    """Tests that the function raises a ValueError if no polygons are defined in the layer_stack or layer_views."""
     c = gf.Component()
     with pytest.raises(ValueError):
         to_3d(c)
 
 
-# Tests that the function excludes layers specified in exclude_layers argument
 def test_exclude_layers() -> None:
-    c = gf.components.rectangle(layer=(0, 0))
+    """Tests that the function correctly excludes layers specified in exclude_layers argument."""
+    c = gf.components.rectangle(layer=LAYER.WG)
     with pytest.raises(ValueError):
-        scene = to_3d(c, exclude_layers=((0, 0),))
+        scene = to_3d(c, exclude_layers=(LAYER.WG,))
         assert len(scene.geometry) == 0, len(scene.geometry)
 
 
-# Tests that the function correctly handles invisible layers
 def test_invisible_layers() -> None:
+    """Tests that the function correctly handles invisible layers."""
     c = gf.Component()
-    c.add_polygon([(0, 0), (1, 0), (1, 1), (0, 1)], layer=(0, 0))
+    c.add_polygon([(0, 0), (1, 0), (1, 1), (0, 1)], layer=(2, 0))
     with pytest.raises(ValueError):
-        scene = to_3d(c)
-        assert len(scene.geometry) == 0
+        scene = to_3d(c, layer_stack=get_layer_stack())
+        assert len(scene.geometry) == 0, len(scene.geometry)
 
 
-# Tests that the function correctly handles missing zmin or thickness values for a layer
 def test_missing_zmin_or_thickness() -> None:
-    c = gf.components.rectangle()
+    """Tests that the function correctly handles missing zmin or thickness values for a layer."""
+    c = gf.components.rectangle(layer=LAYER.SLAB90)
     layer_stack = get_layer_stack()
     with pytest.raises(ValueError):
         to_3d(c, layer_stack=layer_stack)
 
 
 if __name__ == "__main__":
+    # test_valid_component()
     # test_no_polygons_defined()
     # test_exclude_layers()
     # test_invisible_layers()
-    # c = gf.components.rectangle(layer=(0,0))
-    # to_3d(c, layer_views=None, layer_stack=None)
-    c = gf.components.rectangle()
-    layer_stack = get_layer_stack()
-    to_3d(c, layer_stack=layer_stack)
+    test_missing_zmin_or_thickness()
