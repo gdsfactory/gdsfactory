f611875c7bf99d7f22c2feebd87ae5387577462a
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-05-23 10:15:19 -0400

    simpler config

--- gdsfactory/config.py
+++ gdsfactory/config.py
@@ -3,21 +3,15 @@
 from __future__ import annotations
 
 import importlib
-import os
 import pathlib
-import re
 import sys
 import tempfile
-import traceback
 from enum import Enum, auto
-from itertools import takewhile
 from typing import TYPE_CHECKING
 
-import loguru
 from dotenv import find_dotenv
-from kfactory.conf import config
+from kfactory.conf import config, get_affinity
 from loguru import logger as logger
-from pydantic import BaseModel
 from rich.console import Console
 from rich.table import Table
 
@@ -35,6 +29,8 @@ home_path = pathlib.Path.home() / ".gdsfactory"
 diff_path = repo_path / "gds_diff"
 logpath = home_path / "log.log"
 dotenv_path = find_dotenv(usecwd=True)
+
+get_number_of_cores = get_affinity
 
 GDSDIR_TEMP = pathlib.Path(tempfile.TemporaryDirectory().name).parent / "gdsfactory"
 
@@ -63,36 +59,6 @@ pdks = [
     "ubcpdk",
     "gvtt",
 ]
-
-
-class LogLevel(str, Enum):
-    TRACE = "TRACE"
-    DEBUG = "DEBUG"
-    INFO = "INFO"
-    SUCCESS = "SUCCESS"
-    WARNING = "WARNING"
-    ERROR = "ERROR"
-    CRITICAL = "CRITICAL"
-
-
-class LogFilter(BaseModel):
-    """Filter certain messages by log level or regex.
-
-    Filtered messages are not evaluated and discarded.
-    """
-
-    level: LogLevel = LogLevel.INFO
-    regex: str | None = None
-
-    def __call__(self, record: loguru.Record) -> bool:
-        """Loguru needs the filter to be callable."""
-        levelno = logger.level(self.level).no
-        if self.regex is None:
-            return record["level"].no >= levelno
-        else:
-            return record["level"].no >= levelno and not bool(
-                re.search(self.regex, record["message"])
-            )
 
 
 class ErrorType(Enum):
@@ -160,46 +126,6 @@ def print_version_pdks() -> None:
 
     console = Console()
     console.print(table)
-
-
-def get_number_of_cores() -> int:
-    """Get number of cores/threads available.
-
-    On (most) linux we can get it through the scheduling affinity. Otherwise,
-    fall back to the multiprocessing cpu count.
-    """
-    try:
-        threads = len(os.sched_getaffinity(0))
-    except AttributeError:
-        import multiprocessing
-
-        threads = multiprocessing.cpu_count()
-    return threads
-
-
-def tracing_formatter(record: loguru.Record) -> str:
-    """Traceback filtering.
-
-    Filter out frames coming from Loguru internals.
-    """
-    frames = takewhile(
-        lambda f: "/loguru/" not in f.filename, traceback.extract_stack()
-    )
-    stack = " > ".join(f"{f.filename}:{f.name}:{f.lineno}" for f in frames)
-    record["extra"]["stack"] = stack
-
-    if record["extra"].get("with_backtrace", False):
-        return (
-            "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}</level>"
-            " | <cyan>{extra[stack]}</cyan> - <level>{message}</level>\n{exception}"
-        )
-
-    else:
-        return (
-            "<green>{time:YYYY-MM-DD HH:mm:ss.SSS}</green> | <level>{level: <8}"
-            "</level> | <cyan>{name}</cyan>:<cyan>{function}</cyan>:<cyan>{line}</cyan>"
-            " - <level>{message}</level>\n{exception}"
-        )
 
 
 CONF = config
