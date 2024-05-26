5e48c50c86ac2c99e5c1351b3140f406a7160c63
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-05-26 06:42:26 -0400

    simpler get_netlist

--- tests/test_get_netlist.py
+++ tests/test_get_netlist.py
@@ -21,7 +21,7 @@ def test_netlist_simple_width_mismatch_throws_error() -> None:
     c = gf.Component()
     c1 = c << gf.components.straight(length=1, width=1)
     c2 = c << gf.components.straight(length=2, width=2)
-    c2.connect("o1", c1.ports["o2"])
+    c2.connect("o1", c1.ports["o2"], allow_width_mismatch=True)
     c.add_port("o1", port=c1.ports["o1"])
     c.add_port("o2", port=c2.ports["o2"])
     with pytest.raises(ValueError):
@@ -39,11 +39,11 @@ def test_get_netlist_cell_array() -> None:
     c = gf.components.array(
         gf.components.straight(length=10), spacing=(0, 100), columns=1, rows=5
     )
-    n = c.get_netlist()
-    assert len(c.ports) == 10
-    assert not n["connections"]
-    assert len(n["ports"]) == 10
-    assert len(n["instances"]) == 5
+    n = c.get_netlist(allow_multiple=True)
+    assert len(c.ports) == 10, len(c.ports)
+    assert not n["connections"], n["connections"]
+    assert len(n["ports"]) == 10, n["ports"]
+    assert len(n["instances"]) == 5, n["instances"]
 
 
 def test_get_netlist_cell_array_connecting() -> None:
@@ -102,43 +102,13 @@ def test_get_netlist_promoted() -> None:
     assert "warnings" not in netlist
 
 
-# def test_get_netlist_close_enough() -> None:
-#     """Move connection 1nm inwards."""
-#     c = gf.Component()
-#     i1 = c.add_ref(gf.components.straight(), "i1")
-#     i2 = c.add_ref(gf.components.straight(), "i2")
-#     i2.connect("o2", i1.ports["o1"])
-#     i2.d.movex(0.001)
-#     netlist = c.get_netlist(tolerance=2)
-#     connections = netlist["connections"]
-#     assert len(connections) == 1
-#     cpairs = list(connections.items())
-#     extracted_port_pair = set(cpairs[0])
-#     expected_port_pair = {"i2,o2", "i1,o1"}
-#     assert extracted_port_pair == expected_port_pair
-
-# def test_get_netlist_close_enough_orthogonal() -> None:
-#     c = gf.Component()
-#     i1 = c.add_ref(gf.components.straight(), "i1")
-#     i2 = c.add_ref(gf.components.straight(), "i2")
-#     i2.connect("o2", i1.ports["o1"])
-#     i2.d.movey(0.001)
-#     netlist = c.get_netlist(tolerance=2)
-#     connections = netlist["connections"]
-#     assert len(connections) == 1
-#     cpairs = list(connections.items())
-#     extracted_port_pair = set(cpairs[0])
-#     expected_port_pair = {"i2,o2", "i1,o1"}
-#     assert extracted_port_pair == expected_port_pair
-
-
 def test_get_netlist_close_enough_fails() -> None:
     """Move connection 1nm outwards."""
     c = gf.Component()
     i1 = c.add_ref(gf.components.straight(), "i1")
     i2 = c.add_ref(gf.components.straight(), "i2")
     i2.connect("o2", i1.ports["o1"])
-    i2.d.movex(0.001)
+    i2.movex(1)
     netlist = c.get_netlist()
     connections = netlist["connections"]
     assert len(connections) == 0
@@ -150,24 +120,9 @@ def test_get_netlist_close_enough_orthogonal_fails() -> None:
     i2 = c.add_ref(gf.components.straight(), "i2")
     i2.connect("o2", i1.ports["o1"])
     i2.d.movey(0.001)
-    netlist = c.get_netlist(tolerance=1)
+    netlist = c.get_netlist()
     connections = netlist["connections"]
     assert len(connections) == 0
-
-
-def test_get_netlist_close_enough_both() -> None:
-    c = gf.Component()
-    i1 = c.add_ref(gf.components.straight(), "i1")
-    i2 = c.add_ref(gf.components.straight(), "i2")
-    i2.connect("o2", i1.ports["o1"])
-    i2.d.move((0.001, 0.001))
-    netlist = c.get_netlist(tolerance=2)
-    connections = netlist["connections"]
-    assert len(connections) == 1
-    cpairs = list(connections.items())
-    extracted_port_pair = set(cpairs[0])
-    expected_port_pair = {"i2,o2", "i1,o1"}
-    assert extracted_port_pair == expected_port_pair
 
 
 def test_get_netlist_close_enough_rotated() -> None:
@@ -175,8 +130,8 @@ def test_get_netlist_close_enough_rotated() -> None:
     i1 = c.add_ref(gf.components.straight(), "i1")
     i2 = c.add_ref(gf.components.straight(), "i2")
     i2.connect("o2", i1.ports["o1"])
-    i2.rotate(angle=0.01, center="o2")
-    netlist = c.get_netlist(tolerance=2)
+    i2.d.rotate(angle=0.01)
+    netlist = c.get_netlist()
     connections = netlist["connections"]
     assert len(connections) == 1
     cpairs = list(connections.items())
@@ -190,9 +145,9 @@ def test_get_netlist_throws_error_bad_rotation() -> None:
     i1 = c.add_ref(gf.components.straight(), "i1")
     i2 = c.add_ref(gf.components.straight(), "i2")
     i2.connect("o2", i1.ports["o1"])
-    i2.rotate(angle=90, center="o2")
+    i2.d.rotate(90)
     with pytest.raises(ValueError):
-        c.get_netlist(tolerance=2)
+        c.get_netlist()
 
 
 def test_get_netlist_tiny() -> None:
@@ -207,7 +162,7 @@ def test_get_netlist_tiny() -> None:
     i3.connect("o2", i2.ports["o1"])
     i4.connect("o2", i3.ports["o1"])
 
-    netlist = c.get_netlist(tolerance=5)
+    netlist = c.get_netlist()
     connections = netlist["connections"]
     assert len(connections) == 3
     # cpairs = list(connections.items())
@@ -220,7 +175,7 @@ def test_get_netlist_rotated() -> None:
     c = gf.Component()
     i1 = c.add_ref(gf.components.straight(), "i1")
     i2 = c.add_ref(gf.components.straight(), "i2")
-    i1.rotate(35)
+    i1.d.rotate(35)
     i2.connect("o2", i1.ports["o1"])
 
     netlist = c.get_netlist()
@@ -238,7 +193,7 @@ def test_get_netlist_electrical_simple() -> None:
     i2 = c.add_ref(gf.components.wire_straight(), "i2")
     i3 = c.add_ref(gf.components.wire_straight(), "i3")
     i2.connect("e2", i1.ports["e1"])
-    i3.movey(-100)
+    i3.d.movey(-100)
     netlist = c.get_netlist()
     connections = netlist["connections"]
     assert len(connections) == 1
@@ -253,22 +208,7 @@ def test_get_netlist_electrical_rotated_joint() -> None:
     i1 = c.add_ref(gf.components.wire_straight(), "i1")
     i2 = c.add_ref(gf.components.wire_straight(), "i2")
     i2.connect("e2", i1.ports["e1"])
-    i2.rotate(45, "e2")
-    netlist = c.get_netlist()
-    connections = netlist["connections"]
-    assert len(connections) == 1
-    cpairs = list(connections.items())
-    extracted_port_pair = set(cpairs[0])
-    expected_port_pair = {"i2,e2", "i1,e1"}
-    assert extracted_port_pair == expected_port_pair
-
-
-def test_get_netlist_electrical_allowable_offset() -> None:
-    c = gf.Component()
-    i1 = c.add_ref(gf.components.wire_straight(), "i1")
-    i2 = c.add_ref(gf.components.wire_straight(), "i2")
-    i2.connect("e2", i1.ports["e1"])
-    i2.move((0.001, 0.001))
+    i2.d.rotate(45)
     netlist = c.get_netlist()
     connections = netlist["connections"]
     assert len(connections) == 1
@@ -283,11 +223,10 @@ def test_get_netlist_electrical_different_widths() -> None:
     c = gf.Component()
     i1 = c.add_ref(gf.components.straight(width=1, cross_section="metal1"), "i1")
     i2 = c.add_ref(gf.components.straight(width=10, cross_section="metal1"), "i2")
-    i2.connect("e2", i1.ports["e1"])
-    i2.movex(0.001)
-    netlist = c.get_netlist(tolerance=2)
+    i2.connect("e2", i1.ports["e1"], allow_width_mismatch=True)
+    netlist = c.get_netlist()
     connections = netlist["connections"]
-    assert len(connections) == 1
+    assert len(connections) == 1, len(connections)
     cpairs = list(connections.items())
     extracted_port_pair = set(cpairs[0])
     expected_port_pair = {"i2,e2", "i1,e1"}
@@ -300,7 +239,7 @@ def test_get_netlist_transformed() -> None:
     c = gf.Component(cname)
     i1 = c.add_ref(gf.components.straight(), "i1")
     i2 = c.add_ref(gf.components.straight(), "i2")
-    i1.rotate(rotation_value)
+    i1.d.rotate(rotation_value)
     i2.connect("o2", i1.ports["o1"])
 
     # perform the initial sanity checks on the netlist
@@ -315,7 +254,7 @@ def test_get_netlist_transformed() -> None:
     recursive_netlist = get_netlist_recursive(c)
     top_netlist = recursive_netlist[cname]
     # the recursive netlist should have 3 entries, for the top level and two rotated straights
-    assert len(recursive_netlist) == 3
+    assert len(recursive_netlist) == 3, len(recursive_netlist)
     # confirm that the child netlists have reference attributes properly set
 
     i1_cell_name = top_netlist["instances"]["i1"]["component"]
@@ -330,4 +269,8 @@ def test_get_netlist_transformed() -> None:
 
 
 if __name__ == "__main__":
+    # test_get_netlist_electrical_rotated_joint()
+    # test_get_netlist_electrical_different_widths()
+    # test_netlist_simple_width_mismatch_throws_error()
+    # test_get_netlist_cell_array()
     test_get_netlist_transformed()
