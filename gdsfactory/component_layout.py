1446bc1cef0e69b75384b3809b2721176ae23caa
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-06-03 03:28:10 -0700

    ports has values

--- gdsfactory/component_layout.py
+++ gdsfactory/component_layout.py
@@ -7,6 +7,7 @@ from __future__ import annotations
 
 import typing
 
+import kfactory as kf
 import numpy as np
 from numpy import cos, pi, sin
 from numpy.linalg import norm
@@ -14,16 +15,16 @@ from rich.console import Console
 from rich.table import Table
 
 if typing.TYPE_CHECKING:
-    from gdsfactory.port import Port
+    pass
 
 Coordinate = tuple[float, float]
 
 
-def pprint_ports(ports: dict[str, Port] or list[Port]) -> None:
+def pprint_ports(ports: kf.Ports) -> None:
     """Prints ports in a rich table."""
     console = Console()
     table = Table(show_header=True, header_style="bold")
-    ports_list = ports if isinstance(ports, list) else list(ports.values())
+    ports_list = ports
     if not ports_list:
         return
     p0 = ports_list[0]
