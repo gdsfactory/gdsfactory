3ae543053ed9883f45004c5b6c9f2e116ce4cfc5
Troy Tamas <tvtamas@gmail.com>
2024-06-04 15:45:48 +0900

    adding another test for get_netlist_recursive

--- tests/test_get_netlist_recursive.py
+++ tests/test_get_netlist_recursive.py
@@ -13,3 +13,33 @@ def test_no_effect_on_original_components():
         passive_mzi, exclude_port_types="optical"
     )
     assert passive_mzi_phase_shifter_netlist_electrical is not None
+
+
+@gf.cell
+def hcomponent_top():
+    c = gf.Component()
+    c << hcomponent_l2()
+    return c
+
+
+@gf.cell
+def hcomponent_l2():
+    c = gf.Component()
+    c << hcomponent_l3()
+    c << hcomponent_l3()
+    return c
+
+
+@gf.cell
+def hcomponent_l3():
+    c = gf.Component()
+    return c
+
+
+def test_n_netlists():
+    c = hcomponent_top()
+    netlists = get_netlist_recursive(c)
+    # only netlists with hierarchy should be reported
+    assert len(netlists) == 2
+    assert "hcomponent_top" in netlists
+    assert "hcomponent_l2" in netlists
