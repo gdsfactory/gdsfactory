007299721c738bc56dfecd09c7a8561a931e8c55
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-06-09 09:28:25 +0200

    expose AbstractLayer

--- gdsfactory/technology/__init__.py
+++ gdsfactory/technology/__init__.py
@@ -1,5 +1,6 @@
 from gdsfactory.technology.layer_map import LayerMap, lyp_to_dataclass
 from gdsfactory.technology.layer_stack import (
+    AbstractLayer,
     DerivedLayer,
     LayerLevel,
     LayerStack,
@@ -16,4 +17,5 @@ __all__ = [
     "lyp_to_dataclass",
     "LogicalLayer",
     "DerivedLayer",
+    "AbstractLayer",
 ]
