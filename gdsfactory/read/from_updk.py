1d1fb039c403f9bc7156e8706343e2c520dcfd4f
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-06-20 14:24:11 +0200

    improve from_updk

--- gdsfactory/read/from_updk.py
+++ gdsfactory/read/from_updk.py
@@ -22,12 +22,15 @@ def from_updk(
     layer_label: tuple[int, int] | None = None,
     layer_pin_label: tuple[int, int] | None = None,
     layer_pin: tuple[int, int] | None = None,
+    layer_pin_optical: tuple[int, int] | None = None,
+    layer_pin_electrical: tuple[int, int] | None = None,
     optical_xsections: list[str] | None = None,
     electrical_xsections: list[str] | None = None,
-    layers_text: list[LayerSpec] | None = None,
+    layer_text: LayerSpec | None = None,
     text_size: float = 2.0,
     activate_pdk: bool = False,
     read_xsections: bool = True,
+    use_port_layer: bool = False,
     prefix: str = "",
     suffix: str = "",
 ) -> str:
@@ -38,15 +41,18 @@ def from_updk(
         filepath_out: optional filepath to save script. if None only returns script and does not save it.
         layer_bbox: layer to draw bounding boxes.
         layer_bbmetal: layer to draw bounding boxes for metal.
-        optical_xsections: Optional list of names of xsections that will add optical ports.
         layer_label: layer to draw labels.
         layer_pin_label: layer to draw pin labels.
         layer_pin: layer to draw pins.
+        layer_pin_optical: layer to draw optical pins.
+        layer_pin_electrical: layer to draw electrical pins.
+        optical_xsections: Optional list of names of xsections that will add optical ports.
         electrical_xsections: Optional list of names of xsections that will add electrical ports.
-        layers_text: Optional list of layers to add text labels.
+        layer_text: Optional list of layers to add text labels.
         text_size: text size for labels.
         activate_pdk: if True, activate the pdk after writing the script.
         read_xsections: if True, read xsections from uPDK.
+        use_port_layer: if True, use the xsection layer for the port.
         prefix: optional prefix to add to the script.
         suffix: optional suffix to add to the script.
     """
@@ -70,14 +76,24 @@ def from_updk(
     script += f"""
 
 import sys
+from functools import partial
 import gdsfactory as gf
 from gdsfactory.get_factories import get_cells
 from gdsfactory.add_pins import add_pins_inside2um
 
+cell = gf.cell
 layer_bbox = {layer_bbox}
 layer_bbmetal = {layer_bbmetal}
 layer_pin_label = {layer_pin_label}
 layer_pin = {layer_pin}
+layer_pin_optical = {layer_pin_optical}
+layer_pin_electrical = {layer_pin_electrical}
+layer_label = {layer_label}
+
+layer_text = {layer_text or (1, 0)}
+text_function = partial(gf.components.text, layer=layer_text, justify="center", size={text_size})
+
+add_pins = partial(add_pins_inside2um, layer_label=layer_label, layer=layer_pin_optical)
 """
 
     if layer_label:
@@ -93,7 +109,12 @@ layer_pin = {layer_pin}
         script += "\n"
 
     for block_name, block in conf.blocks.items():
-        parameters = block.parameters
+        if hasattr(block, "parameters"):
+            parameters = block.parameters
+        else:
+            print(f"{block_name=}, {block=} does not have parameters")
+            continue
+
         parameters_string = (
             ", ".join(
                 [f"{p_name}:{p.type}={p.value}" for p_name, p in parameters.items()]
@@ -130,6 +151,8 @@ layer_pin = {layer_pin}
             if layer_label and parameters_colon
             else ""
         )
+        list_parameters = "\\n".join(f"{p_name}" for p_name in parameters_equal)
+        parameters_labels = f"    c.add_label(text=f'Parameters:\\n{list_parameters}', position=(0,0), layer=layer_label)\n"
 
         if parameters:
             doc = f'"""{block.doc}\n\n    Args:\n    {parameters_doc}\n    """'
@@ -144,14 +167,18 @@ layer_pin = {layer_pin}
 
         points = str(block.bbox).replace("'", "")
         script += f"""
-@gf.cell
+@cell
 def {block_name}({parameters_string})->gf.Component:
     {doc}
     c = gf.Component()
-    p = c.add_polygon({points}, layer=layer_bbox)
-    xc, yc = p.dcenter
-    ysize = p.ysize
+    c.add_polygon({points}, layer=layer_bbox)
+    xc = c.dx
+    yc = c.dy
     name = f{cell_name!r}
+"""
+        if "ysize" in parameters_labels:
+            script += """
+    ysize = p.ysize
 """
         if layer_bbmetal and "bb_metal" in block:
             for bbmetal in block["bb_metal"].values():
@@ -160,30 +187,33 @@ def {block_name}({parameters_string})->gf.Component:
 
         script += parameters_labels
 
+        port_layer = "layer" if use_port_layer else "cross_section"
+
         for port_name, port in block.pins.items():
             port_type = (
                 "electrical" if port.xsection in electrical_xsections else "optical"
             )
 
-            if port.xsection != "None":
-                script += f"    c.add_port(name={port_name!r}, width={port.width}, cross_section={port.xsection!r}, center=({port.xya[0]}, {port.xya[1]}), orientation={port.xya[2]}, port_type={port_type!r})\n"
+            if port.xsection != "None" and not use_port_layer:
+                script += f"    c.add_port(name={port_name!r}, {port_layer}={port.xsection!r}, center=({port.xya[0]}, {port.xya[1]}), orientation={port.xya[2]}, port_type={port_type!r})\n"
             else:
-                script += f"    c.add_port(name={port_name!r}, width={port.width}, layer=(0, 0), center=({port.xya[0]}, {port.xya[1]}), orientation={port.xya[2]}, port_type={port_type!r})\n"
+                script += f"    c.add_port(name={port_name!r}, width={port.width}, layer={port.xsection!r}, center=({port.xya[0]}, {port.xya[1]}), orientation={port.xya[2]}, port_type={port_type!r})\n"
 
             if layer_pin_label:
                 d = OmegaConf.to_container(port)
                 d["name"] = port_name
                 text = OmegaConf.to_yaml(d)
                 script += f"    c.add_label(text={text!r}, position=({port.xya[0]}, {port.xya[1]}), layer=layer_pin_label)\n"
-        if layers_text:
-            for layer_text in layers_text:
-                script += f"    text = c << gf.c.text(text=name, size={text_size}, position=(xc, yc), layer={layer_text},justify='center')\n"
-                script += "    c.absorb(text)\n"
+        if layer_text:
+            script += "    text = c << text_function(text=name)\n"
+
+            script += "    text.dx = xc\n"
+            script += "    text.dy = yc\n"
 
         script += """
     c.name = name
     if layer_pin:
-        add_pins_inside2um(c, layer=layer_pin)
+        add_pins(c, layer=layer_pin)
     return c
 """
 
@@ -199,9 +229,11 @@ pdk.activate()
 
 if __name__ == "__main__":
     c = {block_name}()
-    c.show( )
+    c.show()
 """
     if filepath_out:
+        dirpath = filepath_out.parent
+        dirpath.mkdir(parents=True, exist_ok=True)
         filepath_out = pathlib.Path(filepath_out)
         filepath_out.write_text(script)
     return script
