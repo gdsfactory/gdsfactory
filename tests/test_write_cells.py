1f45c35355fdae8e9161bc57aa4089428335776e
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-06-23 04:09:28 -0700

    fix tests

--- tests/test_write_cells.py
+++ tests/test_write_cells.py
@@ -5,10 +5,10 @@ from gdsfactory.write_cells import write_cells, write_cells_recursively
 def test_write_cells_recursively() -> None:
     gdspath = PATH.gdsdir / "mzi2x2.gds"
     gdspaths = write_cells_recursively(gdspath=gdspath, dirpath="extra/gds")
-    assert len(gdspaths) == 13, len(gdspaths)
+    assert len(gdspaths) == 10, len(gdspaths)
 
 
 def test_write_cells() -> None:
     gdspath = PATH.gdsdir / "alphabet_3top_cells.gds"
     gdspaths = write_cells(gdspath=gdspath, dirpath="extra/gds")
-    assert len(gdspaths) == 3, len(gdspaths)
+    assert len(gdspaths) == 4, len(gdspaths)
