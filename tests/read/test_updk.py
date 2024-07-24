19f1cb287bc297e46931c4564ac78b12e6fda1b7
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-07-23 17:51:38 -0700

    fix test

--- tests/read/test_updk.py
+++ tests/read/test_updk.py
@@ -1,12 +1,17 @@
+from gdsfactory.config import GDSDIR_TEMP
+from gdsfactory.generic_tech import get_generic_pdk
 from gdsfactory.read.from_updk import from_updk
-from gdsfactory.samples.pdk.fab_c import PDK
 
 
 def test_updk() -> None:
+    from gdsfactory.samples.pdk.fab_c import PDK
+
+    PDK.activate()
     yaml_pdk_decription = PDK.to_updk()
-    gdsfactory_script = from_updk(yaml_pdk_decription)
+    filepath = GDSDIR_TEMP / "pdk.yaml"
+    filepath.write_text(yaml_pdk_decription)
+    gdsfactory_script = from_updk(filepath)
     assert gdsfactory_script
 
-
-if __name__ == "__main__":
-    test_updk()
+    PDK = get_generic_pdk()
+    PDK.activate()
