9a806f98f8c4d2f00f6b8219bd6b803592438336
Joaquin Matres <jmatres@google.com>
2024-10-09 10:19:25 -0700

    add tests for waypoints

--- tests/routing/test_route_single_from_steps.py
+++ tests/routing/test_route_single_from_steps.py
@@ -1,7 +1,10 @@
+import numpy as np
+
 import gdsfactory as gf
 
 
 def test_route_from_steps() -> None:
+    """Test route_single with steps."""
     c = gf.Component()
     w = gf.components.straight()
     left = c << w
@@ -29,5 +32,76 @@ def test_route_from_steps() -> None:
     )
 
 
+def test_route_waypoints() -> None:
+    """Test route_single with waypoints."""
+    c = gf.Component("electrical")
+    w = gf.components.wire_straight()
+    left = c << w
+    right = c << w
+    right.dmove((100, 80))
+    obstacle = gf.components.rectangle(size=(100, 10))
+    obstacle1 = c << obstacle
+    obstacle2 = c << obstacle
+    obstacle1.dymin = 40
+    obstacle2.dxmin = 25
+
+    p0 = left.ports["e2"]
+    p1 = right.ports["e2"]
+    p0x, p0y = left.ports["e2"].dcenter
+    p1x, p1y = right.ports["e2"].dcenter
+    o = 10  # vertical offset to overcome bottom obstacle
+    ytop = 20
+
+    gf.routing.route_single(
+        c,
+        p0,
+        p1,
+        cross_section="metal_routing",
+        waypoints=[
+            (p0x + o, p0y),
+            (p0x + o, ytop),
+            (p1x + o, ytop),
+            (p1x + o, p1y),
+        ],
+    )
+
+
+def test_route_waypoints_numpy() -> None:
+    """Test route_single with waypoints."""
+    c = gf.Component()
+    w = gf.components.wire_straight()
+    left = c << w
+    right = c << w
+    right.dmove((100, 80))
+    obstacle = gf.components.rectangle(size=(100, 10))
+    obstacle1 = c << obstacle
+    obstacle2 = c << obstacle
+    obstacle1.dymin = 40
+    obstacle2.dxmin = 25
+
+    p0 = left.ports["e2"]
+    p1 = right.ports["e2"]
+    p0x, p0y = left.ports["e2"].dcenter
+    p1x, p1y = right.ports["e2"].dcenter
+    o = 10  # vertical offset to overcome bottom obstacle
+    ytop = 20
+
+    gf.routing.route_single(
+        c,
+        p0,
+        p1,
+        cross_section="metal_routing",
+        waypoints=np.array(
+            [
+                (p0x + o, p0y),
+                (p0x + o, ytop),
+                (p1x + o, ytop),
+                (p1x + o, p1y),
+            ]
+        ),
+    )
+
+
 if __name__ == "__main__":
-    test_route_from_steps()
+    # test_route_from_steps()
+    test_route_waypoints()
