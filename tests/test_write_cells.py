4cf2d6d354deddd7dbdb74b3a56d8fb734faa558
Joaquin Matres <jmatres@google.com>
2024-10-10 15:59:06 -0700

    fix tests

--- tests/test_write_cells.py
+++ tests/test_write_cells.py
@@ -1,3 +1,4 @@
+import gdsfactory as gf
 from gdsfactory.component import GDSDIR_TEMP
 from gdsfactory.config import PATH
 from gdsfactory.write_cells import write_cells, write_cells_recursively
@@ -5,11 +6,13 @@ from gdsfactory.write_cells import write_cells, write_cells_recursively
 
 def test_write_cells_recursively() -> None:
     gdspath = PATH.gdsdir / "mzi2x2.gds"
+    gf.clear_cache()
     gdspaths = write_cells_recursively(gdspath=gdspath, dirpath=GDSDIR_TEMP)
     assert len(gdspaths) == 10, len(gdspaths)
 
 
 def test_write_cells() -> None:
     gdspath = PATH.gdsdir / "alphabet_3top_cells.gds"
+    gf.clear_cache()
     gdspaths = write_cells(gdspath=gdspath, dirpath=GDSDIR_TEMP)
-    assert len(gdspaths) == 4, len(gdspaths)
+    assert len(gdspaths) == 3, len(gdspaths)
