64e154142d27453fa26a0948bcb07325895660eb
JellyLab <jellylabgaming@gmail.com>
2024-11-04 23:19:48 +0800

    Updated graphviz as an optional dependency

--- gdsfactory/schematic.py
+++ gdsfactory/schematic.py
@@ -3,7 +3,6 @@ from typing import Any
 
 import yaml
 from pydantic import BaseModel, Field, model_validator
-from graphviz import Digraph
 
 from gdsfactory.config import PATH
 from gdsfactory.typings import Anchor, Component
@@ -191,26 +190,65 @@ class Schematic(BaseModel):
             with_labels: bool = True,
             font_weight: str = "normal",
     ):
-        """Plots a netlist graph with Graphviz.
+        """Plots a netlist graph with Graphviz (optional dependency) or Networkx
 
         Args:
             with_labels: add label to each node.
             font_weight: normal, bold (for consistency with original code).
         """
-        dot = Digraph(comment="Netlist Diagram")
-        dot.attr(dpi='300', layout="neato", overlap="false")
+        try:
+            from graphviz import Digraph
+            dot = Digraph(comment="Netlist Diagram")
+            dot.attr(dpi='300', layout="neato", overlap="false")
 
-        for node, placement in self.placements.items():
-            label = node if with_labels else ""
-            pos = f"{placement.x},{placement.y}!"
-            dot.node(node, label=label, pos=pos)
+            for node, placement in self.placements.items():
+                label = node if with_labels else ""
+                pos = f"{placement.x},{placement.y}!"
+                dot.node(node, label=label, pos=pos)
 
-        for net in self.nets:
-            p1_instance = net.p1.split(",")[0]
-            p2_instance = net.p2.split(",")[0]
-            dot.edge(p1_instance, p2_instance)
+            for net in self.nets:
+                p1_instance = net.p1.split(",")[0]
+                p2_instance = net.p2.split(",")[0]
+                dot.edge(p1_instance, p2_instance)
 
-        dot.render('netlist_diagram', format='png', view=True)
+            dot.render('netlist_diagram', format='png', view=True)
+            return dot
+        except ImportError:
+            # Fallback to networkx if Graphviz is not available
+            import matplotlib.pyplot as plt
+            import networkx as nx
+            plt.figure()
+            netlist = self.netlist
+            connections = netlist.connections
+            placements = self.placements or netlist.placements
+            G = nx.Graph()
+            G.add_edges_from(
+                [
+                    (",".join(k.split(",")[:-1]), ",".join(v.split(",")[:-1]))
+                    for k, v in connections.items()
+                ]
+            )
+            pos = {k: (v["x"], v["y"]) for k, v in placements.items()}
+            labels = {k: ",".join(k.split(",")[:1]) for k in placements.keys()}
+
+            for node, placement in placements.items():
+                if not G.has_node(
+                        node
+                ):  # Check if the node is already in the graph (from connections), to avoid duplication.
+                    G.add_node(node)
+                    pos[node] = (placement.x, placement.y)
+
+            for net in self.nets:
+                G.add_edge(net.p1.split(",")[0], net.p2.split(",")[0])
+
+            nx.draw(
+                G,
+                with_labels=with_labels,
+                font_weight=font_weight,
+                labels=labels,
+                pos=pos,
+            )
+            return G
 
 
 def write_schema(
