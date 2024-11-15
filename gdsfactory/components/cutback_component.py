19c1da52cf4548c6ffd60b051f67055558d2a32c
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-11-15 08:55:49 -0800

    fix cutback_component

--- gdsfactory/components/cutback_component.py
+++ gdsfactory/components/cutback_component.py
@@ -52,9 +52,10 @@ def cutback_component(
 
     component = gf.get_component(component, **kwargs)
     bendu = gf.get_component(bend180, cross_section=xs)
-    straight_component = straight(
-        length=straight_length or xs.radius * 2, cross_section=xs
-    )
+
+    straight_length = xs.radius * 2 if straight_length is None else straight_length
+    straight_component = straight(length=straight_length, cross_section=xs)
+
     straight_pair = straight(length=straight_length_pair or 0, cross_section=xs)
 
     # Define a map between symbols and (component, input port, output port)
