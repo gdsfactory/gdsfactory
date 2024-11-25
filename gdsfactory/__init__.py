9602a2ceb65c65916c1d9d37f590931fdf4bcfd4
Sebastian Goeldi <sebastian.goeldi@protonmail.com>
2024-11-25 23:56:35 +0100

    clear import cache on clearing layout cache

--- gdsfactory/__init__.py
+++ gdsfactory/__init__.py
@@ -80,6 +80,7 @@ c = components
 def clear_cache(kcl: kf.KCLayout = kf.kcl) -> None:
     """Clears the whole layout object cache for the default layout."""
     kcl.clear_kcells()
+    import_gds.cache_clear()
 
 
 __all__ = (
