b11f45724781d0d0b509e7da9ec33974850b4326
Troy Tamas <tvtamas@gmail.com>
2024-12-12 17:08:34 +0900

    using the right terminology

--- gdsfactory/technology/yaml_utils.py
+++ gdsfactory/technology/yaml_utils.py
@@ -11,7 +11,7 @@ class TechnologyDumper(yaml.SafeDumper):
     pass
 
 
-def add_color_yaml_presenter(prefer_named_color: bool = True) -> None:
+def add_color_yaml_representer(prefer_named_color: bool = True) -> None:
     """Add a custom YAML presenter for Color objects."""
 
     def _color_presenter(
@@ -25,7 +25,7 @@ def add_color_yaml_presenter(prefer_named_color: bool = True) -> None:
     TechnologyDumper.add_representer(Color, _color_presenter)
 
 
-def add_tuple_yaml_presenter() -> None:
+def add_tuple_yaml_representer() -> None:
     """Add a custom YAML presenter for tuple objects."""
 
     def _tuple_presenter(
@@ -36,7 +36,7 @@ def add_tuple_yaml_presenter() -> None:
     TechnologyDumper.add_representer(tuple, _tuple_presenter)
 
 
-def add_multiline_str_yaml_presenter() -> None:
+def add_multiline_str_yaml_representer() -> None:
     """Add a custom YAML presenter for multiline strings."""
 
     def _str_presenter(
@@ -49,6 +49,6 @@ def add_multiline_str_yaml_presenter() -> None:
     TechnologyDumper.add_representer(str, _str_presenter)
 
 
-add_color_yaml_presenter()
-add_tuple_yaml_presenter()
-add_multiline_str_yaml_presenter()
+add_color_yaml_representer()
+add_tuple_yaml_representer()
+add_multiline_str_yaml_representer()
