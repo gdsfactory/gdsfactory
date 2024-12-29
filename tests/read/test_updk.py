d1e86f3c04f769f1fe9235a5f5c10fccd43fcf69
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-12-29 05:44:15 -0800

    fix test_updk

--- tests/read/test_updk.py
+++ tests/read/test_updk.py
@@ -1,9 +1,6 @@
-import pytest
-
 from gdsfactory.config import GDSDIR_TEMP
 from gdsfactory.generic_tech import get_generic_pdk
 from gdsfactory.read.from_updk import from_updk
-from gdsfactory.samples.pdk.fab_c import PDK
 
 exclude = [
     "add_fiber_array_optical_south_electrical_north",
@@ -14,25 +11,16 @@ exclude = [
     "pack_doe_grid",
     "straight_piecewise",
     "text_freetype",
+    "ring_double_pn",
+    "grating_coupler_elliptical_lumerical_etch70",
 ]
 
 
-@pytest.mark.skip("not consistent")
 def test_updk_generic() -> None:
     PDK = get_generic_pdk()
     yaml_pdk_description = PDK.to_updk(exclude=exclude)
     filepath = GDSDIR_TEMP / "pdk.yaml"
     GDSDIR_TEMP.mkdir(exist_ok=True)
     filepath.write_text(yaml_pdk_description)
-    gdsfactory_script = from_updk(filepath)
-    assert gdsfactory_script
-
-
-def test_updk() -> None:
-    PDK.activate()
-    yaml_pdk_decription = PDK.to_updk()
-    GDSDIR_TEMP.mkdir(exist_ok=True)
-    filepath = GDSDIR_TEMP / "pdk.yaml"
-    filepath.write_text(yaml_pdk_decription)
     gdsfactory_script = from_updk(filepath)
     assert gdsfactory_script
