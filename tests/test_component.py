cb47e1dacb740488ba26f1dac508f7099cb32488
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2024-12-29 03:34:14 -0800

    fix test

--- tests/test_component.py
+++ tests/test_component.py
@@ -6,7 +6,7 @@ from gdsfactory.component import LockedError
 from gdsfactory.generic_tech import LAYER
 
 
-def test_component_dup() -> None:
+def test_component_copy() -> None:
     c1 = gf.components.straight(length=10)
     c2 = c1.dup()
     assert c1.settings.length == 10
@@ -51,7 +51,7 @@ def test_get_polygons() -> None:
 
 def test_trim() -> None:
     layer = (1, 0)
-    c1 = gf.c.rectangle(size=(9, 9), centered=True, layer=layer).dup()
+    c1 = gf.c.rectangle(size=(9, 9), centered=True, layer=layer).copy()
     c1.flatten()
     c1_area = c1.area(layer=layer)
 
@@ -64,33 +64,19 @@ def test_from_kcell() -> None:
     gf.Component.from_kcell(kf.cells.straight.straight(1, 1, gf.kcl.get_info(LAYER.WG)))
 
 
-def test_remove_layers_recursive() -> None:
-    comp = gf.Component()
-    r1 = gf.components.compass(size=(1, 15), layer=(1, 0))
-    _ = comp << r1
-    r2 = gf.components.compass(size=(2, 30), layer=(2, 0))
-    _ = comp << r2
-    comp.flatten()
-
-    copy = comp.dup()
-    copy.remove_layers(layers=[(2, 0)])
-
-    assert comp.area((1, 0)) == 15, f"{comp.area((1, 0))}"
-    assert comp.area((2, 0)) == 60, f"{comp.area((2, 0))}"
-
-
-def test_remove_layers_flat() -> None:
+def test_remove_layers() -> None:
     c = gf.Component()
+    c.add_polygon([(0, 0), (0, 10), (10, 10), (10, 0)], layer=(1, 0))
     c.add_polygon([(0, 0), (0, 10), (10, 10), (10, 0)], layer=(2, 0))
 
-    empty = c.dup()
-    empty.remove_layers(layers=[(2, 0)])
-    assert c.area((2, 0)) == 100, f"{c.area((2, 0))}"
-    assert empty.area((2, 0)) == 0, f"{empty.area((2, 0))}"
+    c.remove_layers(layers=[(2, 0)])
+    assert c.area((1, 0)) == 100, f"{c.area((1, 0))}"
+    assert c.area((2, 0)) == 0, f"{c.area((2, 0))}"
 
 
 def test_locked_cell() -> None:
-    c = gf.components.straight()
+    c = gf.Component()
+    c._locked = True
 
     with pytest.raises(LockedError):
         c.add_polygon([(0, 0), (0, 10), (10, 10), (10, 0)], layer=(2, 0))
