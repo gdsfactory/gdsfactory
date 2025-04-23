5ed3cabcb9aa66155d7a165e252c1a37e159f6a4
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2025-04-23 11:41:10 -0700

    enable metal sbend router

--- gdsfactory/routing/route_bundle_sbend.py
+++ gdsfactory/routing/route_bundle_sbend.py
@@ -20,6 +20,7 @@ def route_bundle_sbend(
     allow_width_mismatch: bool | None = None,
     allow_layer_mismatch: bool | None = None,
     allow_type_mismatch: bool | None = None,
+    port_name: str = "o1",
     **kwargs: Any,
 ) -> list[ManhattanRoute]:
     """Places sbend routes from ports1 to ports2.
@@ -34,6 +35,7 @@ def route_bundle_sbend(
         allow_width_mismatch: allows width mismatch.
         allow_layer_mismatch: allows layer mismatch.
         allow_type_mismatch: allows type mismatch.
+        port_name: name of the port to connect to the sbend.
         kwargs: cross_section settings.
 
     """
@@ -50,7 +52,7 @@ def route_bundle_sbend(
         bend = gf.get_component(bend_s, size=(xsize, ysize), **kwargs)
         sbend = component << bend
         sbend.connect(
-            "o1",
+            port_name,
             p1,
             allow_width_mismatch=allow_width_mismatch,
             allow_layer_mismatch=allow_layer_mismatch,
@@ -95,14 +97,28 @@ if __name__ == "__main__":
     #     c, right_ports, left_ports, enforce_port_ordering=False
     # )
 
+    # c = gf.Component()
+    # c1 = c << gf.components.nxn(east=3, ysize=20)
+    # c2 = c << gf.components.nxn(west=3)
+    # c2.move((80, 0))
+    # route_bundle_sbend(
+    #     c,
+    #     c1.ports.filter(orientation=0),
+    #     c2.ports.filter(orientation=180),
+    #     enforce_port_ordering=False,
+    # )
+
     c = gf.Component()
-    c1 = c << gf.components.nxn(east=3, ysize=20)
-    c2 = c << gf.components.nxn(west=3)
-    c2.move((80, 0))
+    c1 = c << gf.components.straight_heater_metal()
+    p1 = c << gf.c.pad()
+    p1.movey(200)
     route_bundle_sbend(
         c,
-        c1.ports.filter(orientation=0),
-        c2.ports.filter(orientation=180),
+        [p1["e4"]],
+        [c1.ports["l_e2"]],
         enforce_port_ordering=False,
+        cross_section="metal3",
+        port_name="e1",
+        allow_width_mismatch=True,
     )
     c.show()
