59089641eea4fad50f97b50c10ede7bd52f6abd7
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2025-05-15 16:43:17 -0400

    add_bubbles

---
+++ gdsfactory/bubbles.py
@@ -0,0 +1,11 @@
+"""Testing bubble sort algorithm."""
+
+
+def sorter(arr):
+    for i in range(len(arr)):
+        for j in range(len(arr) - 1):
+            if arr[j] > arr[j + 1]:
+                temp = arr[j]
+                arr[j] = arr[j + 1]
+                arr[j + 1] = temp
+    return arr
