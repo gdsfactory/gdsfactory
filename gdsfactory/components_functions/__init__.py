97b7522b437cc0b8f2238a18290730caf9bb6ddf
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2025-08-05 12:10:18 -0700

    add die to init

--- gdsfactory/components_functions/__init__.py
+++ gdsfactory/components_functions/__init__.py
@@ -4,12 +4,14 @@ This module contains pure functions that create components without any decorator
 The actual components module will import these and apply decorators as needed.
 """
 
+from gdsfactory.components_functions.dies.die import die
 from gdsfactory.components_functions.waveguides import (
     straight_all_angle_function,
     straight_function,
 )
 
 __all__ = [
+    "die",
     "straight_all_angle_function",
     "straight_function",
 ]
