6a103f76fb5e560e70d6e4c2402b3c4a8297a1e5
Gregor Ronniger <ronniger@ims-chips.de>
2025-08-13 14:02:35 +0200

    extend optional parameters for lyp_from_dataclass - optionally turn off sorting layers by name - allow definition of LayerMap name in generated script - allow specification of output filepath

--- gdsfactory/technology/layer_map.py
+++ gdsfactory/technology/layer_map.py
@@ -10,31 +10,37 @@ class LayerMap(gf.LayerEnum):
     layout = gf.constant(gf.kcl.layout)
 
 
-def lyp_to_dataclass(lyp_filepath: str | pathlib.Path, overwrite: bool = True) -> str:
+def lyp_to_dataclass(
+    lyp_filepath: str | pathlib.Path,
+    overwrite: bool = True,
+    sort_by_name: bool = True,  # sort_by_name=False means same order as in lyp file
+    map_name: str = "LayerMapFab",
+    filepathout: pathlib.Path | None = None,
+) -> str:
     """Returns python LayerMap script from a klayout layer properties file lyp."""
     filepathin = pathlib.Path(lyp_filepath)
-    filepathout = filepathin.with_suffix(".py")
+    filepathout = filepathout or filepathin.with_suffix(".py")
 
     if filepathout.exists() and not overwrite:
         raise FileExistsError(f"You can delete {filepathout}")
 
-    script = """
-from gdsfactory.typings import Layer
+    script = f"""from gdsfactory.typings import Layer
 from gdsfactory.technology.layer_map import LayerMap
 
 
-class LayerMapFab(LayerMap):
+class {map_name}(LayerMap):
 """
     lys = LayerViews.from_lyp(filepathin)
-    for layer_name, layer in sorted(lys.get_layer_views().items()):
+    maybe_sort = sorted if sort_by_name else lambda seq: seq
+    for layer_name, layer in maybe_sort(lys.get_layer_views().items()):
         if layer.layer is not None:
             script += (
                 f"    {layer_name}: Layer = ({layer.layer[0]}, {layer.layer[1]})\n"
             )
 
-    script += """
+    script += f"""
 
-LAYER = LayerMapFab
+LAYER = {map_name}
 """
 
     filepathout.write_text(script)
