c9eb58c3dcd28782a340aa012d26eb26b04fd9d9
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2025-08-28 16:51:38 +0100

    Fix mypy

--- gdsfactory/components/filters/fiber_array.py
+++ gdsfactory/components/filters/fiber_array.py
@@ -2,7 +2,7 @@ from __future__ import annotations
 
 import gdsfactory as gf
 from gdsfactory.component import Component
-from gdsfactory.components.shapes import circle
+from gdsfactory.components.shapes.circle import circle
 from gdsfactory.typings import LayerSpec
 
 
