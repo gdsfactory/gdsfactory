8d9e91ef93cf429181abd8bb6bba1cae5cfa465a
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2025-10-03 19:53:00 +0200

    better names

--- gdsfactory/components/dies/__init__.py
+++ gdsfactory/components/dies/__init__.py
@@ -1,15 +1,19 @@
 from gdsfactory.components.dies.align import (
-    add_frame,
+    add_die_frame,
     align_wafer,
 )
 from gdsfactory.components.dies.die import (
     die,
 )
+from gdsfactory.components.dies.die_die_frame_with_pads import (
+    die_frame,
+    die_frame_phix,
+    die_frame_phix_dc,
+    die_frame_phix_rf,
+    die_frame_with_pads,
+)
 from gdsfactory.components.dies.die_with_pads import (
     die_with_pads,
-    die_with_pads_gsg,
-    die_with_pads_phix_dc,
-    die_with_pads_phix_rf,
 )
 from gdsfactory.components.dies.seal_ring import (
     seal_ring,
@@ -20,13 +24,15 @@ from gdsfactory.components.dies.wafer import (
 )
 
 __all__ = [
-    "add_frame",
+    "add_die_frame",
     "align_wafer",
     "die",
+    "die_frame",
+    "die_frame_phix",
+    "die_frame_phix_dc",
+    "die_frame_phix_rf",
+    "die_frame_with_pads",
     "die_with_pads",
-    "die_with_pads_gsg",
-    "die_with_pads_phix_dc",
-    "die_with_pads_phix_rf",
     "seal_ring",
     "seal_ring_segmented",
     "wafer",
