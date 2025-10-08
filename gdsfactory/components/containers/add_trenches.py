ac737e344ca71c3f0dd9e79253bd8ffb22bf7341
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2025-10-08 21:46:59 +0200

    better add trenches

--- gdsfactory/components/containers/add_trenches.py
+++ gdsfactory/components/containers/add_trenches.py
@@ -1,7 +1,6 @@
 from __future__ import annotations
 
 from functools import partial
-from typing import Any
 
 import gdsfactory as gf
 from gdsfactory.typings import ComponentSpec, CrossSectionSpec, LayerSpec
@@ -13,18 +12,17 @@ def add_trenches(
     layer_component: LayerSpec = "WG",
     layer_trench: LayerSpec = "DEEP_ETCH",
     width_trench: float = 2.0,
-    cross_section: CrossSectionSpec = "rib_with_trenches",
+    cross_section: CrossSectionSpec | None = None,
     top: float | None = None,
     bot: float | None = None,
     right: float | None = 0,
     left: float | None = 0,
-    **kwargs: Any,
 ) -> gf.Component:
-    """Return component with trenches.
+    """Return inverted component with trenches.
 
     Args:
         component: component to add to the trenches.
-        layer_component: layer of the component.
+        layer_component: layer of the component to invert.
         layer_trench: layer of the trenches.
         width_trench: width of the trenches.
         cross_section: spec (CrossSection, string or dict).
@@ -32,11 +30,8 @@ def add_trenches(
         bot: width of the trench on the bottom. If None uses width_trench.
         right: width of the trench on the right. If None uses width_trench.
         left: width of the trench on the left. If None uses width_trench.
-        kwargs: component settings.
     """
-    component = gf.get_component(component, **kwargs)
-    xs = gf.get_cross_section(cross_section)
-
+    component = gf.get_component(component)
     top = top if top is not None else width_trench
     bot = bot if bot is not None else width_trench
     right = right if right is not None else width_trench
@@ -57,10 +52,16 @@ def add_trenches(
 
     c.add_ports(component.ports)
     c.copy_child_info(component)
-    xs.add_bbox(c)
+    if cross_section is not None:
+        xs = gf.get_cross_section(cross_section)
+        xs.add_bbox(c)
     return c
 
 
 add_trenches90 = partial(
     add_trenches, component="bend_euler", top=0, left=0, right=None
 )
+
+if __name__ == "__main__":
+    c = add_trenches(bot=300)
+    c.show()
