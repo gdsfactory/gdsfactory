083059222637384a2b0f78e97203b300f27cefbd
Abdelhakim Qbaich <abdelhakim.qbaich@xanadu.ai>
2026-01-14 17:00:09 -0500

    Tests for the decorator markings

--- tests/test_get_factories.py
+++ tests/test_get_factories.py
@@ -1,5 +1,6 @@
 from functools import partial
 
+import gdsfactory as gf
 from gdsfactory.components.containers.add_trenches import add_trenches, add_trenches90
 from gdsfactory.get_factories import is_cell
 
@@ -13,3 +14,39 @@ def test_is_cell() -> None:
 
     assert not is_cell(random_function)
     assert not is_cell(partial(random_function, 1))
+
+
+def test_is_gf_cell() -> None:
+    @gf.cell
+    def with_no_paren() -> gf.Component:
+        return gf.Component()
+
+    @gf.cell()
+    def with_paren() -> gf.Component:
+        return gf.Component()
+
+    @gf.cell(basename="my_component")
+    def with_args() -> gf.Component:
+        return gf.Component()
+
+    assert getattr(with_no_paren, "is_gf_cell", None) is True
+    assert getattr(with_paren, "is_gf_cell", None) is True
+    assert getattr(with_args, "is_gf_cell", None) is True
+
+
+def test_is_gf_vcell() -> None:
+    @gf.vcell
+    def with_no_paren() -> gf.ComponentAllAngle:
+        return gf.ComponentAllAngle()
+
+    @gf.vcell()
+    def with_paren() -> gf.ComponentAllAngle:
+        return gf.ComponentAllAngle()
+
+    @gf.vcell(basename="my_component")
+    def with_args() -> gf.ComponentAllAngle:
+        return gf.ComponentAllAngle()
+
+    assert getattr(with_no_paren, "is_gf_vcell", None) is True
+    assert getattr(with_paren, "is_gf_vcell", None) is True
+    assert getattr(with_args, "is_gf_vcell", None) is True
