45662ac6c9e5eafb8c589247f81d412dfdb24041
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2026-02-01 10:05:30 -0800

    remove redundant with_euler parameter as we now expose p value

--- gdsfactory/components/bends/bend_s.py
+++ gdsfactory/components/bends/bend_s.py
@@ -196,19 +196,6 @@ def bend_s(
     )
 
 
-def _get_arc_sbend_angle_middle_length_from_jog(
-    jog: float, radius: float
-) -> tuple[float, float]:
-    """Compute the Euler bend angle and middle straight length for an S-bend."""
-    if jog < 2 * radius:
-        angle = np.rad2deg(2 * np.arcsin(np.sqrt(jog / (4 * radius))))
-        middle_length = 0.0
-    else:
-        angle = 90.0
-        middle_length = jog - 2 * radius
-    return (angle, middle_length)
-
-
 def _get_euler_sbend_angle_middle_length_from_jog(
     jog: float, radius: float, p: float = 1, use_eff: bool = False
 ) -> tuple[float, float]:
@@ -262,24 +249,19 @@ def bend_s_offset(
     radius: float | None = 10.0,
     cross_section: CrossSectionSpec = "strip",
     width: float | None = None,
-    with_euler: bool = True,
     p: float = 1,
     with_arc_floorplan: bool = False,
     npoints: int | None = None,
     angular_step: float | None = None,
 ) -> gf.Component:
-    """Return S bend made of two euler bends with a straight section.
-
-    stores min_bend_radius property in self.info['min_bend_radius']
-    min_bend_radius depends on height and length
+    """Return S bend made of two bends with a straight section.
 
     Args:
         offset: in um.
         radius: in um. if None, uses cross_section_radius.
         cross_section: spec.
         width: width to use. Defaults to cross_section.width.
-        with_euler: use euler bend instead of arc bend.
-        p: parameter for Euler bend. 1 means standard Euler bend. 0 means circular bend.
+        p: 1 means standard Euler bend. 0 means circular arc.
         with_arc_floorplan: if True the size of the bend will be adjusted to match an arc bend with the specified radius. If False: `radius` is the minimum radius of curvature.
         npoints: number of points.
         angular_step: If provided, determines the angular step (in degrees) between points. Mutually exclusive with npoints.
@@ -293,45 +275,26 @@ def bend_s_offset(
     assert radius is not None, "radius cannot be None"
 
     xs.validate_radius(radius)
-    if with_euler:
-        angle, middle_length = _get_euler_sbend_angle_middle_length_from_jog(
-            jog=offset / 2, radius=radius, p=p, use_eff=with_arc_floorplan
-        )
-        path = gf.path.euler(
-            radius=radius,
-            angle=+angle,
-            p=p,
-            use_eff=with_arc_floorplan,
-            npoints=npoints,
-            angular_step=angular_step,
-        )
-        path += gf.path.straight(length=middle_length)
-        path += gf.path.euler(
-            radius=radius,
-            angle=-angle,
-            p=p,
-            use_eff=with_arc_floorplan,
-            npoints=npoints,
-            angular_step=angular_step,
-        )
-    else:
-        angle, middle_length = _get_arc_sbend_angle_middle_length_from_jog(
-            jog=offset,
-            radius=radius,
-        )
-        path = gf.path.arc(
-            radius=radius,
-            angle=+angle,
-            npoints=npoints,
-            angular_step=angular_step,
-        )
-        path += gf.path.straight(length=middle_length)
-        path += gf.path.arc(
-            radius=radius,
-            angle=-angle,
-            npoints=npoints,
-            angular_step=angular_step,
-        )
+    angle, middle_length = _get_euler_sbend_angle_middle_length_from_jog(
+        jog=offset / 2, radius=radius, p=p, use_eff=with_arc_floorplan
+    )
+    path = gf.path.euler(
+        radius=radius,
+        angle=+angle,
+        p=p,
+        use_eff=with_arc_floorplan,
+        npoints=npoints,
+        angular_step=angular_step,
+    )
+    path += gf.path.straight(length=middle_length)
+    path += gf.path.euler(
+        radius=radius,
+        angle=-angle,
+        p=p,
+        use_eff=with_arc_floorplan,
+        npoints=npoints,
+        angular_step=angular_step,
+    )
 
     return gf.path.extrude(path, cross_section=xs)
 
