33314de687bf0e559dcd9c013ff8a222a71e6bef
Joaquin Matres <4514346+joamatab@users.noreply.github.com>
2026-03-06 10:05:18 -0500

    add samples to 3d

---
+++ gdsfactory/samples/sample_to_3d_electrical.py
@@ -0,0 +1,17 @@
+"""Write a GDS with all cells."""
+
+import gdsfactory as gf
+from gdsfactory.gpdk import PDK
+
+cells = gf.c
+
+if __name__ == "__main__":
+    PDK.activate()
+
+    c0 = cells.straight_heater_meander()
+    c1 = cells.via_stack_m1_mtop()
+    c2 = cells.via_stack_heater_mtop()
+    c = gf.grid([c0, c1, c2])
+    c.show()
+    s = c.to_3d()
+    s.show()
